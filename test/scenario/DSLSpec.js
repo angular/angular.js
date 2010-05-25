4fec828cf6ce896452945c1dd8aa2db61e2bc746
Andres Ornelas <aornelas@google.com>
2010-05-25 09:52:52 -0700

    appended lastDocument to the document.body

--- test/scenario/DSLSpec.js
+++ test/scenario/DSLSpec.js
@@ -11,6 +11,7 @@ describe("DSL", function() {
     };
     executeStep = function(step, html, callback) {
       lastDocument =_jQuery('<div>' + html + '</div>');
+      _jQuery(document.body).append(lastDocument);
       var specThis = {
         testWindow: window,
         testDocument: lastDocument
@@ -32,8 +33,10 @@ describe("DSL", function() {
     it('should select', function() {
       input('gender').select('female');
       expect(lastStep.name).toEqual("Select radio 'gender' to 'female'");
-      executeStep(lastStep, '<input type="radio" name="0@gender" value="male"/>' +
+      executeStep(lastStep,
+        '<input type="radio" name="0@gender" value="male" checked/>' +
         '<input type="radio" name="0@gender" value="female"/>');
+      expect(lastDocument.find(':radio:checked').length).toEqual(1);
       expect(lastDocument.find(':radio:checked').val()).toEqual('female');
     });
   });
