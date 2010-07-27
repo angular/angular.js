32c4aee1cdb601550e948b52a3fa7b4824cfbf1f
Andres Ornelas <aornelas@google.com>
2010-07-27 15:53:55 -0700

    before pulling testcontext out

--- src/scenario/Matcher.js
+++ src/scenario/Matcher.js
@@ -1,21 +1,21 @@
-//function Matcher(future, logger) {
-//  var self = this;
-//  this.logger = logger;
-//  this.future = future;
-//}
-//
-//Matcher.addMatcher = function(name, matcher){
-//  Matcher.prototype[name] = function(expected) {
-//    var future = this.future;
-//    $scenario.addFuture(
-//      'expect ' + future.name + ' ' + name + ' ' + expected,
-//      function(done){
-//        if (matcher(future.value, expected))
-//          throw "Expected " + expected + ' but was ' + future.value;
-//        done();
-//      }
-//    );
-//  };
-//};
-//
-//Matcher.addMatcher('toEqual', function(a,b){ return a == b; });
+function Matcher(future, logger) {
+  var self = this;
+  this.logger = logger;
+  this.future = future;
+}
+
+Matcher.addMatcher = function(name, matcher){
+  Matcher.prototype[name] = function(expected) {
+    var future = this.future;
+    $scenario.addFuture(
+      'expect ' + future.name + ' ' + name + ' ' + expected,
+      function(done){
+        if (matcher(future.value, expected))
+          throw "Expected " + expected + ' but was ' + future.value;
+        done();
+      }
+    );
+  };
+};
+
+Matcher.addMatcher('toEqual', function(a,b){ return a == b; });
