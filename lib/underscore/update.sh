cb669300889926595fcbd4f1dbf1b6c9af4ea0dd
Bolek Szewczyk <bolu@google.com>
2010-09-23 12:38:28 +0200

    delete unused files (e.g. swfobject and underscore libs)

--- lib/underscore/update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-underscore=$(dirname $0)
-github='http://github.com/documentcloud/underscore/raw/master'
-
-wget $github/underscore-min.js -O $underscore/underscore-min.js
-wget $github/underscore.js -O $underscore/underscore.js
