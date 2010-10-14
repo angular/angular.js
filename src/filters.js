0f104317dff5628765e26cc68df7dd1175b2aa5e
Misko Hevery <misko@hevery.com>
2010-10-13 23:09:26 -0700

    Added support for date filter
    Date filter now supports:
    yyyy: four digit year
      yy: two digit year
      MM: two digit month
      dd: two digit day of month
      HH: two digit hour in 0-23
      KK: two digit hour in 0-12
      mm: two digit minute
      ss: two digit second
       a: am/pm
       Z: four digit timezone offset
    
    example {{ timestamp | date:'yyyy-MM-dd HH:mm:ss' }} becomes 2010-10-13 14:45:23

--- src/filters.js
+++ src/filters.js
@@ -30,12 +30,55 @@ angularFilter.number = function(amount, fractionSize){
   }
   return text;
 };
+function padNumber(num, digits, trim) {
+  var neg = '';
+  if (num < 0) {
+    neg =  '-';
+    num = -num;
+  }
+  num = '' + num;
+  while(num.length < digits) num = '0' + num;
+  if (trim)
+    num = num.substr(num.length - digits);
+  return neg + num;
+}
+function dateGetter(name, size, option) {
+  return function(date) {
+    var value = date[name].call(date) + 1*(option===1);
+    if (option == -12 && value > 12) value += option;
+    return padNumber(value, size, option === true);
+  };
+}
+var DATE_FORMATS = {
+  yyyy: dateGetter('getFullYear', 4),
+  yy:   dateGetter('getFullYear', 2, true),
+  MM:   dateGetter('getMonth', 2, 1),
+  dd:   dateGetter('getDate', 2),
+  HH:   dateGetter('getHours', 2),
+  KK:   dateGetter('getHours', 2, -12),
+  mm:   dateGetter('getMinutes', 2),
+  ss:   dateGetter('getSeconds', 2),
+  a:    function(date){return date.getHours() < 12 ? 'am' : 'pm'; },
+  Z:    function(date){
+          var offset = date.getTimezoneOffset();
+          return padNumber(offset / 60, 2) + padNumber(Math.abs(offset % 60), 2);
+        }
+};
+var DATE_FORMATS_SPLIT = new RegExp('('+
+    map(DATE_FORMATS, function(value, key){return key;}).join('|')+')');
+console.log(DATE_FORMATS_SPLIT);
 
-angularFilter.date = function(date) {
-  if (date instanceof Date)
-    return date.toLocaleDateString();
-  else
-    return date;
+angularFilter.date = function(date, format) {
+  if (!date instanceof Date) return date;
+  var text = date.toLocaleDateString(), fn;
+  if (format && isString(format)) {
+    text = '';
+    foreach(format.split(DATE_FORMATS_SPLIT), function(value){
+      fn = DATE_FORMATS[value];
+      text += fn ? fn(date) : value;
+    });
+  }
+  return text;
 };
 
 angularFilter.json = function(object) {
