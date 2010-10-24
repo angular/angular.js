4e9a2aa10ec6e84377aebd37fd3ae44af3d9423a
Igor Minar <iiminar@gmail.com>
2010-10-23 18:26:47 -0700

    Revert "added support for treating numbers as date in miliseconds"
    This reverts commit 1391f19fb49275af59230afef51b472c58d7818c.

--- src/filters.js
+++ src/filters.js
@@ -75,14 +75,8 @@ var DATE_FORMATS = {
 var DATE_FORMATS_SPLIT = /([^yMdHhmsaZ]*)(y+|M+|d+|H+|h+|m+|s+|a|Z)(.*)/;
 
 angularFilter.date = function(date, format) {
-  var text, fn;
-  if (!date) return date;
-  if (!(date instanceof Date)) {
-    text = parseInt(date, 10);
-    date = new Date();
-    date.setTime(text);
-  }
-  text = date.toLocaleDateString();
+  if (!(date instanceof Date)) return date;
+  var text = date.toLocaleDateString(), fn;
   if (format && isString(format)) {
     text = '';
     var parts = [];
