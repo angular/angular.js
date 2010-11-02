94676f3bff91d58e96d352753924ada1f0088c34
Igor Minar <iiminar@gmail.com>
2010-11-02 09:27:16 -0700

    adding watchr-docs.rb

---
+++ watchr-docs.rb
@@ -0,0 +1,8 @@
+# config file for watchr http://github.com/mynyml/watchr
+# install: gem install watchr
+# run: watch watchr.rb
+# note: make sure that you have jstd server running (server.sh) and a browser captured
+
+watch( '^src/' )  do
+   %x{ echo "\n\ndoc run started @ `date`" > logs/docs.log; node docs/collect.js &> logs/docs.log & }
+end
