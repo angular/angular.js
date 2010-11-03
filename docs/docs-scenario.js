bbb19a4d160eaeecabb061fc47ff61aee8faff77
Igor Minar <iiminar@gmail.com>
2010-11-03 12:55:17 -0700

    fixing docs scenario runner template

--- docs/docs-scenario.js
+++ docs/docs-scenario.js
@@ -1,7 +1,7 @@
 {{#all}}
 describe('{{name}}', function(){
   beforeEach(function(){
-    navigateTo('index.html#{{name}}');
+    browser().navigateTo('index.html#{{name}}');
   });
   // {{raw.file}}:{{raw.line}}
 {{{scenario}}}
