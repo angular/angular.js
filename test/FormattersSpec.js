454626ad39bc19f03390363f3033ee88c3efd417
Misko Hevery <misko@hevery.com>
2011-01-06 14:34:21 -0800

    converted last of tests to specs

--- test/FormattersSpec.js
+++ test/FormattersSpec.js
@@ -1,37 +1,37 @@
-TestCase("formatterTest", {
-  testNoop: function(){
+describe("formatter", function(){
+  it('should noop', function(){
     assertEquals("abc", angular.formatter.noop.format("abc"));
     assertEquals("xyz", angular.formatter.noop.parse("xyz"));
     assertEquals(null, angular.formatter.noop.parse(null));
-  },
+  });
 
-  testList: function() {
+  it('should List', function() {
     assertEquals('a, b', angular.formatter.list.format(['a', 'b']));
     assertEquals('', angular.formatter.list.format([]));
     assertEquals(['abc', 'c'], angular.formatter.list.parse("  , abc , c ,,"));
     assertEquals([], angular.formatter.list.parse(""));
     assertEquals([], angular.formatter.list.parse(null));
-  },
+  });
 
-  testBoolean: function() {
+  it('should Boolean', function() {
     assertEquals('true', angular.formatter['boolean'].format(true));
     assertEquals('false', angular.formatter['boolean'].format(false));
     assertEquals(true, angular.formatter['boolean'].parse("true"));
     assertEquals(false, angular.formatter['boolean'].parse(""));
     assertEquals(false, angular.formatter['boolean'].parse("false"));
     assertEquals(false, angular.formatter['boolean'].parse(null));
-  },
+  });
 
-  testNumber: function() {
+  it('should Number', function() {
     assertEquals('1', angular.formatter.number.format(1));
     assertEquals(1, angular.formatter.number.format('1'));
-  },
+  });
 
-  testTrim: function() {
+  it('should Trim', function() {
     assertEquals('', angular.formatter.trim.format(null));
     assertEquals('', angular.formatter.trim.format(""));
     assertEquals('a', angular.formatter.trim.format(" a "));
     assertEquals('a', angular.formatter.trim.parse(' a '));
-  }
+  });
 
 });
