823370b5b8e6a77a3cbfe96230cace21d2c45846
Misko Hevery <misko@hevery.com>
2011-02-01 10:01:02 -0800

    addded cookbook

--- docs/src/templates/docs.js
+++ docs/src/templates/docs.js
@@ -2,26 +2,27 @@ DocsController.$inject = ['$location', '$browser', '$window'];
 function DocsController($location, $browser, $window) {
   this.pages = NG_PAGES;
   window.$root = this.$root;
+  this.$location = $location;
+
+  this.$watch('$location.hashPath', function(hashPath){
+    hashPath = hashPath || '!angular';
+    if (hashPath.match(/^!/)) {
+      this.partialId = hashPath.substring(1);
+      this.partialTitle = (angular.Array.filter(NG_PAGES, {id:this.partialId})[0]||{}).name;
+    }
+  });
 
   this.getUrl = function(page){
     return '#!' + page.id;
   };
 
   this.getCurrentPartial = function(){
-    return './' + this.getTitle() + '.html';
-  };
-
-  this.getTitle = function(){
-    var hashPath = $location.hashPath || '!angular';
-    if (hashPath.match(/^!/)) {
-      this.partialTitle = hashPath.substring(1);
-    }
-    return this.partialTitle;
+    return './' + this.partialId + '.html';
   };
 
   this.getClass = function(page) {
     var depth = page.depth,
-        cssClass = 'level-' + depth + (page.name == this.getTitle() ? ' selected' : '');
+        cssClass = 'level-' + depth + (page.name == this.partialId ? ' selected' : '');
 
     if (depth == 1 && page.type !== 'overview') cssClass += ' level-angular';
 
