92206e243d9550683d3fe9620bc24031d6f60832
Misko Hevery <misko@hevery.com>
2011-02-12 08:58:11 -0800

    reformated multiline trinary expressions to have a leading ?/:.

--- src/jqLite.js
+++ src/jqLite.js
@@ -5,12 +5,12 @@
 var jqCache = {},
     jqName = 'ng-' + new Date().getTime(),
     jqId = 1,
-    addEventListenerFn = (window.document.addEventListener ?
-      function(element, type, fn) {element.addEventListener(type, fn, false);} :
-      function(element, type, fn) {element.attachEvent('on' + type, fn);}),
-    removeEventListenerFn = (window.document.removeEventListener ?
-      function(element, type, fn) {element.removeEventListener(type, fn, false); } :
-      function(element, type, fn) {element.detachEvent('on' + type, fn); });
+    addEventListenerFn = (window.document.addEventListener
+      ? function(element, type, fn) {element.addEventListener(type, fn, false);}
+      : function(element, type, fn) {element.attachEvent('on' + type, fn);}),
+    removeEventListenerFn = (window.document.removeEventListener
+      ? function(element, type, fn) {element.removeEventListener(type, fn, false); }
+      : function(element, type, fn) {element.detachEvent('on' + type, fn); });
 
 function jqNextId() { return (jqId++); }
 
