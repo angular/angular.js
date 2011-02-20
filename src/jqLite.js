cb30cce630840036791005deff5c66ca1975ff26
Misko Hevery <misko@hevery.com>
2011-02-19 20:13:50 -0800

    correct hashchange event registration on window

--- src/jqLite.js
+++ src/jqLite.js
@@ -132,7 +132,7 @@ function JQLiteAddClass(element, selector ) {
 
 function JQLiteAddNodes(root, elements) {
   if (elements) {
-    elements = (!elements.nodeName && isDefined(elements.length))
+    elements = (!elements.nodeName && isDefined(elements.length) && !isWindow(elements))
       ? elements
       : [ elements ];
     for(var i=0; i < elements.length; i++) {
