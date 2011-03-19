2514c7d59419e9f9a45559855902226d5db2d758
Misko Hevery <misko@hevery.com>
2011-03-19 09:57:18 +0530

    Created a performance test harness and reporter

--- perf/jsonPerfSpec.js
+++ perf/jsonPerfSpec.js
@@ -1,28 +1,16 @@
 describe('json', function() {
 
   it('angular parser', function() {
-    var duration = time(function() {
-      expect(angular.fromJson(largeJsonString)).toBeTruthy();
-    }, 1);
-
-    dump(duration/1 + ' ms per iteration');
-  });
-
-
-  it('angular delegating to native parser', function() {
-    var duration = time(function() {
-      expect(angular.fromJson(largeJsonString, true)).toBeTruthy();
-    }, 100);
-
-    dump(duration/100 + ' ms per iteration');
-  });
-
-
-  it('native json', function() {
-    var duration = time(function() {
-      expect(JSON.parse(largeJsonString)).toBeTruthy();
-    }, 100);
-
-    dump(duration/100 + ' ms per iteration');
+    perf(
+      function angular() {
+        fromJson(largeJsonString);
+      },
+      function nativeDelegate() {
+        fromJson(largeJsonString, true);
+      },
+      function nativeJSON() {
+        JSON.parse(largeJsonString);
+      }
+    );
   });
 });
