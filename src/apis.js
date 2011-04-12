bb67ee8d28f2cddb4b503dc8909649994a4d67e1
Misko Hevery <misko@hevery.com>
2011-04-12 16:15:05 -0700

    Added HashMap

--- src/apis.js
+++ src/apis.js
@@ -807,6 +807,39 @@ var angularFunction = {
   }
 };
 
+function hashKey(obj) {
+  var objType = typeof obj;
+  var key = obj;
+  if (objType == 'object') {
+    if (typeof (key = obj.$hashKey) == 'function') {
+      // must invoke on object to keep the right this
+      key = obj.$hashKey();
+    } else if (key === undefined) {
+      key = obj.$hashKey = nextUid();
+    }
+  };
+  return objType + ':' + key;
+}
+
+function HashMap(){}
+HashMap.prototype = {
+  put: function(key, value) {
+    var _key = hashKey(key);
+    var oldValue = this[_key];
+    this[_key] = value;
+    return oldValue;
+  },
+  get: function(key) {
+    return this[hashKey(key)];
+  },
+  remove: function(key) {
+    var _key = hashKey(key);
+    var value = this[_key];
+    delete this[_key];
+    return value;
+  }
+};
+
 function defineApi(dst, chain){
   angular[dst] = angular[dst] || {};
   forEach(chain, function(parent){
@@ -819,6 +852,8 @@ defineApi('Array', [angularGlobal, angularCollection, angularArray]);
 defineApi('Object', [angularGlobal, angularCollection, angularObject]);
 defineApi('String', [angularGlobal, angularString]);
 defineApi('Date', [angularGlobal, angularDate]);
+// TODO: enable and document this API
+//defineApi('HashMap', [HashMap]);
 //IE bug
 angular['Date']['toString'] = angularDate['toString'];
 defineApi('Function', [angularGlobal, angularCollection, angularFunction]);
