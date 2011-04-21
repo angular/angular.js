4295b3dded510affe75f7fe6b56f4747385b864c
Misko Hevery <misko@hevery.com>
2011-04-21 15:12:53 -0700

    Allow disabling of shell scripts without changing eclipse configuration

--- test-coverage.sh
+++ test-coverage.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-tests=$1
-if [[ $tests = "" ]]; then
-  tests="all"
-fi
-
-java -Xmx1g -jar lib/jstestdriver/JsTestDriver.jar --config jsTestDriver-coverage.conf --testOutput=tmp/lcov --tests "$tests"
+java -Xmx1g -jar lib/jstestdriver/JsTestDriver.jar --config jsTestDriver-coverage.conf --testOutput=tmp/lcov --tests all $@
 genhtml -o tmp/coverage-html/ tmp/lcov/jsTestDriver.conf-coverage.dat
 echo "done! check out tmp/coverage-html/index.html"
--- test-perf.sh
+++ test-perf.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
-tests=$1
-norecompile=$2
 
-if [[ $tests = "" ]]; then
-  tests="all"
-fi
+norecompile=$1
 
 if [[ $norecompile = "" ]]; then
   rake compile
 fi
 
-java -jar lib/jstestdriver/JsTestDriver.jar --tests "$tests" --config jsTestDriver-perf.conf
+java -jar lib/jstestdriver/JsTestDriver.jar --tests all --config jsTestDriver-perf.conf $@
