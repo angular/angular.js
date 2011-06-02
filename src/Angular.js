1b6f07bada241e756aadb2c77712ef7277edeaf0
Vojta Jina <vojta.jina@gmail.com>
2011-06-02 18:39:44 +0200

    Fix formatError for FF4 and Opera
    Other browsers prepend "Error: <Exception name>" to stack, but FF4 and Opera do not.
    So when formatting error we prepend it by hand, when not present...

--- src/Angular.js
+++ src/Angular.js
@@ -180,7 +180,8 @@ function forEachSorted(obj, iterator, context) {
 function formatError(arg) {
   if (arg instanceof Error) {
     if (arg.stack) {
-      arg = arg.stack;
+      arg = (arg.message && arg.stack.indexOf(arg.message) === -1) ?
+            'Error: ' + arg.message + '\n' + arg.stack : arg.stack;
     } else if (arg.sourceURL) {
       arg = arg.message + '\n' + arg.sourceURL + ':' + arg.line;
     }
