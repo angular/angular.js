b99b0a80723ad3a9b43e9ef36869f521eaec19ec
Vojta Jina <vojta.jina@gmail.com>
2011-06-23 16:52:47 +0200

    feat(test): toHaveBeenCalledOnce jasmine matcher

--- test/testabilityPatch.js
+++ test/testabilityPatch.js
@@ -90,6 +90,28 @@ beforeEach(function(){
         return "Expected " + expected + " to match an Error with message " + toJson(messageRegexp);
       };
       return this.actual.name == 'Error' && messageRegexp.test(this.actual.message);
+    },
+
+    toHaveBeenCalledOnce: function() {
+      if (arguments.length > 0) {
+        throw new Error('toHaveBeenCalledOnce does not take arguments, use toHaveBeenCalledWith');
+      }
+
+      if (!jasmine.isSpy(this.actual)) {
+        throw new Error('Expected a spy, but got ' + jasmine.pp(this.actual) + '.');
+      }
+
+      this.message = function() {
+        var msg = 'Expected spy ' + this.actual.identity + ' to have been called once, but was ',
+            count = this.actual.callCount;
+        return [
+          count == 0 ? msg + 'never called.'
+                     : msg + 'called ' + count + ' times.',
+          msg.replace('to have', 'not to have') + 'called once.'
+        ];
+      };
+
+      return this.actual.callCount == 1;
     }
   });
 
