8f3276bbcd444f2bd21c8d8620be92fd6b5d6f33
Vojta Jina <vojta.jina@gmail.com>
2011-07-09 11:04:00 +0200

    chore(scripts): add test-jquery.sh for running unit tests with jQuery

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 if [ ! -e test.dissable ]; then
   java -jar lib/jstestdriver/JsTestDriver.jar --tests all $@
-  #java -jar lib/jstestdriver/JsTestDriver.jar --tests all --config jsTestDriver-jquery.conf $@
 fi
