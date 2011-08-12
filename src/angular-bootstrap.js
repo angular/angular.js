061e470a88e003fb63c4aab784c6bae639d0d987
Vojta Jina <vojta.jina@gmail.com>
2011-08-12 02:34:07 +0200

    Removing $xhr.*

--- src/angular-bootstrap.js
+++ src/angular-bootstrap.js
@@ -117,9 +117,6 @@
              'service/route.js',
              'service/updateView.js',
              'service/window.js',
-             'service/xhr.bulk.js',
-             'service/xhr.cache.js',
-             'service/xhr.error.js',
              'service/xhr.js',
 
              'apis.js',
