4a9ccc0abcbad65842e1933818ad302e9f3a7a65
Di Peng <pengdi@google.com>
2011-08-17 17:27:15 -0700

    test(i18n): Update some i18n/e2e tests

--- i18n/e2e/i18n-e2e.js
+++ i18n/e2e/i18n-e2e.js
@@ -49,6 +49,48 @@ describe("localized filters", function() {
       expect(binding('input | number')).toBe('234,234,443,432');
       expect(binding('input | currency')).toBe('$234,234,443,432.00');
     });
+
+
+    describe('ng:pluralize for en locale', function() {
+      it('should show pluralized strings', function() {
+        expect(element('.ng-pluralize:first').html()).toBe('You have one email!');
+
+        input('plInput').enter('0');
+        expect(element('.ng-pluralize:first').html()).toBe('You have no email!');
+
+        input('plInput').enter('3');
+        expect(element('.ng-pluralize:first').html()).toBe('You have 3 emails!');
+      });
+
+      it('should show pluralized strings with offsets', function() {
+        expect(element('.ng-pluralize:last').html()).toBe('Shanjian is viewing!');
+
+        input('plInput2').enter('0');
+        expect(element('.ng-pluralize:last').html()).toBe('Nobody is viewing!');
+
+        input('plInput2').enter('2');
+        expect(element('.ng-pluralize:last').html()).toBe('Shanjian and Di are viewing!');
+
+        input('plInput2').enter('3');
+        expect(element('.ng-pluralize:last').html()).
+            toBe('Shanjian, Di and one other person are viewing!');
+
+        input('plInput2').enter('4');
+        expect(element('.ng-pluralize:last').html()).
+            toBe('Shanjian, Di and 2 other people are viewing!');
+      });
+
+      it('should show pluralized strings with correct data-binding', function() {
+        input('plInput2').enter('2');
+        expect(element('.ng-pluralize:last').html()).toBe('Shanjian and Di are viewing!');
+
+        input('person1').enter('Igor');
+        expect(element('.ng-pluralize:last').html()).toBe('Igor and Di are viewing!');
+
+        input('person2').enter('Vojta');
+        expect(element('.ng-pluralize:last').html()).toBe('Igor and Vojta are viewing!');
+      });
+    })
   });
 
   describe("sk locale", function() {
@@ -62,6 +104,32 @@ describe("localized filters", function() {
       expect(binding('input | number')).toBe('234&nbsp;234&nbsp;443&nbsp;432');
       expect(binding('input | currency')).toBe('234&nbsp;234&nbsp;443&nbsp;432,00&nbsp;Sk');
     });
+
+
+    describe('ng:pluralize for sk locale', function() {
+      it('should show pluralized strings', function() {
+        expect(element('.ng-pluralize').html()).toBe('Mas jeden email!');
+
+        input('plInput').enter('0');
+        expect(element('.ng-pluralize:first').html()).toBe('Mas 0 emailov!');
+
+        input('plInput').enter('3');
+        expect(element('.ng-pluralize:first').html()).toBe('Mas 3 emaily!');
+
+        input('plInput').enter('4');
+        expect(element('.ng-pluralize:first').html()).toBe('Mas 4 emaily!');
+
+        input('plInput').enter('6');
+        expect(element('.ng-pluralize:first').html()).toBe('Mas 6 emailov!');
+      });
+
+      it('should show pluralized strings with offsets', function() {
+        //TODO(Igor): add offsets for sk
+      });
+
+      it('should show pluralized strings with correct data-binding', function() {
+      });
+    })
   });
 
   describe("zh locale", function() {
@@ -75,5 +143,43 @@ describe("localized filters", function() {
       expect(binding('input | number')).toBe('234,234,443,432');
       expect(binding('input | currency')).toBe('¥234,234,443,432.00');
     });
+
+
+    describe('ng:pluralize for zh locale', function() {
+      it('should show pluralized strings', function() {
+        expect(element('.ng-pluralize:first').html()).toBe('1人在浏览该文件!');
+
+        input('plInput').enter('0');
+        expect(element('.ng-pluralize:first').html()).toBe('0人在浏览该文件!');
+
+        input('plInput').enter('3');
+        expect(element('.ng-pluralize:first').html()).toBe('3人在浏览该文件!');
+      });
+
+      it('should show pluralized strings with offsets', function() {
+        expect(element('.ng-pluralize:last').html()).toBe('Shanjian 在浏览该文件!');
+
+        input('plInput2').enter('0');
+        expect(element('.ng-pluralize:last').html()).toBe('没有人在浏览该文件!');
+
+        input('plInput2').enter('2');
+        expect(element('.ng-pluralize:last').html()).toBe('Shanjian 和 Di 在浏览该文件!');
+
+        input('plInput2').enter('3');
+        expect(element('.ng-pluralize:last').html()).
+            toBe('Shanjian, Di 还有其他1 人在浏览该文件!');
+      });
+
+      it('should show pluralized strings with correct data-binding', function() {
+        input('plInput2').enter('2');
+        expect(element('.ng-pluralize:last').html()).toBe('Shanjian 和 Di 在浏览该文件!');
+
+        input('person1').enter('彭迪');
+        expect(element('.ng-pluralize:last').html()).toBe('彭迪 和 Di 在浏览该文件!');
+
+        input('person2').enter('一哥');
+        expect(element('.ng-pluralize:last').html()).toBe('彭迪 和 一哥 在浏览该文件!');
+      });
+    })
   });
 });
