7c6b9c68677cb03ef5a48b396378784847eb9c37
Misko Hevery <misko@hevery.com>
2011-08-25 22:23:10 -0700

    feat(docs): add support for events

--- docs/spec/ngdocSpec.js
+++ docs/spec/ngdocSpec.js
@@ -252,11 +252,14 @@ describe('ngdoc', function(){
       var methodB = new Doc({name: 'methodB', methodOf: 'angular.service.abc'});
       var propA = new Doc({name: 'propA', propertyOf: 'angular.service.abc'});
       var propB = new Doc({name: 'propB', propertyOf: 'angular.service.abc'});
-      var docs = [methodB, methodA, propB, propA, parent]; // keep wrong order;
+      var eventA = new Doc({name: 'eventA', eventOf: 'angular.service.abc'});
+      var eventB = new Doc({name: 'eventB', eventOf: 'angular.service.abc'});
+      var docs = [methodB, methodA, eventB, eventA, propA, propB, parent]; // keep wrong order;
       ngdoc.merge(docs);
       expect(docs.length).toEqual(1);
       expect(docs[0].id).toEqual('angular.service.abc');
       expect(docs[0].methods).toEqual([methodA, methodB]);
+      expect(docs[0].events).toEqual([eventA, eventB]);
       expect(docs[0].properties).toEqual([propA, propB]);
     });
 
