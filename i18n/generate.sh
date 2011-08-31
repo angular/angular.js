db7cb301737a1f407b33282ff7dd6c47527e9e07
Misko Hevery <misko@hevery.com>
2011-08-31 11:49:17 -0700

    feat(jasmine-node): use the jasmine-node NPM to run our non-browser specs

--- i18n/generate.sh
+++ i18n/generate.sh
@@ -4,4 +4,4 @@ BASE_DIR=`dirname $0`
 cd $BASE_DIR
 
 
-/usr/bin/env jasmine-node spec/ --noColor | grep -v '/lib/jasmine' && node src/closureSlurper.js
+/usr/bin/env jasmine-node spec/ --noColor && node src/closureSlurper.js
