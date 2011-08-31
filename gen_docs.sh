e5da0c956b5bc6f2da835f4307b2322de0785a9b
Misko Hevery <misko@hevery.com>
2011-08-31 11:49:17 -0700

    feat(jasmine-node): use the jasmine-node NPM to run our non-browser specs

--- gen_docs.sh
+++ gen_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ ! -e gen_docs.disable ]; then
-  /usr/bin/env node docs/spec/specs.js --noColor | grep -v '/lib/jasmine' && node docs/src/gen-docs.js
+  /usr/bin/env jasmine-node docs/spec -i docs/src -i lib --noColor && node docs/src/gen-docs.js
 fi
