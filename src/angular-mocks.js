acbe092823e0a8d15d796cb6d3622bf8f846aa97
Igor Minar <igor@angularjs.org>
2011-09-16 01:39:03 +0200

    fix(angular-mocks): fix forEach -> angular.forEach in $browser.defer.cancel

--- src/angular-mocks.js
+++ src/angular-mocks.js
@@ -279,7 +279,7 @@ function MockBrowser() {
   self.defer.cancel = function(deferId) {
     var fnIndex;
 
-    forEach(self.deferredFns, function(fn, index) {
+    angular.forEach(self.deferredFns, function(fn, index) {
       if (fn.id === deferId) fnIndex = index;
     });
 
