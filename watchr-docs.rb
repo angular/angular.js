95fdb1231f21254f9cf6e68dcee689fb9e3a2b52
Igor Minar <igor@angularjs.org>
2011-10-30 15:13:22 -0700

    chore(watchr): watchr scripts should output logs to terminal
    Having the extra level of indirection by using logs/*.log file
    only makes it more difficult to use these scripts. After this
    change it will be enough to just start the watchr and watch the
    terminal it was started in.

--- watchr-docs.rb
+++ watchr-docs.rb
@@ -1,8 +1,7 @@
 # config file for watchr http://github.com/mynyml/watchr
 # install: gem install watchr
-# run: watch watchr.rb
-# note: make sure that you have jstd server running (server.sh) and a browser captured
+# run: watch watchr-docs.rb
 
 watch( '^src/|^docs/' )  do
-   %x{ echo "\n\ndoc run started @ `date`" > logs/docs.log; node docs/src/gen-docs.js &> logs/docs.log & }
+   system 'echo "\n\ndoc run started @ `date`"; node docs/src/gen-docs.js'
 end
