4bb9e1d0146c4c0add1b29c1522570e4010ad4ef
Igor Minar <igor@angularjs.org>
2011-11-29 19:36:43 -0500

    feat($location): name the watch function to ease debugging

--- src/service/location.js
+++ src/service/location.js
@@ -533,7 +533,7 @@ function $LocationProvider(){
 
     // update browser
     var changeCounter = 0;
-    $rootScope.$watch(function() {
+    $rootScope.$watch(function $locationWatch() {
       if ($browser.url() != currentUrl.absUrl()) {
         changeCounter++;
         $rootScope.$evalAsync(function() {
