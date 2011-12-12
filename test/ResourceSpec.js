d3612641561511a4397124c68ed68e4569202bb0
Igor Minar <igor@angularjs.org>
2011-12-12 14:08:24 -0800

    style(ResourceSpec): style clean up

--- test/ResourceSpec.js
+++ test/ResourceSpec.js
@@ -1,27 +1,25 @@
 'use strict';
 
 describe("resource", function() {
-  var resource, CreditCard, callback;
+  var resource, CreditCard, callback, $httpBackend;
 
-  function nakedExpect(obj) {
-    return expect(angular.fromJson(angular.toJson(obj)));
-  }
-
-  beforeEach(inject(function($http) {
-      resource = new ResourceFactory($http);
-      CreditCard = resource.route('/CreditCard/:id:verb', {id:'@id.key'}, {
-        charge:{
-          method:'POST',
-          params:{verb:'!charge'}
-        }
-      });
-      callback = jasmine.createSpy();
-    })
-  );
-
-  afterEach(inject(function($httpBackend) {
-    $httpBackend.verifyNoOutstandingExpectation();
+  beforeEach(inject(function($injector, $http) {
+    $httpBackend = $injector.get('$httpBackend');
+    resource = new ResourceFactory($http);
+    CreditCard = resource.route('/CreditCard/:id:verb', {id:'@id.key'}, {
+      charge:{
+        method:'POST',
+        params:{verb:'!charge'}
+      }
+    });
+    callback = jasmine.createSpy();
   }));
+
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+  });
+
 
   it("should build resource", function() {
     expect(typeof CreditCard).toBe('function');
@@ -32,12 +30,14 @@ describe("resource", function() {
     expect(typeof CreditCard.query).toBe('function');
   });
 
-  it('should default to empty parameters', inject(function($httpBackend) {
+
+  it('should default to empty parameters', function() {
     $httpBackend.expect('GET', 'URL').respond({});
     resource.route('URL').query();
-  }));
+  });
 
-  it('should ignore slashes of undefinend parameters', inject(function($httpBackend) {
+
+  it('should ignore slashes of undefinend parameters', function() {
     var R = resource.route('/Path/:a/:b/:c');
 
     $httpBackend.when('GET').respond('{}');
@@ -50,16 +50,18 @@ describe("resource", function() {
     R.get({a:1});
     R.get({a:2, b:3});
     R.get({a:4, b:5, c:6});
-  }));
+  });
 
-  it('should support escaping collons in url template', inject(function($httpBackend) {
+
+  it('should support escaping collons in url template', function() {
     var R = resource.route('http://localhost\\:8080/Path/:a/\\:stillPath/:b');
 
     $httpBackend.expect('GET', 'http://localhost:8080/Path/foo/:stillPath/bar').respond();
     R.get({a: 'foo', b: 'bar'});
-  }));
+  });
 
-  it('should correctly encode url params', inject(function($httpBackend) {
+
+  it('should correctly encode url params', function() {
     var R = resource.route('/Path/:a');
 
     $httpBackend.expect('GET', '/Path/foo%231').respond('{}');
@@ -67,9 +69,10 @@ describe("resource", function() {
 
     R.get({a: 'foo#1'});
     R.get({a: 'doh!@foo', bar: 'baz#1'});
-  }));
+  });
 
-  it('should not encode @ in url params', inject(function($httpBackend) {
+
+  it('should not encode @ in url params', function() {
     //encodeURIComponent is too agressive and doesn't follow http://www.ietf.org/rfc/rfc3986.txt
     //with regards to the character set (pchar) allowed in path segments
     //so we need this test to make sure that we don't over-encode the params and break stuff like
@@ -78,62 +81,68 @@ describe("resource", function() {
     var R = resource.route('/Path/:a');
     $httpBackend.expect('GET', '/Path/doh@fo%20o?!do%26h=g%3Da+h&:bar=$baz@1').respond('{}');
     R.get({a: 'doh@fo o', ':bar': '$baz@1', '!do&h': 'g=a h'});
-  }));
+  });
 
-  it('should encode & in url params', inject(function($httpBackend) {
+
+  it('should encode & in url params', function() {
     var R = resource.route('/Path/:a');
     $httpBackend.expect('GET', '/Path/doh&foo?bar=baz%261').respond('{}');
     R.get({a: 'doh&foo', bar: 'baz&1'});
-  }));
+  });
 
-  it('should build resource with default param', inject(function($httpBackend) {
+
+  it('should build resource with default param', function() {
     $httpBackend.expect('GET', '/Order/123/Line/456.visa?minimum=0.05').respond({id: 'abc'});
     var LineItem = resource.route('/Order/:orderId/Line/:id:verb',
                                   {orderId: '123', id: '@id.key', verb:'.visa', minimum: 0.05});
     var item = LineItem.get({id: 456});
     $httpBackend.flush();
-    nakedExpect(item).toEqual({id:'abc'});
-  }));
+    expect(item).toEqualData({id:'abc'});
+  });
 
-  it("should build resource with action default param overriding default param", inject(function($httpBackend) {
+
+  it("should build resource with action default param overriding default param", function() {
     $httpBackend.expect('GET', '/Customer/123').respond({id: 'abc'});
     var TypeItem = resource.route('/:type/:typeId', {type: 'Order'},
                                   {get: {method: 'GET', params: {type: 'Customer'}}});
     var item = TypeItem.get({typeId: 123});
 
     $httpBackend.flush();
-    nakedExpect(item).toEqual({id: 'abc'});
-  }));
+    expect(item).toEqualData({id: 'abc'});
+  });
 
-  it("should create resource", inject(function($httpBackend) {
+
+  it("should create resource", function() {
     $httpBackend.expect('POST', '/CreditCard', '{"name":"misko"}').respond({id: 123, name: 'misko'});
 
     var cc = CreditCard.save({name: 'misko'}, callback);
-    nakedExpect(cc).toEqual({name: 'misko'});
+    expect(cc).toEqualData({name: 'misko'});
     expect(callback).not.toHaveBeenCalled();
 
     $httpBackend.flush();
-    nakedExpect(cc).toEqual({id: 123, name: 'misko'});
+    expect(cc).toEqualData({id: 123, name: 'misko'});
     expect(callback).toHaveBeenCalledOnce();
     expect(callback.mostRecentCall.args[0]).toEqual(cc);
     expect(callback.mostRecentCall.args[1]()).toEqual({});
-  }));
+  });
 
-  it("should read resource", inject(function($httpBackend) {
+
+  it("should read resource", function() {
     $httpBackend.expect('GET', '/CreditCard/123').respond({id: 123, number: '9876'});
     var cc = CreditCard.get({id: 123}, callback);
 
     expect(cc instanceof CreditCard).toBeTruthy();
-    nakedExpect(cc).toEqual({});
+    expect(cc).toEqualData({});
     expect(callback).not.toHaveBeenCalled();
 
     $httpBackend.flush();
-    nakedExpect(cc).toEqual({id: 123, number: '9876'});
+    expect(cc).toEqualData({id: 123, number: '9876'});
     expect(callback.mostRecentCall.args[0]).toEqual(cc);
     expect(callback.mostRecentCall.args[1]()).toEqual({});
-  }));
+  });
 
-  it("should read partial resource", inject(function($httpBackend) {
+
+  it("should read partial resource", function() {
     $httpBackend.expect('GET', '/CreditCard').respond([{id:{key:123}}]);
     var ccs = CreditCard.query();
 
@@ -150,19 +159,21 @@ describe("resource", function() {
     expect(callback.mostRecentCall.args[0]).toEqual(cc);
     expect(callback.mostRecentCall.args[1]()).toEqual({});
     expect(cc.number).toEqual('9876');
-  }));
+  });
 
-  it("should update resource", inject(function($httpBackend) {
+
+  it("should update resource", function() {
     $httpBackend.expect('POST', '/CreditCard/123', '{"id":{"key":123},"name":"misko"}').
                  respond({id: {key: 123}, name: 'rama'});
 
     var cc = CreditCard.save({id: {key: 123}, name: 'misko'}, callback);
-    nakedExpect(cc).toEqual({id:{key:123}, name:'misko'});
+    expect(cc).toEqualData({id:{key:123}, name:'misko'});
     expect(callback).not.toHaveBeenCalled();
     $httpBackend.flush();
-  }));
+  });
 
-  it("should query resource", inject(function($httpBackend) {
+
+  it("should query resource", function() {
     $httpBackend.expect('GET', '/CreditCard?key=value').respond([{id: 1}, {id: 2}]);
 
     var ccs = CreditCard.query({key: 'value'}, callback);
@@ -170,29 +181,31 @@ describe("resource", function() {
     expect(callback).not.toHaveBeenCalled();
 
     $httpBackend.flush();
-    nakedExpect(ccs).toEqual([{id:1}, {id:2}]);
+    expect(ccs).toEqualData([{id:1}, {id:2}]);
     expect(callback.mostRecentCall.args[0]).toEqual(ccs);
     expect(callback.mostRecentCall.args[1]()).toEqual({});
-  }));
+  });
 
-  it("should have all arguments optional", inject(function($httpBackend) {
+
+  it("should have all arguments optional", function() {
     $httpBackend.expect('GET', '/CreditCard').respond([{id:1}]);
 
     var log = '';
     var ccs = CreditCard.query(function() { log += 'cb;'; });
 
     $httpBackend.flush();
-    nakedExpect(ccs).toEqual([{id:1}]);
+    expect(ccs).toEqualData([{id:1}]);
     expect(log).toEqual('cb;');
-  }));
+  });
 
-  it('should delete resource and call callback', inject(function($httpBackend) {
+
+  it('should delete resource and call callback', function() {
     $httpBackend.expect('DELETE', '/CreditCard/123').respond({});
     CreditCard.remove({id:123}, callback);
     expect(callback).not.toHaveBeenCalled();
 
     $httpBackend.flush();
-    nakedExpect(callback.mostRecentCall.args[0]).toEqual({});
+    expect(callback.mostRecentCall.args[0]).toEqualData({});
     expect(callback.mostRecentCall.args[1]()).toEqual({});
 
     callback.reset();
@@ -201,24 +214,27 @@ describe("resource", function() {
     expect(callback).not.toHaveBeenCalled();
 
     $httpBackend.flush();
-    nakedExpect(callback.mostRecentCall.args[0]).toEqual({});
+    expect(callback.mostRecentCall.args[0]).toEqualData({});
     expect(callback.mostRecentCall.args[1]()).toEqual({});
-  }));
+  });
 
-  it('should post charge verb', inject(function($httpBackend) {
+
+  it('should post charge verb', function() {
     $httpBackend.expect('POST', '/CreditCard/123!charge?amount=10', '{"auth":"abc"}').respond({success: 'ok'});
     CreditCard.charge({id:123, amount:10}, {auth:'abc'}, callback);
-  }));
+  });
 
-  it('should post charge verb on instance', inject(function($httpBackend) {
+
+  it('should post charge verb on instance', function() {
     $httpBackend.expect('POST', '/CreditCard/123!charge?amount=10',
         '{"id":{"key":123},"name":"misko"}').respond({success: 'ok'});
 
     var card = new CreditCard({id:{key:123}, name:'misko'});
     card.$charge({amount:10}, callback);
-  }));
+  });
 
-  it('should create on save', inject(function($httpBackend) {
+
+  it('should create on save', function() {
     $httpBackend.expect('POST', '/CreditCard', '{"name":"misko"}').respond({id: 123}, {header1: 'a'});
 
     var cc = new CreditCard();
@@ -229,15 +245,16 @@ describe("resource", function() {
 
     cc.name = 'misko';
     cc.$save(callback);
-    nakedExpect(cc).toEqual({name:'misko'});
+    expect(cc).toEqualData({name:'misko'});
 
     $httpBackend.flush();
-    nakedExpect(cc).toEqual({id:123});
+    expect(cc).toEqualData({id:123});
     expect(callback.mostRecentCall.args[0]).toEqual(cc);
     expect(callback.mostRecentCall.args[1]()).toEqual({header1: 'a'});
-  }));
+  });
 
-  it('should not mutate the resource object if response contains no body', inject(function($httpBackend) {
+
+  it('should not mutate the resource object if response contains no body', function() {
     var data = {id:{key:123}, number:'9876'};
     $httpBackend.expect('GET', '/CreditCard/123').respond(data);
 
@@ -251,17 +268,19 @@ describe("resource", function() {
     cc.$save();
     $httpBackend.flush();
     expect(idBefore).toEqual(cc.id);
-  }));
+  });
 
-  it('should bind default parameters', inject(function($httpBackend) {
+
+  it('should bind default parameters', function() {
     $httpBackend.expect('GET', '/CreditCard/123.visa?minimum=0.05').respond({id: 123});
     var Visa = CreditCard.bind({verb:'.visa', minimum:0.05});
     var visa = Visa.get({id:123});
     $httpBackend.flush();
-    nakedExpect(visa).toEqual({id:123});
-  }));
+    expect(visa).toEqualData({id:123});
+  });
 
-  it('should excersize full stack', inject(function($httpBackend, $resource) {
+
+  it('should excersize full stack', inject(function($resource) {
     var Person = $resource('/Person/:id');
 
     $httpBackend.expect('GET', '/Person/123').respond('\n{\n"name":\n"misko"\n}\n');
@@ -269,6 +288,7 @@ describe("resource", function() {
     $httpBackend.flush();
     expect(person.name).toEqual('misko');
   }));
+
 
   describe('failure mode', function() {
     var ERROR_CODE = 500,
@@ -282,23 +302,24 @@ describe("resource", function() {
       });
     });
 
-    it('should call the error callback if provided on non 2xx response',
-        inject(function($httpBackend, $rootScope) {
+
+    it('should call the error callback if provided on non 2xx response', function() {
       $httpBackend.expect('GET', '/CreditCard/123').respond(ERROR_CODE, ERROR_RESPONSE);
 
       CreditCard.get({id:123}, callback, errorCB);
       $httpBackend.flush();
       expect(errorCB).toHaveBeenCalledOnce();
       expect(callback).not.toHaveBeenCalled();
-    }));
+    });
 
-    it('should call the error callback if provided on non 2xx response', inject(function($httpBackend) {
+
+    it('should call the error callback if provided on non 2xx response', function() {
       $httpBackend.expect('GET', '/CreditCard').respond(ERROR_CODE, ERROR_RESPONSE);
 
       CreditCard.get(callback, errorCB);
       $httpBackend.flush();
       expect(errorCB).toHaveBeenCalledOnce();
       expect(callback).not.toHaveBeenCalled();
-    }));
+    });
   });
 });
