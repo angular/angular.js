5f785ff979278ddce44abfc7ee7c9e90c89f6e9b
Igor Minar <igor@angularjs.org>
2011-12-15 01:10:09 -0800

    chore(log): generic test log service with custom toEquals matcher
    - any test that needs a logger can just inject provideLog
    - logger has susict api that makes tests more readable
    - custom toEquals matcher allows for pretty expectations

--- test/testabilityPatch.js
+++ test/testabilityPatch.js
@@ -185,3 +185,35 @@ function assertVisible(node) {
   }
 }
 
+function provideLog($provide) {
+  $provide.factory('log', function() {
+      var messages = [];
+
+      function log(msg) {
+        messages.push(msg);
+        return msg;
+      }
+
+      log.toString = function() {
+        return messages.join('; ');
+      }
+
+      log.toArray = function() {
+        return messages;
+      }
+
+      log.reset = function() {
+        messages = [];
+      }
+
+      log.fn = function(msg) {
+        return function() {
+          log(msg);
+        }
+      }
+
+      log.$$log = true;
+
+      return log;
+    });
+}
