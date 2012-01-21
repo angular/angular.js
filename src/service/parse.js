dc85bcf6f97cee4184c77babe5438ac3d703cb46
Igor Minar <igor@angularjs.org>
2012-01-21 00:02:22 -0800

    fix($parse): fixing typos in JS_KEYWORDS

--- src/service/parse.js
+++ src/service/parse.js
@@ -675,7 +675,7 @@ var getterFnCache = {},
 forEach(
     ("abstract,boolean,break,byte,case,catch,char,class,const,continue,debugger,default," +
     "delete,do,double,else,enum,export,extends,false,final,finally,float,for,function,goto," +
-    "if,implements,import,ininstanceof,intinterface,long,native,new,null,package,private," +
+    "if,implements,import,in,instanceof,int,interface,long,native,new,null,package,private," +
     "protected,public,return,short,static,super,switch,synchronized,this,throw,throws," +
     "transient,true,try,typeof,var,volatile,void,undefined,while,with").split(/,/),
   function(key){ JS_KEYWORDS[key] = true;}
