b360437a4fb369cacc39c867ab361274804083ca
Igor Minar <igor@angularjs.org>
2012-03-09 14:49:40 -0800

    fix(startTag): fix tagname extraction
    the original code magically worked for ng:foo but for nothing else

--- test/service/compilerSpec.js
+++ test/service/compilerSpec.js
@@ -715,8 +715,8 @@ describe('$compile', function() {
           inject(function($compile){
             expect(function() {
               $compile('<div><div class="sync async"></div></div>');
-            }).toThrow('Multiple directives [sync, async] asking for template on: <'+
-                (msie <= 8 ? 'DIV' : 'div') + ' class="sync async">');
+            }).toThrow('Multiple directives [sync, async] asking for template on: '+
+                '<div class="sync async">');
           });
         });
 
@@ -1004,8 +1004,7 @@ describe('$compile', function() {
             expect(function(){
               $compile('<div class="iscope-a; scope-b"></div>');
             }).toThrow('Multiple directives [iscopeA, scopeB] asking for isolated scope on: ' +
-                '<' + (msie < 9 ? 'DIV' : 'div') +
-                ' class="iscope-a; scope-b ng-isolate-scope ng-scope">');
+                '<div class="iscope-a; scope-b ng-isolate-scope ng-scope">');
           })
         );
 
@@ -1015,8 +1014,7 @@ describe('$compile', function() {
             expect(function(){
               $compile('<div class="iscope-a; iscope-b"></div>');
             }).toThrow('Multiple directives [iscopeA, iscopeB] asking for isolated scope on: ' +
-                '<' + (msie < 9 ? 'DIV' : 'div') +
-                ' class="iscope-a; iscope-b ng-isolate-scope ng-scope">');
+                '<div class="iscope-a; iscope-b ng-isolate-scope ng-scope">');
           })
         );
 
@@ -1694,8 +1692,8 @@ describe('$compile', function() {
       inject(function($compile) {
         expect(function() {
           $compile('<div class="first second"></div>');
-        }).toThrow('Multiple directives [first, second] asking for transclusion on: <' +
-            (msie <= 8 ? 'DIV' : 'div') + ' class="first second ng-isolate-scope ng-scope">');
+        }).toThrow('Multiple directives [first, second] asking for transclusion on: ' +
+            '<div class="first second ng-isolate-scope ng-scope">');
       });
     });
 
