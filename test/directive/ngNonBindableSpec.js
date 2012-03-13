6af5950833e32bffe114d331952ffb5ef081127d
Misko Hevery <misko@hevery.com>
2012-03-13 14:42:26 -0700

    bug(compile): boolean attributes too agresive
    #bug
    - compiler would rewrite boolean attributes on all elements. This is too aggressive and interferes with some third-party frameworks

--- test/directive/ngNonBindableSpec.js
+++ test/directive/ngNonBindableSpec.js
@@ -10,7 +10,7 @@ describe('ng-non-bindable', function() {
   });
 
 
-  iit('should prevent compilation of the owning element and its children',
+  it('should prevent compilation of the owning element and its children',
       inject(function($rootScope, $compile) {
     element = $compile('<div ng-non-bindable text="{{name}}"><span ng-bind="name"></span></div>')($rootScope);
     $rootScope.name =  'misko';
