ad973365e309aa45baf95d4631d60c897b2870f1
Igor Minar <igor@angularjs.org>
2012-03-19 11:15:09 -0700

    fix($log): avoid console.log.apply calls in IE
    In IE window.console.log and friends are functions that don't have apply or call fns.
    
    For this reason we have to treat them specially and do our best to log at least
    something when running in this browser.
    
    Closes #805

--- src/service/log.js
+++ src/service/log.js
@@ -93,19 +93,23 @@ function $LogProvider(){
     }
 
     function consoleLog(type) {
-      var console = $window.console || {};
-      var logFn = console[type] || console.log || noop;
+      var console = $window.console || {},
+          logFn = console[type] || console.log || noop;
+
       if (logFn.apply) {
         return function() {
           var args = [];
-          forEach(arguments, function(arg){
+          forEach(arguments, function(arg) {
             args.push(formatError(arg));
           });
           return logFn.apply(console, args);
         };
-      } else {
-        // we are IE, in which case there is nothing we can do
-        return logFn;
+      }
+
+      // we are IE which either doesn't have window.console => this is noop and we do nothing,
+      // or we are IE where console.log doesn't have apply so we log at least first 2 args
+      return function(arg1, arg2) {
+        logFn(arg1, arg2);
       }
     }
   }];
