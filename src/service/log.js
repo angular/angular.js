64660cf6738ba9e8501e6811e053eb6f12c81fe1
Igor Minar <igor@angularjs.org>
2012-03-19 09:44:05 -0700

    style(log): reformat code for readability

--- src/service/log.js
+++ src/service/log.js
@@ -82,8 +82,9 @@ function $LogProvider(){
     function formatError(arg) {
       if (arg instanceof Error) {
         if (arg.stack) {
-          arg = (arg.message && arg.stack.indexOf(arg.message) === -1) ?
-                'Error: ' + arg.message + '\n' + arg.stack : arg.stack;
+          arg = (arg.message && arg.stack.indexOf(arg.message) === -1)
+              ? 'Error: ' + arg.message + '\n' + arg.stack
+              : arg.stack;
         } else if (arg.sourceURL) {
           arg = arg.message + '\n' + arg.sourceURL + ':' + arg.line;
         }
