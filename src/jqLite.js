5fdab52dd7c269f99839f4fa6b5854d9548269fa
Igor Minar <igor@angularjs.org>
2012-03-21 15:16:41 -0700

    feat(jqLite): make injector() and scope() work with the document object
    For typical app that has ng-app directive on the html element, we now can do:
    
    angular.element(document).injector() or .injector()
    angular.element(document).scope() or .scope()
    
    instead of:
    
    angular.element(document.getElementsByTagName('html')[0]).injector()
    ...

--- src/jqLite.js
+++ src/jqLite.js
@@ -279,6 +279,13 @@ function JQLiteController(element, name) {
 
 function JQLiteInheritedData(element, name, value) {
   element = jqLite(element);
+
+  // if element is the document object work with the html element instead
+  // this makes $(document).scope() possible
+  if(element[0].nodeType == 9) {
+    element = element.find('html');
+  }
+
   while (element.length) {
     if (value = element.data(name)) return value;
     element = element.parent();
