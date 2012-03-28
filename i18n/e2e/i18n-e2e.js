678b6206c0c28f334065504e99736b5109f856d4
Misko Hevery <misko@hevery.com>
2012-03-28 11:16:10 -0700

    fix(i18n e2e tests):

--- i18n/e2e/i18n-e2e.js
+++ i18n/e2e/i18n-e2e.js
@@ -8,7 +8,7 @@ describe("localized filters", function() {
       expect(binding('input | date:"medium"')).toBe('03/06/1977 18:07:23');
       expect(binding('input | date:"longDate"')).toBe("3 de junio de 1977");
       expect(binding('input | number')).toBe('234.234.443.432');
-      expect(binding('input | currency')).toBe('€&nbsp;234.234.443.432,00');
+      expect(binding('input | currency')).toBe('€\u00a0234.234.443.432,00');
     });
   });
 
@@ -20,8 +20,8 @@ describe("localized filters", function() {
     it('should check filters for cs locale', function() {
       expect(binding('input | date:"medium"')).toBe('3.6.1977 18:07:23');
       expect(binding('input | date:"longDate"')).toBe("3. června 1977");
-      expect(binding('input | number')).toBe('234&nbsp;234&nbsp;443&nbsp;432');
-      expect(binding('input | currency')).toBe('234&nbsp;234&nbsp;443&nbsp;432,00&nbsp;K\u010d');
+      expect(binding('input | number')).toBe('234\u00a0234\u00a0443\u00a0432');
+      expect(binding('input | currency')).toBe('234\u00a0234\u00a0443\u00a0432,00\u00a0K\u010d');
     });
   });
 
@@ -34,7 +34,7 @@ describe("localized filters", function() {
       expect(binding('input | date:"medium"')).toBe('03.06.1977 18:07:23');
       expect(binding('input | date:"longDate"')).toBe("3. Juni 1977");
       expect(binding('input | number')).toBe('234.234.443.432');
-      expect(binding('input | currency')).toBe('234.234.443.432,00&nbsp;€');
+      expect(binding('input | currency')).toBe('234.234.443.432,00\u00a0€');
     });
   });
 
@@ -53,42 +53,42 @@ describe("localized filters", function() {
 
     describe('ng:pluralize for en locale', function() {
       it('should show pluralized strings', function() {
-        expect(element('.ng-pluralize:first').html()).toBe('You have one email!');
+        expect(element('ng-pluralize:first').html()).toBe('You have one email!');
 
         input('plInput').enter('0');
-        expect(element('.ng-pluralize:first').html()).toBe('You have no email!');
+        expect(element('ng-pluralize:first').html()).toBe('You have no email!');
 
         input('plInput').enter('3');
-        expect(element('.ng-pluralize:first').html()).toBe('You have 3 emails!');
+        expect(element('ng-pluralize:first').html()).toBe('You have 3 emails!');
       });
 
       it('should show pluralized strings with offsets', function() {
-        expect(element('.ng-pluralize:last').html()).toBe('Shanjian is viewing!');
+        expect(element('ng-pluralize:last').html()).toBe('Shanjian is viewing!');
 
         input('plInput2').enter('0');
-        expect(element('.ng-pluralize:last').html()).toBe('Nobody is viewing!');
+        expect(element('ng-pluralize:last').html()).toBe('Nobody is viewing!');
 
         input('plInput2').enter('2');
-        expect(element('.ng-pluralize:last').html()).toBe('Shanjian and Di are viewing!');
+        expect(element('ng-pluralize:last').html()).toBe('Shanjian and Di are viewing!');
 
         input('plInput2').enter('3');
-        expect(element('.ng-pluralize:last').html()).
+        expect(element('ng-pluralize:last').html()).
             toBe('Shanjian, Di and one other person are viewing!');
 
         input('plInput2').enter('4');
-        expect(element('.ng-pluralize:last').html()).
+        expect(element('ng-pluralize:last').html()).
             toBe('Shanjian, Di and 2 other people are viewing!');
       });
 
       it('should show pluralized strings with correct data-binding', function() {
         input('plInput2').enter('2');
-        expect(element('.ng-pluralize:last').html()).toBe('Shanjian and Di are viewing!');
+        expect(element('ng-pluralize:last').html()).toBe('Shanjian and Di are viewing!');
 
         input('person1').enter('Igor');
-        expect(element('.ng-pluralize:last').html()).toBe('Igor and Di are viewing!');
+        expect(element('ng-pluralize:last').html()).toBe('Igor and Di are viewing!');
 
         input('person2').enter('Vojta');
-        expect(element('.ng-pluralize:last').html()).toBe('Igor and Vojta are viewing!');
+        expect(element('ng-pluralize:last').html()).toBe('Igor and Vojta are viewing!');
       });
     })
   });
@@ -101,26 +101,26 @@ describe("localized filters", function() {
     it('should check filters for sk locale', function() {
       expect(binding('input | date:"medium"')).toBe('3.6.1977 18:07:23');
       expect(binding('input | date:"longDate"')).toBe("3. júna 1977");
-      expect(binding('input | number')).toBe('234&nbsp;234&nbsp;443&nbsp;432');
-      expect(binding('input | currency')).toBe('234&nbsp;234&nbsp;443&nbsp;432,00&nbsp;Sk');
+      expect(binding('input | number')).toBe('234\u00a0234\u00a0443\u00a0432');
+      expect(binding('input | currency')).toBe('234\u00a0234\u00a0443\u00a0432,00\u00a0Sk');
     });
 
 
     describe('ng:pluralize for sk locale', function() {
       it('should show pluralized strings', function() {
-        expect(element('.ng-pluralize').html()).toBe('Mas jeden email!');
+        expect(element('ng-pluralize').html()).toBe('Mas jeden email!');
 
         input('plInput').enter('0');
-        expect(element('.ng-pluralize:first').html()).toBe('Mas 0 emailov!');
+        expect(element('ng-pluralize:first').html()).toBe('Mas 0 emailov!');
 
         input('plInput').enter('3');
-        expect(element('.ng-pluralize:first').html()).toBe('Mas 3 emaily!');
+        expect(element('ng-pluralize:first').html()).toBe('Mas 3 emaily!');
 
         input('plInput').enter('4');
-        expect(element('.ng-pluralize:first').html()).toBe('Mas 4 emaily!');
+        expect(element('ng-pluralize:first').html()).toBe('Mas 4 emaily!');
 
         input('plInput').enter('6');
-        expect(element('.ng-pluralize:first').html()).toBe('Mas 6 emailov!');
+        expect(element('ng-pluralize:first').html()).toBe('Mas 6 emailov!');
       });
 
       it('should show pluralized strings with offsets', function() {
@@ -147,38 +147,38 @@ describe("localized filters", function() {
 
     describe('ng:pluralize for zh locale', function() {
       it('should show pluralized strings', function() {
-        expect(element('.ng-pluralize:first').html()).toBe('1人在浏览该文件!');
+        expect(element('ng-pluralize:first').html()).toBe('1人在浏览该文件!');
 
         input('plInput').enter('0');
-        expect(element('.ng-pluralize:first').html()).toBe('0人在浏览该文件!');
+        expect(element('ng-pluralize:first').html()).toBe('0人在浏览该文件!');
 
         input('plInput').enter('3');
-        expect(element('.ng-pluralize:first').html()).toBe('3人在浏览该文件!');
+        expect(element('ng-pluralize:first').html()).toBe('3人在浏览该文件!');
       });
 
       it('should show pluralized strings with offsets', function() {
-        expect(element('.ng-pluralize:last').html()).toBe('Shanjian 在浏览该文件!');
+        expect(element('ng-pluralize:last').html()).toBe('Shanjian 在浏览该文件!');
 
         input('plInput2').enter('0');
-        expect(element('.ng-pluralize:last').html()).toBe('没有人在浏览该文件!');
+        expect(element('ng-pluralize:last').html()).toBe('没有人在浏览该文件!');
 
         input('plInput2').enter('2');
-        expect(element('.ng-pluralize:last').html()).toBe('Shanjian 和 Di 在浏览该文件!');
+        expect(element('ng-pluralize:last').html()).toBe('Shanjian 和 Di 在浏览该文件!');
 
         input('plInput2').enter('3');
-        expect(element('.ng-pluralize:last').html()).
+        expect(element('ng-pluralize:last').html()).
             toBe('Shanjian, Di 还有其他1 人在浏览该文件!');
       });
 
       it('should show pluralized strings with correct data-binding', function() {
         input('plInput2').enter('2');
-        expect(element('.ng-pluralize:last').html()).toBe('Shanjian 和 Di 在浏览该文件!');
+        expect(element('ng-pluralize:last').html()).toBe('Shanjian 和 Di 在浏览该文件!');
 
         input('person1').enter('彭迪');
-        expect(element('.ng-pluralize:last').html()).toBe('彭迪 和 Di 在浏览该文件!');
+        expect(element('ng-pluralize:last').html()).toBe('彭迪 和 Di 在浏览该文件!');
 
         input('person2').enter('一哥');
-        expect(element('.ng-pluralize:last').html()).toBe('彭迪 和 一哥 在浏览该文件!');
+        expect(element('ng-pluralize:last').html()).toBe('彭迪 和 一哥 在浏览该文件!');
       });
     })
   });
