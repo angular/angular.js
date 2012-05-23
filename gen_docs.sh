8bf1d23df806b70875d8c32ead12b8b68de509f7
Igor Minar <igor@angularjs.org>
2012-05-22 23:39:07 -0700

    chore(package.json): add simple package.json with npm dependencies

--- gen_docs.sh
+++ gen_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 if [ ! -e gen_docs.disable ]; then
-  jasmine-node docs/spec --noColor && node docs/src/gen-docs.js
+  ./node_modules/.bin/jasmine-node docs/spec --noColor && node docs/src/gen-docs.js
 fi
