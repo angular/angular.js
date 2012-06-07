8ba7e289c444c031530772539f06f8be9565a52f
Brian Ford <btford@umich.edu>
2012-06-07 14:23:33 -0700

    moved appContextMock to its own file

--- extension/test/TreeCtrlSpec.js
+++ extension/test/TreeCtrlSpec.js
@@ -1,48 +1,5 @@
 inspect = jasmine.createSpy('inspect');
 
-function createAppContextMock () {
-  var scopeMocks = {
-    "ZZZ": {
-      id: "ZZZ",
-      $apply: function () {},
-      someKey: 'someOldValue'
-    }
-  };
-  var scopeEltMocks = {
-    "ZZZ": "elementMock"
-  };
-  var regScope;
-  return {
-    executeOnScope: function (scopeId, fn, args, cb) {
-      if (regScope && regScope.$id === scopeId) {
-        fn(regScope, 'elementMock', args);
-        if (cb) {
-          cb();
-        }
-      }
-      if (scopeMocks[scopeId] && scopeEltMocks[scopeId]) {
-        fn(scopeMocks[scopeId],
-          scopeEltMocks[scopeId],
-          args);
-
-        if (cb) {
-          cb();
-        }
-      }
-    },
-    registerScope: function (scope) {
-      regScope = scope;
-    },
-    getDebugInfo: function (cb) {
-      cb({
-        roots: [ "YYY" ],
-        trees: {}
-      });
-    },
-    watchRefresh: function (cb) {}
-  }
-}
-
 describe('panelApp:TreeCtrl', function () {
 
   beforeEach(module('panelApp'));
---
+++ extension/test/mock/AppContextMock.js
@@ -0,0 +1,42 @@
+function createAppContextMock () {
+  var scopeMocks = {
+    "ZZZ": {
+      id: "ZZZ",
+      $apply: function () {},
+      someKey: 'someOldValue'
+    }
+  };
+  var scopeEltMocks = {
+    "ZZZ": "elementMock"
+  };
+  var regScope;
+  return {
+    executeOnScope: function (scopeId, fn, args, cb) {
+      if (regScope && regScope.$id === scopeId) {
+        fn(regScope, 'elementMock', args);
+        if (cb) {
+          cb();
+        }
+      }
+      if (scopeMocks[scopeId] && scopeEltMocks[scopeId]) {
+        fn(scopeMocks[scopeId],
+          scopeEltMocks[scopeId],
+          args);
+
+        if (cb) {
+          cb();
+        }
+      }
+    },
+    registerScope: function (scope) {
+      regScope = scope;
+    },
+    getDebugInfo: function (cb) {
+      cb({
+        roots: [ "YYY" ],
+        trees: {}
+      });
+    },
+    watchRefresh: function (cb) {}
+  }
+}
