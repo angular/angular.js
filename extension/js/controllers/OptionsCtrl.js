9945faa882fd857cd89f8cb8d538605ff10af66c
Brian Ford <btford@umich.edu>
2012-06-18 13:23:35 -0700

    fixed issue with show/hide scopes/bindings

--- extension/js/controllers/OptionsCtrl.js
+++ extension/js/controllers/OptionsCtrl.js
@@ -7,17 +7,53 @@ panelApp.controller('OptionsCtrl', function OptionsCtrl($scope, appContext, chro
 
   $scope.$watch('debugger.scopes', function (newVal, oldVal) {
     if (newVal) {
-      chromeExtension.sendRequest('showScopes');
+      chromeExtension.eval(function () {
+        var addCssRule = function (selector, rule) {
+          var styleSheet = document.styleSheets[document.styleSheets.length - 1];
+          styleSheet.insertRule(selector + '{' + rule + '}', styleSheet.cssRules.length);
+        };
+        addCssRule('.ng-scope', 'border: 1px solid red');
+      });
     } else {
-      chromeExtension.sendRequest('hideScopes');
+      chromeExtension.eval(function () {
+        removeCssRule = function (selector, rule) {
+          var styleSheet = document.styleSheets[document.styleSheets.length - 1];
+
+          var i;
+          for (i = styleSheet.cssRules.length - 1; i >= 0; i -= 1) {
+            if (styleSheet.cssRules[i].cssText === selector + ' { ' + rule + '; }') {
+              styleSheet.deleteRule(i);
+            }
+          }
+        };
+        removeCssRule('.ng-scope', 'border: 1px solid red');
+      });
     }
   });
 
   $scope.$watch('debugger.bindings', function (newVal, oldVal) {
     if (newVal) {
-      chromeExtension.sendRequest('showBindings');
+      chromeExtension.eval(function () {
+        var addCssRule = function (selector, rule) {
+          var styleSheet = document.styleSheets[document.styleSheets.length - 1];
+          styleSheet.insertRule(selector + '{' + rule + '}', styleSheet.cssRules.length);
+        };
+        addCssRule('.ng-binding', 'border: 1px solid blue');
+      });
     } else {
-      chromeExtension.sendRequest('hideBindings');
+      chromeExtension.eval(function () {
+        removeCssRule = function (selector, rule) {
+          var styleSheet = document.styleSheets[document.styleSheets.length - 1];
+
+          var i;
+          for (i = styleSheet.cssRules.length - 1; i >= 0; i -= 1) {
+            if (styleSheet.cssRules[i].cssText === selector + ' { ' + rule + '; }') {
+              styleSheet.deleteRule(i);
+            }
+          }
+        };
+        removeCssRule('.ng-binding', 'border: 1px solid blue');
+      });
     }
   });
 });
