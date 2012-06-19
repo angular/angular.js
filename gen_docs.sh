14593762ba8598c99562ccbad61867a214a2f607
Jonathan Zacsh <jzacsh@gmail.com>
2012-06-19 17:26:18 -0400

    minor bashy goodness bit.

--- gen_docs.sh
+++ gen_docs.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-if [ ! -e gen_docs.disable ]; then
+#!/usr/bin/env bash
+
+if [[ ! -e gen_docs.disable ]]; then
+  echo 'Testing, then building documentation...'
   jasmine-node docs/spec --noColor && node docs/src/gen-docs.js
 fi
+
