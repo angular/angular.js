f0a090ddf256d0c144e705c0cdf4216d824140f9
Igor Minar <igor@angularjs.org>
2012-06-26 14:50:19 -0700

    fix(docs): correctly generate sitemap

--- docs/spec/sitemapSpec.js
+++ docs/spec/sitemapSpec.js
@@ -15,7 +15,7 @@ describe('sitemap', function() {
     var map = new SiteMap([new Doc({section: 'foo', id: 'a.b.c<>\'"&'})]);
     expect(map.render()).toContain([
       ' <url>',
-      '<loc>http://docs.angularjs.org/#!/foo/a.b.c&lt;&gt;&apos;&quot;&amp;</loc>',
+      '<loc>http://docs.angularjs.org/foo/a.b.c&lt;&gt;&apos;&quot;&amp;</loc>',
       '<changefreq>weekly</changefreq>',
       '</url>'].join(''));
 
