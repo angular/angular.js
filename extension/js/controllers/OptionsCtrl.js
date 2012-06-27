e0858a39a74608ac8a16754cf95cadc1ac619115
Brian Ford <btford@umich.edu>
2012-06-27 14:49:21 -0700

    added 'show apps' option

--- extension/js/controllers/OptionsCtrl.js
+++ extension/js/controllers/OptionsCtrl.js
@@ -2,7 +2,8 @@ panelApp.controller('OptionsCtrl', function OptionsCtrl($scope, appContext, chro
 
   $scope.debugger = {
     scopes: false,
-    bindings: false
+    bindings: false,
+    app: false
   };
 
   $scope.$watch('debugger.scopes', function (newVal, oldVal) {
@@ -53,6 +54,36 @@ panelApp.controller('OptionsCtrl', function OptionsCtrl($scope, appContext, chro
           }
         };
         removeCssRule('.ng-binding', 'border: 1px solid blue');
+      });
+    }
+  });
+
+  $scope.$watch('debugger.app', function (newVal, oldVal) {
+    if (newVal) {
+      chromeExtension.eval(function () {
+        var addCssRule = function (selector, rule) {
+          var styleSheet = document.styleSheets[document.styleSheets.length - 1];
+          styleSheet.insertRule(selector + '{' + rule + '}', styleSheet.cssRules.length);
+        };
+        addCssRule('[ng-app]', 'border: 1px solid green');
+        //addCssRule('ng-app:', 'border: 1px solid green');
+        addCssRule('[app-run]', 'border: 1px solid green');
+      });
+    } else {
+      chromeExtension.eval(function () {
+        removeCssRule = function (selector, rule) {
+          var styleSheet = document.styleSheets[document.styleSheets.length - 1];
+
+          var i;
+          for (i = styleSheet.cssRules.length - 1; i >= 0; i -= 1) {
+            if (styleSheet.cssRules[i].cssText === selector + ' { ' + rule + '; }') {
+              styleSheet.deleteRule(i);
+            }
+          }
+        };
+        removeCssRule('[ng-app]', 'border: 1px solid green');
+        //removeCssRule('ng-app:', 'border: 1px solid green');
+        removeCssRule('[app-run]', 'border: 1px solid green');
       });
     }
   });
