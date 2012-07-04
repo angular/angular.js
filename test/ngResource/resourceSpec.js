10cbbbe112484a364ac570ee12281efc4bf12b75
Pascal Corpet <lascap@google.com>
2012-07-04 10:07:09 +0200

    feat($resource): allow dynamic default parameters
    Default resource params can now be calculated at runtime if defined
    via a function.

--- test/ngResource/resourceSpec.js
+++ test/ngResource/resourceSpec.js
@@ -356,6 +356,35 @@ describe("resource", function() {
   });
 
 
+  it('should support dynamic default parameters (global)', function() {
+    var currentGroup = 'students',
+        Person = $resource('/Person/:group/:id', { group: function() { return currentGroup; }});
+
+
+    $httpBackend.expect('GET', '/Person/students/fedor').respond({id: 'fedor', email: 'f@f.com'});
+
+    var fedor = Person.get({id: 'fedor'});
+    $httpBackend.flush();
+
+    expect(fedor).toEqualData({id: 'fedor', email: 'f@f.com'});
+  });
+
+
+  it('should support dynamic default parameters (action specific)', function() {
+    var currentGroup = 'students',
+        Person = $resource('/Person/:group/:id', {}, {
+          fetch: {method: 'GET', params: {group: function() { return currentGroup; }}}
+        });
+
+    $httpBackend.expect('GET', '/Person/students/fedor').respond({id: 'fedor', email: 'f@f.com'});
+
+    var fedor = Person.fetch({id: 'fedor'});
+    $httpBackend.flush();
+
+    expect(fedor).toEqualData({id: 'fedor', email: 'f@f.com'});
+  });
+
+
   it('should exercise full stack', function() {
     var Person = $resource('/Person/:id');
 
