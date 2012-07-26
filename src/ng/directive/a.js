c81d8176cc55cd15acae05259ead73f90a01f0b7
sgtpep <danil.mail@gmail.com>
2012-07-27 01:26:46 +0400

    fix(a): prevent Opera from incorrectly navigating on link click
    we handle the navigation by ourselves, so we need to prevent the default action.
    
    Opera ignores event.preventDefault() call so we must return false.

--- src/ng/directive/a.js
+++ src/ng/directive/a.js
@@ -27,6 +27,7 @@ var htmlAnchorDirective = valueFn({
         // if we have no href url, then don't navigate anywhere.
         if (!element.attr('href')) {
           event.preventDefault();
+          return false; // Needed for opera
         }
       });
     }
