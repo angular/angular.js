9a3a9b46e5d596811d85f11be16ac805ddfdc8f1
Julie <ju.ralph@gmail.com>
2012-10-09 11:13:26 -0700

    fix(scenario): include error messages in XML output
    Fix the XML output of scenario tests so that it properly includes error
    messages from failing specs.

--- test/ngScenario/output/xmlSpec.js
+++ test/ngScenario/output/xmlSpec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('angular.scenario.output.json', function() {
+describe('angular.scenario.output.xml', function() {
   var output, context;
   var runner, model, $window;
   var spec, step;
@@ -32,5 +32,18 @@ describe('angular.scenario.output.json', function() {
     runner.emit('RunnerEnd');
     expect(context.find('it').attr('status')).toEqual('success');
     expect(context.find('it step').attr('status')).toEqual('success');
+  });
+
+  it('should output errors to the XML', function() {
+    runner.emit('SpecBegin', spec);
+    runner.emit('StepBegin', spec, step);
+    runner.emit('StepFailure', spec, step, 'error reason');
+    runner.emit('StepEnd', spec, step);
+    runner.emit('SpecEnd', spec);
+    runner.emit('RunnerEnd');
+
+    expect(context.find('it').attr('status')).toEqual('failure');
+    expect(context.find('it step').attr('status')).toEqual('failure');
+    expect(context.find('it step').text()).toEqual('error reason');
   });
 });
