19a324ce116b6f5a728f5f78f5c19a364a779e22
Haralan Dobrev <hkdobrev@gmail.com>
2012-10-18 11:03:14 +0300

    docs(angular.module): improve angular.Module#run docs

--- src/loader.js
+++ src/loader.js
@@ -215,8 +215,8 @@ function setupModuleLoader(window) {
            * @param {Function} initializationFn Execute this function after injector creation.
            *    Useful for application initialization.
            * @description
-           * Use this method to register work which needs to be performed when the injector with
-           * with the current module is finished loading.
+           * Use this method to register work which should be performed when the injector is done
+           * loading all modules.
            */
           run: function(block) {
             runBlocks.push(block);
