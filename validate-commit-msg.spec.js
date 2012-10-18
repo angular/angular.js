adb3c5f59083808f3a18afce8d26b3b4ae6571b2
Igor Minar <igor@angularjs.org>
2012-10-18 03:26:08 -0700

    chore(validate-commit-msg): allow * and - in scope string

--- validate-commit-msg.spec.js
+++ validate-commit-msg.spec.js
@@ -29,6 +29,8 @@ describe('validate-commit-msg.js', function() {
       expect(m.validateMessage('refactor($httpBackend): something')).toBe(VALID);
       expect(m.validateMessage('test($resource): something')).toBe(VALID);
       expect(m.validateMessage('chore($controller): something')).toBe(VALID);
+      expect(m.validateMessage('chore(foo-bar): something')).toBe(VALID);
+      expect(m.validateMessage('chore(*): something')).toBe(VALID);
       expect(errors).toEqual([]);
     });
 
