4eb7c3e113360ee987713c324fd967088e52858c
neocris <gmcris+git@gmail.com>
2012-11-13 14:14:31 +0000

    added dropdown support to tabbable tabs directive
    http://plnkr.co/edit/o3hSr8

--- src/bootstrap/bootstrap.js
+++ src/bootstrap/bootstrap.js
@@ -76,10 +76,19 @@ directive.tabbable = function() {
           }
           if($viewValue) {
             for(var i = 0, ii = tabs.length; i < ii; i++) {
-              if ($viewValue == tabs[i].value) {
-                selectedTab = tabs[i];
-                break;
-              }
+              if(tabs[i].subTabs.length>0)
+                for(var j = 0, jj = tabs[i].subTabs.length; j < jj; j++){
+                  if ($viewValue == tabs[i].subTabs[j].value) {
+                    selectedTab = tabs[i].subTabs[j];
+                    selectedTab.paneElement.parent().addClass('active');
+                    break;
+                  }
+                }
+              else
+                if ($viewValue == tabs[i].value) {
+                  selectedTab = tabs[i];
+                  break;
+                }
             }
             if (selectedTab) {
               selectedTab.paneElement.addClass('active');
@@ -91,20 +100,49 @@ directive.tabbable = function() {
       };
 
       this.addPane = function(element, attr) {
-        var li = angular.element('<li><a href></a></li>'),
+        var fbind, _a, close,
+        li = null;
+        if(!attr.type)
+          li = angular.element('<li><a href></a></li>');
+        else
+          li = angular.element('<li class="dropdown"><a href data-toggle="dropdown" class="dropdown-toggle"><b class="caret"></b></a><ul class="dropdown-menu"></ul></li>');
+        var li_drop_item = null,
             a = li.find('a'),
+            ul = li.find('ul'),
             tab = {
               paneElement: element,
               paneAttrs: attr,
-              tabElement: li
+              tabElement: li,
+              subTabs: []
             };
 
         tabs.push(tab);
 
-        attr.$observe('value', update)();
-        attr.$observe('title', function(){ update(); a.text(tab.title); })();
+        attr.$observe('value', update);
+        attr.$observe('title', function(){ update(); a.prepend(tab.title); });
+        attr.$observe('type', function(){
+          update();
+          if(tab.type){
+            //atr was mock obj to pass as arg to addPane()
+            var atr = {$observe: function(){}}, _li, ndrops = element.children().length, elem;
+            //outer tab has no content
+            tab.paneElement = null;
+            for(var i=0; i<ndrops; i++){
+              elem = angular.element(element.children()[i]);
+              atr.title = elem.attr('_title');
+              _li = angular.element('<li><a href></a></li>');
+              _a = _li.find('a');
+              _a.text(atr.title);
+              ul.append(_li);
+              tab.subTabs.push({paneElement: elem, value: atr.title, tabElement: li});
+              _a.click(fbind);
+            }
+            return;
+          }
+        });
 
         function update() {
+          tab.type = attr.type || null;
           tab.title = attr.title;
           tab.value = attr.value || attr.title;
           if (!ngModel.$setViewValue && (!ngModel.$viewValue || tab == selectedTab)) {
@@ -115,9 +153,59 @@ directive.tabbable = function() {
         }
 
         navTabs.append(li);
-        li.bind('click', function(event) {
+
+        function fbind(event){
           event.preventDefault();
           event.stopPropagation();
+
+           var tabval;
+
+          tab.subTabs.forEach(function(e,i){
+            if($(event.target).text()==e.value){
+              tabval = e.value;
+              console.log(tabval);
+            }
+          });
+
+          if (ngModel.$setViewValue) {
+            $scope.$apply(function() {
+              ngModel.$setViewValue(tabval);
+              ngModel.$render();
+            });
+          } else {
+            // we are not part of angular
+            ngModel.$viewValue = tabval;
+            ngModel.$render();
+          }
+        }
+
+        a.bind('click', function(event) {console.log(tab.value);
+          event.preventDefault();
+          event.stopPropagation();
+
+          if(attr.type){
+            var iWasOpen = false, elm = $(this);
+
+            if ($(this).parent().hasClass('open')) {
+              iWasOpen = true;
+              close();
+            }
+
+            if (!iWasOpen){
+              $(this).parent().addClass('open');
+
+              close = function (event) {console.log('closing');
+                event && event.preventDefault();
+                event && event.stopPropagation();
+                $(document).unbind('click', close);
+                elm.parent().removeClass('open');
+                close = null;
+              }
+
+              $(document).bind('click', close);}
+              return;
+          }
+
           if (ngModel.$setViewValue) {
             $scope.$apply(function() {
               ngModel.$setViewValue(tab.value);
