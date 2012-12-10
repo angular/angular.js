e3ccade8b6c35a47894901a2eea5fc97387115f1
Uri Goldshtein <uri.goldshtein@gmail.com>
2012-12-10 19:01:59 +0200

    feat($filter): currency (currencySymbol, fractionSize, isMinus)
    Added new costumization possibilities to currency:
    - Fraction size
    - Change default negative numbers parentheses to '-' minus symbol
    
    Previous behavior remains the same but when using the new optional parameters, the currency symbol becomes obligatory.

--- test/ng/filter/filtersSpec.js
+++ test/ng/filter/filtersSpec.js
@@ -86,7 +86,6 @@ describe('filters', function() {
       expect(currency(1234.5678, "USD$")).toEqual('USD$1,234.57');
       expect(currency(1234.56, "$",0)).toBe('$1,235');
       expect(currency(-1234.56, "$",2,true)).toBe('$-1,234.56');
-      expect(currency('amount | currency:"$":0')).toBe('$1,234');
     });
 
 
