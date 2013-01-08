de6cc287e51dcdd91c63d7427dbdc28cae111ad5
Igor Minar <igor@angularjs.org>
2013-01-08 14:36:03 -0800

    fix($injector): remove bogus fn arg
    getService fn takes only one argument, removing the second one.
    
    Closes #1711

--- src/auto/injector.js
+++ src/auto/injector.js
@@ -557,7 +557,7 @@ function createInjector(modulesToLoad) {
         args.push(
           locals && locals.hasOwnProperty(key)
           ? locals[key]
-          : getService(key, path)
+          : getService(key)
         );
       }
       if (!fn.$inject) {
