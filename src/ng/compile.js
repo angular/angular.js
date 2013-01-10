2ba458387d19543b45c3bfdbe8db719d2c0f1f15
Igor Minar <igor@angularjs.org>
2013-01-09 22:07:33 -0800

    fix($compile): safely create transclude comment nodes
    Closes #1740

--- src/ng/compile.js
+++ src/ng/compile.js
@@ -575,7 +575,7 @@ function $CompileProvider($provide) {
           if (directiveValue == 'element') {
             $template = jqLite(compileNode);
             $compileNode = templateAttrs.$$element =
-                jqLite('<!-- ' + directiveName + ': ' + templateAttrs[directiveName]  + ' -->');
+                jqLite(document.createComment(' ' + directiveName + ': ' + templateAttrs[directiveName] + ' '));
             compileNode = $compileNode[0];
             replaceWith($rootElement, jqLite($template[0]), compileNode);
             childTranscludeFn = compile($template, transcludeFn, terminalPriority);
