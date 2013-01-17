224d7d6e9023395e9bf3f7217984c04fd550f4e7
Fred Sauer <fredsa@google.com>
2013-01-16 18:01:03 -0800

    docs(exceptionHandler): document testing
    Update src/ng/exceptionHandler.js
    
    Here's an iniitla attempt at documenting how one might write a
    test using $exceptionHandlerProvider. The key take-away is the use
    of this pattern:
    
    it(...
    
     module(...
       $exceptionHandlerProvider.mode('log');
     });
    
     inject(...
     );
    
    });

--- src/ng/exceptionHandler.js
+++ src/ng/exceptionHandler.js
@@ -11,11 +11,12 @@
  * the browser console.
  *
  * In unit tests, if `angular-mocks.js` is loaded, this service is overridden by
- * {@link ngMock.$exceptionHandler mock $exceptionHandler}
+ * {@link ngMock.$exceptionHandler mock $exceptionHandler} which aids in testing.
  *
  * @param {Error} exception Exception associated with the error.
  * @param {string=} cause optional information about the context in which
  *       the error was thrown.
+ *
  */
 function $ExceptionHandlerProvider() {
   this.$get = ['$log', function($log){
