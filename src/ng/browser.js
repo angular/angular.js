2b92322db7e5726d924a00a83dca62e54d79aa1a
Fredrik Bonander <carl.fredrik.bonander@gmail.com>
2013-01-29 11:52:40 +0100

    fix(): cookiePath now defaults to '' when basePath is undefined

--- src/ng/browser.js
+++ src/ng/browser.js
@@ -245,7 +245,7 @@ function Browser(window, document, $log, $sniffer) {
   //////////////////////////////////////////////////////////////
   var lastCookies = {};
   var lastCookieString = '';
-  var cookiePath = self.baseHref();
+  var cookiePath = self.baseHref() || '';
 
   /**
    * @name ng.$browser#cookies
