8a96393179d81c57a4ea0ea459aa7f77d63ae5d5
Dave Geddes <davidcgeddes@gmail.com>
2013-03-06 13:43:56 -0700

    chore(Grunt): don't remove root dir from zip

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
   grunt.loadTasks('lib/grunt');
 
   var NG_VERSION = util.getVersion();
+  var dist = 'angular-'+ NG_VERSION.full;
 
 
   //global beforeEach
@@ -147,8 +148,8 @@ module.exports = function(grunt) {
 
     compress: {
       build: {
-        options: {archive: 'build/angular-'+ NG_VERSION.full +'.zip'},
-        src: ['**'], cwd: 'build', expand: true
+        options: {archive: 'build/' + dist +'.zip'},
+        src: ['**'], cwd: 'build', expand: true, dest: dist + '/'
       }
     },
 
