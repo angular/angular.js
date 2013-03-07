98b94f4e9e11e5de76c27fb2b5617790f3e8eef5
Jason Morrison <jason.p.morrison@gmail.com>
2013-03-07 14:17:34 -0800

    docs($injector): remove extranneous 'the' from injector docs

--- src/auto/injector.js
+++ src/auto/injector.js
@@ -254,7 +254,7 @@ function annotate(fn) {
  * @description
  *
  * Use `$provide` to register new providers with the `$injector`. The providers are the factories for the instance.
- * The providers share the same name as the instance they create with the `Provider` suffixed to them.
+ * The providers share the same name as the instance they create with `Provider` suffixed to them.
  *
  * A provider is an object with a `$get()` method. The injector calls the `$get` method to create a new instance of
  * a service. The Provider can have additional methods which would allow for configuration of the provider.
