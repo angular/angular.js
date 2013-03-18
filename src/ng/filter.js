76c0ddfc0b0794c22da94be441b4e52bf9fdc0c9
Bruno Coelho <bcs.cin@gmail.com>
2013-03-18 11:40:17 -0300

    docs(filter): Using indefinite article
    This doc was using both definite article and indefinite article at the same time.

--- src/ng/filter.js
+++ src/ng/filter.js
@@ -7,7 +7,7 @@
  *
  * Filters are just functions which transform input to an output. However filters need to be Dependency Injected. To
  * achieve this a filter definition consists of a factory function which is annotated with dependencies and is
- * responsible for creating a the filter function.
+ * responsible for creating a filter function.
  *
  * <pre>
  *   // Filter registration
