fec4ef38815340e8e5a6b65fd6c08f5c74e701d8
Igor Minar <igor@angularjs.org>
2013-04-03 14:36:57 -0700

    feat(Scenario): autodisable animations when running e2e tests
    animations cause the dom to contain elements that have been removed
    from the model but are being animated out.
    
    we could teach the e2e runner to wait for animations but that would
    make all tests slower. it should be quite safe to just disable
    animations automatically when the app is running via the e2e test
    runner.
    
    this change disables only css animations. we should make additional
    change that disables js animations as well, but since we don't need
    this right now I'm punting on it.

--- src/ngScenario/Application.js
+++ src/ngScenario/Application.js
@@ -49,7 +49,7 @@ angular.scenario.Application.prototype.getWindow_ = function() {
  */
 angular.scenario.Application.prototype.navigateTo = function(url, loadFn, errorFn) {
   var self = this;
-  var frame = this.getFrame_();
+  var frame = self.getFrame_();
   //TODO(esprehn): Refactor to use rethrow()
   errorFn = errorFn || function(e) { throw e; };
   if (url === 'about:blank') {
@@ -57,21 +57,39 @@ angular.scenario.Application.prototype.navigateTo = function(url, loadFn, errorF
   } else if (url.charAt(0) === '#') {
     url = frame.attr('src').split('#')[0] + url;
     frame.attr('src', url);
-    this.executeAction(loadFn);
+    self.executeAction(loadFn);
   } else {
     frame.remove();
-    this.context.find('#test-frames').append('<iframe>');
-    frame = this.getFrame_();
+    self.context.find('#test-frames').append('<iframe>');
+    frame = self.getFrame_();
+
+    frame[0].contentWindow.name = "NG_DEFER_BOOTSTRAP!";
+
     frame.load(function() {
       frame.unbind();
       try {
+        var $window = self.getWindow_();
+
+        if ($window.angular) {
+          // Disable animations
+
+          // TODO(i): this doesn't disable javascript animations
+          //          we don't need that for our tests, but it should be done
+          $window.angular.resumeBootstrap([['$provide', function($provide) {
+            $provide.decorator('$sniffer', function($delegate) {
+              $delegate.supportsTransitions = false;
+              return $delegate;
+            });
+          }]]);
+        }
+
         self.executeAction(loadFn);
       } catch (e) {
         errorFn(e);
       }
     }).attr('src', url);
   }
-  this.context.find('> h2 a').attr('href', url).text(url);
+  self.context.find('> h2 a').attr('href', url).text(url);
 };
 
 /**
