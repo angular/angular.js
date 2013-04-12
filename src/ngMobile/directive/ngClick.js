da3da75a7d36f708b50af65c3bc8d428cde5df3a
Stephen von Takach <steve@advancedcontrol.com.au>
2013-04-12 19:46:59 +1000

    Only prevent ghost clicks when touch events are in use
    Pointers or mouse events will be used otherwise and pointers don't need their clicks busted.

--- src/ngMobile/directive/ngClick.js
+++ src/ngMobile/directive/ngClick.js
@@ -38,12 +38,7 @@ ngMobile.directive('ngClick', ['$parse', '$mobile', function($parse, $mobile) {
   
   // Actual linking function.
   return function(scope, element, attrs) {
-    var firstId,
-      tapElement, // Used to blur the element after a tap.
-      startTime,  // Used to check if the tap was held too long.
-      touchStartX,
-      touchStartY,
-      expressionFn = $parse(attrs.ngClick),
+    var expressionFn = $parse(attrs.ngClick),
       clickHandler = function(event) {
         scope.$apply(function() {
           // TODO(braden): This is sending the touchend, click or pointerup. Is that kosher?
@@ -51,65 +46,73 @@ ngMobile.directive('ngClick', ['$parse', '$mobile', function($parse, $mobile) {
         });
       };
 
-    $mobile.getPointerEvents(scope, element, {
-      down: function(id, pointer, event) {
-        if(!firstId) {
-          firstId = id;
-          tapElement = event.target ? event.target : event.srcElement;  // IE uses srcElement.
-
-          // Hack for Safari, which can target text nodes instead of containers.
-          if(tapElement.nodeType == 3) {
-            tapElement = tapElement.parentNode;
-          }
-
-          startTime = Date.now();
-
-          touchStartX = pointer.clientX;
-          touchStartY = pointer.clientY;
-        }
-      },
-      move: function(id, pointer, event) {  // Resets the state
-        if(firstId === id) {
-          firstId = undefined;
-        }
-      },
-      up: function(id, pointer, event) {
-        if(firstId === id) {
-          var diff = Date.now() - startTime,
-            x = pointer.clientX,
-            y = pointer.clientY,
-            dist = Math.sqrt( Math.pow(x - touchStartX, 2) + Math.pow(y - touchStartY, 2) );
-
-          try {
-            if (event.type == 'touchend' && diff < TAP_DURATION && dist < MOVE_TOLERANCE) {
-              $mobile.preventGhostClick(x, y);
-
-              // Blur the focused element (the button, probably) before firing the callback.
-              // This doesn't work perfectly on Android Chrome, but seems to work elsewhere.
-              // I couldn't get anything to work reliably on Android Chrome.
-              if (tapElement) {
-                tapElement.blur();
-              }
-              
-              // Send the pointer as it has the clientX / Y set so more useful then the raw
-              //  event object on touch devices. Mouse / Pointers this is the event object.
-              clickHandler(pointer);
+    if ("ontouchend" in document) {
+      var firstId,
+      tapElement, // Used to blur the element after a tap.
+      startTime,  // Used to check if the tap was held too long.
+      touchStartX,
+      touchStartY;
+      
+      $mobile.getPointerEvents(scope, element, {
+        down: function(id, pointer, event) {
+          if(!firstId && event.type != 'touchstart') {    // ignore any mouse events
+            firstId = id;
+            tapElement = event.target; //? event.target : event.srcElement;  // IE uses srcElement.
+  
+            // Hack for Safari, which can target text nodes instead of containers.
+            if(tapElement.nodeType == 3) {
+              tapElement = tapElement.parentNode;
             }
-          } finally {
-            firstId = undefined;    // Resets the state
+  
+            startTime = Date.now();
+  
+            touchStartX = pointer.clientX;
+            touchStartY = pointer.clientY;
+          }
+        },
+        move: function(id, pointer, event) {  // Resets the state
+          if(firstId === id) {
+            firstId = undefined;
+          }
+        },
+        up: function(id, pointer, event) {
+          if(firstId === id) {
+            var diff = Date.now() - startTime,
+              x = pointer.clientX,
+              y = pointer.clientY,
+              dist = Math.sqrt( Math.pow(x - touchStartX, 2) + Math.pow(y - touchStartY, 2) );
+  
+            try {
+              if (diff < TAP_DURATION && dist < MOVE_TOLERANCE) {
+                $mobile.preventGhostClick(x, y);
+  
+                // Blur the focused element (the button, probably) before firing the callback.
+                // This doesn't work perfectly on Android Chrome, but seems to work elsewhere.
+                // I couldn't get anything to work reliably on Android Chrome.
+                if (tapElement) {
+                  tapElement.blur();
+                }
+                
+                // Send the pointer as it has the clientX / Y set so more useful then the raw
+                //  event object on touch devices. Mouse / Pointers this is the event object.
+                clickHandler(pointer);
+              }
+            } finally {
+              firstId = undefined;    // Resets the state
+            }
+          }
+        },
+        cancel: function(id, event) {
+          if(firstId === id) {
+            firstId = undefined;      // Resets the state
           }
         }
-      },
-      cancel: function(id, event) {
-        if(firstId === id) {
-          firstId = undefined;      // Resets the state
-        }
-      }
-    });
-    
-    // Hack for iOS Safari's benefit. It goes searching for onclick handlers and is liable to click
-    // something else nearby.
-    element.onclick = function(event) { };
+      });
+      
+      // Hack for iOS Safari's benefit. It goes searching for onclick handlers and is liable to click
+      // something else nearby.
+      element.onclick = function(event) { };
+    }
 
     // Fallback click handler.
     // Busted clicks don't get this far, and adding this handler allows ng-tap to be used on
