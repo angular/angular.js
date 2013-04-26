3bf0120c9bac0bcd3bb3b1f2d7f3086ba8579a89
joshkurz <jkurz25@gmail.com>
2013-04-26 17:31:56 -0400

    feature(urlKeyValue): not rewriting duplicate params in url
    parseKeyValue and toKeyValue can work with duplicates:
        1)parseKeyValue looks for presence of obj[key]
        2)detects and replaces obj[key] with [obj[key],val]
        3)then pushes more duplicates if neccessary
        4)toKeyValue decodes array correctly
        5)(not changed)$location.search({param: 'key'}) still replaces if neccessary
        6)(not changed)$location.search({param: ['key','key2']}) sets the url with duplicates
    
    BREAKING CHANGE: if the server relied on the buggy behavior then either the
    backend should be fixed or a simple serialization of the array should be done
    on the client before calling the resource service.

--- test/AngularSpec.js
+++ test/AngularSpec.js
@@ -253,10 +253,22 @@ describe('angular', function() {
       expect(parseKeyValue('flag1&key=value&flag2')).
       toEqual({flag1: true, key: 'value', flag2: true});
     });
+
+    it('should parse a string into key-value pairs with duplicates grouped in an array', function() {
+      expect(parseKeyValue('')).toEqual({});
+      expect(parseKeyValue('duplicate=pair')).toEqual({duplicate: 'pair'});
+      expect(parseKeyValue('first=1&first=2')).toEqual({first: ['1','2']});
+      expect(parseKeyValue('escaped%20key=escaped%20value&&escaped%20key=escaped%20value2')).
+      toEqual({'escaped key': ['escaped value','escaped value2']});
+      expect(parseKeyValue('flag1&key=value&flag1')).
+      toEqual({flag1: [true,true], key: 'value'});
+      expect(parseKeyValue('flag1&flag1=value&flag1=value2&flag1')).
+      toEqual({flag1: [true,'value','value2',true]});
+    });
   });
 
   describe('toKeyValue', function() {
-    it('should parse key-value pairs into string', function() {
+    it('should serialize key-value pairs into string', function() {
       expect(toKeyValue({})).toEqual('');
       expect(toKeyValue({simple: 'pair'})).toEqual('simple=pair');
       expect(toKeyValue({first: '1', second: '2'})).toEqual('first=1&second=2');
@@ -265,8 +277,14 @@ describe('angular', function() {
       expect(toKeyValue({emptyKey: ''})).toEqual('emptyKey=');
     });
 
-    it('should parse true values into flags', function() {
+    it('should serialize true values into flags', function() {
       expect(toKeyValue({flag1: true, key: 'value', flag2: true})).toEqual('flag1&key=value&flag2');
+    });
+
+    it('should serialize duplicates into duplicate param strings', function() {
+      expect(toKeyValue({key: [323,'value',true]})).toEqual('key=323&key=value&key');
+      expect(toKeyValue({key: [323,'value',true, 1234]})).
+      toEqual('key=323&key=value&key&key=1234');
     });
   });
 
