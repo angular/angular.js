58a307c25cde1c16550b3381117699c0b547edda
Lucas Galfasó <lgalfaso@gmail.com>
2013-05-04 21:44:14 -0300

    fix(i18n): Do not transform arrays into objects
    Do not trasnform arrays into objects when generating the locale objects
    Add unit test for this check

--- src/ngLocale/angular-locale_el-gr.js
+++ src/ngLocale/angular-locale_el-gr.js
@@ -2,56 +2,56 @@ angular.module("ngLocale", [], ["$provide", function($provide) {
 var PLURAL_CATEGORY = {ZERO: "zero", ONE: "one", TWO: "two", FEW: "few", MANY: "many", OTHER: "other"};
 $provide.value("$locale", {
   "DATETIME_FORMATS": {
-    "AMPMS": {
-      "0": "\u03c0.\u03bc.",
-      "1": "\u03bc.\u03bc."
-    },
-    "DAY": {
-      "0": "\u039a\u03c5\u03c1\u03b9\u03b1\u03ba\u03ae",
-      "1": "\u0394\u03b5\u03c5\u03c4\u03ad\u03c1\u03b1",
-      "2": "\u03a4\u03c1\u03af\u03c4\u03b7",
-      "3": "\u03a4\u03b5\u03c4\u03ac\u03c1\u03c4\u03b7",
-      "4": "\u03a0\u03ad\u03bc\u03c0\u03c4\u03b7",
-      "5": "\u03a0\u03b1\u03c1\u03b1\u03c3\u03ba\u03b5\u03c5\u03ae",
-      "6": "\u03a3\u03ac\u03b2\u03b2\u03b1\u03c4\u03bf"
-    },
-    "MONTH": {
-      "0": "\u0399\u03b1\u03bd\u03bf\u03c5\u03b1\u03c1\u03af\u03bf\u03c5",
-      "1": "\u03a6\u03b5\u03b2\u03c1\u03bf\u03c5\u03b1\u03c1\u03af\u03bf\u03c5",
-      "2": "\u039c\u03b1\u03c1\u03c4\u03af\u03bf\u03c5",
-      "3": "\u0391\u03c0\u03c1\u03b9\u03bb\u03af\u03bf\u03c5",
-      "4": "\u039c\u03b1\u0390\u03bf\u03c5",
-      "5": "\u0399\u03bf\u03c5\u03bd\u03af\u03bf\u03c5",
-      "6": "\u0399\u03bf\u03c5\u03bb\u03af\u03bf\u03c5",
-      "7": "\u0391\u03c5\u03b3\u03bf\u03cd\u03c3\u03c4\u03bf\u03c5",
-      "8": "\u03a3\u03b5\u03c0\u03c4\u03b5\u03bc\u03b2\u03c1\u03af\u03bf\u03c5",
-      "9": "\u039f\u03ba\u03c4\u03c9\u03b2\u03c1\u03af\u03bf\u03c5",
-      "10": "\u039d\u03bf\u03b5\u03bc\u03b2\u03c1\u03af\u03bf\u03c5",
-      "11": "\u0394\u03b5\u03ba\u03b5\u03bc\u03b2\u03c1\u03af\u03bf\u03c5"
-    },
-    "SHORTDAY": {
-      "0": "\u039a\u03c5\u03c1",
-      "1": "\u0394\u03b5\u03c5",
-      "2": "\u03a4\u03c1\u03b9",
-      "3": "\u03a4\u03b5\u03c4",
-      "4": "\u03a0\u03b5\u03bc",
-      "5": "\u03a0\u03b1\u03c1",
-      "6": "\u03a3\u03b1\u03b2"
-    },
-    "SHORTMONTH": {
-      "0": "\u0399\u03b1\u03bd",
-      "1": "\u03a6\u03b5\u03b2",
-      "2": "\u039c\u03b1\u03c1",
-      "3": "\u0391\u03c0\u03c1",
-      "4": "\u039c\u03b1\u03ca",
-      "5": "\u0399\u03bf\u03c5\u03bd",
-      "6": "\u0399\u03bf\u03c5\u03bb",
-      "7": "\u0391\u03c5\u03b3",
-      "8": "\u03a3\u03b5\u03c0",
-      "9": "\u039f\u03ba\u03c4",
-      "10": "\u039d\u03bf\u03b5",
-      "11": "\u0394\u03b5\u03ba"
-    },
+    "AMPMS": [
+      "\u03c0.\u03bc.",
+      "\u03bc.\u03bc."
+    ],
+    "DAY": [
+      "\u039a\u03c5\u03c1\u03b9\u03b1\u03ba\u03ae",
+      "\u0394\u03b5\u03c5\u03c4\u03ad\u03c1\u03b1",
+      "\u03a4\u03c1\u03af\u03c4\u03b7",
+      "\u03a4\u03b5\u03c4\u03ac\u03c1\u03c4\u03b7",
+      "\u03a0\u03ad\u03bc\u03c0\u03c4\u03b7",
+      "\u03a0\u03b1\u03c1\u03b1\u03c3\u03ba\u03b5\u03c5\u03ae",
+      "\u03a3\u03ac\u03b2\u03b2\u03b1\u03c4\u03bf"
+    ],
+    "MONTH": [
+      "\u0399\u03b1\u03bd\u03bf\u03c5\u03b1\u03c1\u03af\u03bf\u03c5",
+      "\u03a6\u03b5\u03b2\u03c1\u03bf\u03c5\u03b1\u03c1\u03af\u03bf\u03c5",
+      "\u039c\u03b1\u03c1\u03c4\u03af\u03bf\u03c5",
+      "\u0391\u03c0\u03c1\u03b9\u03bb\u03af\u03bf\u03c5",
+      "\u039c\u03b1\u0390\u03bf\u03c5",
+      "\u0399\u03bf\u03c5\u03bd\u03af\u03bf\u03c5",
+      "\u0399\u03bf\u03c5\u03bb\u03af\u03bf\u03c5",
+      "\u0391\u03c5\u03b3\u03bf\u03cd\u03c3\u03c4\u03bf\u03c5",
+      "\u03a3\u03b5\u03c0\u03c4\u03b5\u03bc\u03b2\u03c1\u03af\u03bf\u03c5",
+      "\u039f\u03ba\u03c4\u03c9\u03b2\u03c1\u03af\u03bf\u03c5",
+      "\u039d\u03bf\u03b5\u03bc\u03b2\u03c1\u03af\u03bf\u03c5",
+      "\u0394\u03b5\u03ba\u03b5\u03bc\u03b2\u03c1\u03af\u03bf\u03c5"
+    ],
+    "SHORTDAY": [
+      "\u039a\u03c5\u03c1",
+      "\u0394\u03b5\u03c5",
+      "\u03a4\u03c1\u03b9",
+      "\u03a4\u03b5\u03c4",
+      "\u03a0\u03b5\u03bc",
+      "\u03a0\u03b1\u03c1",
+      "\u03a3\u03b1\u03b2"
+    ],
+    "SHORTMONTH": [
+      "\u0399\u03b1\u03bd",
+      "\u03a6\u03b5\u03b2",
+      "\u039c\u03b1\u03c1",
+      "\u0391\u03c0\u03c1",
+      "\u039c\u03b1\u03ca",
+      "\u0399\u03bf\u03c5\u03bd",
+      "\u0399\u03bf\u03c5\u03bb",
+      "\u0391\u03c5\u03b3",
+      "\u03a3\u03b5\u03c0",
+      "\u039f\u03ba\u03c4",
+      "\u039d\u03bf\u03b5",
+      "\u0394\u03b5\u03ba"
+    ],
     "fullDate": "EEEE, d MMMM y",
     "longDate": "d MMMM y",
     "medium": "d MMM y h:mm:ss a",
@@ -65,8 +65,8 @@ $provide.value("$locale", {
     "CURRENCY_SYM": "\u20ac",
     "DECIMAL_SEP": ",",
     "GROUP_SEP": ".",
-    "PATTERNS": {
-      "0": {
+    "PATTERNS": [
+      {
         "gSize": 3,
         "lgSize": 3,
         "macFrac": 0,
@@ -78,7 +78,7 @@ $provide.value("$locale", {
         "posPre": "",
         "posSuf": ""
       },
-      "1": {
+      {
         "gSize": 3,
         "lgSize": 3,
         "macFrac": 0,
@@ -90,7 +90,7 @@ $provide.value("$locale", {
         "posPre": "",
         "posSuf": "\u00a0\u00a4"
       }
-    }
+    ]
   },
   "id": "el-gr",
   "pluralCat": function (n) {  if (n == 1) {   return PLURAL_CATEGORY.ONE;  }  return PLURAL_CATEGORY.OTHER;}
--- src/ngLocale/angular-locale_vi-vn.js
+++ src/ngLocale/angular-locale_vi-vn.js
@@ -2,56 +2,56 @@ angular.module("ngLocale", [], ["$provide", function($provide) {
 var PLURAL_CATEGORY = {ZERO: "zero", ONE: "one", TWO: "two", FEW: "few", MANY: "many", OTHER: "other"};
 $provide.value("$locale", {
   "DATETIME_FORMATS": {
-    "AMPMS": {
-      "0": "SA",
-      "1": "CH"
-    },
-    "DAY": {
-      "0": "Ch\u1ee7 nh\u1eadt",
-      "1": "Th\u1ee9 hai",
-      "2": "Th\u1ee9 ba",
-      "3": "Th\u1ee9 t\u01b0",
-      "4": "Th\u1ee9 n\u0103m",
-      "5": "Th\u1ee9 s\u00e1u",
-      "6": "Th\u1ee9 b\u1ea3y"
-    },
-    "MONTH": {
-      "0": "th\u00e1ng m\u1ed9t",
-      "1": "th\u00e1ng hai",
-      "2": "th\u00e1ng ba",
-      "3": "th\u00e1ng t\u01b0",
-      "4": "th\u00e1ng n\u0103m",
-      "5": "th\u00e1ng s\u00e1u",
-      "6": "th\u00e1ng b\u1ea3y",
-      "7": "th\u00e1ng t\u00e1m",
-      "8": "th\u00e1ng ch\u00edn",
-      "9": "th\u00e1ng m\u01b0\u1eddi",
-      "10": "th\u00e1ng m\u01b0\u1eddi m\u1ed9t",
-      "11": "th\u00e1ng m\u01b0\u1eddi hai"
-    },
-    "SHORTDAY": {
-      "0": "CN",
-      "1": "Th 2",
-      "2": "Th 3",
-      "3": "Th 4",
-      "4": "Th 5",
-      "5": "Th 6",
-      "6": "Th 7"
-    },
-    "SHORTMONTH": {
-      "0": "thg 1",
-      "1": "thg 2",
-      "2": "thg 3",
-      "3": "thg 4",
-      "4": "thg 5",
-      "5": "thg 6",
-      "6": "thg 7",
-      "7": "thg 8",
-      "8": "thg 9",
-      "9": "thg 10",
-      "10": "thg 11",
-      "11": "thg 12"
-    },
+    "AMPMS": [
+      "SA",
+      "CH"
+    ],
+    "DAY": [
+      "Ch\u1ee7 nh\u1eadt",
+      "Th\u1ee9 hai",
+      "Th\u1ee9 ba",
+      "Th\u1ee9 t\u01b0",
+      "Th\u1ee9 n\u0103m",
+      "Th\u1ee9 s\u00e1u",
+      "Th\u1ee9 b\u1ea3y"
+    ],
+    "MONTH": [
+      "th\u00e1ng m\u1ed9t",
+      "th\u00e1ng hai",
+      "th\u00e1ng ba",
+      "th\u00e1ng t\u01b0",
+      "th\u00e1ng n\u0103m",
+      "th\u00e1ng s\u00e1u",
+      "th\u00e1ng b\u1ea3y",
+      "th\u00e1ng t\u00e1m",
+      "th\u00e1ng ch\u00edn",
+      "th\u00e1ng m\u01b0\u1eddi",
+      "th\u00e1ng m\u01b0\u1eddi m\u1ed9t",
+      "th\u00e1ng m\u01b0\u1eddi hai"
+    ],
+    "SHORTDAY": [
+      "CN",
+      "Th 2",
+      "Th 3",
+      "Th 4",
+      "Th 5",
+      "Th 6",
+      "Th 7"
+    ],
+    "SHORTMONTH": [
+      "thg 1",
+      "thg 2",
+      "thg 3",
+      "thg 4",
+      "thg 5",
+      "thg 6",
+      "thg 7",
+      "thg 8",
+      "thg 9",
+      "thg 10",
+      "thg 11",
+      "thg 12"
+    ],
     "fullDate": "EEEE, 'ng\u00e0y' dd MMMM 'n\u0103m' y",
     "longDate": "'Ng\u00e0y' dd 'th\u00e1ng' M 'n\u0103m' y",
     "medium": "dd-MM-yyyy HH:mm:ss",
@@ -65,8 +65,8 @@ $provide.value("$locale", {
     "CURRENCY_SYM": "\u20ab",
     "DECIMAL_SEP": ",",
     "GROUP_SEP": ".",
-    "PATTERNS": {
-      "0": {
+    "PATTERNS": [
+      {
         "gSize": 3,
         "lgSize": 3,
         "macFrac": 0,
@@ -78,7 +78,7 @@ $provide.value("$locale", {
         "posPre": "",
         "posSuf": ""
       },
-      "1": {
+      {
         "gSize": 3,
         "lgSize": 3,
         "macFrac": 0,
@@ -90,7 +90,7 @@ $provide.value("$locale", {
         "posPre": "",
         "posSuf": "\u00a0\u00a4"
       }
-    }
+    ]
   },
   "id": "vi-vn",
   "pluralCat": function (n) {  return PLURAL_CATEGORY.OTHER;}
