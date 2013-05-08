f2aa4a32d3e8144d26a3f272291394ee08cc5913
Robin Böhm <robinboehm@googlemail.com>
2013-05-08 02:32:19 +0200

    feat(mobile):add ngSensor services for mobile-device sensor events

---
+++ src/ngMobile/ngSensor.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function registerSensorService(eventType) {
+    angular.module('ruhrjsApp')
+        .factory("$"+angular.lowercase(eventType.replace("Device","")),
+            ["$timeout","$window",function ($timeout, $window) {
+                return {
+                    //TODO: (un-)register correct wording?
+                    register  : function (callback) {
+                        if (!angular.isUndefined(window[eventType + 'Event'])) {
+                            $window.addEventListener(angular.lowercase(eventType), callback);
+                        }
+                    },
+                    unregister: function (callback) {
+                        $window.removeEventListener(
+                            angular.lowercase(eventType),
+                            callback);
+                    }
+                };
+            }]);
+}
+
+//https://dvcs.w3.org/hg/dap/raw-file/tip/sensor-api/Overview.html#datatypes
+angular.forEach([
+    "DeviceOrientation","DeviceMotion","DeviceHumidity","DeviceLight",
+    "DeviceNoise","DevicePressure","DeviceProximity","DeviceTemperature"],
+    function(eventName){
+        registerSensorService(eventName);
+    });
+
