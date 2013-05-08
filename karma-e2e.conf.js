29efd39745ac14c89ef8bc79993b51450fd0a8b9
Misko Hevery <misko@hevery.com>
2013-05-08 16:40:00 -0700

    fix(scenario): update to use our angular-scenario.js rather then karma

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -1,6 +1,6 @@
 var angularFiles = require(__dirname + '/angularFiles.js');
 
-files = [ANGULAR_SCENARIO, ANGULAR_SCENARIO_ADAPTER, 'build/docs/docs-scenario.js'];
+files = ['build/angular-scenario.js', ANGULAR_SCENARIO_ADAPTER, 'build/docs/docs-scenario.js'];
 
 autoWatch = false;
 singleRun = true;
