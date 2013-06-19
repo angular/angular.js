46d8594cb7450b2b8d1c4978302f520f85356ccb
Igor Minar <igor@angularjs.org>
2013-06-18 18:00:02 -0700

    WIP: tunnel id

--- sauce-test.js
+++ sauce-test.js
@@ -1,12 +1,15 @@
 var wd = require('wd')
   , assert = require('assert')
-  , browser = wd.remote("ondemand.saucelabs.com", 80, process.env.SAUCE_USER_NAME, process.env.SAUCE_ACCESS_KEY)
+  , browser = wd.remote("ondemand.saucelabs.com", 80,
+                        process.env.SAUCE_USER_NAME,
+                        process.env.SAUCE_ACCESS_KEY)
 
 
 browser.init({
     browserName:'chrome'
     , tags : ["examples"]
-    , name: "This is an example test"
+    , name: "This is an example test from Travis"
+    , "tunnel-identifier": process.env.TRAVIS_JOB_NUMBER
   }, function() {
 
   browser.get("http://localhost:8000/build/docs/api", function() {
