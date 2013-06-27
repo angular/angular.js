6d3c9c800808fceebf5144610c3f891cc8f71bfa
Igor Minar <igor@angularjs.org>
2013-06-27 14:55:47 -0700

    chore(build): temporarily add more logging to debug flakiness on CI

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -4,7 +4,7 @@ files = ['build/angular-scenario.js', ANGULAR_SCENARIO_ADAPTER, 'build/docs/docs
 
 autoWatch = false;
 singleRun = true;
-logLevel = LOG_INFO;
+logLevel = LOG_DEBUG;
 logColors = true;
 browsers = ['Chrome'];
 
