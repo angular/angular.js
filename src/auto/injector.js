3d1d5de5dce6c2fc4b3999ea2f4c1240d5dd7a56
Chris Trahey <christrahey@gmail.com>
2013-07-10 17:01:02 -0700

    Allow service factory functions to return promises

--- src/auto/injector.js
+++ src/auto/injector.js
@@ -557,7 +557,9 @@ function createInjector(modulesToLoad) {
       var args = [],
           $inject = annotate(fn),
           length, i,
-          key;
+          key,
+          promisedDependencies = [],
+          invocationDeferredReturn;
 
       for(i = 0, length = $inject.length; i < length; i++) {
         key = $inject[i];
@@ -569,27 +571,57 @@ function createInjector(modulesToLoad) {
           ? locals[key]
           : getService(key)
         );
+        
+        // make note of which arg indexes are promises at this point.
+        // (which service dependencies are not yet fulfilled)
+        if(args[i] && args[i].then && typeof args[i].then == "function") {
+          promisedDependencies.push(i);
+        }
       }
       if (!fn.$inject) {
         // this means that we must be an array.
         fn = fn[length];
       }
 
+      // this will either be invoked immediately or
+      // once all dependencies have been fulfilled.
+      function performInvocation(fn, self, args) {
+        // Performance optimization: http://jsperf.com/apply-vs-call-vs-invoke
+        switch (self ? -1 : args.length) {
+          case  0: return fn();
+          case  1: return fn(args[0]);
+          case  2: return fn(args[0], args[1]);
+          case  3: return fn(args[0], args[1], args[2]);
+          case  4: return fn(args[0], args[1], args[2], args[3]);
+          case  5: return fn(args[0], args[1], args[2], args[3], args[4]);
+          case  6: return fn(args[0], args[1], args[2], args[3], args[4], args[5]);
+          case  7: return fn(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+          case  8: return fn(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+          case  9: return fn(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+          case 10: return fn(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
+          default: return fn.apply(self, args);
+        }
+      }
 
-      // Performance optimization: http://jsperf.com/apply-vs-call-vs-invoke
-      switch (self ? -1 : args.length) {
-        case  0: return fn();
-        case  1: return fn(args[0]);
-        case  2: return fn(args[0], args[1]);
-        case  3: return fn(args[0], args[1], args[2]);
-        case  4: return fn(args[0], args[1], args[2], args[3]);
-        case  5: return fn(args[0], args[1], args[2], args[3], args[4]);
-        case  6: return fn(args[0], args[1], args[2], args[3], args[4], args[5]);
-        case  7: return fn(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
-        case  8: return fn(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
-        case  9: return fn(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
-        case 10: return fn(args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
-        default: return fn.apply(self, args);
+      // If there are some unfulfilled dependencies, do "reference counting" so that 
+      // we can invoke the original function once they are all resolved.
+      var numPromises = promisedDependencies.length;
+      if(numPromises) {
+        invocationDeferredReturn = cache['$q'].defer();
+        for(i = 0; i< numPromises; i++){
+          var thisIndex = promisedDependencies[i];
+          var thisPromise = args[thisIndex];
+          thisPromise.then(function(serviceValue){
+            args[thisIndex] = serviceValue;
+            numPromises--;
+            if(numPromises == 0) {
+              invocationDeferredReturn.resolve(performInvocation(fn, self, args));
+            }
+          });
+        }
+        return invocationDeferredReturn.promise;
+      } else {
+        return performInvocation(fn, self, args);
       }
     }
 
