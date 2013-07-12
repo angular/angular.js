fcd761b9d7c3c91673efce9b980ac5e7973adf3d
Pete Bacon Darwin <pete@bacondarwin.com>
2013-07-12 09:29:06 +0100

    fix(sanitize): match URI schemes case-insensitively
    According to RFC 3986 (http://tools.ietf.org/html/rfc3986#section-3.1)
    schemes such as http or mailto are case-insensitive. So links such as
    http://server/ and HTTP://server/ are valid and equivalent.
    
    Closes #3210

--- test/ngSanitize/sanitizeSpec.js
+++ test/ngSanitize/sanitizeSpec.js
@@ -227,9 +227,13 @@ describe('HTML', function() {
 
       it('should be URI', function() {
         expect(isUri('http://abc')).toBeTruthy();
+        expect(isUri('HTTP://abc')).toBeTruthy();
         expect(isUri('https://abc')).toBeTruthy();
+        expect(isUri('HTTPS://abc')).toBeTruthy();
         expect(isUri('ftp://abc')).toBeTruthy();
+        expect(isUri('FTP://abc')).toBeTruthy();
         expect(isUri('mailto:me@example.com')).toBeTruthy();
+        expect(isUri('MAILTO:me@example.com')).toBeTruthy();
         expect(isUri('#anchor')).toBeTruthy();
       });
 
