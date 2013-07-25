2b4d272972596127a3b56611b8964be0deeecf68
Matias Niemelä <matias@yearofmoo.com>
2013-07-24 23:22:09 -0400

    feat(ngMock): $timeout.flushNext can expect specific timeout delays
    the $timeout mock's flush method allows flushing queued up requests
    but doesn't allow to for checking with what delay a task was queued
    up. flushNext flushes the next queued up task and can asserts the
    scheduled delay.

--- src/ngMock/angular-mocks.js
+++ src/ngMock/angular-mocks.js
@@ -118,6 +118,22 @@ angular.mock.$Browser = function() {
       self.deferredFns.shift().fn();
     }
   };
+
+  /**
+   * @name ngMock.$browser#defer.flushNext
+   * @methodOf ngMock.$browser
+   *
+   * @description
+   * Flushes next pending request and compares it to the provided delay
+   *
+   * @param {number=} expectedDelay the delay value that will be asserted against the delay of the next timeout function
+   */
+  self.defer.flushNext = function(expectedDelay) {
+    var tick = self.deferredFns.shift();
+    expect(tick.time).toEqual(expectedDelay);
+    tick.fn();
+  };
+
   /**
    * @name ngMock.$browser#defer.now
    * @propertyOf ngMock.$browser
@@ -1492,6 +1508,20 @@ angular.mock.$TimeoutDecorator = function($delegate, $browser) {
    */
   $delegate.flush = function(delay) {
     $browser.defer.flush(delay);
+  };
+
+  /**
+   * @ngdoc method
+   * @name ngMock.$timeout#flushNext
+   * @methodOf ngMock.$timeout
+   * @description
+   *
+   * Flushes the next timeout in the queue and compares it to the provided delay
+   *
+   * @param {number=} expectedDelay the delay value that will be asserted against the delay of the next timeout function
+   */
+  $delegate.flushNext = function(expectedDelay) {
+    $browser.defer.flushNext(expectedDelay);
   };
 
   /**
