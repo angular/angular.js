6365b5a2e5464446fbfd95ba85d45144430e036f
Ken Sheedlo <ovrkenthousand@gmail.com>
2013-07-30 15:25:54 -0700

    chore(bower): write grunt task for running bower

---
+++ travis_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -xe
+
+warn() {
+  tput setaf 1
+  echo "[ERROR] Received $1"
+  tput sgr0
+  exit 1
+}
+
+trap "warn SIGINT" SIGINT
+trap "warn SIGTERM" SIGTERM
+trap "warn SIGHUP" SIGHUP
+
+export SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
+./lib/sauce/sauce_connect_setup.sh
+npm install -g grunt-cli
+grunt ci-checks package
+./lib/sauce/sauce_connect_block.sh
+grunt parallel:travis --reporters dots --browsers SL_Chrome
