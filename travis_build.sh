93876f38804cdffc8a5415638668e97e4aacfd58
Igor Minar <igor@angularjs.org>
2013-07-31 15:31:53 -0700

    chore(travis): print grunt exit code for debugging

--- travis_build.sh
+++ travis_build.sh
@@ -17,5 +17,10 @@ export SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
 ./lib/sauce/sauce_connect_setup.sh
 npm install -g grunt-cli
 grunt ci-checks package
+
+echo ">>> grunt exited with code: $?"
+echo ""
+echo ""
+
 ./lib/sauce/sauce_connect_block.sh
 grunt parallel:travis --reporters dots --browsers SL_Chrome
