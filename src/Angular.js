c50c6ef72d6ff7bfec279c9782d5bbba6f1a8a67
Andy Hitchman <andy@hitchman.net>
2013-08-06 11:25:44 +1000

    fix(angular.copy): change angular.copy to correcly clone RegExp
    angular.copy previously copied RegExp as an empty object. Change detects
    RegExp instance and clones into new RegExp. This change is based on a previous
    fix to allow Date to be copied.
    
    Closes #3473
    Closes #3474

--- src/Angular.js
+++ src/Angular.js
@@ -611,6 +611,8 @@ function copy(source, destination){
         destination = copy(source, []);
       } else if (isDate(source)) {
         destination = new Date(source.getTime());
+      } else if (isRegExp(source)) {
+        destination = new RegExp(source.source);
       } else if (isObject(source)) {
         destination = copy(source, {});
       }
