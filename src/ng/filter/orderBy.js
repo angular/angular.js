48f2e193b638390fee0cd0237df5f8e1ab716272
Eddie Monge <eddie@eddiemonge.com>
2013-08-14 19:51:27 -0700

    fix(orderBy): fix mixed case arrays with refactoring
    Refactored, removing redundant code while fixing mixed array sorting

--- src/ng/filter/orderBy.js
+++ src/ng/filter/orderBy.js
@@ -89,51 +89,64 @@
 orderByFilter.$inject = ['$parse'];
 function orderByFilter($parse){
   return function(array, sortPredicate, reverseOrder) {
-    if (!isArray(array)) return array;
-    if (!sortPredicate) return array;
+    if (!isArray(array) || !sortPredicate) {
+      return array;
+    }
+
     sortPredicate = isArray(sortPredicate) ? sortPredicate: [sortPredicate];
     sortPredicate = map(sortPredicate, function(predicate){
-      var descending = false, get = predicate || identity;
+      var descending = false,
+        get = predicate || identity;
+
       if (isString(predicate)) {
-        if ((predicate.charAt(0) == '+' || predicate.charAt(0) == '-')) {
+        if ((predicate.charAt(0) === '+' || predicate.charAt(0) === '-')) {
           descending = predicate.charAt(0) == '-';
           predicate = predicate.substring(1);
         }
         get = $parse(predicate);
       }
+
       return reverseComparator(function(a,b){
         return compare(get(a),get(b));
       }, descending);
     });
-    var arrayCopy = [];
-    for ( var i = 0; i < array.length; i++) { arrayCopy.push(array[i]); }
-    return arrayCopy.sort(reverseComparator(comparator, reverseOrder));
+
+    return array.sort(reverseComparator(comparator, reverseOrder));
 
     function comparator(o1, o2){
-      for ( var i = 0; i < sortPredicate.length; i++) {
-        var comp = sortPredicate[i](o1, o2);
-        if (comp !== 0) return comp;
+      var len = sortPredicate.length,
+          i, comp;
+      for (i = 0; i < len; i++) {
+        comp = sortPredicate[i](o1, o2);
+
+        if (comp !== 0) {
+          return comp;
+        }
       }
       return 0;
     }
+
     function reverseComparator(comp, descending) {
-      return toBoolean(descending)
-          ? function(a,b){return comp(b,a);}
-          : comp;
+      return toBoolean(descending) ?
+        function(a,b) { return comp(b,a); } :
+        comp;
     }
+
     function compare(v1, v2){
       var t1 = typeof v1;
-      var t2 = typeof v2;
-      if (t1 == t2) {
-        if (t1 == "string") {
+
+      if (t1 === typeof v2) {
+        if (t1 === "string") {
            v1 = v1.toLowerCase();
            v2 = v2.toLowerCase();
         }
-        if (v1 === v2) return 0;
-        return v1 < v2 ? -1 : 1;
-      } else {
-        return t1 < t2 ? -1 : 1;
+
+        if (v1 === v2) {
+          return 0;
+        }
       }
+
+      return v1 < v2 ? -1 : 1;
     }
   }
 }
--- test/ng/filter/orderBySpec.js
+++ test/ng/filter/orderBySpec.js
@@ -11,24 +11,104 @@ describe('Filter: orderBy', function() {
     expect(orderBy(array)).toBe(array);
   });
 
-  it('shouldSortArrayInReverse', function() {
-    expect(orderBy([{a:15}, {a:2}], 'a', true)).toEqualData([{a:15}, {a:2}]);
-    expect(orderBy([{a:15}, {a:2}], 'a', "T")).toEqualData([{a:15}, {a:2}]);
-    expect(orderBy([{a:15}, {a:2}], 'a', "reverse")).toEqualData([{a:15}, {a:2}]);
+  it('should return same object if object is not an array', function() {
+    var array = { name: 'angular', company: 'google' };
+    expect(orderBy(array)).toBe(array);
+  });
+
+  it('should sort normal array', function() {
+    var array = [{a:15}, {a:2}, {a:3}],
+        sorted = [{a:2}, {a:3}, {a:15}];
+    expect(orderBy(array, 'a', false)).toEqualData(sorted);
+    expect(orderBy(array, 'a', "F")).toEqualData(sorted);
+    expect(orderBy(array, 'a', "0")).toEqualData(sorted);
+    expect(orderBy(array, 'a', 0)).toEqualData(sorted);
+  });
+
+  it('should sort mixed array', function() {
+    var array = [{a:15}, {a:2}, {a:'3'}],
+        sorted = [{a:2}, {a:'3'}, {a:15}];
+    expect(orderBy(array, 'a', false)).toEqualData(sorted);
+    expect(orderBy(array, 'a', "F")).toEqualData(sorted);
+    expect(orderBy(array, 'a', "0")).toEqualData(sorted);
+    expect(orderBy(array, 'a', 0)).toEqualData(sorted);
+  });
+
+  it('should sort normal array in reverse', function() {
+    var array = [{a:15}, {a:2}, {a:3}],
+        sorted = [{a:15}, {a:3}, {a:2}];
+    expect(orderBy(array, 'a', true)).toEqualData(sorted);
+    expect(orderBy(array, 'a', "T")).toEqualData(sorted);
+    expect(orderBy(array, 'a', 1)).toEqualData(sorted);
+    expect(orderBy(array, 'a', "reverse")).toEqualData(sorted);
+  });
+
+  it('should sort mixed array in reverse', function() {
+    var array = [{a:15}, {a:2}, {a:'3'}],
+        sorted = [{a:15}, {a:'3'}, {a:2}];
+    expect(orderBy(array, 'a', true)).toEqualData(sorted);
+    expect(orderBy(array, 'a', "T")).toEqualData(sorted);
+    expect(orderBy(array, 'a', "1")).toEqualData(sorted);
+    expect(orderBy(array, 'a', 1)).toEqualData(sorted);
   });
 
   it('should sort array by predicate', function() {
-    expect(orderBy([{a:15, b:1}, {a:2, b:1}], ['a', 'b'])).toEqualData([{a:2, b:1}, {a:15, b:1}]);
-    expect(orderBy([{a:15, b:1}, {a:2, b:1}], ['b', 'a'])).toEqualData([{a:2, b:1}, {a:15, b:1}]);
-    expect(orderBy([{a:15, b:1}, {a:2, b:1}], ['+b', '-a'])).toEqualData([{a:15, b:1}, {a:2, b:1}]);
+    var array = [{a:7,b:5}, {a:2,b:7}, {a:2,b:1}, {a:2,b:4}, {a:3,b:1}];
+
+    expect(orderBy(array, ['a', 'b'])).toEqualData(
+      [{a:2,b:1}, {a:2,b:4}, {a:2,b:7}, {a:3,b:1}, {a:7,b:5}]
+    );
+
+    expect(orderBy(array, ['+a', 'b'])).toEqualData(
+      [{a:2,b:1}, {a:2,b:4}, {a:2,b:7}, {a:3,b:1}, {a:7,b:5}]
+    );
+    expect(orderBy(array, ['+a', '+b'])).toEqualData(
+      [{a:2,b:1}, {a:2,b:4}, {a:2,b:7}, {a:3,b:1}, {a:7,b:5}]
+    );
+    expect(orderBy(array, ['+a', '-b'])).toEqualData(
+      [{a:2,b:7}, {a:2,b:4}, {a:2,b:1}, {a:3,b:1}, {a:7,b:5}]
+    );
+
+    expect(orderBy(array, ['-a', 'b'])).toEqualData(
+      [{a:7,b:5}, {a:3,b:1}, {a:2,b:1}, {a:2,b:4}, {a:2,b:7}]
+    );
+    expect(orderBy(array, ['-a', '+b'])).toEqualData(
+      [{a:7,b:5}, {a:3,b:1}, {a:2,b:1}, {a:2,b:4}, {a:2,b:7}]
+    );
+    expect(orderBy(array, ['-a', '-b'])).toEqualData(
+      [{a:7,b:5}, {a:3,b:1}, {a:2,b:7}, {a:2,b:4}, {a:2,b:1}]
+    );
+
+    expect(orderBy(array, ['b', 'a'])).toEqualData(
+      [{a:2,b:1}, {a:3,b:1}, {a:2,b:4}, {a:7,b:5}, {a:2,b:7}]
+    );
+
+    expect(orderBy(array, ['+b', 'a'])).toEqualData(
+      [{a:2,b:1}, {a:3,b:1}, {a:2,b:4}, {a:7,b:5}, {a:2,b:7}]
+    );
+    expect(orderBy(array, ['+b', '+a'])).toEqualData(
+      [{a:2,b:1}, {a:3,b:1}, {a:2,b:4}, {a:7,b:5}, {a:2,b:7}]
+    );
+    expect(orderBy(array, ['+b', '-a'])).toEqualData(
+      [{a:3,b:1}, {a:2,b:1}, {a:2,b:4}, {a:7,b:5}, {a:2,b:7}]
+    );
+
+    expect(orderBy(array, ['-b', 'a'])).toEqualData(
+      [{a:2,b:7}, {a:7,b:5}, {a:2,b:4}, {a:2,b:1}, {a:3,b:1}]
+    );
+    expect(orderBy(array, ['-b', '+a'])).toEqualData(
+      [{a:2,b:7}, {a:7,b:5}, {a:2,b:4}, {a:2,b:1}, {a:3,b:1}]
+    );
+    expect(orderBy(array, ['-b', '-a'])).toEqualData(
+      [{a:2,b:7}, {a:7,b:5}, {a:2,b:4}, {a:3,b:1}, {a:2,b:1}]
+    );
   });
 
   it('should use function', function() {
-    expect(
-      orderBy(
-        [{a:15, b:1},{a:2, b:1}],
-        function(value) { return value.a; })).
-    toEqual([{a:2, b:1},{a:15, b:1}]);
+    expect(orderBy(
+      [{a:15, b:1},{a:2, b:1}],
+       function(value) { return value.a; }
+    )).toEqual([{a:2, b:1},{a:15, b:1}]);
   });
 
 });
