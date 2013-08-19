fa8f04ae7cc80c8af0217c993f3490ced7bfb6f7
Adam Shannon <adam@ashannon.us>
2013-08-19 14:33:18 -0500

    Fixing $resource call in docs.

--- src/ngResource/resource.js
+++ src/ngResource/resource.js
@@ -36,7 +36,7 @@ var $resourceMinErr = angular.$$minErr('$resource');
  *   `http://example.com:8080/api`), it will be respected.
  *
  *   If you are using a url with a suffix, just add the suffix, like this:
- *   `$resource('http://example.com/resource.json')` or `$resource('http://example.com/:id.json')
+ *   `$resource('http://example.com/resource.json')` or `$resource('http://example.com/:id.json')`
  *   or even `$resource('http://example.com/resource/:resource_id.:format')`
  *   If the parameter before the suffix is empty, :resource_id in this case, then the `/.` will be
  *   collapsed down to a single `.`.  If you need this sequence to appear and not collapse then you
