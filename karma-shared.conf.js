327a38ed2024e279fca3357967954c29874b3dfa
Vojta Jina <vojta.jina@gmail.com>
2013-08-20 14:43:15 -0700

    chore: run more browsers on Travis (IE8,IE9,IE10,Safari,FF)
    Also instead of running everything in parallel, there are only two parallel tasks:
    - e2e tests running in the background (only on Chrome)
    - all the unit tests running sequentially

--- karma-shared.conf.js
+++ karma-shared.conf.js
@@ -18,6 +18,34 @@ module.exports = function(config) {
       'SL_Chrome': {
         base: 'SauceLabs',
         browserName: 'chrome'
+      },
+      'SL_Firefox': {
+        base: 'SauceLabs',
+        browserName: 'firefox'
+      },
+      'SL_Safari': {
+        base: 'SauceLabs',
+        browserName: 'safari',
+        platform: 'Mac 10.8',
+        version: '6'
+      },
+      'SL_IE_8': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 7',
+        version: '8'
+      },
+      'SL_IE_9': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 2008',
+        version: '9'
+      },
+      'SL_IE_10': {
+        base: 'SauceLabs',
+        browserName: 'internet explorer',
+        platform: 'Windows 2012',
+        version: '10'
       }
     }
   });
--- travis_build.sh
+++ travis_build.sh
@@ -7,4 +7,7 @@ export SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
 npm install -g grunt-cli
 grunt ci-checks package
 ./lib/sauce/sauce_connect_block.sh
-grunt parallel:travis --reporters dots --browsers SL_Chrome
+
+grunt parallel:travis --reporters dots \
+  --browsers SL_Chrome,SL_Firefox,SL_Safari,SL_IE_8,SL_IE_9,SL_IE_10 \
+  --e2e-browsers SL_Chrome
