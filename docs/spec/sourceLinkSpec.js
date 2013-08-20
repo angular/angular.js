95ae39bc22dd0e6ab31089f14cbedf2aa1132944
James Daily <jamesdaily@gmail.com>
2013-08-20 13:49:41 -0400

    Included test to check that gruntUtil.getVersion().cdnVersion is defined
    In reaction to Issue 3616, I've added a check that the version code embedded in the View Source link is in fact a defined value. Previously, it referenced gruntUtil.getVersion().number which was an undefined value.

--- docs/spec/sourceLinkSpec.js
+++ docs/spec/sourceLinkSpec.js
@@ -18,6 +18,10 @@ describe('Docs Links', function() {
       expect(doc.html()).
         toContain('<a href="http://github.com/angular/angular.js/edit/master/test.js" class="improve-docs btn btn-primary"><i class="icon-edit"> </i> Improve this doc</a>');
     });
+    
+    it("should have a defined gruntUtil cdnVersion", function () {  
+      expect(gruntUtil.getVersion().cdnVersion).toBeDefined();
+    }); 
 
     it('should have an "view source" button', function() {
       expect(doc.html()).
