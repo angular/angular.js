d90fab4ac7bf96621f3b1d3837fee3e0a52e2f1c
pussinboots <pussinboots666@googlemail.com>
2013-09-04 11:27:13 +0200

    encoding was always still true fixed it with a simple null check (it is nasty but should work)

--- src/ngResource/resource.js
+++ src/ngResource/resource.js
@@ -470,7 +470,7 @@ angular.module('ngResource', ['ng']).
           });
 
           httpConfig.data = data;
-          var encoding = action.encoding || true;
+          var encoding = (action.encoding == null)? true : action.encoding;
           route.setUrlParams(httpConfig, extend({}, extractParams(data, action.params || {}), params), action.url, encoding);
 
           var promise = $http(httpConfig).then(function(response) {
