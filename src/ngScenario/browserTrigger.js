02ed1fc7a5666c7c117498e3940e16b28d43520f
Riceball <snowyu.lee@gmail.com>
2013-09-04 14:28:58 +0800

    fix(ngScenario/browserTrigger): create event objects with pressed keys to get it working on IE<9

--- src/ngScenario/browserTrigger.js
+++ src/ngScenario/browserTrigger.js
@@ -74,7 +74,14 @@
       element.style.posLeft;
 
       // TODO(vojta): create event objects with pressed keys to get it working on IE<9
-      var ret = element.fireEvent('on' + eventType);
+      var evnt = document.createEventObject();
+      evnt.clientX = x;
+      evnt.clientY = y;
+      evnt.ctlKey = pressed('ctrl');
+      evnt.altKey = pressed('alt');
+      evnt.shiftKey = pressed('shift');
+      evnt.returnValue = false;
+      var ret = element.fireEvent('on' + eventType, evnt);
       if (inputType == 'submit') {
         while(element) {
           if (element.nodeName.toLowerCase() == 'form') {
@@ -86,43 +93,29 @@
       }
       return ret;
     } else {
-      var evnt,
-          originalPreventDefault,
+      var evnt = document.createEvent('MouseEvents'),
+          originalPreventDefault = evnt.preventDefault,
           appWindow = element.ownerDocument.defaultView,
           fakeProcessDefault = true,
           finalProcessDefault,
           angular = appWindow.angular || {};
 
+      // igor: temporary fix for https://bugzilla.mozilla.org/show_bug.cgi?id=684208
+      angular['ff-684208-preventDefault'] = false;
+      evnt.preventDefault = function() {
+        fakeProcessDefault = false;
+        return originalPreventDefault.apply(evnt, arguments);
+      };
+
       x = x || 0;
       y = y || 0;
+      evnt.initMouseEvent(eventType, true, true, window, 0, x, y, x, y, pressed('ctrl'), pressed('alt'),
+          pressed('shift'), pressed('meta'), 0, element);
 
-      if (document.createEvent) {
-        evnt = document.createEvent('MouseEvents');
-        originalPreventDefault = evnt.preventDefault;
-        // igor: temporary fix for https://bugzilla.mozilla.org/show_bug.cgi?id=684208
-        angular['ff-684208-preventDefault'] = false;
-        evnt.preventDefault = function() {
-          fakeProcessDefault = false;
-          return originalPreventDefault.apply(evnt, arguments);
-        };
-        evnt.initMouseEvent(eventType, true, true, window, 0, x, y, x, y, pressed('ctrl'), pressed('alt'),
-            pressed('shift'), pressed('meta'), 0, element);
+      element.dispatchEvent(evnt);
+      finalProcessDefault = !(angular['ff-684208-preventDefault'] || !fakeProcessDefault);
 
-        element.dispatchEvent(evnt);
-        finalProcessDefault = !(angular['ff-684208-preventDefault'] || !fakeProcessDefault);
-
-        delete angular['ff-684208-preventDefault'];
-      } else if (document.createEventObject) {
-        evnt = document.createEventObject();
-        evnt.clientX = x;
-        evnt.clientY = y;
-        evnt.ctlKey = pressed('ctrl');
-        evnt.altKey = pressed('alt');
-        evnt.shiftKey = pressed('shift');
-        evnt.returnValue = false;
-        element.fireEvent('on'+eventType, evnt);
-        finalProcessDefault = true;
-      }
+      delete angular['ff-684208-preventDefault'];
 
       return finalProcessDefault;
     }
