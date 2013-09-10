ae2fd55575af2a195f59c3c63224e7be9c0b3c40
naorye <naorye@gmail.com>
2013-09-10 09:57:45 +0200

    docs($q): clarify what happens when rejected
    Closes #3943

--- src/ng/q.js
+++ src/ng/q.js
@@ -377,8 +377,8 @@ function qFactory(nextTick, exceptionHandler) {
    * @param {Array.<Promise>} promises An array of promises.
    * @returns {Promise} Returns a single promise that will be resolved with an array of values,
    *   each value corresponding to the promise at the same index in the `promises` array. If any of
-   *   the promises is resolved with a rejection, this resulting promise will be resolved with the
-   *   same rejection.
+   *   the promises is resolved with a rejection, this resulting promise will be rejected with the
+   *   same rejection value.
    */
   function all(promises) {
     var deferred = defer(),
