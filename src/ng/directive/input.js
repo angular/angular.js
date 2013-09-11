230e124ddbc1837d52a9e444b0492cb6589dfdf9
Paxton Hare <paxton@greenllama.com>
2013-09-11 13:51:20 -0400

    docs(ngModel): provide link for best practices.
    Closes #3973

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -1093,6 +1093,10 @@ var NgModelController = ['$scope', '$exceptionHandler', '$attrs', '$element', '$
  * current scope. If the property doesn't already exist on this scope, it will be created
  * implicitly and added to the scope.
  *
+ * For best practices on using `ngModel`, see:
+ *
+ *  - {@link https://github.com/angular/angular.js/wiki/Understanding-Scopes}
+ *
  * For basic examples, how to use `ngModel`, see:
  *
  *  - {@link ng.directive:input input}
