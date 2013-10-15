4e2a84410abfe763f515eae6b93a338fbc4c4df5
Igor Minar <igor@angularjs.org>
2013-10-15 11:28:47 -0700

    docs($rootScope): better document infinite digest and ttl

--- src/ng/rootScope.js
+++ src/ng/rootScope.js
@@ -40,10 +40,14 @@
  * @methodOf ng.$rootScopeProvider
  * @description
  *
- * Sets the number of digest iterations the scope should attempt to execute before giving up and
- * assuming that the model is unstable.
+ * Sets the number of `$digest` iterations the scope should attempt to execute before giving up and assuming that the model is unstable.
  *
  * The current default is 10 iterations.
+ *
+ * In complex applications it's possible that the dependencies between `$watch`s will result in several digest iterations.
+ * However if an application needs more than the default 10 digest iterations for its model to stabilize then you should investigate what is causing the model to continuously change during the digest.
+ *
+ * Increasing the TTL could have performance implications, so you should not change it without proper justification.
  *
  * @param {number} limit The number of digest iterations.
  */
