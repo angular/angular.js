7813d1f886f9ecb0ed3a4d503be5c5970c36040b
Robin Böhm <robinboehm@googlemail.com>
2013-10-18 22:13:50 +0200

    feat(ngSensor): add angular Module init for ngSensor

---
+++ src/ngSensor/sensor.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngSensor
+ * @description
+ *
+ * # ngSensor
+ *
+ * `ngSensor` is the name of the optional Angular module that provides sensor events and other
+ * helpers for sensor-enabled devices.
+ * The implementation is based on W3C sensor-api spec
+ * ([https://w3.org](https://dvcs.w3.org/hg/dap/raw-file/tip/sensor-api/))
+ *
+ * {@installModule sensor}
+ *
+ * See {@link ngSensor.$motion `$motion`} for usage.
+ */
+
+// define ngSensor module
+var ngSensor = angular.module('ngSensor', []);
+
