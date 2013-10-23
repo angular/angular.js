fc0c2b59a8235a44d53f24d232b384f6e771d036
Matias Niemelä <matias@yearofmoo.com>
2013-10-23 17:44:37 -0400

    chore(docs): fix jsHint errors with long lines

--- src/ngTouch/touch.js
+++ src/ngTouch/touch.js
@@ -8,7 +8,8 @@
  * # ngTouch
  *
  * The `ngTouch` module provides touch events and other helpers for touch-enabled devices.
- * The implementation is based on jQuery Mobile touch event handling ([jquerymobile.com](http://jquerymobile.com/)).
+ * The implementation is based on jQuery Mobile touch event handling 
+ * ([jquerymobile.com](http://jquerymobile.com/)).
  *
  * {@installModule touch}
  *
