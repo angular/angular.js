048ff7eee9acd69cb695528624791b35dcaeecd7
Pete Bacon Darwin <pete@bacondarwin.com>
2013-10-24 22:36:05 +0100

    style($route): fix line length

--- src/ngRoute/route.js
+++ src/ngRoute/route.js
@@ -89,12 +89,14 @@ function $RouteProvider(){
    *        `$location.path()` by applying the current route
    *
    *    - `resolve` - `{Object.<string, function>=}` - An optional map of dependencies which should
-   *      be injected into the controller. If any of these dependencies are promises, the router will
-   *      wait for them all to be resolved or one to be rejected before the controller is instantiated.
-   *      If all the promises are resolved successfully, the values of the resolved promises are injected 
-   *      and {@link ngRoute.$route#$routeChangeSuccess $routeChangeSuccess} event is fired.
-   *      If any of the promises are rejected the {@link ngRoute.$route#$routeChangeError $routeChangeError}
-   *      event is fired. The map object is:
+   *      be injected into the controller. If any of these dependencies are promises, the router
+   *      will wait for them all to be resolved or one to be rejected before the controller is
+   *      instantiated.
+   *      If all the promises are resolved successfully, the values of the resolved promises are
+   *      injected and {@link ngRoute.$route#$routeChangeSuccess $routeChangeSuccess} event is
+   *      fired. If any of the promises are rejected the
+   *      {@link ngRoute.$route#$routeChangeError $routeChangeError} event is fired. The map object
+   *      is:
    *
    *      - `key` – `{string}`: a name of a dependency to be injected into the controller.
    *      - `factory` - `{string|function}`: If `string` then it is an alias for a service.
