f9af6bd4478e3ca1cd685be6d8d7466f28aad6b4
rjferguson21 <rjferguson21@gmail.com>
2013-10-24 11:53:26 -0500

    docs($q): promises are no longer automatically unwrapped, by default
    Although it is still possible to turn this on, the feature is deprecated and it is
    best to remove this mention completely here.
    
    Closes #4632

--- src/ng/q.js
+++ src/ng/q.js
@@ -141,8 +141,6 @@
  * - $q is integrated with the {@link ng.$rootScope.Scope} Scope model observation
  *   mechanism in angular, which means faster propagation of resolution or rejection into your
  *   models and avoiding unnecessary browser repaints, which would result in flickering UI.
- * - $q promises are recognized by the templating engine in angular, which means that in templates
- *   you can treat promises attached to a scope as if they were the resulting values.
  * - Q has many more features than $q, but that comes at a cost of bytes. $q is tiny, but contains
  *   all the important functionality needed for common async tasks.
  *
