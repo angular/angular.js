41fb2bf0e62b71614bf6f8080c15f2bccdcfbb6a
Francesco Moggia FFG <fmoggia@gmail.com>
2013-10-25 11:49:01 -0500

    fix(jqLite): find check if the element is a text node
    find check if the element is a text node.
    If element is a text node return an empty array.
    Otherwise invoke getElementsByTagName on the element and return the result
    
    Closes #4120

--- src/jqLite.js
+++ src/jqLite.js
@@ -809,7 +809,11 @@ forEach({
   },
 
   find: function(element, selector) {
-    return element.getElementsByTagName(selector);
+    if (element.nodeType==3) {
+      return [];
+    } else {
+      return element.getElementsByTagName(selector);
+    }
   },
 
   clone: jqLiteClone,
