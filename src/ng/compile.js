cc996affe2c95896f5e7fe56d7dafa303d6923d7
Martin Field <mfield@yelp.com>
2013-11-08 14:08:03 -0800

    docs($compile): Explain that post-link functions run in reverse order.
    Update the $compile docs to mention the change introduced in #4266.
    
    Closes #4843

--- src/ng/compile.js
+++ src/ng/compile.js
@@ -115,8 +115,9 @@
  * When there are multiple directives defined on a single DOM element, sometimes it
  * is necessary to specify the order in which the directives are applied. The `priority` is used
  * to sort the directives before their `compile` functions get called. Priority is defined as a
- * number. Directives with greater numerical `priority` are compiled first. The order of directives with
- * the same priority is undefined. The default priority is `0`.
+ * number. Directives with greater numerical `priority` are compiled first. Pre-link functions
+ * are also run in priority order, but post-link functions are run in reverse order. The order
+ * of directives with the same priority is undefined. The default priority is `0`.
  *
  * #### `terminal`
  * If set to true then the current `priority` will be the last set of directives
