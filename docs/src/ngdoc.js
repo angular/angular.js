b07fe465801f010cdc8fa3b49d3b33136950b916
Pete Bacon Darwin <pete@bacondarwin.com>
2013-11-13 20:42:36 +0000

    docs(ngdoc): clarify extra module installation options
    Closes #4811

--- docs/src/ngdoc.js
+++ docs/src/ngdoc.js
@@ -1374,10 +1374,16 @@ function explainModuleInstallation(moduleName){
     '    &lt;script src=&quot;angular.js&quot;&gt;\n' +
     '    &lt;script src=&quot;' + modulePackageFile + '&quot;&gt;</pre></code>' +
 
-    '<p>You can also find this file on the [Google CDN](https://developers.google.com/speed/libraries/devguide#angularjs), ' +
-    '<a href="http://bower.io/">Bower</a> (as <code>' + modulePackage + '</code>), ' +
-    'and on <a href="http://code.angularjs.org/">code.angularjs.org</a>.</p>' +
-
+    '<p>You can download this file from the following places:</p>' +
+    '<ul>' +
+      '<li>[Google CDN](https://developers.google.com/speed/libraries/devguide#angularjs)<br>' +
+        'e.g. <code>"//ajax.googleapis.com/ajax/libs/angularjs/X.Y.Z/' + modulePackageFile + '"</code></li>' +
+      '<li>[Bower](http://bower.io)<br>' +
+       'e.g. <code>bower install ' + modulePackage + '@X.Y.Z</code></li>' +
+      '<li><a href="http://code.angularjs.org/">code.angularjs.org</a><br>' +
+        'e.g. <code>"//code.angularjs.org/X.Y.Z/' + modulePackageFile + '"</code></li>' +
+    '</ul>' +
+    '<p>where X.Y.Z is the AngularJS version you are running.</p>' +
     '<p>Then load the module in your application by adding it as a dependent module:</p><pre><code>' +
     '    angular.module(\'app\', [\'' + ngMod + '\']);</pre></code>' +
 
