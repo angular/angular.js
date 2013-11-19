0ea270e148e51a9d83335df9af6219f330b4570b
Brett Porter <brett@apache.org>
2013-11-19 16:38:35 +1100

    fix(angular.encodeUriSegment): do not encode semi-colon
    RFC 3986 indicates that ; is not encoded as part of the URI, as is the case
    with other members of sub-delim. Changed encodeUriSegment to match that
    behaviour, along with the corresponding spec.

--- src/Angular.js
+++ src/Angular.js
@@ -1065,6 +1065,7 @@ function toKeyValue(obj) {
  */
 function encodeUriSegment(val) {
   return encodeUriQuery(val, true).
+             replace(/%3B/gi, ';').
              replace(/%26/gi, '&').
              replace(/%3D/gi, '=').
              replace(/%2B/gi, '+');
