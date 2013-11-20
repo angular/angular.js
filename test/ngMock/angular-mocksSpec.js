6ba5ee9e338460deecb43ca9b8807d2538c43805
Andrew C. Greenberg <wizardwerdna@gmail.com>
2013-11-19 17:42:09 -0800

    ok, so its e.description versus e.message

--- test/ngMock/angular-mocksSpec.js
+++ test/ngMock/angular-mocksSpec.js
@@ -870,7 +870,7 @@ describe('ngMock', function() {
         try {
           inject(function(){ throw new Error('test message'); });
         } catch(e) {
-          expect(e.toString()).toMatch(/test message/);
+          expect(e.description || e.message).toMatch(/test message/);
         }
       });
     });
