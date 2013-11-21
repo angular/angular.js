fc2a23a4cf887e96da8fe9d6eee595a74e3b240c
Wladimir Coka <wlacoka@gmail.com>
2013-11-21 16:38:29 -0500

    refactor(ngScenario): improve readability on conditional assignment
    Use logical OR operator instead of if statement

--- src/ngScenario/Scenario.js
+++ src/ngScenario/Scenario.js
@@ -313,11 +313,7 @@ _jQuery.fn.bindings = function(windowJquery, bindExp) {
         }
         for(var fns, j=0, jj=binding.length;  j<jj; j++) {
           fns = binding[j];
-          if (fns.parts) {
-            fns = fns.parts;
-          } else {
-            fns = [fns];
-          }
+          fns = fns.parts || [fns];
           for (var scope, fn, i = 0, ii = fns.length; i < ii; i++) {
             if(match((fn = fns[i]).exp)) {
               push(fn(scope = scope || element.scope()));
