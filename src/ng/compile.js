cf8c05a091a9b192367d60093100b5f412342e57
Wladimir Coka <wlacoka@gmail.com>
2013-11-21 16:31:27 -0500

    refactor($compile): improve readability on conditional assignment
    Use ternary operator instead of if statement

--- src/ng/compile.js
+++ src/ng/compile.js
@@ -1369,11 +1369,9 @@ function $CompileProvider($provide) {
       function nodeLinkFn(childLinkFn, scope, linkNode, $rootElement, boundTranscludeFn) {
         var attrs, $element, i, ii, linkFn, controller, isolateScope, elementControllers = {}, transcludeFn;
 
-        if (compileNode === linkNode) {
-          attrs = templateAttrs;
-        } else {
-          attrs = shallowCopy(templateAttrs, new Attributes(jqLite(linkNode), templateAttrs.$attr));
-        }
+        attrs = (compileNode === linkNode)
+          ? templateAttrs
+          : shallowCopy(templateAttrs, new Attributes(jqLite(linkNode), templateAttrs.$attr));
         $element = attrs.$$element;
 
         if (newIsolateScopeDirective) {
