348377e4762beeccff0ae76c578ca99d21d61f81
Igor Minar <igor@angularjs.org>
2013-11-22 00:52:57 -0800

    test($compile): correct the assertion to make test pass on IE11

--- test/ng/compileSpec.js
+++ test/ng/compileSpec.js
@@ -4297,7 +4297,7 @@ describe('$compile', function() {
         element = $compile('<iframe srcdoc="{{html}}"></iframe>')($rootScope);
         $rootScope.html = $sce.trustAsHtml('<div onclick="">hello</div>');
         $rootScope.$digest();
-        expect(angular.lowercase(element[0].srcdoc)).toEqual('<div onclick="">hello</div>');
+        expect(angular.lowercase(element.attr('srcdoc'))).toEqual('<div onclick="">hello</div>');
       }));
     });
   }
