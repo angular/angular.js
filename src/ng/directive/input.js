1c24237ae1789a01164b90eae3d5977b6e803c3b
Tobias Bosch <tbosch1009@gmail.com>
2013-11-22 16:34:33 -0800

    fix(input): Support form auto complete on modern browser
    Although modern browser support the "input" event, they still only fire
    the "change" event when they auto complete form elements
    other than the currently selected one.
    
    Related to #1460

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -449,15 +449,15 @@ function textInputType(scope, element, attr, ctrl, $sniffer, $browser) {
       deferListener();
     });
 
-    // if user paste into input using mouse, we need "change" event to catch it
-    element.on('change', listener);
-
     // if user modifies input value using context menu in IE, we need "paste" and "cut" events to catch it
     if ($sniffer.hasEvent('paste')) {
       element.on('paste cut', deferListener);
     }
   }
 
+  // if user paste into input using mouse on older browser
+  // or form autocomplete on newer browser, we need "change" event to catch it
+  element.on('change', listener);
 
   ctrl.$render = function() {
     element.val(ctrl.$isEmpty(ctrl.$viewValue) ? '' : ctrl.$viewValue);
