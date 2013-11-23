033023e9d1509de29720b3a5e97ce12bc7f003b8
Andrei Korzhevskii <a.korzhevskiy@gmail.com>
2013-11-23 16:52:39 +0300

    refactor(minErr): remove duplicated code

--- src/minErr.js
+++ src/minErr.js
@@ -35,38 +35,30 @@ function minErr(module) {
       template = arguments[1],
       templateArgs = arguments,
       stringify = function (obj) {
-        if (typeof obj === 'function') {
-          return obj.toString().replace(/ \{[\s\S]*$/, '');
-        } else if (typeof obj === 'undefined') {
-          return 'undefined';
-        } else if (typeof obj !== 'string') {
-          return JSON.stringify(obj);
+        var val = obj;
+        if (typeof obj == 'function') {
+          val = obj.toString().replace(/ ?\{[\s\S]*$/, '');
+        } else if (typeof obj == 'undefined') {
+          val = 'undefined';
+        } else if (typeof obj != 'string') {
+          val = toJson(obj);
         }
-        return obj;
+        return val;
       },
       message, i;
 
     message = prefix + template.replace(/\{\d+\}/g, function (match) {
-      var index = +match.slice(1, -1), arg;
-
+      var index = +match.slice(1, -1);
       if (index + 2 < templateArgs.length) {
-        arg = templateArgs[index + 2];
-        if (typeof arg === 'function') {
-          return arg.toString().replace(/ ?\{[\s\S]*$/, '');
-        } else if (typeof arg === 'undefined') {
-          return 'undefined';
-        } else if (typeof arg !== 'string') {
-          return toJson(arg);
-        }
-        return arg;
+        return stringify(templateArgs[index + 2]);
       }
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/"NG_VERSION_FULL"/' +
+    message += '\nhttp://errors.angularjs.org/"NG_VERSION_FULL"/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
-      message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
+      message += (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
         encodeURIComponent(stringify(arguments[i]));
     }
 
