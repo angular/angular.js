2d0f6ccba896fe34141d6d4f59eef6fba580c5c2
Daniel Tabuenca <dtabuenca@solutionstream.com>
2013-11-27 22:01:22 -0800

    fix($compile): ensure isolated local watches' lastValue is always in sync
    When using two-way binding with isolate scope, under some circumstances
    the lastValue variable captured in the parentValueWatch function can get
    out of sync.
    
    Specifically, if both the value in the origin scope as well as the value
    in the isolate scope get independently updated to the same value within
    one digest cycle, the lastValue is never updated. This potentially causes
    the watch to make the wrong decision as to which side to update on subsequent
    passes.
    
    This fixes things by ensuring lastValue is always set to the last seen
    value even if the watch's logic was short circuited because there was no
    difference between the values in the original and isolate scopes.
    
    Closes #5182

--- src/ng/compile.js
+++ src/ng/compile.js
@@ -1438,13 +1438,13 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
                     // we are out of sync and need to copy
                     if (parentValue !== lastValue) {
                       // parent changed and it has precedence
-                      lastValue = isolateScope[scopeName] = parentValue;
+                      isolateScope[scopeName] = parentValue;
                     } else {
                       // if the parent can be assigned then do so
-                      parentSet(scope, parentValue = lastValue = isolateScope[scopeName]);
+                      parentSet(scope, parentValue = isolateScope[scopeName]);
                     }
                   }
-                  return parentValue;
+                  return lastValue = parentValue;
                 });
                 break;
 
