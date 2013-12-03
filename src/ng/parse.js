69e2bed1bae28a95b159a71186d35be212f9bb3b
Karl Seamon <bannanafiend@gmail.com>
2013-12-03 13:37:47 -0500

    Fixes the not-workiness.

--- src/ng/parse.js
+++ src/ng/parse.js
@@ -55,7 +55,7 @@ function ensureSafeObject(obj, fullExpression) {
           'Referencing the Window in Angular expressions is disallowed! Expression: {0}',
           fullExpression);
     } else if (// isElement(obj)
-        obj.nodeType) {
+        (obj.children && (obj.nodeName || (obj.on && obj.find)))) {
       throw $parseMinErr('isecdom',
           'Referencing DOM nodes in Angular expressions is disallowed! Expression: {0}',
           fullExpression);
