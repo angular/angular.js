c34b8b767d731204660ea51807fc80840e8a6e89
Michał Gołębiowski <m.goleb@gmail.com>
2013-12-05 14:59:13 +0100

    fix(input): prevent double $digest when using jQuery trigger.
    If an event was performed natively, jQuery sets the isTrigger property.
    When triggering event manually, the field is not present. Manually
    triggered events are performed synchronously which causes the "$digest
    already in progress" error.
    
    Closes #5293

--- test/ng/directive/inputSpec.js
+++ test/ng/directive/inputSpec.js
@@ -533,6 +533,23 @@ describe('input', function() {
       'event so that form auto complete works',function() {
       assertBrowserSupportsChangeEvent(true);
     });
+
+    if (!_jqLiteMode) {
+      it('should not cause the double $digest when triggering an event using jQuery', function() {
+        $sniffer.hasEvent = function(eventName) {
+          return eventName !== 'input';
+        };
+
+        compileInput('<input type="text" ng-model="name" name="alias" ng-change="change()" />');
+
+        scope.field = 'fake field';
+        scope.$watch('field', function() {
+          // We need to use _originalTrigger since trigger is modified by Angular Scenario.
+          inputElm._originalTrigger('change');
+        });
+        scope.$apply();
+      });
+    }
   });
 
   describe('"paste" and "cut" events', function() {
