1367650f8eccad0addaec11b3601b4e696ee9dcb
Ezekiel Victor <zekevictor@gmail.com>
2013-12-07 23:21:00 -0800

    fix($http,$location): fix undefined global after use of noConflict()
    The angular global is not available after using noConflict() so local copy must be used.

--- src/ng/location.js
+++ src/ng/location.js
@@ -638,7 +638,7 @@ function $LocationProvider(){
           $location.$$parse(rewrittenUrl);
           $rootScope.$apply();
           // hack to work around FF6 bug 684208 when scenario runner clicks on links
-          window.angular['ff-684208-preventDefault'] = true;
+          angular['ff-684208-preventDefault'] = true;
         }
       }
     });
