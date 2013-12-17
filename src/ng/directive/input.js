3486de255953ce8caf2f5cdca869e5a220fdefac
Ward Meremans <ward.meremans@persgroep.be>
2013-12-17 15:17:40 +0100

    Fixing an issue with iOS7 sending the change event before the value has been updated, which causes validations to fail. Delaying the listener with 5 milliseconds solves the problem.

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -435,7 +435,7 @@ function textInputType(scope, element, attr, ctrl, $sniffer, $browser) {
         timeout = $browser.defer(function() {
           listener();
           timeout = null;
-        });
+        },5); // Adding 5 milliseconds delay for iOS7
       }
     };
 
