96080d95966d6f8d9c00169b03b46a2e11e95f6a
Pete Bacon Darwin <pete@bacondarwin.com>
2013-12-17 12:29:22 +0000

    docs(): fix jshint issues

--- src/ngResource/resource.js
+++ src/ngResource/resource.js
@@ -276,31 +276,32 @@ function shallowClearAndCopy(src, dst) {
  * @example
  * # Creating a custom 'PUT' request
  * In this example we create a custom method on our resource to make a PUT request
- 	<pre>
-		var app = angular.module('app', ['ngResource', 'ngRoute']);
-
-		// Some APIs expect a PUT request in the format URL/object/ID
-		// Here we are creating an 'update' method 
-		app.factory('Notes', ['$resource', function($resource) {
-		    return $resource('/notes/:id', null,
-		        {
-		            'update': { method:'PUT' }
-		        });
-		}]);
-
-		// In our controller we get the ID from the URL using ngRoute and $routeParams
-		// We pass in $routeParams and our Notes factory along with $scope
-		app.controller('NotesCtrl', ['$scope', '$routeParams', 'Notes', function($scope, $routeParams, Notes) {
-		    // First get a note object from the factory
-		    var note = Notes.get({ id:$routeParams.id });
-		    $id = note.id;
-
-		    // Now call update passing in the ID first then the object you are updating
-		    Notes.update({ id:$id }, note);
-
-		    // This will PUT /notes/ID with the note object in the request payload
-		}]);
- 	</pre>
+ * <pre>
+ *		var app = angular.module('app', ['ngResource', 'ngRoute']);
+ *
+ *		// Some APIs expect a PUT request in the format URL/object/ID
+ *		// Here we are creating an 'update' method 
+ *		app.factory('Notes', ['$resource', function($resource) {
+ *    return $resource('/notes/:id', null,
+ *        {
+ *            'update': { method:'PUT' }
+ *        });
+ *		}]);
+ *
+ *		// In our controller we get the ID from the URL using ngRoute and $routeParams
+ *		// We pass in $routeParams and our Notes factory along with $scope
+ *		app.controller('NotesCtrl', ['$scope', '$routeParams', 'Notes',
+                                      function($scope, $routeParams, Notes) {
+ *    // First get a note object from the factory
+ *    var note = Notes.get({ id:$routeParams.id });
+ *    $id = note.id;
+ *
+ *    // Now call update passing in the ID first then the object you are updating
+ *    Notes.update({ id:$id }, note);
+ *
+ *    // This will PUT /notes/ID with the note object in the request payload
+ *		}]);
+ * </pre>
  */
 angular.module('ngResource', ['ng']).
   factory('$resource', ['$http', '$q', function($http, $q) {
