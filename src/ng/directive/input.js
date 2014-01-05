5adea0ba64b758f8f5700da73f6faca8ae1983be
Andreas Gruenbacher <andreas.gruenbacher@gmail.com>
2014-01-05 16:58:32 +0100

    docs(ngChange): clarify difference between ng-change DOM change event
    The ng-change event triggers immediately, which makes a difference for text input fields and text
    areas, where the JavaScript onchange event would only be called at the end of the change.
    
    Closes #5640

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -1188,7 +1188,10 @@ var ngModelDirective = function() {
  * @name ng.directive:ngChange
  *
  * @description
- * Evaluate given expression when user changes the input.
+ * Evaluate the given expression when the user changes the input.
+ * The expression is evaluated immediately, unlike the JavaScript onchange event
+ * which only triggers at the end of a change (usually, when the user leaves the
+ * form element or presses the return key).
  * The expression is not evaluated when the value change is coming from the model.
  *
  * Note, this directive requires `ngModel` to be present.
