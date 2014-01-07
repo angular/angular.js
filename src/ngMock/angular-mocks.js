95f0bf9b526fda8964527c6d4aef1ad50a47f1f3
John Papa <john@johnpapa.net>
2014-01-07 07:33:39 -0500

    fix(mocks): refactor currentSpec to work w/ Jasmine 2
    Closes #5662

--- src/ngMock/angular-mocks.js
+++ src/ngMock/angular-mocks.js
@@ -1947,7 +1947,7 @@ if(window.jasmine || window.mocha) {
 
   var currentSpec = null,
       isSpecRunning = function() {
-        return currentSpec && (window.mocha || currentSpec.queue.running);
+        return !!currentSpec;
       };
 
 
