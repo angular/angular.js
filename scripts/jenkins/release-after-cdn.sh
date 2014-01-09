c79e745332209029f74de1690b0dc5bec8e3a469
Tobias Bosch <tbosch1009@gmail.com>
2014-01-09 15:22:50 -0800

    chore(build): check cdn before executing the release-after-cdn script

--- scripts/jenkins/release-after-cdn.sh
+++ scripts/jenkins/release-after-cdn.sh
@@ -4,14 +4,8 @@ ARG_DEFS=(
   # require the git dryrun flag so the script can't be run without
   # thinking about this!
   "--git-push-dryrun=(true|false)"
+  "--cdn-version=(.*)"
 )
-
-function findLatestRelease {
-  # returns e.g. v1.2.7
-  LATEST_TAG=$(git describe --abbrev=0 --tags)
-  # returns e.g. 1.2.7
-  echo ${LATEST_TAG:1}
-}
 
 function init {
   NG_ARGS=("$@")
@@ -23,14 +17,22 @@ function init {
 
 function phase {
   ACTION_ARG="--action=$1"
-  CDN_VERSION_ARG="--cdn-version=$LATEST_VERSION"
+  CDN_VERSION_ARG="--cdn-version=$CDN_VERSION"
   ./scripts/angular.js/publish-cdn-version.sh $ACTION_ARG $CDN_VERSION_ARG $VERBOSE_ARG
   ./scripts/angularjs.org/publish.sh $ACTION_ARG $CDN_VERSION_ARG $VERBOSE_ARG
 }
 
+function checkCdn {
+  STATUS=$(curl http://ajax.googleapis.com/ajax/libs/angularjs/$CDN_VERSION/angular.min.js --write-out '%{http_code}' -o /dev/null -silent)
+  if [[ $STATUS != 200 ]]; then
+    echo "Could not find release $CDN_VERSION on CDN"
+    exit 1
+  fi
+}
+
 function run {
   cd ../..
-  LATEST_VERSION=$(findLatestRelease)
+  checkCdn
 
   phase prepare
   phase publish
