e4f28b6098370c458eaffd5113367f362a6f2139
Julie <ju.ralph@gmail.com>
2014-01-13 15:20:44 -0800

    test(doc:protractor): turn off animation for doc end to end tests to speed things up

--- protractor-conf.js
+++ protractor-conf.js
@@ -14,6 +14,17 @@ exports.config = {
 
   framework: 'jasmine',
 
+  onPrepare: function() {
+    // Disable animations so e2e tests run more quickly
+    var disableNgAnimate = function() {
+      angular.module('disableNgAnimate', []).run(function($animate) {
+        $animate.enabled(false);
+      });
+    };
+
+    browser.addMockModule('disableNgAnimate', disableNgAnimate);
+  },
+
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
   }
