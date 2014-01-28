33b28e4a2a7d77292773141fd49686a33e2b6c77
Igor Minar <igor@angularjs.org>
2014-01-27 17:35:27 -0800

    fix(filterFilter): don't interpret dots in predicate object fields as paths
    Closes #6005
    Closes #6009

--- src/ng/filter/filter.js
+++ src/ng/filter/filter.js
@@ -188,7 +188,7 @@ function filterFilter() {
           (function(path) {
             if (typeof expression[path] == 'undefined') return;
             predicates.push(function(value) {
-              return search(path == '$' ? value : getter(value, path), expression[path]);
+              return search(path == '$' ? value : (value && value[path]), expression[path]);
             });
           })(key);
         }
