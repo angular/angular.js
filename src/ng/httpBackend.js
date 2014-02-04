24699ee8f04c1f1459be1d36207e654421d58ff0
Caitlin Potter <caitpotter88@gmail.com>
2014-02-04 18:34:23 -0500

    fix($http): ignore xhr.responseType setter exception if value is "json"
    WebKit added support for the json responseType value on 09/03/2013
    https://bugs.webkit.org/show_bug.cgi?id=73648. Versions of Safari prior to 7 are known to throw when
    setting the value "json" as the response type. Other older browsers implementing the responseType.
    Other browsers with infrequent update cycles may also be affected.
    
    The json responseType value can be ignored if not supported, because JSON payloads are parsed on the
    client-side regardless.
    
    Closes #6115
    Closes #6122

--- src/ng/httpBackend.js
+++ src/ng/httpBackend.js
@@ -107,7 +107,20 @@ function createHttpBackend($browser, createXhr, $browserDefer, callbacks, rawDoc
       }
 
       if (responseType) {
-        xhr.responseType = responseType;
+        try {
+          xhr.responseType = responseType;
+        } catch (e) {
+          // WebKit added support for the json responseType value on 09/03/2013
+          // https://bugs.webkit.org/show_bug.cgi?id=73648. Versions of Safari prior to 7 are
+          // known to throw when setting the value "json" as the response type. Other older
+          // browsers implementing the responseType 
+          //
+          // The json response type can be ignored if not supported, because JSON payloads are
+          // parsed on the client-side regardless.
+          if (responseType !== 'json') {
+            throw e;
+          }
+        }
       }
 
       xhr.send(post || null);
