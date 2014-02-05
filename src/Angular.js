394cd486fa1ffacac74732f3505d8c8088b74943
Hopiu <ben.willi@gmail.com>
2014-02-05 10:39:03 +0100

    docs(forEach): correct spelling error
    Closes #6124

--- src/Angular.js
+++ src/Angular.js
@@ -192,7 +192,7 @@ function isArrayLike(obj) {
  * is the value of an object property or an array element and `key` is the object property key or
  * array element index. Specifying a `context` for the function is optional.
  *
- * It is worth nothing that `.forEach` does not iterate over inherited properties because it filters
+ * It is worth noting that `.forEach` does not iterate over inherited properties because it filters
  * using the `hasOwnProperty` method.
  *
    <pre>
