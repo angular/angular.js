7398d42cbe363653f45427c0ba6609fc624ff807
cgwyllie <cgwyllie@gmail.com>
2014-02-13 09:38:10 +0000

    docs($http): fix auth default headers example

--- src/ng/http.js
+++ src/ng/http.js
@@ -277,7 +277,7 @@ function $HttpProvider() {
      *
      * ```
      * module.run(function($http) {
-     *   $http.defaults.headers.common.Authentication = 'Basic YmVlcDpib29w'
+     *   $http.defaults.headers.common.Authorization = 'Basic YmVlcDpib29w'
      * });
      * ```
      *
