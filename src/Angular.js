bdbb80d83f7ff38c1c8e6759215fbe5712398a44
rodyhaddad <rody@rodyhaddad.com>
2014-02-15 01:04:40 -0500

    fix(angular.toJson): only strip properties beginning with $$, not $
    BREAKING CHANGE:
    
    If you expected `toJson` to strip these types of properties before,
    you will have to manually do this yourself now.

--- src/Angular.js
+++ src/Angular.js
@@ -975,7 +975,7 @@ function bind(self, fn) {
 function toJsonReplacer(key, value) {
   var val = value;
 
-  if (typeof key === 'string' && key.charAt(0) === '$') {
+  if (typeof key === 'string' && key.charAt(0) === '$' && key.charAt(1) === '$') {
     val = undefined;
   } else if (isWindow(value)) {
     val = '$WINDOW';
@@ -996,7 +996,7 @@ function toJsonReplacer(key, value) {
  * @function
  *
  * @description
- * Serializes input into a JSON-formatted string. Properties with leading $ characters will be
+ * Serializes input into a JSON-formatted string. Properties with leading $$ characters will be
  * stripped since angular uses this notation internally.
  *
  * @param {Object|Array|Date|string|number} obj Input to be serialized into JSON.
