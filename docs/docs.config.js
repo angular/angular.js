94b1c01f3402ab64433ef553e2a6bb78e7c0b9dd
Peter Bacon Darwin <pete@bacondarwin.com>
2014-02-17 14:01:30 +0000

    chore(docs-assets): add versions to paths for imported assets
    This should prevent stale caches in browsers if we upgrade any of these
    assets.

--- docs/docs.config.js
+++ docs/docs.config.js
@@ -9,6 +9,12 @@ module.exports = function(config) {
   var version = gruntUtils.getVersion();
   var cdnUrl = "//ajax.googleapis.com/ajax/libs/angularjs/" + version.cdn;
 
+  var getVersion = function(component, sourceFolder, packageFile) {
+    sourceFolder = sourceFolder || '../bower_components';
+    packageFile = packageFile || 'bower.json';
+    return require(path.join(sourceFolder,component,packageFile)).version;
+  };
+
 
   config = basePackage(config);
 
@@ -38,20 +44,20 @@ module.exports = function(config) {
         '../angular-sanitize.js',
         '../angular-touch.js',
         '../angular-animate.js',
-        'components/marked/lib/marked.js',
+        'components/marked-' + getVersion('marked', '../node_modules', 'package.json') + '/lib/marked.js',
         'js/angular-bootstrap/bootstrap.js',
         'js/angular-bootstrap/bootstrap-prettify.js',
         'js/angular-bootstrap/dropdown-toggle.js',
-        'components/lunr.js/lunr.js',
-        'components/google-code-prettify/src/prettify.js',
-        'components/google-code-prettify/src/lang-css.js',
+        'components/lunr.js-' + getVersion('lunr.js') + '/lunr.js',
+        'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/prettify.js',
+        'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/lang-css.js',
         'js/versions-data.js',
         'js/pages-data.js',
         'js/docs.js'
       ],
       stylesheets: [
-        'css/bootstrap/css/bootstrap.css',
-        'components/open-sans-fontface/open-sans.css',
+        'components/bootstrap-' + getVersion('bootstrap') + '/dist/css/bootstrap.css',
+        'components/open-sans-fontface-' + getVersion('open-sans-fontface') + '/open-sans.css',
         'css/prettify-theme.css',
         'css/docs.css',
         'css/animations.css'
@@ -67,20 +73,20 @@ module.exports = function(config) {
         '../angular-sanitize.min.js',
         '../angular-touch.min.js',
         '../angular-animate.min.js',
-        'components/marked/lib/marked.js',
+        'components/marked-' + getVersion('marked', '../node_modules', 'package.json') + '/lib/marked.js',
         'js/angular-bootstrap/bootstrap.js',
         'js/angular-bootstrap/bootstrap-prettify.js',
         'js/angular-bootstrap/dropdown-toggle.js',
-        'components/lunr.js/lunr.min.js',
-        'components/google-code-prettify/src/prettify.js',
-        'components/google-code-prettify/src/lang-css.js',
+        'components/lunr.js-' + getVersion('lunr.js') + '/lunr.min.js',
+        'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/prettify.js',
+        'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/lang-css.js',
         'js/versions-data.js',
         'js/pages-data.js',
         'js/docs.js'
       ],
       stylesheets: [
-        'components/bootstrap/dist/css/bootstrap.css',
-        'components/open-sans-fontface/open-sans.css',
+        'components/bootstrap-' + getVersion('bootstrap') + '/dist/css/bootstrap.min.css',
+        'components/open-sans-fontface-' + getVersion('open-sans-fontface') + '/open-sans.css',
         'css/prettify-theme.css',
         'css/docs.css',
         'css/animations.css'
@@ -96,20 +102,20 @@ module.exports = function(config) {
         cdnUrl + '/angular-sanitize.min.js',
         cdnUrl + '/angular-touch.min.js',
         cdnUrl + '/angular-animate.min.js',
-        'components/marked/lib/marked.js',
+        'components/marked-' + getVersion('marked', '../node_modules', 'package.json') + '/lib/marked.js',
         'js/angular-bootstrap/bootstrap.js',
         'js/angular-bootstrap/bootstrap-prettify.js',
         'js/angular-bootstrap/dropdown-toggle.js',
-        'components/lunr.js/lunr.min.js',
-        'components/google-code-prettify/src/prettify.js',
-        'components/google-code-prettify/src/lang-css.js',
+        'components/lunr.js-' + getVersion('lunr.js') + '/lunr.min.js',
+        'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/prettify.js',
+        'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/lang-css.js',
         'js/versions-data.js',
         'js/pages-data.js',
         'js/docs.js'
       ],
       stylesheets: [
-        'components/bootstrap/dist/css/bootstrap.css',
-        'components/open-sans-fontface/open-sans.css',
+        'components/bootstrap-' + getVersion('bootstrap') + '/dist/css/bootstrap.min.css',
+        'components/open-sans-fontface-' + getVersion('open-sans-fontface') + '/open-sans.css',
         'css/prettify-theme.css',
         'css/docs.css',
         'css/animations.css'
