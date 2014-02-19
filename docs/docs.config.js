4d7e1cef370586c6644e226c55db1fcd234346ad
Julie <ju.ralph@gmail.com>
2014-02-19 11:53:02 -0800

    chore(doc-gen): generate examples for each deployment, e.g. jquery
    Update to the latest dgeni-packages, which supports multiple
    deployment environments for the examples.
    
    Add a jQuery deployment environment for the examples.
    
    Currently, the target of the runnable example iframe always points
    to the default deployment environment, not to the environment under
    which the main app is running.
    
    Closes #6361

--- docs/docs.config.js
+++ docs/docs.config.js
@@ -25,13 +25,6 @@ module.exports = function(config) {
     { pattern: '**/*.ngdoc', basePath: path.resolve(basePath, 'content') }
   ]);
 
-  config.set('processing.examples.commonFiles', {
-    scripts: [ '../../../angular.js' ],
-    stylesheets: []
-  });
-  config.set('processing.examples.dependencyPath', '../../..');
-
-
   config.set('processing.errors.minerrInfoPath', path.resolve(basePath, '../build/errors.json'));
 
   config.set('rendering.outputFolder', '../build/docs');
@@ -41,6 +34,12 @@ module.exports = function(config) {
   config.merge('deployment', {
     environments: [{
       name: 'debug',
+      examples: {
+        commonFiles: {
+          scripts: [ '../../../angular.js' ]
+        },
+        dependencyPath: '../../..'
+      },
       scripts: [
         '../angular.js',
         '../angular-resource.js',
@@ -70,6 +69,12 @@ module.exports = function(config) {
     },
     {
       name: 'default',
+      examples: {
+        commonFiles: {
+          scripts: [ '../../../angular.min.js' ]
+        },
+        dependencyPath: '../../..'
+      },
       scripts: [
         '../angular.min.js',
         '../angular-resource.min.js',
@@ -98,7 +103,52 @@ module.exports = function(config) {
       ]
     },
     {
+      name: 'jquery',
+      examples: {
+        commonFiles: {
+          scripts: [
+            '../../components/jquery-' + getVersion('jquery') + '/jquery.js',
+            '../../../angular.js'
+          ]
+        },
+        dependencyPath: '../../..'
+      },
+      scripts: [
+        'components/jquery-' + getVersion('jquery') + '/jquery.js',
+        '../angular.min.js',
+        '../angular-resource.min.js',
+        '../angular-route.min.js',
+        '../angular-cookies.min.js',
+        '../angular-sanitize.min.js',
+        '../angular-touch.min.js',
+        '../angular-animate.min.js',
+        'components/marked-' + getVersion('marked', '../node_modules', 'package.json') + '/lib/marked.js',
+        'js/angular-bootstrap/bootstrap.js',
+        'js/angular-bootstrap/bootstrap-prettify.js',
+        'js/angular-bootstrap/dropdown-toggle.js',
+        'components/lunr.js-' + getVersion('lunr.js') + '/lunr.min.js',
+        'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/prettify.js',
+        'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/lang-css.js',
+        'js/versions-data.js',
+        'js/pages-data.js',
+        'js/docs.js'
+      ],
+      stylesheets: [
+        'components/bootstrap-' + getVersion('bootstrap') + '/dist/css/bootstrap.min.css',
+        'components/open-sans-fontface-' + getVersion('open-sans-fontface') + '/open-sans.css',
+        'css/prettify-theme.css',
+        'css/docs.css',
+        'css/animations.css'
+      ]
+    },
+    {
       name: 'production',
+      examples: {
+        commonFiles: {
+          scripts: [ cdnUrl + '/angular.min.js' ]
+        },
+        dependencyPath: cdnUrl
+      },
       scripts: [
         cdnUrl + '/angular.min.js',
         cdnUrl + '/angular-resource.min.js',
