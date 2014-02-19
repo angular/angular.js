6890a84ace4a6b8dd534e726c52e260c76c358ee
Julie <ju.ralph@gmail.com>
2014-02-19 11:53:02 -0800

    chore(doc-gen): generate examples for each deployment, e.g. jquery
    Update to the latest dgeni-packages, which supports multiple
    deployment environments for the examples.
    
    Add a jQuery deployment environment for the examples.
    
    Currently, the target of the runnable example iframe always points
    to the default deployment environment, not to the environment under
    which the main app is running.
    
    Closes #6361

--- docs/config/index.js
+++ docs/config/index.js
@@ -2,11 +2,13 @@ var _ = require('lodash');
 var path = require('canonical-path');
 var packagePath = __dirname;
 
-var angularjsPackage = require('dgeni-packages/ngdoc');
+var basePackage = require('dgeni-packages/ngdoc');
+var examplesPackage = require('dgeni-packages/examples');
 
 module.exports = function(config) {
 
-  config = angularjsPackage(config);
+  config = basePackage(config);
+  config = examplesPackage(config);
   
   config.append('processing.processors', [
     require('./processors/git-data'),
