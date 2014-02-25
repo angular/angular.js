809a3721b00f1f79cbaeb5c7108daea59e7383a1
Matias Niemelä <matias@yearofmoo.com>
2014-02-24 19:21:20 -0500

    chore(core): create a wrapper to manage async callbacks

---
+++ src/ng/asyncCallback.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function $$AsyncCallbackProvider(){
+  this.$get = ['$$rAF', '$timeout', function($$rAF, $timeout) {
+    return $$rAF.supported
+      ? function(fn) { return $$rAF(fn); }
+      : function(fn) {
+        return $timeout(fn, 0, false);
+      };
+  }];
+}
