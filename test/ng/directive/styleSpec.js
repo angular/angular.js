3b881a4a8f658e9cd1a212b11db5c21b84568bbb
Sekib Omazic <sekib.omazic@gmail.com>
2014-02-28 13:54:17 +0100

    test changed to show that binding works for complex expressions

--- test/ng/directive/styleSpec.js
+++ test/ng/directive/styleSpec.js
@@ -10,19 +10,30 @@ describe('style', function() {
 
 
   it('should compile style element', inject(function($compile, $rootScope) {
-    element = jqLite('<style type="text/css">.some-container{ width: {{elementWidth}}px; }</style>');
+    element = jqLite('<style type="text/css">' +
+      '.navigation{font-size:1.5em; h3{font-size:1.5em}}' +
+      '.footer{h3{font-size:{{fontSize}}em}}' +
+      '.header{font-size:1.5em; h3{font-size:{{fontSize}}{{unit}}}}' +
+      '</style>');
     $compile(element)($rootScope);
     $rootScope.$digest();
 
     // read innerHTML and trim to pass on IE8
-    expect(trim(element[0].innerHTML)).toBe('.some-container{ width: px; }');
+    expect(trim(element[0].innerHTML)).toBe(
+      '.navigation{font-size:1.5em; h3{font-size:1.5em}}' +
+      '.footer{h3{font-size:em}}' + 
+      '.header{font-size:1.5em; h3{font-size:}}');
 
     $rootScope.$apply(function() {
-      $rootScope.elementWidth = 200;
+      $rootScope.fontSize = 1.5;
+      $rootScope.unit = 'em';
     });
 
     // read innerHTML and trim to pass on IE8
-    expect(trim(element[0].innerHTML)).toBe('.some-container{ width: 200px; }');
+    expect(trim(element[0].innerHTML)).toBe(
+      '.navigation{font-size:1.5em; h3{font-size:1.5em}}' +
+      '.footer{h3{font-size:1.5em}}' +
+      '.header{font-size:1.5em; h3{font-size:1.5em}}');
   }));
 
 
