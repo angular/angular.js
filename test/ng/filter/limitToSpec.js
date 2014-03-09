55a7b8b4f840dbd9fca343ab4aa262062e9e1732
Tamer Aydin <tamer.aydin@gmail.com>
2014-03-09 23:02:59 +0100

    feat(limitTo): extend the filter to take a beginning index argument
    Extend the limitTo filter to take an optional argument for beginning index. It provides a slice-alike functionality to manipulate the input.
    
    Closes #5355

--- test/ng/filter/limitToSpec.js
+++ test/ng/filter/limitToSpec.js
@@ -19,6 +19,19 @@ describe('Filter: limitTo', function() {
     expect(limitTo(str, '3')).toEqual("tuv");
   });
 
+  it('should return the first X items beginning from index Y when X and Y are positive', function() {
+    expect(limitTo(items, 3, '3')).toEqual(['d', 'e', 'f']);
+    expect(limitTo(items, '3', 3)).toEqual(['d', 'e', 'f']);
+    expect(limitTo(str, 3, 3)).toEqual("wxy");
+    expect(limitTo(str, '3', '3')).toEqual("wxy");
+  });
+
+  it('should return the first X items beginning from index Y when X is positive and Y is negative', function() {
+    expect(limitTo(items, 3, '-3')).toEqual(['f', 'g', 'h']);
+    expect(limitTo(items, '3', -3)).toEqual(['f', 'g', 'h']);
+    expect(limitTo(str, 3, -3)).toEqual("xyz");
+    expect(limitTo(str, '3', '-3')).toEqual("xyz");
+  });
 
   it('should return the last X items when X is negative', function() {
     expect(limitTo(items, -3)).toEqual(['f', 'g', 'h']);
@@ -27,6 +40,19 @@ describe('Filter: limitTo', function() {
     expect(limitTo(str, '-3')).toEqual("xyz");
   });
 
+  it('should return the last X items until index Y when X and Y are negative', function() {
+    expect(limitTo(items, -3, '-3')).toEqual(['c', 'd', 'e']);
+    expect(limitTo(items, '-3', -3)).toEqual(['c', 'd', 'e']);
+    expect(limitTo(str, -3, -3)).toEqual("uvw");
+    expect(limitTo(str, '-3', '-3')).toEqual("uvw");
+  });
+
+  it('should return the last X items until index Y when X is negative and Y is positive', function() {
+    expect(limitTo(items, -3, '4')).toEqual(['b', 'c', 'd']);
+    expect(limitTo(items, '-3', 4)).toEqual(['b', 'c', 'd']);
+    expect(limitTo(str, -3, 4)).toEqual("uvw");
+    expect(limitTo(str, '-3', '4')).toEqual("uvw");
+  });
 
   it('should return an empty array when X cannot be parsed', function() {
     expect(limitTo(items, 'bogus')).toEqual([]);
@@ -42,6 +68,19 @@ describe('Filter: limitTo', function() {
     expect(limitTo(str, 'undefined')).toEqual("");
     expect(limitTo(str, null)).toEqual("");
     expect(limitTo(str, undefined)).toEqual("");
+  });
+
+  it('should take 0 as beginning index value when Y cannot be parsed', function() {
+    expect(limitTo(items, 3, 'bogus')).toEqual(limitTo(items, 3, 0));
+    expect(limitTo(items, -3, 'null')).toEqual(limitTo(items, -3));
+    expect(limitTo(items, '3', 'undefined')).toEqual(limitTo(items, '3', 0));
+    expect(limitTo(items, '-3', null)).toEqual(limitTo(items, '-3'));
+    expect(limitTo(items, 3, undefined)).toEqual(limitTo(items, 3, 0));
+    expect(limitTo(str, 3, 'bogus')).toEqual(limitTo(str, 3));
+    expect(limitTo(str, -3, 'null')).toEqual(limitTo(str, -3, 0));
+    expect(limitTo(str, '3', 'undefined')).toEqual(limitTo(str, '3'));
+    expect(limitTo(str, '-3', null)).toEqual(limitTo(str, '-3', 0));
+    expect(limitTo(str, 3, undefined)).toEqual(limitTo(str, 3));
   });
 
 
@@ -67,5 +106,33 @@ describe('Filter: limitTo', function() {
     expect(limitTo(str, '9')).toEqual(str);
     expect(limitTo(str, -9)).toEqual(str);
     expect(limitTo(str, '-9')).toEqual(str);
-  })
+  });
+
+  it('should return an empty array if Y exceeds input length', function() {
+    expect(limitTo(items, '3', 12)).toEqual([]);
+    expect(limitTo(items, 4, '-12')).toEqual([]);
+    expect(limitTo(items, -3, '12')).toEqual([]);
+    expect(limitTo(items, '-4', -12)).toEqual([]);
+  });
+
+  it('should return an empty string if Y exceeds input length', function() {
+    expect(limitTo(str, '3', 12)).toEqual("");
+    expect(limitTo(str, 4, '-12')).toEqual("");
+    expect(limitTo(str, -3, '12')).toEqual("");
+    expect(limitTo(str, '-4', -12)).toEqual("");
+  });
+
+  it('should return the entire string beginning from Y if X is positive and X+Y exceeds input length', function() {
+    expect(limitTo(items, 7, 3)).toEqual(['d', 'e', 'f', 'g', 'h']);
+    expect(limitTo(items, 7, -3)).toEqual(['f', 'g', 'h']);
+    expect(limitTo(str, 6, 3)).toEqual("wxyz");
+    expect(limitTo(str, 6, -3)).toEqual("xyz");
+  });
+
+  it('should return the entire string until index Y if X is negative and X+Y exceeds input length', function() {
+    expect(limitTo(items, -7, 3)).toEqual(['a', 'b', 'c']);
+    expect(limitTo(items, -7, -3)).toEqual(['a', 'b', 'c', 'd', 'e']);
+    expect(limitTo(str, -6, 3)).toEqual("tuv");
+    expect(limitTo(str, -6, -3)).toEqual("tuvw");
+  });
 });
