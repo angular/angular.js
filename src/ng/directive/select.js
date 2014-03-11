4f3083f9ef3b010188704704438273cd936186a9
Alberto Aldegheri <albyrock87@gmail.com>
2014-03-11 16:40:12 +0100

    fix(select): isolated scope options refresh IE9
    Fixes #6211

--- src/ng/directive/select.js
+++ src/ng/directive/select.js
@@ -570,6 +570,10 @@ var selectDirective = ['$compile', '$parse', function($compile,   $parse) {
             while(existingOptions.length > index) {
               existingOptions.pop().element.remove();
             }
+            // force OPTIONs refresh in IE <= 9
+            if (msie && msie <= 9 && selectElement[0].options.length) {
+                selectElement[0].options[0].label = "" + selectElement[0].options[0].label;
+            }
           }
           // remove any excessive OPTGROUPs from select
           while(optionGroupsCache.length > groupIndex) {
