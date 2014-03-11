11c5bb7f3de13722a84c5503129097393056061e
Peter Bacon Darwin <pete@bacondarwin.com>
2014-03-11 06:34:09 +0000

    docs(versions): rework the version info extraction
    The docs were relying on the grunt/util module for getting version info
    but this was unreliable and full of custom regexes.  This is moved into
    a new version-info module that makes much better use of the semver library.

--- docs/config/processors/git-data.js
+++ docs/config/processors/git-data.js
@@ -1,4 +1,5 @@
 var gruntUtils = require('../../../lib/grunt/utils');
+var versionInfo = require('../../../lib/versions/version-info');
 
 module.exports = {
   name: 'git-data',
@@ -6,9 +7,9 @@ module.exports = {
   description: 'This processor adds information from the local git repository to the extraData injectable',
   init: function(config, injectables) {
     injectables.value('gitData', {
-      version: gruntUtils.getVersion(),
-      versions: gruntUtils.getPreviousVersions(),
-      info: gruntUtils.getGitRepoInfo()
+      version: versionInfo.currentVersion,
+      versions: versionInfo.previousVersions,
+      info: versionInfo.gitRepoInfo
     });
   },
   process: function(extraData, gitData) {
