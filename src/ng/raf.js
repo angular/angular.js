62193d12bb06c8c66bfda501518d8f8ba8afb02d
Matias Niemelä <matias@yearofmoo.com>
2014-03-14 12:01:45 -0400

    fix($$rAF): always fallback to a $timeout incase native rAF isn't supported
    Closes #6654

--- src/ng/raf.js
+++ src/ng/raf.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function $$RAFProvider(){ //rAF
-  this.$get = ['$window', function($window) {
+  this.$get = ['$window', '$timeout', function($window, $timeout) {
     var requestAnimationFrame = $window.requestAnimationFrame ||
                                 $window.webkitRequestAnimationFrame ||
                                 $window.mozRequestAnimationFrame;
@@ -10,14 +10,22 @@ function $$RAFProvider(){ //rAF
                                $window.webkitCancelAnimationFrame ||
                                $window.mozCancelAnimationFrame;
 
-    var raf = function(fn) {
-      var id = requestAnimationFrame(fn);
-      return function() {
-        cancelAnimationFrame(id);
-      };
-    };
+    var rafSupported = !!requestAnimationFrame;
+    var raf = rafSupported
+      ? function(fn) {
+          var id = requestAnimationFrame(fn);
+          return function() {
+            cancelAnimationFrame(id);
+          };
+        }
+      : function(fn) {
+          var timer = $timeout(fn, 16.66, false); // 1000 / 60 = 16.666
+          return function() {
+            $timeout.cancel(timer);
+          };
+        };
 
-    raf.supported = !!requestAnimationFrame;
+    raf.supported = rafSupported;
 
     return raf;
   }];
