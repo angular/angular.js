562c4e424b0ed5f8d4bffba0cd18e66db2059043
Shahar Talmi <shahar.talmi@gmail.com>
2014-04-01 23:40:10 +0300

    fix($compile): set $isolateScope correctly for sync template directives
    All isolated scope directives that do not have `templateUrl` were marked
    as `$isolateScopeNoTemplate` even if they did have a `template` attribute.
    
    This caused `jqLite#scope()` to return the wrong value for child elements
    within the directive's template.
    
    Closes #6942

--- test/ng/compileSpec.js
+++ test/ng/compileSpec.js
@@ -1797,6 +1797,19 @@ describe('$compile', function() {
                 }
               };
             });
+            directive('stscope' + uppercase(name), function(log) {
+              return {
+                scope: true,
+                restrict: 'CA',
+                template: '<span></span>',
+                compile: function() {
+                  return function (scope, element) {
+                    log(scope.$id);
+                    expect(element.data('$scope')).toBe(scope);
+                  };
+                }
+              };
+            });
             directive('trscope' + uppercase(name), function(log) {
               return {
                 scope: true,
@@ -1816,6 +1829,20 @@ describe('$compile', function() {
                 scope: {},
                 restrict: 'CA',
                 templateUrl: 'tiscope.html',
+                compile: function() {
+                  return function (scope, element) {
+                    iscope = scope;
+                    log(scope.$id);
+                    expect(element.data('$isolateScope')).toBe(scope);
+                  };
+                }
+              };
+            });
+            directive('stiscope' + uppercase(name), function(log) {
+              return {
+                scope: {},
+                restrict: 'CA',
+                template: '<span></span>',
                 compile: function() {
                   return function (scope, element) {
                     iscope = scope;
@@ -1999,6 +2026,13 @@ describe('$compile', function() {
                 expect(element.find('a').scope().$parent).toBe($rootScope);
               })
             );
+
+            it('should return the new scope for children in the directive sync template', inject(
+              function($rootScope, $compile) {
+                element = $compile('<div stscope></div>')($rootScope);
+                expect(element.find('span').scope().$parent).toBe($rootScope);
+              })
+            );
           });
 
 
@@ -2041,6 +2075,14 @@ describe('$compile', function() {
                 expect(element.isolateScope()).not.toBe($rootScope);
               })
             );
+
+            it('should return the isolate scope for children in directive sync template', inject(
+              function($rootScope, $compile) {
+                element = $compile('<div stiscope></div>')($rootScope);
+                expect(element.find('span').scope()).toBe(element.isolateScope());
+                expect(element.isolateScope()).not.toBe($rootScope);
+              })
+            );
           });
 
 
@@ -2065,6 +2107,17 @@ describe('$compile', function() {
                 element = $compile('<div><a ng-if="true" tiscope></a></div>')($rootScope);
                 $rootScope.$apply();
                 $httpBackend.flush();
+                directiveElement = element.find('a');
+                child = directiveElement.find('span');
+                expect(child.scope()).toBe(directiveElement.isolateScope());
+              })
+            );
+
+            it('should return the isolate scope for child elements in directive sync template', inject(
+              function($rootScope, $compile) {
+                var directiveElement, child;
+                element = $compile('<div><a ng-if="true" stiscope></a></div>')($rootScope);
+                $rootScope.$apply();
                 directiveElement = element.find('a');
                 child = directiveElement.find('span');
                 expect(child.scope()).toBe(directiveElement.isolateScope());
