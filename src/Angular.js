814c9847e8489809b150d3b54e9da25c1fceb581
Josh Kurz <josh.kurz@turner.com>
2014-04-03 00:59:21 -0400

    fix(Angular.js): toKeyValue is not serializing null values
    Signed-off-by: Josh Kurz <jkurz25@gmail.com>

--- src/Angular.js
+++ src/Angular.js
@@ -1062,14 +1062,16 @@ function parseKeyValue(/**string*/keyValue) {
 function toKeyValue(obj) {
   var parts = [];
   forEach(obj, function(value, key) {
-    if (isArray(value)) {
-      forEach(value, function(arrayValue) {
+    if (value !== null) {
+      if (isArray(value)) {
+        forEach(value, function(arrayValue) {
+          parts.push(encodeUriQuery(key, true) +
+                     (arrayValue === true ? '' : '=' + encodeUriQuery(arrayValue, true)));
+        });
+      } else {
         parts.push(encodeUriQuery(key, true) +
-                   (arrayValue === true ? '' : '=' + encodeUriQuery(arrayValue, true)));
-      });
-    } else {
-    parts.push(encodeUriQuery(key, true) +
-               (value === true ? '' : '=' + encodeUriQuery(value, true)));
+                   (value === true ? '' : '=' + encodeUriQuery(value, true)));
+      }
     }
   });
   return parts.length ? parts.join('&') : '';
