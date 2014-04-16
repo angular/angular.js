fee3a98f20a20c16287b671b29cf5fb1dc43d15c
Shahar Talmi <shahar.talmi@gmail.com>
2014-04-16 23:59:31 +0300

    fix(ngClass): support multiple classes in key

--- test/ng/directive/ngClassSpec.js
+++ test/ng/directive/ngClassSpec.js
@@ -209,6 +209,23 @@ describe('ngClass', function() {
     })
   );
 
+  it('should allow ngClass with overlapping classes', inject(function($rootScope, $compile, $animate) {
+      element = $compile('<div ng-class="{\'same yes\': test, \'same no\': !test}"></div>')($rootScope);
+      $rootScope.$digest();
+
+      expect(element).toHaveClass('same');
+      expect(element).not.toHaveClass('yes');
+      expect(element).toHaveClass('no');
+
+      $rootScope.$apply(function() {
+        $rootScope.test = true;
+      });
+
+      expect(element).toHaveClass('same');
+      expect(element).toHaveClass('yes');
+      expect(element).not.toHaveClass('no');
+  }));
+
   it('should allow both ngClass and ngClassOdd/Even with multiple classes', inject(function($rootScope, $compile) {
     element = $compile('<ul>' +
       '<li ng-repeat="i in [0,1]" ng-class="[\'A\', \'B\']" ' +
