1e516586d5399e9b9ceb1ee1cb00bc1e63a28ff8
Joel Hooks <joelhooks@gmail.com>
2014-04-22 16:20:39 -0500

    fix(ngModelOptions): enable overriding the default with a debounce of zero
    Because of how the logic was set up, a value of `0` was assumed to be the
    same as `undefined`, which meant that you couldn't override the default
    debounce delay with a value of zero.
    
    For example, the following assigned a debounce delay of 500ms to the `blur`
    event.
    
    ```
    ngModelOptions="{ updateOn: 'default blur', debounce: {'default': 500, 'blur':
    0} }"
    ```
    
    Closes #7205

--- test/ng/directive/inputSpec.js
+++ test/ng/directive/inputSpec.js
@@ -858,6 +858,25 @@ describe('input', function() {
       expect(scope.checkbox).toBe(false);
     }));
 
+    it('should allow selecting 0 for non-default debounce timeouts for each event on checkboxes', inject(function($timeout) {
+      compileInput('<input type="checkbox" ng-model="checkbox" '+
+        'ng-model-options="{ '+
+          'updateOn: \'default blur\', debounce: { default: 10000, blur: 0 } }"'+
+        '/>');
+
+      inputElm[0].checked = false;
+      browserTrigger(inputElm, 'click');
+      expect(scope.checkbox).toBe(undefined);
+      $timeout.flush(8000);
+      expect(scope.checkbox).toBe(undefined);
+      $timeout.flush(3000);
+      expect(scope.checkbox).toBe(true);
+      inputElm[0].checked = true;
+      browserTrigger(inputElm, 'click');
+      browserTrigger(inputElm, 'blur');
+      $timeout.flush(0);
+      expect(scope.checkbox).toBe(false);
+    }));
 
     it('should inherit model update settings from ancestor elements', inject(function($timeout) {
       var doc = $compile(
