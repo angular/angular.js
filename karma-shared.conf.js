39c0c67d3a3e2d003d432d71440fe97cf0ae89b3
vnesterovsky <vladimir@nesterovsky-bros.com>
2014-04-28 10:39:33 +0300

    task unit tests

--- karma-shared.conf.js
+++ karma-shared.conf.js
@@ -5,9 +5,9 @@ module.exports = function(config, specificOptions) {
     logLevel: config.LOG_INFO,
     logColors: true,
     browsers: ['Chrome'],
-    browserDisconnectTimeout: 10000,
+    browserDisconnectTimeout: 10000000,
     browserDisconnectTolerance: 2,
-    browserNoActivityTimeout: 20000,
+    browserNoActivityTimeout: 20000000,
 
 
     // SauceLabs config for local development.
