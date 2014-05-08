ebf874031b0453098f236a6f1479f0c3c57e25cd
Thomas Tuts <thomas@darwinanalytics.com>
2014-05-08 11:34:10 +0200

    style(core): make code formatting more consistent and legible
    This commit changes the code formatting of the Angular.js core to adhere
    more closely to Google's JavaScript style guide. This includes adding
    whitespace and blank lines where needed too. It also removes some
    unused variables, while renaming others to provide more clarity as to
    what it does (e.g. `ii` to `attributePrefixLength`).

--- src/minErr.js
+++ src/minErr.js
@@ -3,26 +3,24 @@
 /**
  * @description
  *
- * This object provides a utility for producing rich Error messages within
- * Angular. It can be called as follows:
+ * This object provides a utility for producing rich Error messages within Angular.
+ * It can be called as follows:
  *
  * var exampleMinErr = minErr('example');
  * throw exampleMinErr('one', 'This {0} is {1}', foo, bar);
  *
- * The above creates an instance of minErr in the example namespace. The
- * resulting error will have a namespaced error code of example.one.  The
- * resulting error will replace {0} with the value of foo, and {1} with the
- * value of bar. The object is not restricted in the number of arguments it can
- * take.
+ * The above creates an instance of minErr in the example namespace. The resulting error will have
+ * a namespaced error code of example.one. The resulting error will replace {0} with the value of
+ * foo, and {1} with the value of bar. The object is not restricted in the number of arguments it
+ * can take.
  *
- * If fewer arguments are specified than necessary for interpolation, the extra
- * interpolation markers will be preserved in the final string.
+ * If fewer arguments are specified than necessary for interpolation, the extra interpolation
+ * markers will be preserved in the final string.
  *
- * Since data will be parsed statically during a build step, some restrictions
- * are applied with respect to how minErr instances are created and called.
- * Instances should have names of the form namespaceMinErr for a minErr created
- * using minErr('namespace') . Error codes, namespaces and template strings
- * should all be static strings, not variables or general expressions.
+ * Since data will be parsed statically during a build step, some restrictions are applied with
+ * respect to how minErr instances are created and called. Instances should have names of the form
+ * namespaceMinErr for a minErr created using minErr('namespace'). Error codes, namespaces and
+ * template strings should all be static strings, not variables or general expressions.
  *
  * @param {string} module The namespace to use for the new minErr instance.
  * @returns {function(code:string, template:string, ...templateArgs): Error} minErr instance
@@ -43,10 +41,9 @@ function minErr(module) {
           return JSON.stringify(obj);
         }
         return obj;
-      },
-      message, i;
+      };
 
-    message = prefix + template.replace(/\{\d+\}/g, function (match) {
+    var message = prefix + template.replace(/\{\d+\}/g, function (match) {
       var index = +match.slice(1, -1), arg;
 
       if (index + 2 < templateArgs.length) {
@@ -65,7 +62,8 @@ function minErr(module) {
 
     message = message + '\nhttp://errors.angularjs.org/"NG_VERSION_FULL"/' +
       (module ? module + '/' : '') + code;
-    for (i = 2; i < arguments.length; i++) {
+
+    for (var i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
         encodeURIComponent(stringify(arguments[i]));
     }
