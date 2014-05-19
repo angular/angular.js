16647f29f2443ba01501f9834940a57e64398b7e
Vojta Jina <vojta.jina@gmail.com>
2014-05-19 23:43:40 +0200

    chore(travis/e2e): use Chrome on Mac
    Because of this[1] issue, which is Linux specific, we see very frequent failures.
    
    [1]: https://code.google.com/p/chromedriver/issues/detail?id=467

--- protractor-travis-conf.js
+++ protractor-travis-conf.js
@@ -5,6 +5,7 @@ config.sauceKey = process.env.SAUCE_ACCESS_KEY;
 
 config.multiCapabilities = [{
   'browserName': 'chrome',
+  'platform': 'OS X 10.9',
   'name': 'Angular E2E',
   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
   'build': process.env.TRAVIS_BUILD_NUMBER,
