4e1dae5a0ac627085778aeed7258e70707e1c18e
Julie <ju.ralph@gmail.com>
2014-05-22 17:42:37 -0700

    chore(tests): remote iit that sneaked into ngStyle

--- src/ng/directive/ngStyle.js
+++ src/ng/directive/ngStyle.js
@@ -36,7 +36,7 @@
      <file name="protractor.js" type="protractor">
        var colorSpan = element(by.css('span'));
 
-       iit('should check ng-style', function() {
+       it('should check ng-style', function() {
          expect(colorSpan.getCssValue('color')).toBe('rgba(0, 0, 0, 1)');
          element(by.css('input[value=\'set color\']')).click();
          expect(colorSpan.getCssValue('color')).toBe('rgba(255, 0, 0, 1)');
