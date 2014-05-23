dfc3e81d5c3cabc44fb4a0c774972b4c77851d9a
Peter Bacon Darwin <pete@bacondarwin.com>
2014-05-23 11:45:03 +0100

    fix(ngIf): ensure that the correct (transcluded) scope is used

--- test/ng/directive/ngIfSpec.js
+++ test/ng/directive/ngIfSpec.js
@@ -199,6 +199,25 @@ describe('ngIf and transcludes', function() {
       dealoc(element);
     });
   });
+
+
+  it('should use the correct transcluded scope', function() {
+    module(function($compileProvider) {
+      $compileProvider.directive('iso', valueFn({
+        restrict: 'E',
+        transclude: true,
+        template: '<div ng-if="true"><div ng-transclude></div></div>',
+        scope: {}
+      }));
+    });
+    inject(function($compile, $rootScope) {
+      $rootScope.val = 'transcluded content';
+      var element = $compile('<iso><span ng-bind="val"></span></iso>')($rootScope);
+      $rootScope.$digest();
+      expect(trim(element.text())).toEqual('transcluded content');
+      dealoc(element);
+    });
+  });
 });
 
 describe('ngIf animations', function () {
