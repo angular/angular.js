82d4f001378d5438217d14d4e809775b451b979c
genuinefafa <genuinefafa@gmail.com>
2014-05-24 21:38:51 +0300

    Update filters.js
    currencyFilter now accepts fractionSize so it behaves "similar" to a numberFilter, it even uses locale patterns

--- src/ng/filter/filters.js
+++ src/ng/filter/filters.js
@@ -50,9 +50,9 @@
 currencyFilter.$inject = ['$locale'];
 function currencyFilter($locale) {
   var formats = $locale.NUMBER_FORMATS;
-  return function(amount, currencySymbol){
+  return function(amount, currencySymbol, fractionSize){
     if (isUndefined(currencySymbol)) currencySymbol = formats.CURRENCY_SYM;
-    return formatNumber(amount, formats.PATTERNS[1], formats.GROUP_SEP, formats.DECIMAL_SEP, 2).
+    return formatNumber(amount, formats.PATTERNS[1], formats.GROUP_SEP, formats.DECIMAL_SEP, fractionSize).
                 replace(/\u00A4/g, currencySymbol);
   };
 }
