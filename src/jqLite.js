197b40918d4182cad9f4a1742b8049c85cc026eb
Dominic Watson <intellix@users.noreply.github.com>
2014-05-27 11:47:27 +0200

    Update jqLite.js
    The jQuery css() getter functionality utilises getComputedStyle() whereas jqLite only retrieves what is declared inline on an element.

--- src/jqLite.js
+++ src/jqLite.js
@@ -45,7 +45,7 @@
  * - [`children()`](http://api.jquery.com/children/) - Does not support selectors
  * - [`clone()`](http://api.jquery.com/clone/)
  * - [`contents()`](http://api.jquery.com/contents/)
- * - [`css()`](http://api.jquery.com/css/)
+ * - [`css()`](http://api.jquery.com/css/) - Getter function only retrieves inline-styles
  * - [`data()`](http://api.jquery.com/data/)
  * - [`empty()`](http://api.jquery.com/empty/)
  * - [`eq()`](http://api.jquery.com/eq/)
