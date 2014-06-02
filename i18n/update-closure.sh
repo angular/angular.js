36a0e59decffea3b5f175ee19a0934fcd1c2f80f
Caitlin Potter <caitpotter88@gmail.com>
2014-06-02 13:07:22 -0400

    chore(ngLocale): change update-closure script to pull from github, not code.google.com
    The closure-library migrated a while ago, so the script would fail. This is change
    should allow us to update these more frequently.
    
    Closes #7668

--- i18n/update-closure.sh
+++ i18n/update-closure.sh
@@ -7,9 +7,11 @@ cd $BASE_DIR
 
 set -x  # Trace commands as they're executed.
 
+I18N_BASE="https://raw.githubusercontent.com/google/closure-library/master/closure/goog/i18n"
+
 # use the github repo as it is more up to date than the svn repo
-curl https://closure-library.googlecode.com/git/closure/goog/i18n/currency.js > closure/currencySymbols.js
-curl https://closure-library.googlecode.com/git/closure/goog/i18n/datetimesymbols.js > closure/datetimeSymbols.js
-curl https://closure-library.googlecode.com/git/closure/goog/i18n/datetimesymbolsext.js > closure/datetimeSymbolsExt.js
-curl https://closure-library.googlecode.com/git/closure/goog/i18n/numberformatsymbols.js > closure/numberSymbols.js
-curl https://closure-library.googlecode.com/git/closure/goog/i18n/pluralrules.js > closure/pluralRules.js
+curl "$I18N_BASE/currency.js" > closure/currencySymbols.js
+curl "$I18N_BASE/datetimesymbols.js" > closure/datetimeSymbols.js
+curl "$I18N_BASE/datetimesymbolsext.js" > closure/datetimeSymbolsExt.js
+curl "$I18N_BASE/numberformatsymbols.js" > closure/numberSymbols.js
+curl "$I18N_BASE/pluralrules.js" > closure/pluralRules.js
