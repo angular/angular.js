05eaa6b63ba5a214988b07f27b5aad6bc62d0e92
Peter Bacon Darwin <pete@bacondarwin.com>
2014-06-06 14:59:23 +0100

    chore(docs): add button to link to stable docs
    Closes #7513

--- docs/app/src/versions.js
+++ docs/app/src/versions.js
@@ -4,11 +4,23 @@ angular.module('versions', [])
   $scope.docs_versions = NG_VERSIONS;
   $scope.docs_version  = NG_VERSIONS[0];
 
+  for (var i = 0; i < NG_VERSIONS.length; i++) {
+    if ( NG_VERSIONS[i].isStable ) {
+      $scope.docs_stable_version = NG_VERSIONS[i];
+      break;
+    }
+  }
+
+  $scope.getGroupName = function(v) {
+    return v.isStable ? 'Stable' : 'Unstable';
+  };
+
+
   $scope.jumpToDocsVersion = function(version) {
     var currentPagePath = $location.path();
 
     // TODO: We need to do some munging of the path for different versions of the API...
-    
+
 
     $window.location = version.docsUrl + currentPagePath;
   };
