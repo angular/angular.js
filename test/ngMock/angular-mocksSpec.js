2f0a4488731fdb0e8217325dbb52a576defd09bd
Shahar Talmi <shahar.talmi@gmail.com>
2014-06-08 21:31:17 +0300

    fix(injector): allow multiple loading of function modules
    Change HashMap to give $$hashKey also for functions so it will be possible to load multiple module
    function instances. In order to prevent problem in angular's test suite,  added an option to HashMap
    to maintain its own id counter and added cleanup of $$hashKey from all module functions after each
    test.
    
    Before this CL, functions were added to the HashMap via toString(), which could potentially return
    the same value for different actual instances of a function. This corrects this behaviour by
    ensuring that functions are mapped with hashKeys, and ensuring that hashKeys are removed from
    functions and objects at the end of tests.
    
    In addition to these changes, the injector uses its own set of UIDs in order to prevent confusingly
    breaking tests which expect scopes or ng-repeated items to have specific hash keys.
    
    Closes #7255

--- test/ngMock/angular-mocksSpec.js
+++ test/ngMock/angular-mocksSpec.js
@@ -808,6 +808,23 @@ describe('ngMock', function() {
             expect(example).toEqual('win');
           });
         });
+
+        describe('module cleanup', function() {
+          function testFn() {
+
+          }
+
+          it('should add hashKey to module function', function() {
+            module(testFn);
+            inject(function () {
+              expect(testFn.$$hashKey).toBeDefined();
+            });
+          });
+
+          it('should cleanup hashKey after previous test', function() {
+            expect(testFn.$$hashKey).toBeUndefined();
+          });
+        });
       });
 
       describe('in DSL', function() {
