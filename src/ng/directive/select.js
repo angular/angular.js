c557b7b8d83ed946f6c30772d4b3ab6b173233c7
Syu Kato <ukyo.web@gmail.com>
2014-06-13 19:12:56 +0900

    fix the protractor example(by.select to by.model).
    I changed to `by.model` because `by.select` is deprecated.

--- src/ng/directive/select.js
+++ src/ng/directive/select.js
@@ -123,7 +123,7 @@ var ngOptionsMinErr = minErr('ngOptions');
       <file name="protractor.js" type="protractor">
          it('should check ng-options', function() {
            expect(element(by.binding('{selected_color:myColor}')).getText()).toMatch('red');
-           element.all(by.select('myColor')).first().click();
+           element.all(by.model('myColor')).first().click();
            element.all(by.css('select[ng-model="myColor"] option')).first().click();
            expect(element(by.binding('{selected_color:myColor}')).getText()).toMatch('black');
            element(by.css('.nullable select[ng-model="myColor"]')).click();
