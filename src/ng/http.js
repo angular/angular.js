24d922f65ff92439b732be07fca487c373159613
Joey Yang <joey@joeyyang.com>
2014-06-16 15:03:42 -0700

    docs($http): fix broken markdown link in withCredentials description
    Markdown typo in $http config documentation

--- src/ng/http.js
+++ src/ng/http.js
@@ -481,7 +481,7 @@ function $HttpProvider() {
      *    - **timeout** – `{number|Promise}` – timeout in milliseconds, or {@link ng.$q promise}
      *      that should abort the request when resolved.
      *    - **withCredentials** - `{boolean}` - whether to set the `withCredentials` flag on the
-     *      XHR object. See [requests with credentials]https://developer.mozilla.org/en/http_access_control#section_5
+     *      XHR object. See [requests with credentials](https://developer.mozilla.org/en/http_access_control#section_5)
      *      for more information.
      *    - **responseType** - `{string}` - see
      *      [requestType](https://developer.mozilla.org/en-US/docs/DOM/XMLHttpRequest#responseType).
