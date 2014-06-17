angular/angular.js

commit c0b4e2db9cbc8bc3164cedc4646145d3ab72536e

fix(injector): invoke config blocks for module after all providers
This change ensures that a module's config blocks are always invoked after all of its providers are
registered.

BREAKING CHANGE:

Previously, config blocks would be able to control behaviour of provider registration, due to being
invoked prior to provider registration. Now, provider registration always occurs prior to configuration
for a given module, and therefore config blocks are not able to have any control over a providers
registration.

Example:

Previously, the following:

   angular.module('foo', [])
     .provider('$rootProvider', function() {
       this.$get = function() { ... }
     })
     .config(function($rootProvider) {
       $rootProvider.dependentMode = "B";
     })
     .provider('$dependentProvider', function($rootProvider) {
       if ($rootProvider.dependentMode === "A") {
         this.$get = function() {
           // Special mode!
         }
       } else {
         this.$get = function() {
           // something else
         }
       }
     });

would have "worked", meaning behaviour of the config block between the registration of "$rootProvider"
and "$dependentProvider" would have actually accomplished something and changed the behaviour of the
app. This is no longer possible within a single module.

Fixes #7139
Closes #7147

src/auto/injector.js
@@ -693,22 +693,27 @@ function createInjector(modulesToLoad, strictDi) {
   // Module Loading
   ////////////////////////////////////
   function loadModules(modulesToLoad){
-    var runBlocks = [], moduleFn, invokeQueue, i, ii;
+    var runBlocks = [], moduleFn, invokeQueue;
     forEach(modulesToLoad, function(module) {
       if (loadedModules.get(module)) return;
       loadedModules.put(module, true);
+
+      function runInvokeQueue(queue) {
+        var i, ii;
+        for(i = 0, ii = queue.length; i < ii; i++) {
+          var invokeArgs = queue[i],
+              provider = providerInjector.get(invokeArgs[0]);
+
+          provider[invokeArgs[1]].apply(provider, invokeArgs[2]);
+        }
+      }
 
       try {
         if (isString(module)) {
           moduleFn = angularModule(module);
           runBlocks = runBlocks.concat(loadModules(moduleFn.requires)).concat(moduleFn._runBlocks);
-
-          for(invokeQueue = moduleFn._invokeQueue, i = 0, ii = invokeQueue.length; i < ii; i++) {
-            var invokeArgs = invokeQueue[i],
-                provider = providerInjector.get(invokeArgs[0]);
-
-            provider[invokeArgs[1]].apply(provider, invokeArgs[2]);
-          }
+          runInvokeQueue(moduleFn._invokeQueue);
+          runInvokeQueue(moduleFn._configBlocks);
         } else if (isFunction(module)) {
             runBlocks.push(providerInjector.invoke(module));
         } else if (isArray(module)) {

test/loaderSpec.js
@@ -46,7 +46,6 @@ describe('module loader', function() {
     expect(myModule.requires).toEqual(['other']);
     expect(myModule._invokeQueue).toEqual([
       ['$provide', 'constant', ['abc', 123] ],
-      ['$injector', 'invoke', ['config'] ],
       ['$provide', 'provider', ['sk', 'sv'] ],
       ['$provide', 'factory', ['fk', 'fv'] ],
       ['$provide', 'service', ['a', 'aa'] ],
@@ -54,6 +53,9 @@ describe('module loader', function() {
       ['$filterProvider', 'register', ['f', 'ff'] ],
       ['$compileProvider', 'directive', ['d', 'dd'] ],
       ['$controllerProvider', 'register', ['ctrl', 'ccc']],
+    ]);
+    expect(myModule._configBlocks).toEqual([
+      ['$injector', 'invoke', ['config'] ],
       ['$injector', 'invoke', ['init2'] ]
     ]);
     expect(myModule._runBlocks).toEqual(['runBlock']);

src/loader.js
@@ -100,14 +100,18 @@ function setupModuleLoader(window) {
         var invokeQueue = [];
 
         /** @type {!Array.<Function>} */
+        var configBlocks = [];
+
+        /** @type {!Array.<Function>} */
         var runBlocks = [];
 
-        var config = invokeLater('$injector', 'invoke');
+        var config = invokeLater('$injector', 'invoke', 'push', configBlocks);
 
         /** @type {angular.Module} */
         var moduleInstance = {
           // Private state
           _invokeQueue: invokeQueue,
+          _configBlocks: configBlocks,
           _runBlocks: runBlocks,
 
           /**
@@ -299,9 +303,10 @@ function setupModuleLoader(window) {
          * @param {String=} insertMethod
          * @returns {angular.Module}
          */
-        function invokeLater(provider, method, insertMethod) {
+        function invokeLater(provider, method, insertMethod, queue) {
+          if (!queue) queue = invokeQueue;
           return function() {
-            invokeQueue[insertMethod || 'push']([provider, method, arguments]);
+            queue[insertMethod || 'push']([provider, method, arguments]);
             return moduleInstance;
           };
         }

test/auto/injectorSpec.js
@@ -308,6 +308,29 @@ describe('injector', function() {
       expect(log).toEqual('abABCD');
     });
 
+    it('should execute own config blocks after all own providers are invoked', function() {
+      var log = '';
+      angular.module('a', ['b'])
+      .config(function($aProvider) {
+        log += 'aConfig;';
+      })
+      .provider('$a', function() {
+        log += '$aProvider;';
+        this.$get = function() {};
+      });
+      angular.module('b', [])
+      .config(function($bProvider) {
+        log += 'bConfig;';
+      })
+      .provider('$b', function() {
+        log += '$bProvider;';
+        this.$get = function() {};
+      });
+
+      createInjector(['a']);
+      expect(log).toBe('$bProvider;bConfig;$aProvider;aConfig;');
+    });
+
     describe('$provide', function() {
 
       it('should throw an exception if we try to register a service called "hasOwnProperty"', function() {

commit ef4cd8dce39d2dddfa06b0b6b16ca669fee66d53

fix(loader): allow 'ng' module to be extended with filters/directives/etc
Due to a regression in c0b4e2d, it is no longer possible to add new controllers, directives,
filters, or animations to the `ng` module. This is becuase `config` blocks are always
evaluated after regular invoke blocks, and the registration of these types depend on angular's
config blocks to have been run.

This solution simply ensures that `ng`'s config blocks are run before its invoke blocks are run.

Closes #7709

src/auto/injector.js
@@ -712,8 +712,10 @@ function createInjector(modulesToLoad, strictDi) {
         if (isString(module)) {
           moduleFn = angularModule(module);
           runBlocks = runBlocks.concat(loadModules(moduleFn.requires)).concat(moduleFn._runBlocks);
+          var isCore = module === 'ng';
+          if (isCore) runInvokeQueue(moduleFn._configBlocks);
           runInvokeQueue(moduleFn._invokeQueue);
-          runInvokeQueue(moduleFn._configBlocks);
+          if (!isCore) runInvokeQueue(moduleFn._configBlocks);
         } else if (isFunction(module)) {
             runBlocks.push(providerInjector.invoke(module));
         } else if (isArray(module)) {

test/loaderSpec.js
@@ -84,4 +84,46 @@ describe('module loader', function() {
   it('should expose `$$minErr` on the `angular` object', function() {
     expect(window.angular.$$minErr).toEqual(jasmine.any(Function));
   });
+
+  describe('extending "ng" module', function() {
+    var rootElement, angular, run;
+    beforeEach(function() {
+      rootElement = jqLite('<div></div>');
+      jqLite(document.body).append(rootElement);
+      angular = window.angular;
+      publishExternalAPI(angular);
+      run = jasmine.createSpy('run block');
+    });
+
+    afterEach(function() {
+      expect(run).toHaveBeenCalledOnce();
+      rootElement.remove();
+      dealoc(rootElement);
+    });
+
+    it('should allow filters to be registered', function() {
+      run.andCallFake(function($filter) { expect($filter('noop')).toBe(noop); });
+      angularModule("ng").filter('noop', function() { return noop; }).run(['$filter', run]);
+      angular.bootstrap(rootElement, []);
+    });
+
+    it('should allow directives to be registered', function() {
+      var linkMe = jasmine.createSpy('linkMe');
+      run.andCallFake(function($compile, $rootScope) {
+        dealoc($compile('<div link-me></div>')($rootScope));
+        expect(linkMe).toHaveBeenCalledOnce();
+      });
+      angularModule("ng").directive('linkMe', valueFn(linkMe)).run(['$compile', '$rootScope', run]);
+      angular.bootstrap(rootElement, []);
+    });
+
+    it('should allow controllers to be registered', function() {
+      function Ctrl($scope) {}
+      run.andCallFake(function($controller, $rootScope) {
+        expect($controller('Ctrl', { $scope: $rootScope }) instanceof Ctrl).toBe(true);
+      });
+      angularModule("ng").controller('Ctrl', Ctrl).run(['$controller', '$rootScope', run]);
+      angular.bootstrap(rootElement, []);
+    });
+  });
 });