ae4997f272cd91211f947f7b0ed2db3b60f07a9c
Domenic Denicola <domenic@domenicdenicola.com>
2014-06-26 01:21:44 -0400

    docs($q): remove unnecessary $scope.apply wrapping
    As of Angular 1.2, this kind of thing is no longer necessary (thank goodness!)

--- src/ng/q.js
+++ src/ng/q.js
@@ -23,17 +23,13 @@
  *     var deferred = $q.defer();
  *
  *     setTimeout(function() {
- *       // since this fn executes async in a future turn of the event loop, we need to wrap
- *       // our code into an $apply call so that the model changes are properly observed.
- *       scope.$apply(function() {
- *         deferred.notify('About to greet ' + name + '.');
+ *       deferred.notify('About to greet ' + name + '.');
  *
- *         if (okToGreet(name)) {
- *           deferred.resolve('Hello, ' + name + '!');
- *         } else {
- *           deferred.reject('Greeting ' + name + ' is not allowed.');
- *         }
- *       });
+ *       if (okToGreet(name)) {
+ *         deferred.resolve('Hello, ' + name + '!');
+ *       } else {
+ *         deferred.reject('Greeting ' + name + ' is not allowed.');
+ *       }
  *     }, 1000);
  *
  *     return deferred.promise;
