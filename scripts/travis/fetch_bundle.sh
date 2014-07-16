3e55a015ab34623b84c96ef052378ba2a7d8bc7e
Caitlin Potter <caitpotter88@gmail.com>
2014-07-16 11:33:32 -0400

    revert: chore(travis): get npm dependencies from npm-bundle-deps
    This commit was causing some issues with CI testing, so it's being temporarily removed until that's
    resolved.
    
    This reverts commit 8881606cd93cc42cb8295c0d63dac7b81e6cc8cc.

--- scripts/travis/fetch_bundle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# normalize the working dir to the directory of the script
-cd $(dirname $0);
-
-cd ../..
-curl "http://23.251.144.68:8000/fetch-tar/$TRAVIS_REPO_SLUG/$TRAVIS_COMMIT" | tar xz
