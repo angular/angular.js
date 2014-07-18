3ba98186b8a50f4ee2c778cef5edfa95aa8c115c
Jeff Cross <middlefloor@gmail.com>
2014-07-17 18:54:03 -0700

    docs(input[date]): add note about lacking browser support

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -105,7 +105,9 @@ var inputType = {
      * @description
      * Input with date validation and transformation. In browsers that do not yet support
      * the HTML5 date input, a text element will be used. In that case, text must be entered in a valid ISO-8601
-     * date format (yyyy-MM-dd), for example: `2009-01-06`. The model must always be a Date object.
+     * date format (yyyy-MM-dd), for example: `2009-01-06`. Since many
+     * modern browsers do not yet support this input type, it is important to provide cues to users on the
+     * expected input format via a placeholder or label. The model must always be a Date object.
      *
      * @param {string} ngModel Assignable angular expression to data-bind to.
      * @param {string=} name Property name of the form under which the control is published.
