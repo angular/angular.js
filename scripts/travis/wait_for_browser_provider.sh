bb516fe43d40ff13f980dad543ccf9975a7c9f93
Julie Ralph <ju.ralph@gmail.com>
2014-07-28 15:53:46 -0700

    WIP - trying something else, logging in wait for browser provider

--- scripts/travis/wait_for_browser_provider.sh
+++ scripts/travis/wait_for_browser_provider.sh
@@ -3,5 +3,16 @@
 
 # Wait for Connect to be ready before exiting
 while [ ! -f $BROWSER_PROVIDER_READY_FILE ]; do
-  sleep .5
+  sleep 20
+
+  LOG_FILES=$LOGS_DIR/*
+
+  for FILE in $LOG_FILES; do
+    echo -e "\n\n\n"
+    echo "================================================================================"
+    echo " $FILE"
+    echo "================================================================================"
+    cat $FILE
+  done
+
 done
