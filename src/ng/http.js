428b81cba95ade2f9e76842302fae2fff9973cc5
k-funk <k-funk@users.noreply.github.com>
2014-07-30 16:41:15 -0700

    docs($http): add link to `$http.path()`
    Closes #8424

--- src/ng/http.js
+++ src/ng/http.js
@@ -275,6 +275,7 @@ function $HttpProvider() {
      * - {@link ng.$http#put $http.put}
      * - {@link ng.$http#delete $http.delete}
      * - {@link ng.$http#jsonp $http.jsonp}
+     * - {@link ng.$http#patch $http.patch}
      *
      *
      * # Setting HTTP Headers
