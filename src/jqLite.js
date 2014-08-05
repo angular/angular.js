a7e5d25bf15b6ffe7956c39f7a8ebf4d6b461162
Igor Minar <igor@angularjs.org>
2014-08-04 20:14:15 -0700

    refactor(jqLite): remove code duplication

--- src/jqLite.js
+++ src/jqLite.js
@@ -776,9 +776,9 @@ forEach({
       var eventFns = events[type];
 
       if (!eventFns) {
-        if (type == 'mouseenter' || type == 'mouseleave') {
-          events[type] = [];
+        events[type] = [];
 
+        if (type == 'mouseenter' || type == 'mouseleave') {
           // Refer to jQuery's implementation of mouseenter & mouseleave
           // Read about mouseenter and mouseleave:
           // http://www.quirksmode.org/js/events_mouse.html#link8
@@ -794,7 +794,6 @@ forEach({
 
         } else {
           addEventListenerFn(element, type, handle);
-          events[type] = [];
         }
         eventFns = events[type];
       }
