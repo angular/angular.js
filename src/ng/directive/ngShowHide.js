ed82385cf702d5508991a0d13d952c66d1ddd09c
Milan Nagy <123.wizek@gmail.com>
2014-08-07 11:49:21 +0200

    fix(ngShow): Make ng-show consistent with ng-if
    Consistent in terms of propagation. The expected behavior is that both
    are evaluated from the outside in.

--- src/ng/directive/ngShowHide.js
+++ src/ng/directive/ngShowHide.js
@@ -159,10 +159,12 @@ var ngShowDirective = ['$animate', function($animate) {
   return {
     restrict: 'A',
     multiElement: true,
-    link: function(scope, element, attr) {
-      scope.$watch(attr.ngShow, function ngShowWatchAction(value){
-        $animate[value ? 'removeClass' : 'addClass'](element, 'ng-hide');
-      });
+    link: {
+      pre: function(scope, element, attr) {
+        scope.$watch(attr.ngShow, function ngShowWatchAction(value){
+          $animate[value ? 'removeClass' : 'addClass'](element, 'ng-hide');
+        });
+      }
     }
   };
 }];
