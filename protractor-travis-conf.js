ed47d811e2c2a78ce1b358e87bac05ada2e54b52
Caitlin Potter <caitpotter88@gmail.com>
2014-08-08 16:37:28 -0400

    chore(travis): specify chrome m34 in protractor configuration
    Closes #8541

--- protractor-travis-conf.js
+++ protractor-travis-conf.js
@@ -7,9 +7,11 @@ config.sauceKey = process.env.SAUCE_ACCESS_KEY;
 
 config.multiCapabilities = [{
   'browserName': 'chrome',
+  'platform': 'OS X 10.9',
   'name': 'Angular E2E',
   'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-  'build': process.env.TRAVIS_BUILD_NUMBER
+  'build': process.env.TRAVIS_BUILD_NUMBER,
+  'version': '34'
 }, {
   'browserName': 'firefox',
   'name': 'Angular E2E',
