e6a9f9e1302c1bb8ac75b2e2b5fa9fc617862f7f
Igor Minar <igor@angularjs.org>
2014-08-08 11:08:07 -0700

    refactor: simplify trim fn now that IE9 has String#trim

--- src/Angular.js
+++ src/Angular.js
@@ -575,19 +575,9 @@ function isPromiseLike(obj) {
 }
 
 
-var trim = (function() {
-  // native trim is way faster: http://jsperf.com/angular-trim-test
-  // but IE doesn't have it... :-(
-  // TODO: we should move this into IE/ES5 polyfill
-  if (!String.prototype.trim) {
-    return function(value) {
-      return isString(value) ? value.replace(/^\s\s*/, '').replace(/\s\s*$/, '') : value;
-    };
-  }
-  return function(value) {
-    return isString(value) ? value.trim() : value;
-  };
-})();
+var trim = function(value) {
+  return isString(value) ? value.trim() : value;
+};
 
 
 /**
