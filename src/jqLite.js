443b521e22f9ec7009b913a2fe78caee0a515e87
Igor Minar <igor@angularjs.org>
2014-08-08 11:09:14 -0700

    perf(jqLite): don't check isString many times in constructor
    Note: no significant perf gain in Chrome

--- src/jqLite.js
+++ src/jqLite.js
@@ -226,17 +226,21 @@ function JQLite(element) {
   if (element instanceof JQLite) {
     return element;
   }
+
+  var argIsString;
+
   if (isString(element)) {
     element = trim(element);
+    argIsString = true;
   }
   if (!(this instanceof JQLite)) {
-    if (isString(element) && element.charAt(0) != '<') {
+    if (argIsString && element.charAt(0) != '<') {
       throw jqLiteMinErr('nosel', 'Looking up elements via selectors is not supported by jqLite! See: http://docs.angularjs.org/api/angular.element');
     }
     return new JQLite(element);
   }
 
-  if (isString(element)) {
+  if (argIsString) {
     jqLiteAddNodes(this, jqLiteParseHTML(element));
   } else {
     jqLiteAddNodes(this, element);
