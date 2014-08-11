d208ba254442649d35f96c76bcd9e47326ec59f3
Igor Minar <igor@angularjs.org>
2014-08-11 11:26:53 -0700

    perf(isObject): use strict comparison
    this is a micro-optimization based on http://jsperf.com/isobject4
    
    no significant improvement in macro-benchmarks, but since it makes the code better it makes
    sense making this change.

--- src/Angular.js
+++ src/Angular.js
@@ -441,7 +441,10 @@ function isDefined(value){return typeof value !== 'undefined';}
  * @param {*} value Reference to check.
  * @returns {boolean} True if `value` is an `Object` but not `null`.
  */
-function isObject(value){return value != null && typeof value === 'object';}
+function isObject(value){
+  // http://jsperf.com/isobject4
+  return value !== null && typeof value === 'object';
+}
 
 
 /**
