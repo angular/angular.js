ec27deacfd7ed0d7365405fdace85968fc890479
Izhaki <roey@gefri.org>
2014-08-20 14:28:58 +0100

    docs($compile): fix documentation for ?^ controller search
    Fixed typo: 'parents parents' to 'parents'
    
    Closes #8690

--- src/ng/compile.js
+++ src/ng/compile.js
@@ -192,7 +192,7 @@
  * * (no prefix) - Locate the required controller on the current element. Throw an error if not found.
  * * `?` - Attempt to locate the required controller or pass `null` to the `link` fn if not found.
  * * `^` - Locate the required controller by searching the element and its parents. Throw an error if not found.
- * * `?^` - Attempt to locate the required controller by searching the element and its parents parents or pass
+ * * `?^` - Attempt to locate the required controller by searching the element and its parents or pass
  *   `null` to the `link` fn if not found.
  *
  *
