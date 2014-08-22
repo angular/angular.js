ffbd276d6def6ff35bfdb30553346e985f4a0de6
Tobias Bosch <tbosch1009@gmail.com>
2014-08-22 11:44:36 -0700

    fix($compile): use the correct namespace for transcluded svg elements
    Via transclusion, svg elements can occur outside an `<svg>` container in an
    Angular template but are put into an `<svg>` container through compilation
    and linking.
    
    E.g.
    Given that `svg-container` is a transcluding directive with
    the following template:
    ```
    <svg ng-transclude></svg>
    ```
    
    The following markup creates a `<circle>` inside of an `<svg>` element
    during runtime:
    ```
    <svg-container>
      <circle></circle>
    </svg-container>
    ```
    
    However, this produces non working `<circle>` elements, as svg elements
    need to be created inside of an `<svg>` element.
    
    This change detects for most cases the correct namespace of transcluded content
    and recreates that content in the correct `<svg>` container
    when needed during compilation. For special cases it adds an addition argument
    to `$transclude` that allows to specify the future parent node of elements
    that will be cloned and attached using the `cloneAttachFn`.
    
    Related to #8494
    Closes #8716

--- test/ng/directive/ngIfSpec.js
+++ test/ng/directive/ngIfSpec.js
@@ -351,4 +351,23 @@ describe('ngIf animations', function () {
     });
   });
 
+  it('should work with svg elements when the svg container is transcluded', function() {
+    module(function($compileProvider) {
+      $compileProvider.directive('svgContainer', function() {
+        return {
+          template: '<svg ng-transclude></svg>',
+          replace: true,
+          transclude: true
+        };
+      });
+    });
+    inject(function($compile, $rootScope) {
+      element = $compile('<svg-container><circle ng-if="flag"></circle></svg-container>')($rootScope);
+      $rootScope.flag = true;
+      $rootScope.$apply();
+
+      var circle = element.find('circle');
+      expect(circle[0].toString()).toMatch(/SVG/);
+    });
+  });
 });
--- test/ng/directive/ngRepeatSpec.js
+++ test/ng/directive/ngRepeatSpec.js
@@ -1463,4 +1463,24 @@ describe('ngRepeat animations', function() {
     })
   );
 
+  it('should work with svg elements when the svg container is transcluded', function() {
+    module(function($compileProvider) {
+      $compileProvider.directive('svgContainer', function() {
+        return {
+          template: '<svg ng-transclude></svg>',
+          replace: true,
+          transclude: true
+        };
+      });
+    });
+    inject(function($compile, $rootScope) {
+      element = $compile('<svg-container><circle ng-repeat="r in rows"></circle></svg-container>')($rootScope);
+      $rootScope.rows = [1];
+      $rootScope.$apply();
+
+      var circle = element.find('circle');
+      expect(circle[0].toString()).toMatch(/SVG/);
+    });
+  });
+
 });
