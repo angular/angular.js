1324db46ef9691ab0ea8403d23b06ec79b72b6dc
Tobias Bosch <tbosch1009@gmail.com>
2014-08-26 15:23:47 -0700

    fix($browser): detect changes to the browser url that happened in sync
    Closes #6976.

--- src/ng/rootScope.js
+++ src/ng/rootScope.js
@@ -687,6 +687,8 @@ function $RootScopeProvider(){
             logIdx, logMsg, asyncTask;
 
         beginPhase('$digest');
+        // Check for changes to browser url that happened in sync with the call to $digest
+        $browser.$$checkUrlChangedOutsideAngular();
 
         lastDirtyWatch = null;
 
