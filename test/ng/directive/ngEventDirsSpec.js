1c2c4c7507fb5dcc2bfdd75613d823a66ce75d3c
Tobias Bosch <tbosch1009@gmail.com>
2014-08-27 15:29:39 -0700

    fix(ngEventDirs): execute `blur` and `focus` expression using `scope.$evalAsync`
    BREAKING CHANGE:
    The `blur` and `focus` event fire synchronously, also during DOM operations
    that remove elements. This lead to errors as the Angular model was not
    in a consistent state. See this [fiddle](http://jsfiddle.net/fq1dq5yb/) for a demo.
    
    This change executes the expression of those events using
    `scope.$evalAsync` if an `$apply` is in progress, otherwise
    keeps the old behavior.
    
    Fixes #4979
    Fixes #5945
    Closes #8803
    Closes #6910
    Closes #5402

--- test/ng/directive/ngEventDirsSpec.js
+++ test/ng/directive/ngEventDirsSpec.js
@@ -39,4 +39,64 @@ describe('event directives', function() {
       expect($rootScope.formSubmitted).toEqual('foo');
     }));
   });
+
+  describe('focus', function() {
+
+    it('should call the listener asynchronously during $apply',
+        inject(function($rootScope, $compile) {
+      element = $compile('<input type="text" ng-focus="focus()">')($rootScope);
+      $rootScope.focus = jasmine.createSpy('focus');
+
+      $rootScope.$apply(function() {
+        element.triggerHandler('focus');
+        expect($rootScope.focus).not.toHaveBeenCalled();
+      });
+
+      expect($rootScope.focus).toHaveBeenCalledOnce();
+    }));
+
+    it('should call the listener synchronously inside of $apply if outside of $apply',
+        inject(function($rootScope, $compile) {
+      element = $compile('<input type="text" ng-focus="focus()" ng-model="value">')($rootScope);
+      $rootScope.focus = jasmine.createSpy('focus').andCallFake(function() {
+        $rootScope.value = 'newValue';
+      });
+
+      element.triggerHandler('focus');
+
+      expect($rootScope.focus).toHaveBeenCalledOnce();
+      expect(element.val()).toBe('newValue');
+    }));
+
+  });
+
+  describe('blur', function() {
+
+    it('should call the listener asynchronously during $apply',
+        inject(function($rootScope, $compile) {
+      element = $compile('<input type="text" ng-blur="blur()">')($rootScope);
+      $rootScope.blur = jasmine.createSpy('blur');
+
+      $rootScope.$apply(function() {
+        element.triggerHandler('blur');
+        expect($rootScope.blur).not.toHaveBeenCalled();
+      });
+
+      expect($rootScope.blur).toHaveBeenCalledOnce();
+    }));
+
+    it('should call the listener synchronously inside of $apply if outside of $apply',
+        inject(function($rootScope, $compile) {
+      element = $compile('<input type="text" ng-blur="blur()" ng-model="value">')($rootScope);
+      $rootScope.blur = jasmine.createSpy('blur').andCallFake(function() {
+        $rootScope.value = 'newValue';
+      });
+
+      element.triggerHandler('blur');
+
+      expect($rootScope.blur).toHaveBeenCalledOnce();
+      expect(element.val()).toBe('newValue');
+    }));
+
+  });
 });
