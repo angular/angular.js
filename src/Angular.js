c305fdda8305c0fd4a9c631a434c1e738ca65926
Richard Harrington <rwharrington87@gmail.com>
2014-08-27 08:35:31 -0400

    docs(shallowCopy): add missing word 'are' and period.
    Closes #8794

--- src/Angular.js
+++ src/Angular.js
@@ -817,7 +817,7 @@ function copy(source, destination, stackSource, stackDest) {
 /**
  * Creates a shallow copy of an object, an array or a primitive.
  *
- * Assumes that there no proto properties for objects
+ * Assumes that there are no proto properties for objects.
  */
 function shallowCopy(src, dst) {
   if (isArray(src)) {
