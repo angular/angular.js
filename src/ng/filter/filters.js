a40b496ba2838cd7eb9c77910a77132742ebb0b2
Igor Minar <igor@angularjs.org>
2014-08-29 10:57:12 -0700

    fix(currencyFilter): pass through null and undefined values
    When these special values are passed through one-time binding will work correctly.
    
    BREAKING CHANGE: previously the currency filter would convert null and undefined values into empty string, after this change
    these values will be passed through.
    
    Only cases when the currency filter is chained with another filter that doesn't expect null/undefined will be affected. This
    should be very rare.
    
    This change will not change the visual output of the filter because the interpolation will convert the null/undefined to
    an empty string.
    
    Closes #8605

--- src/ng/filter/filters.js
+++ src/ng/filter/filters.js
@@ -53,8 +53,12 @@ function currencyFilter($locale) {
   var formats = $locale.NUMBER_FORMATS;
   return function(amount, currencySymbol){
     if (isUndefined(currencySymbol)) currencySymbol = formats.CURRENCY_SYM;
-    return formatNumber(amount, formats.PATTERNS[1], formats.GROUP_SEP, formats.DECIMAL_SEP, 2).
-                replace(/\u00A4/g, currencySymbol);
+
+    // if null or undefined pass it through
+    return (amount == null)
+        ? amount
+        : formatNumber(amount, formats.PATTERNS[1], formats.GROUP_SEP, formats.DECIMAL_SEP, 2).
+            replace(/\u00A4/g, currencySymbol);
   };
 }
 
