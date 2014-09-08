6639ca9d6bc00a6e3a31e54c50474361ae3561c6
Caitlin Potter <caitpotter88@gmail.com>
2014-09-08 14:39:41 -0400

    fix(ngInclude): correctly add svg-namespaced template content
    It is now possible for ngInclude to correctly load SVG content in non-blink browsers, which do not
    sort out the namespace when parsing HTML.
    
    Closes #7538
    Closes #8981
    Closes #8997

--- src/ng/directive/ngInclude.js
+++ src/ng/directive/ngInclude.js
@@ -267,6 +267,18 @@ var ngIncludeFillContentDirective = ['$compile',
       priority: -400,
       require: 'ngInclude',
       link: function(scope, $element, $attr, ctrl) {
+        if (/SVG/.test($element[0].toString())) {
+          // WebKit: https://bugs.webkit.org/show_bug.cgi?id=135698 --- SVG elements do not
+          // support innerHTML, so detect this here and try to generate the contents
+          // specially.
+          $element.empty();
+          $compile(jqLiteBuildFragment(ctrl.template, document).childNodes)(scope,
+              function namespaceAdaptedClone(clone) {
+            $element.append(clone);
+          }, undefined, undefined, $element);
+          return;
+        }
+
         $element.html(ctrl.template);
         $compile($element.contents())(scope);
       }
