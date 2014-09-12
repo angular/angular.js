3303fe41e478bc06e0a9b3a3c5497d283a3fc250
bolasblack <bolasblack@gmail.com>
2014-09-12 16:41:36 +0800

    docs(versions): remove the trailing slash in URLs when switch versions
    Because `https://docs.angularjs.org/api/` can handler the trailing slash,
    but `https://code.angularjs.org/1.2.24/docs/api` can not.
    
    Fix #9043
    Closes #9045

--- docs/app/src/versions.js
+++ docs/app/src/versions.js
@@ -19,7 +19,7 @@ angular.module('versions', [])
   };
 
   $scope.jumpToDocsVersion = function(version) {
-    var currentPagePath = $location.path();
+    var currentPagePath = $location.path().replace(/\/$/, '');
 
     // TODO: We need to do some munging of the path for different versions of the API...
 
