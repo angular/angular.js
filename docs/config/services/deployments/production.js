a62c0eab46435d5a7467978800846f004f08eec8
Peter Bacon Darwin <pete@bacondarwin.com>
2014-09-23 18:43:08 +0100

    chore(docs): refactor the docs app search for better bootup time
    This commit refactors how the search index is built. The docsSearch service
    is now defined by a provider, which returns a different implementation of
    the service depending upon whether the current browser supports WebWorkers
    or now.
    
    * **WebWorker supported**: The index is then built and stored in a new worker.
    The service posts and receives messages to and from this worker to make
    queries on the search index.
    
    * **WebWorker no supported**: The index is built locally but with a 500ms
    delay so that the initial page can render before the browser is blocked as
    the index is built.
    
    Also the way that the current app is identified has been modified so we can
    slim down the js data files (pages-data.js) to again improve startup time.
    
    Closes #9204
    Closes #9203

--- docs/config/services/deployments/production.js
+++ docs/config/services/deployments/production.js
@@ -29,6 +29,7 @@ module.exports = function productionDeployment(getVersion) {
       'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/lang-css.js',
       'js/versions-data.js',
       'js/pages-data.js',
+      'js/nav-data.js',
       'js/docs.js'
     ],
     stylesheets: [
