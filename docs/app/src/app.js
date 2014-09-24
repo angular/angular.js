8ab673d430c5895702ca9fdf99dba5327e65d04f
Peter Bacon Darwin <pete@bacondarwin.com>
2014-09-24 09:51:10 +0100

    chore(docs): ensure DI annotations are in place

--- docs/app/src/app.js
+++ docs/app/src/app.js
@@ -19,6 +19,6 @@ angular.module('docsApp', [
 ])
 
 
-.config(function($locationProvider) {
+.config(['$locationProvider', function($locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
-});
+}]);
