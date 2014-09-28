4bd8e6a7eff10e6bde2a3b955a5f46e375de6c20
Schmitt Christian <c.schmitt@briefdomain.de>
2014-09-28 14:09:38 +0200

    Fixed #9091: ngRoute should load the template after resolve
    Currently ngRoute will load the template while it is still resolving.
    This patch will stop that behavior and will resolve before the template gets loaded, so that people could change the template via:
     $route.current.template/$route.current.templateUrl, so you could build more powereful Applications with the default router. (loading a template based on the resolved resource)

--- src/ngRoute/route.js
+++ src/ngRoute/route.js
@@ -536,15 +536,21 @@ function $RouteProvider(){
         $q.when(next).
           then(function() {
             if (next) {
-              var locals = angular.extend({}, next.resolve),
-                  template, templateUrl;
+              var locals = angular.extend({}, next.resolve);
 
               angular.forEach(locals, function(value, key) {
                 locals[key] = angular.isString(value) ?
                     $injector.get(value) : $injector.invoke(value, null, null, key);
               });
 
-              if (angular.isDefined(template = next.template)) {
+              return $q.all(locals);
+            }
+          }).
+          then(function(locals) {
+           if(next) {
+            var template, templateUrl;
+            
+            if (angular.isDefined(template = next.template)) {
                 if (angular.isFunction(template)) {
                   template = template(next.params);
                 }
@@ -561,8 +567,9 @@ function $RouteProvider(){
               if (angular.isDefined(template)) {
                 locals['$template'] = template;
               }
-              return $q.all(locals);
-            }
+
+             return $q.all(locals);
+           }
           }).
           // after route change
           then(function(locals) {
