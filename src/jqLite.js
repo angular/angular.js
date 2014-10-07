4563b7a1291a23bce28b5ed69b958b263f8ae3ca
Rouven Weßling <me@rouvenwessling.de>
2014-10-07 22:55:54 +0200

    chore(IE8) Remove more special code for IE8

--- src/jqLite.js
+++ src/jqLite.js
@@ -274,11 +274,9 @@ function jqLiteClone(element) {
 function jqLiteDealoc(element, onlyDescendants) {
   if (!onlyDescendants) jqLiteRemoveData(element);
 
-  if (element.querySelectorAll) {
-    var descendants = element.querySelectorAll('*');
-    for (var i = 0, l = descendants.length; i < l; i++) {
-      jqLiteRemoveData(descendants[i]);
-    }
+  var descendants = element.querySelectorAll('*');
+  for (var i = 0, l = descendants.length; i < l; i++) {
+    jqLiteRemoveData(descendants[i]);
   }
 }
 
