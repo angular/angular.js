353bd4dd8791f17e3d94bde72daffafa0a171ffd
Peter Bacon Darwin <pete@bacondarwin.com>
2014-10-07 13:25:25 +0100

    feat($anchorScroll): add support for configurable scroll offset
    Add support for a configurable scroll offset to $anchorScrollProvider.
    
    Related to #9368
    Closes #2070
    Closes #9371

--- test/ng/anchorScrollSpec.js
+++ test/ng/anchorScrollSpec.js
@@ -1,4 +1,5 @@
 'use strict';
+
 describe('$anchorScroll', function() {
 
   var elmSpy;
@@ -6,15 +7,21 @@ describe('$anchorScroll', function() {
   function addElements() {
     var elements = sliceArgs(arguments);
 
-    return function() {
+    return function($window) {
       forEach(elements, function(identifier) {
         var match = identifier.match(/(\w* )?(\w*)=(\w*)/),
             jqElm = jqLite('<' + (match[1] || 'a ') + match[2] + '="' + match[3] + '"/>'),
             elm = jqElm[0];
 
         elmSpy[identifier] = spyOn(elm, 'scrollIntoView');
-        jqLite(document.body).append(jqElm);
+        jqLite($window.document.body).append(jqElm);
       });
+    };
+  }
+
+  function callAnchorScroll() {
+    return function ($anchorScroll) {
+      $anchorScroll();
     };
   }
 
@@ -22,6 +29,14 @@ describe('$anchorScroll', function() {
     return function($location, $anchorScroll) {
       $location.hash(hash);
       $anchorScroll();
+    };
+  }
+
+  function changeHashTo(hash) {
+    return function ($anchorScroll, $location, $rootScope) {
+      $rootScope.$apply(function() {
+        $location.hash(hash);
+      });
     };
   }
 
@@ -33,11 +48,22 @@ describe('$anchorScroll', function() {
     expect($window.scrollTo).toHaveBeenCalledWith(0, 0);
   }
 
-  function expectScrollingTo(identifier) {
+  function expectScrollingTo(identifierCountMap) {
+    var map = {};
+    if (isString(identifierCountMap)) {
+      map[identifierCountMap] = 1;
+    } else if (isArray(identifierCountMap)) {
+      forEach(identifierCountMap, function(identifier) {
+        map[identifier] = 1;
+      });
+    } else {
+      map = identifierCountMap;
+    }
+
     return function($window) {
       forEach(elmSpy, function(spy, id) {
-        if (identifier === id) expect(spy).toHaveBeenCalledOnce();
-        else expect(spy).not.toHaveBeenCalled();
+        var count = map[id] || 0;
+        expect(spy.calls.length).toBe(count);
       });
       expect($window.scrollTo).not.toHaveBeenCalled();
     };
@@ -52,8 +78,12 @@ describe('$anchorScroll', function() {
     elmSpy = {};
     $provide.value('$window', {
       scrollTo: jasmine.createSpy('$window.scrollTo'),
+      scrollBy: jasmine.createSpy('$window.scrollBy'),
       document: document,
-      navigator: {}
+      navigator: {},
+      getComputedStyle: function(elem) {
+        return getComputedStyle(elem);
+      }
     });
   }));
 
@@ -113,14 +143,6 @@ describe('$anchorScroll', function() {
       };
     }
 
-    function changeHashTo(hash) {
-      return function ($location, $rootScope, $anchorScroll) {
-        $rootScope.$apply(function() {
-          $location.hash(hash);
-        });
-      };
-    }
-
     function disableAutoScrolling() {
       return function($anchorScrollProvider) {
         $anchorScrollProvider.disableAutoScrolling();
@@ -172,7 +194,7 @@ describe('$anchorScroll', function() {
     });
 
 
-    it('should not scroll when disabled', function() {
+    it('should not scroll when auto-scrolling is disabled', function() {
       module(
           disableAutoScrolling(),
           initLocation({html5Mode: false, historyApi: false})
@@ -182,6 +204,204 @@ describe('$anchorScroll', function() {
         changeHashTo('fake'),
         expectNoScrolling()
       );
+    });
+
+
+    it('should scroll when called explicitly (even if auto-scrolling is disabled)', function() {
+      module(
+          disableAutoScrolling(),
+          initLocation({html5Mode: false, historyApi: false})
+      );
+      inject(
+        addElements('id=fake'),
+        changeHashTo('fake'),
+        expectNoScrolling(),
+        callAnchorScroll(),
+        expectScrollingTo('id=fake')
+      );
+    });
+  });
+
+  // TODO: Add tests for <body> with:
+  //       1. border/margin/padding !== 0
+  //       2. box-sizing === border-box
+  describe('yOffset', function() {
+
+    function expectScrollingWithOffset(identifierCountMap, offsetList) {
+      var list = isArray(offsetList) ? offsetList : [offsetList];
+
+      return function($window) {
+        expectScrollingTo(identifierCountMap)($window);
+        expect($window.scrollBy.calls.length).toBe(list.length);
+        forEach(list, function(offset, idx) {
+          expect($window.scrollBy.calls[idx].args).toEqual([0, -1 * offset]);
+        });
+      };
+    }
+
+    function expectScrollingWithoutOffset(identifierCountMap) {
+      return expectScrollingWithOffset(identifierCountMap, []);
+    }
+
+    function setupBodyForOffsetTesting() {
+      return function($window) {
+        var style = $window.document.body.style;
+        style.border = 'none';
+        style.margin = '0';
+        style.padding = '0';
+      };
+    }
+
+    function setYOffset(yOffset) {
+      return function ($anchorScroll) {
+        $anchorScroll.yOffset = yOffset;
+      };
+    }
+
+    beforeEach(inject(setupBodyForOffsetTesting()));
+
+    afterEach(inject(function($document) {
+      dealoc($document);
+    }));
+
+
+    describe('when set as a fixed number', function() {
+
+      var yOffsetNumber = 50;
+
+      beforeEach(inject(
+        setupBodyForOffsetTesting(),
+        setYOffset(yOffsetNumber)));
+
+
+      it('should scroll with vertical offset', inject(
+        addElements('id=some'),
+        changeHashTo('some'),
+        expectScrollingWithOffset('id=some', yOffsetNumber)));
+
+
+      it('should use the correct vertical offset when changing `yOffset` at runtime', inject(
+        addElements('id=some'),
+        changeHashTo('some'),
+        setYOffset(yOffsetNumber - 10),
+        callAnchorScroll(),
+        expectScrollingWithOffset({'id=some': 2}, [yOffsetNumber, yOffsetNumber - 10])));
+
+
+      it('should adjust the vertical offset for elements near the end of the page', function() {
+
+        var targetAdjustedOffset = 25;
+
+        inject(
+          addElements('id=some1', 'id=some2'),
+          function($window) {
+            // Make sure the elements are just a little shorter than the viewport height
+            var viewportHeight = $window.document.documentElement.clientHeight;
+            var elemHeight = viewportHeight - (yOffsetNumber - targetAdjustedOffset);
+            var cssText = [
+              'border:none',
+              'display:block',
+              'height:' + elemHeight + 'px',
+              'margin:0',
+              'padding:0',
+              ''].join(';');
+
+            forEach($window.document.body.children, function (elem) {
+              elem.style.cssText = cssText;
+            });
+
+            // Make sure scrolling does actually take place (it is necessary for this test)
+            forEach(elmSpy, function (spy, identifier) {
+              elmSpy[identifier] = spy.andCallThrough();
+            });
+          },
+          changeHashTo('some2'),
+          expectScrollingWithOffset('id=some2', targetAdjustedOffset));
+      });
+    });
+
+
+    describe('when set as a function', function() {
+
+      it('should scroll with vertical offset', function() {
+
+        var val = 0;
+        var increment = 10;
+
+        function yOffsetFunction() {
+          val += increment;
+          return val;
+        }
+
+        inject(
+          addElements('id=id1', 'name=name2'),
+          setYOffset(yOffsetFunction),
+          changeHashTo('id1'),
+          changeHashTo('name2'),
+          changeHashTo('id1'),
+          callAnchorScroll(),
+          expectScrollingWithOffset({
+            'id=id1': 3,
+            'name=name2': 1
+          }, [
+            1 * increment,
+            2 * increment,
+            3 * increment,
+            4 * increment
+          ]));
+      });
+    });
+
+
+    describe('when set as a jqLite element', function() {
+
+      function createAndSetYOffsetElement(styleSpecs) {
+        var cssText = '';
+        forEach(styleSpecs, function(value, key) {
+          cssText += key + ':' + value + ';';
+        });
+
+        var jqElem = jqLite('<div style="' + cssText + '"></div>');
+
+        return function ($anchorScroll, $window) {
+          jqLite($window.document.body).append(jqElem);
+          $anchorScroll.yOffset = jqElem;
+        };
+      }
+
+
+      it('should scroll with vertical offset when `top === 0`', inject(
+        createAndSetYOffsetElement({
+          background: 'DarkOrchid',
+          height: '50px',
+          position: 'fixed',
+          top: '0',
+        }),
+        addElements('id=some'),
+        changeHashTo('some'),
+        expectScrollingWithOffset('id=some', 50)));
+
+
+      it('should scroll with vertical offset when `top > 0`', inject(
+        createAndSetYOffsetElement({
+          height: '50px',
+          position: 'fixed',
+          top: '50px',
+        }),
+        addElements('id=some'),
+        changeHashTo('some'),
+        expectScrollingWithOffset('id=some', 100)));
+
+
+      it('should scroll without vertical offset when `position !== fixed`', inject(
+        createAndSetYOffsetElement({
+          height: '50px',
+          position: 'absolute',
+          top: '0',
+        }),
+        addElements('id=some'),
+        changeHashTo('some'),
+        expectScrollingWithoutOffset('id=some')));
     });
   });
 });
