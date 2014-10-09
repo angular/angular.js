angular/angular.js

commit 0d3b69a5f27b41745b504c7ffd8d72653bac1f85

fix($injector): throw when factory $get method does not return a value
BREAKING CHANGE:

Previously, not returning a value would fail silently, and an application trying to inject the
value owuld inject an undefined value, quite possibly leading to a TypeError. Now, the application
will fail entirely, and a reason will be given.

Closes #4575
Closes #9210

src/auto/injector.js
@@ -662,7 +662,21 @@ function createInjector(modulesToLoad, strictDi) {
     return providerCache[name + providerSuffix] = provider_;
   }
 
-  function factory(name, factoryFn) { return provider(name, { $get: factoryFn }); }
+  function enforceReturnValue(name, factory) {
+    return function enforcedReturnValue() {
+      var result = instanceInjector.invoke(factory);
+      if (isUndefined(result)) {
+        throw $injectorMinErr('undef', "Provider '{0}' must return a value from $get factory method.", name);
+      }
+      return result;
+    };
+  }
+
+  function factory(name, factoryFn, enforce) {
+    return provider(name, {
+      $get: enforce !== false ? enforceReturnValue(name, factoryFn) : factoryFn
+    });
+  }
 
   function service(name, constructor) {
     return factory(name, ['$injector', function($injector) {
@@ -670,7 +684,7 @@ function createInjector(modulesToLoad, strictDi) {
     }]);
   }
 
-  function value(name, val) { return factory(name, valueFn(val)); }
+  function value(name, val) { return factory(name, valueFn(val), false); }
 
   function constant(name, value) {
     assertNotHasOwnProperty(name, 'constant');

test/auto/injectorSpec.js
@@ -45,12 +45,12 @@ describe('injector', function() {
 //   s6
 
 
-    providers('s1', function() { log.push('s1'); }, {$inject: ['s2', 's5', 's6']});
-    providers('s2', function() { log.push('s2'); }, {$inject: ['s3', 's4', 's5']});
-    providers('s3', function() { log.push('s3'); }, {$inject: ['s6']});
-    providers('s4', function() { log.push('s4'); }, {$inject: ['s3', 's5']});
-    providers('s5', function() { log.push('s5'); });
-    providers('s6', function() { log.push('s6'); });
+    providers('s1', function() { log.push('s1'); return {}; }, {$inject: ['s2', 's5', 's6']});
+    providers('s2', function() { log.push('s2'); return {}; }, {$inject: ['s3', 's4', 's5']});
+    providers('s3', function() { log.push('s3'); return {}; }, {$inject: ['s6']});
+    providers('s4', function() { log.push('s4'); return {}; }, {$inject: ['s3', 's5']});
+    providers('s5', function() { log.push('s5'); return {}; });
+    providers('s6', function() { log.push('s6'); return {}; });
 
     injector.get('s1');
 
@@ -982,5 +982,15 @@ describe('strict-di injector', function() {
         $injector.invoke(['$test', function(test) {}]);
       }).toThrowMinErr('$injector', 'strictdi');
     });
+  });
+
+
+  it('should throw if factory does not return a value', function() {
+    module(function($provide) {
+      $provide.factory('$test', function() {});
+    });
+    expect(function() {
+      inject(function($test) {});
+    }).toThrowMinErr('$injector', 'undef');
   });
 });

docs/content/error/$injector/undef.ngdoc
@@ -1,0 +1,33 @@
+@ngdoc error
+@name $injector:undef
+@fullName Undefined Value
+@description
+
+This error results from registering a factory which does not return a value (or whose return value is undefined).
+
+The following is an example of a factory which will throw this error upon injection:
+
+```js
+angular.module("badModule", []).
+  factory("badFactory", function() {
+    doLotsOfThings();
+    butDontReturnAValue();
+  });
+```
+
+In order to prevent the error, return a value of some sort, such as an object which exposes an API for working
+with the injected object.
+
+```js
+angular.module("goodModule", []).
+  factory("goodFactory", function() {
+    doLotsOfThings();
+    butDontReturnAValue();
+
+    return {
+        doTheThing: function methodThatDoesAThing() {
+        }
+    };
+  });
+```
+

commit aa0bb8ffca7012a7d3f2af988ce06035d215f16d

fix($injector): ensure $get method invoked with provider context
0d3b69a5f27b41745b504c7ffd8d72653bac1f85 broke this by calling $get with an undefined
context, which in strict mode would be undefined. This fixes this by ensuring that the
provider is used as the context, as it was originally.

Closes #9511
Closes #9512

src/auto/injector.js
@@ -664,7 +664,7 @@ function createInjector(modulesToLoad, strictDi) {
 
   function enforceReturnValue(name, factory) {
     return function enforcedReturnValue() {
-      var result = instanceInjector.invoke(factory);
+      var result = instanceInjector.invoke(factory, this, undefined, name);
       if (isUndefined(result)) {
         throw $injectorMinErr('undef', "Provider '{0}' must return a value from $get factory method.", name);
       }

test/auto/injectorSpec.js
@@ -993,4 +993,21 @@ describe('strict-di injector', function() {
       inject(function($test) {});
     }).toThrowMinErr('$injector', 'undef');
   });
+
+
+  it('should always use provider as `this` when invoking a factory', function() {
+    // jshint -W040
+    var called = false;
+    function factoryFn() {
+      called = true;
+      expect(typeof this.$get).toBe('function');
+      return this;
+    }
+    module(function($provide) {
+      $provide.factory('$test', factoryFn);
+    });
+    inject(function($test) {});
+    expect(called).toBe(true);
+    // jshint +W040
+  });
 });