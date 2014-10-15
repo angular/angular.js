a0db029d48f9602770a203098d1cf83d8a3622cb
Caitlin Potter <caitpotter88@gmail.com>
2014-10-15 14:23:49 -0400

    WIP: also serve static files from test directories

--- test/e2e/tools/middleware.js
+++ test/e2e/tools/middleware.js
@@ -10,10 +10,16 @@ function middlewareFactory(base) {
   base = base || '/e2e';
   while (base.length && base[base.length-1] === '/') base = base.slice(0, base.length-1);
   var fixture_regexp = new RegExp('^' + base + '/tests/([a-zA-Z0-9_-]+)(/(index.html)?)?$');
+  var static_regexp = new RegExp('^' + base + '/tests/([a-zA-Z0-9_-]+)(/.*)$');
 
   return function(req, res, next) {
     var match;
-    if ((match = fixture_regexp.exec(req.url))) {
+    var basicUrl = req.url;
+    var idx = basicUrl.indexOf('?');
+    if (idx >= 0) {
+      basicUrl = basicUrl.slice(0, idx);
+    }
+    if ((match = fixture_regexp.exec(basicUrl))) {
       if (util.testExists(match[1])) {
         try {
           var query = url.parse(req.url, true).query;
@@ -23,8 +29,14 @@ function middlewareFactory(base) {
           return next(e);
         }
       } else {
-        return next(new Error('Test ' + match[1] + ' not found.'));
+        return next('Test ' + match[1] + ' not found.');
       }
+    } else if ((match = static_regexp.exec(basicUrl))) {
+      var rewritten = util.rewriteTestFile(match[1], match[2]);
+      if (rewritten !== false) {
+        req.url = rewritten;
+      }
+      next();
     } else {
       return next();
     }
--- test/e2e/tools/util.js
+++ test/e2e/tools/util.js
@@ -11,5 +11,16 @@ module.exports = {
   testExists: function(testname) {
     testname = path.resolve(tests, testname);
     return fs.statSync(testname).isDirectory();
+  },
+
+  rewriteTestFile: function(testname, testfile) {
+    var i = 0;
+    while (testfile[i] === '/') ++i;
+    testfile = testfile.slice(i);
+    var stat = fs.statSync(path.resolve(tests, testname, testfile));
+    if (stat && stat.isFile() || stat.isDirectory()) {
+      return ['/test/e2e/tests', testname, testfile].join('/');
+    }
+    return false;
   }
 };
