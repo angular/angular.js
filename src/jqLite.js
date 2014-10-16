284dfef812c205a6c4b2c8d24692a937d3908bae
Georgios Kalpakas <g.kalpakas@hotmail.com>
2014-10-16 09:08:07 +0300

    docs(jqLite): typo: `getComputedStyles()` -> `getComputedStyle()`

--- src/jqLite.js
+++ src/jqLite.js
@@ -44,7 +44,7 @@
  * - [`children()`](http://api.jquery.com/children/) - Does not support selectors
  * - [`clone()`](http://api.jquery.com/clone/)
  * - [`contents()`](http://api.jquery.com/contents/)
- * - [`css()`](http://api.jquery.com/css/) - Only retrieves inline-styles, does not call `getComputedStyles()`
+ * - [`css()`](http://api.jquery.com/css/) - Only retrieves inline-styles, does not call `getComputedStyle()`
  * - [`data()`](http://api.jquery.com/data/)
  * - [`detach()`](http://api.jquery.com/detach/)
  * - [`empty()`](http://api.jquery.com/empty/)
