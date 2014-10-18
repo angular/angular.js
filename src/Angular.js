fb17eaf5086b655f88a122a6247eb93aa2a3eee0
Wédney Yuri <wedneyyuri@gmail.com>
2014-10-17 22:19:24 -0300

    docs(angular.extend): explanation of deep copy.
    It is very common to see many developers confused about it.

--- src/Angular.js
+++ src/Angular.js
@@ -340,6 +340,7 @@ function setHashKey(obj, h) {
  * Extends the destination object `dst` by copying own enumerable properties from the `src` object(s)
  * to `dst`. You can specify multiple `src` objects. If you want to preserve original objects, you can do so
  * by passing an empty object as the target: `var object = angular.extend({}, object1, object2)`.
+ * Note: Keep in mind that `angular.extend` does not support recursive merge (deep copy).
  *
  * @param {Object} dst Destination object.
  * @param {...Object} src Source object(s).
