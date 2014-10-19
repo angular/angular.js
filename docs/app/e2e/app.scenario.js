22b817ec11f7ab1a81342a4b60acd644a3f2a8c3
Peter Bacon Darwin <pete@bacondarwin.com>
2014-10-19 10:28:52 +0100

    chore(docs): fix path to docs-app e2e tests
    These tests are not generated by dgeni and so needed to have the paths
    to the pages updated manually.
    
    Closes #9680

--- docs/app/e2e/app.scenario.js
+++ docs/app/e2e/app.scenario.js
@@ -39,7 +39,7 @@ describe('docs.angularjs.org', function () {
 
 
     it('should change the page content when clicking a link to a service', function () {
-      browser.get('');
+      browser.get('build/docs/index.html');
 
       var ngBindLink = element(by.css('.definition-table td a[href="api/ng/directive/ngClick"]'));
       ngBindLink.click();
@@ -51,33 +51,33 @@ describe('docs.angularjs.org', function () {
 
 
     it('should be resilient to trailing slashes', function() {
-      browser.get('index-debug.html#!/api/ng/function/angular.noop/');
+      browser.get('build/docs/index.html#!/api/ng/function/angular.noop/');
       var pageBody = element(by.css('h1'));
       expect(pageBody.getText()).toEqual('angular.noop');
     });
 
 
     it('should be resilient to trailing "index"', function() {
-      browser.get('index-debug.html#!/api/ng/function/angular.noop/index');
+      browser.get('build/docs/index.html#!/api/ng/function/angular.noop/index');
       var pageBody = element(by.css('h1'));
       expect(pageBody.getText()).toEqual('angular.noop');
     });
 
 
     it('should be resilient to trailing "index/"', function() {
-      browser.get('index-debug.html#!/api/ng/function/angular.noop/index/');
+      browser.get('build/docs/index.html#!/api/ng/function/angular.noop/index/');
       var pageBody = element(by.css('h1'));
       expect(pageBody.getText()).toEqual('angular.noop');
     });
 
 
     it('should display formatted error messages on error doc pages', function() {
-      browser.get('index-debug.html#!error/ng/areq?p0=Missing&p1=not%20a%20function,%20got%20undefined');
+      browser.get('build/docs/index.html#!error/ng/areq?p0=Missing&p1=not%20a%20function,%20got%20undefined');
       expect(element(by.css('.minerr-errmsg')).getText()).toEqual("Argument 'Missing' is not a function, got undefined");
     });
 
     it("should display an error if the page does not exist", function() {
-      browser.get('index-debug.html#!/api/does/not/exist');
+      browser.get('build/docs/index.html#!/api/does/not/exist');
       expect(element(by.css('h1')).getText()).toBe('Oops!');
     });
 
