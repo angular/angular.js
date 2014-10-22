e11d101f072cdbab342d810c330ce9d9609c7844
Cyril Lakech <cyril.lakech@adeoservices.com>
2014-10-22 16:22:03 +0200

    fixup! docs(form.FormController): add 'date' token to FormController $error property

--- src/ng/directive/form.js
+++ src/ng/directive/form.js
@@ -45,6 +45,10 @@ function nullFormRenameControl(control, name) {
  *  - `required`
  *  - `url`
  *  - `date`
+ *  - `datetimelocal`
+ *  - `time`
+ *  - `week`
+ *  - `month`
  *
  * @description
  * `FormController` keeps track of all its controls and nested forms as well as the state of them,
