cfe7b0e9efbcbc4d6dafef568895be265842c071
Peter Bacon Darwin <pete@bacondarwin.com>
2014-10-23 12:02:54 +0200

    docs(ngShowHide): use local bootstrap CSS

--- src/ng/directive/ngShowHide.js
+++ src/ng/directive/ngShowHide.js
@@ -112,7 +112,7 @@ var NG_HIDE_IN_PROGRESS_CLASS = 'ng-hide-animate';
       </div>
     </file>
     <file name="glyphicons.css">
-      @import url(//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css);
+      @import url(../../components/bootstrap-3.1.1/css/bootstrap.css);
     </file>
     <file name="animations.css">
       .animate-show {
@@ -277,7 +277,7 @@ var ngShowDirective = ['$animate', function($animate) {
       </div>
     </file>
     <file name="glyphicons.css">
-      @import url(//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css);
+      @import url(../../components/bootstrap-3.1.1/css/bootstrap.css);
     </file>
     <file name="animations.css">
       .animate-hide {
