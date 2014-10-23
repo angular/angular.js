ea5dea4d614b218cd807e0ae035a186d4eec5898
Anderson, Ben <ben.anderson@neustar.biz>
2014-10-23 13:59:30 -0400

    making the equals method work when the obj graph has circular dependencies

--- src/Angular.js
+++ src/Angular.js
@@ -825,18 +825,24 @@ function shallowCopy(src, dst) {
  * @param {*} o2 Object or value to compare.
  * @returns {boolean} True if arguments are equal.
  */
-function equals(o1, o2) {
+function equals(o1, o2, visitedObjArr) {
   if (o1 === o2) return true;
   if (o1 === null || o2 === null) return false;
   if (o1 !== o1 && o2 !== o2) return true; // NaN === NaN
   var t1 = typeof o1, t2 = typeof o2, length, key, keySet;
   if (t1 == t2) {
     if (t1 == 'object') {
+      if (typeof visitedObjArr == 'undefined') {
+        visitedObjArr = {};
+      } else {
+        if (o1 in visitedObjArr) return true;
+      }
+      visitedObjArr[o1] = true;
       if (isArray(o1)) {
         if (!isArray(o2)) return false;
         if ((length = o1.length) == o2.length) {
           for (key=0; key<length; key++) {
-            if (!equals(o1[key], o2[key])) return false;
+            if (!equals(o1[key], o2[key], visitedObjArr)) return false;
           }
           return true;
         }
@@ -850,7 +856,7 @@ function equals(o1, o2) {
         keySet = {};
         for (key in o1) {
           if (key.charAt(0) === '$' || isFunction(o1[key])) continue;
-          if (!equals(o1[key], o2[key])) return false;
+          if (!equals(o1[key], o2[key], visitedObjArr)) return false;
           keySet[key] = true;
         }
         for (key in o2) {
