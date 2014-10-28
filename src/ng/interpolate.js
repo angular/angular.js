91bb499e35a072ab32c4263ea89804561daf5954
Jason Bedard <jason+github@jbedard.ca>
2014-10-27 21:35:30 -0700

    perf($interpolate): provide a simplified result for constant expressions

--- src/ng/interpolate.js
+++ src/ng/interpolate.js
@@ -88,6 +88,19 @@ function $InterpolateProvider() {
       return '\\\\\\' + ch;
     }
 
+    //TODO: this is the same as the constantWatchDelegate in parse.js
+    function constantWatchDelegate(scope, listener, objectEquality, constantInterp) {
+      var unwatch;
+      return unwatch = scope.$watch(function constantInterpolateWatch(scope) {
+        return constantInterp(scope);
+      }, function constantInterpolateListener(value, old, scope) {
+        if (isFunction(listener)) {
+          listener.apply(this, arguments);
+        }
+        unwatch();
+      }, objectEquality);
+    }
+
     /**
      * @ngdoc service
      * @name $interpolate
@@ -183,6 +196,19 @@ function $InterpolateProvider() {
      * - `context`: evaluation context for all expressions embedded in the interpolated text
      */
     function $interpolate(text, mustHaveExpression, trustedContext, allOrNothing) {
+      // Provide a quick exit and simplified result function for text with no interpolation
+      if (!text.length || text.indexOf(startSymbol) === -1) {
+        var constantInterp;
+        if (!mustHaveExpression) {
+          var unescapedText = unescapeText(text);
+          constantInterp = function constantInterpolationFn() { return unescapedText; };
+          constantInterp.exp = text;
+          constantInterp.expressions = [];
+          constantInterp.$$watchDelegate = constantWatchDelegate;
+        }
+        return constantInterp;
+      }
+
       allOrNothing = !!allOrNothing;
       var startIndex,
           endIndex,
