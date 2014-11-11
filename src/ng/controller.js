c39eb3973eedd0c204506f5d1a97506aee68b11c
Henrique Ramos Limas <henrique.ramos.limas@gmail.com>
2014-11-11 20:45:28 -0200

    docs($controller): add controllerAs definition

--- src/ng/controller.js
+++ src/ng/controller.js
@@ -59,7 +59,7 @@ function $ControllerProvider() {
      *    * if $controllerProvider#allowGlobals, check `window[constructor]` on the global
      *      `window` object (not recommended)
      *
-     *    If use a string with `controller as` syntax, the controller instance is published into a `scope` 
+     *    If use a string with `controller as` syntax, the controller instance is published into a `scope`
      *    property, but the `scope` must be injected into `locals` param.
      *
      * @param {Object} locals Injection locals for Controller.
