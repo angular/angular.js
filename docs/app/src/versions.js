facb666ec98a5ab367be2133a2c5ee2be4e2e590
thorn0 <thorn.mailbox@gmail.com>
2014-11-14 03:13:31 +0200

    chore(docs): stop reflecting the discontinued stable/unstable distinction in the API version selector

--- docs/app/src/versions.js
+++ docs/app/src/versions.js
@@ -2,20 +2,9 @@ angular.module('versions', [])
 
 .controller('DocsVersionsCtrl', ['$scope', '$location', '$window', 'NG_VERSIONS', function($scope, $location, $window, NG_VERSIONS) {
   $scope.docs_version  = NG_VERSIONS[0];
-
-  for(var i=0, minor = NaN; i < NG_VERSIONS.length; i++) {
-    var version = NG_VERSIONS[i];
-    // NaN will give false here
-    if (minor <= version.minor) {
-      continue;
-    }
-    version.isLatest = true;
-    minor = version.minor;
-  }
-
   $scope.docs_versions = NG_VERSIONS;
   $scope.getGroupName = function(v) {
-    return v.isLatest ? 'Latest' : (v.isStable ? 'Stable' : 'Unstable');
+    return 'v' + v.major + '.' + v.minor + '.x';
   };
 
   $scope.jumpToDocsVersion = function(version) {
