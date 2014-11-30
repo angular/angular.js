961e9ee796c8cce1ea8ba6f43b3b84b498817392
Subra <subra@orangebrush.net>
2014-11-30 17:42:49 +0800

    fix(ngAria): handle non-string model values
    Fix for #10212. Compare the string value of the model
    so that aria-checked will be properly applied to
    type=radio in the case where the model is a number
    
    Closes #10212

--- src/ngAria/aria.js
+++ src/ngAria/aria.js
@@ -228,13 +228,13 @@ ngAriaModule.directive('ngShow', ['$aria', function($aria) {
         if (needsTabIndex) {
           needsTabIndex = false;
           return function ngAriaRadioReaction(newVal) {
-            var boolVal = newVal === attr.value;
+            var boolVal = (angular.isUndefined(newVal) ? newVal : newVal.toString()) === attr.value;
             elem.attr('aria-checked', boolVal);
             elem.attr('tabindex', 0 - !boolVal);
           };
         } else {
           return function ngAriaRadioReaction(newVal) {
-            elem.attr('aria-checked', newVal === attr.value);
+            elem.attr('aria-checked', (angular.isUndefined(newVal) ? newVal : newVal.toString()) === attr.value);
           };
         }
       }
