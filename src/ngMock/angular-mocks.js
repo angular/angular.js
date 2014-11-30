1730c94e7437b60530fe1a10918f90f0ef043877
Shahar Talmi <shahar.talmi@gmail.com>
2014-11-30 12:00:57 +0200

    fix(ngMock): annotate $RootScopeDecorator
    Closes #10273

--- src/ngMock/angular-mocks.js
+++ src/ngMock/angular-mocks.js
@@ -2047,7 +2047,7 @@ angular.mock.e2e.$httpBackendDecorator =
  *
  * In addition to all the regular `Scope` methods, the following helper methods are available:
  */
-angular.mock.$RootScopeDecorator = function($delegate) {
+angular.mock.$RootScopeDecorator = ['$delegate', function($delegate) {
 
   var $rootScopePrototype = Object.getPrototypeOf($delegate);
 
@@ -2119,7 +2119,7 @@ angular.mock.$RootScopeDecorator = function($delegate) {
 
     return count;
   }
-};
+}];
 
 
 if (window.jasmine || window.mocha) {
