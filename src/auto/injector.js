f5447461277a777ac2eecb4575b0ed558c69444d
ocombe <olivier.combe@gmail.com>
2014-12-02 01:19:38 +0100

    test($injector): tests for the caller name in injector errors

--- src/auto/injector.js
+++ src/auto/injector.js
@@ -668,7 +668,7 @@ function createInjector(modulesToLoad, strictDi) {
 
   function enforceReturnValue(name, factory) {
     return function enforcedReturnValue() {
-      var result = instanceInjector.invoke(factory, this, undefined, name);
+      var result = instanceInjector.invoke(factory, this);
       if (isUndefined(result)) {
         throw $injectorMinErr('undef', "Provider '{0}' must return a value from $get factory method.", name);
       }
