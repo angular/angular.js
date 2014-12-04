2db0aabee340f1e5554c0d321d263177600aff81
Vojta Jina <vojta.jina@gmail.com>
2014-12-03 16:00:55 -0800

    chore(travis): enable both SauceLabs and BrowserStack
    Setting env var `BROWSER_PROVIDER` to `browserstack` or `saucelabs`
    determines which browser provider will be used.
    
    This does not affect the build as all jobs are set to use SauceLabs.
    
    Switch to Karma with Socket.io 1.x, which solves some issues(*) with BS.
    Thus removing `config.transports` as it is not used anymore
    (Socket.io 1.x starts with polling and tries to upgrade if available).
    
    (*) folks from BS were fiddling with socket.io configuration to get it stable.
    See https://github.com/dhimil/karma/commit/4c04011850bf66a8a7556cd76ad662c568399481
    This is not necessary with Socket.io 1.x.

--- protractor-travis-conf.js
+++ protractor-travis-conf.js
@@ -2,32 +2,81 @@
 
 var config = require('./protractor-shared-conf').config;
 
-config.sauceUser = process.env.SAUCE_USERNAME;
-config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+if (process.env.BROWSER_PROVIDER === 'browserstack') {
+  // Using BrowserStack.
+  config.seleniumAddress = 'http://hub.browserstack.com/wd/hub';
+  config.multiCapabilities = [
+    capabilitiesForBrowserStack({
+      browserName: 'chrome',
+      platform: 'MAC',
+      version: '34'
+    }),
+    capabilitiesForBrowserStack({
+      browserName: 'firefox',
+      version: '28'
+    }),
+    capabilitiesForBrowserStack({
+      browserName: 'safari',
+      platform: 'MAC',
+      version: '7'
+    })
+  ];
+} else {
+  // Using SauceLabs.
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+  config.multiCapabilities = [
+    capabilitiesForSauceLabs({
+      browserName: 'chrome',
+      platform: 'OS X 10.9',
+      version: '34'
+    }),
+    capabilitiesForSauceLabs({
+      browserName: 'firefox',
+      version: '28'
+    }),
+    capabilitiesForSauceLabs({
+      browserName: 'safari',
+      platform: 'OS X 10.9',
+      version: '7'
+    })
+  ];
+}
 
-config.multiCapabilities = [{
-  'browserName': 'chrome',
-  'platform': 'OS X 10.9',
-  'name': 'Angular E2E',
-  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-  'build': process.env.TRAVIS_BUILD_NUMBER,
-  'version': '34'
-}, {
-  'browserName': 'firefox',
-  'name': 'Angular E2E',
-  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-  'build': process.env.TRAVIS_BUILD_NUMBER,
-  'version': '28'
-}, {
-  browserName: 'safari',
-  'platform': 'OS X 10.9',
-  'version': '7',
-  'name': 'Angular E2E',
-  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-  'build': process.env.TRAVIS_BUILD_NUMBER
-}];
 
 config.allScriptsTimeout = 30000;
 config.getPageTimeout = 30000;
 
 exports.config = config;
+
+
+function capabilitiesForBrowserStack(capabilities) {
+  return {
+    'browserstack.user': process.env.BROWSER_STACK_USERNAME,
+    'browserstack.key': process.env.BROWSER_STACK_ACCESS_KEY,
+    'browserstack.local' : 'true',
+    'browserstack.debug': 'true',
+    'browserstack.tunnelIdentifier': process.env.TRAVIS_JOB_NUMBER,
+    'tunnelIdentifier': process.env.TRAVIS_JOB_NUMBER,
+
+    'name': 'Angular E2E',
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+
+    'browserName': capabilities.browserName,
+    'platform': capabilities.platform,
+    'version': capabilities.version
+  };
+}
+
+function capabilitiesForSauceLabs(capabilities) {
+  return {
+    'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+
+    'name': 'Angular E2E',
+    'build': process.env.TRAVIS_BUILD_NUMBER,
+
+    'browserName': capabilities.browserName,
+    'platform': capabilities.platform,
+    'version': capabilities.version
+  };
+}
