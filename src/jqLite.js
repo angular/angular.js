98a0b4447b28df2d5705a31b8dc972f65e957efc
Caitlin Potter <caitpotter88@gmail.com>
2014-12-04 14:22:32 -0500

    feat($$jqLite): export jqLite as a private service
    This makes it easy to use jqLite's nicer class API (compared to jQuery) in modules
    like ngAnimate.

--- src/jqLite.js
+++ src/jqLite.js
@@ -1003,3 +1003,24 @@ forEach({
   JQLite.prototype.bind = JQLite.prototype.on;
   JQLite.prototype.unbind = JQLite.prototype.off;
 });
+
+
+// Provider for private $$jqLite service
+function $$jqLiteProvider() {
+  this.$get = function $$jqLite() {
+    return extend(JQLite, {
+      hasClass: function(node, classes) {
+        if (node.attr) node = node[0];
+        return jqLiteHasClass(node, classes);
+      },
+      addClass: function(node, classes) {
+        if (node.attr) node = node[0];
+        return jqLiteAddClass(node, classes);
+      },
+      removeClass: function(node, classes) {
+        if (node.attr) node = node[0];
+        return jqLiteRemoveClass(node, classes);
+      }
+    });
+  };
+}
