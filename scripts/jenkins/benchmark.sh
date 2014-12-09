d28c9e44dec2c10a87bc458ef06202bf6007f8fc
Jeff Cross <middlefloor@gmail.com>
2014-12-08 21:08:40 -0800

    WIP: add jenkins script

---
+++ scripts/jenkins/benchmark.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+npm install --color false
+grunt build --no-color
+grunt bp_build --no-color
+./node_modules/karma/bin/karma start karma-benchpress.conf.js --single-run=true --no-colors
