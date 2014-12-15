8b56c08327054f978ee2c42106cb60e41bd8c13b
Todd Skinner <todd.skinner@gmail.com>
2014-12-15 01:13:43 -0800

    docs(angular.copy): fix grammar
    Closes #10459

--- src/Angular.js
+++ src/Angular.js
@@ -655,7 +655,7 @@ function arrayRemove(array, value) {
  * Creates a deep copy of `source`, which should be an object or an array.
  *
  * * If no destination is supplied, a copy of the object or array is created.
- * * If a destination is provided, all of its elements (for array) or properties (for objects)
+ * * If a destination is provided, all of its elements (for arrays) or properties (for objects)
  *   are deleted and then all elements/properties from the source are copied to it.
  * * If `source` is not an object or array (inc. `null` and `undefined`), `source` is returned.
  * * If `source` is identical to 'destination' an exception will be thrown.
