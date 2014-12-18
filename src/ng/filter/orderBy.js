68e17ca7f816a42f6b4d7c30c0ea44594b3cdd0b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-12-18 19:16:24 +0100

    fix(orderBy): compare timestamps when sorting date objects
    Fixes #10512

--- src/ng/filter/orderBy.js
+++ src/ng/filter/orderBy.js
@@ -172,12 +172,12 @@ function orderByFilter($parse) {
 
     function objectToString(value) {
       if (value === null) return 'null';
-      if (typeof value.toString === 'function') {
-        value = value.toString();
-        if (isPrimitive(value)) return value;
-      }
       if (typeof value.valueOf === 'function') {
         value = value.valueOf();
+        if (isPrimitive(value)) return value;
+      }
+      if (typeof value.toString === 'function') {
+        value = value.toString();
         if (isPrimitive(value)) return value;
       }
       return '';
