ba23021dc8440fd18d4b53a77bc991cc56d84720
Marcin Wosinek <marcin.wosinek@gmail.com>
2014-12-19 10:27:10 +0000

    feat($time): create time service
    Add simple $time service to allow easier mocking dates in applications.
    
    Closes #10402

--- angularFiles.js
+++ angularFiles.js
@@ -37,6 +37,7 @@ var angularFiles = {
     'src/ng/sniffer.js',
     'src/ng/templateRequest.js',
     'src/ng/testability.js',
+    'src/ng/date.js',
     'src/ng/timeout.js',
     'src/ng/urlUtils.js',
     'src/ng/window.js',
