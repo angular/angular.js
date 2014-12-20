2caec4463290d1c331c8a3f3bcaa20fc9873e5c2
Shahar Talmi <shahar.talmi@gmail.com>
2014-12-20 18:04:28 +0200

    refactor(limitTo): no need for all those checks if we use slice
    Closes #10537

--- src/ng/filter/limitTo.js
+++ src/ng/filter/limitTo.js
@@ -97,34 +97,11 @@ function limitToFilter() {
       limit = int(limit);
     }
 
-    if (isString(input)) {
-      //NaN check on limit
-      if (limit) {
-        return limit >= 0 ? input.slice(0, limit) : input.slice(limit, input.length);
-      } else {
-        return "";
-      }
-    }
-
-    var i, n;
-
-    // if abs(limit) exceeds maximum length, trim it
-    if (limit > input.length)
-      limit = input.length;
-    else if (limit < -input.length)
-      limit = -input.length;
-
-    if (limit > 0) {
-      i = 0;
-      n = limit;
+    //NaN check on limit
+    if (limit) {
+      return limit > 0 ? input.slice(0, limit) : input.slice(limit);
     } else {
-      // zero and NaN check on limit - return empty array
-      if (!limit) return [];
-
-      i = input.length + limit;
-      n = input.length;
+      return isString(input) ? "" : [];
     }
-
-    return input.slice(i, n);
   };
 }
