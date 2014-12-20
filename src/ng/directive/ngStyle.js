a7cc066c789a55fe89fba36122a51080991fecb2
gdi2290 <github@gdi2290.com>
2014-12-19 17:11:25 -0800

    perf(ngStyleDirective): use $watchCollection
    Since we are simply watching a flat object collection it is more performant
    to use $watchCollection than a deepWatch...
    
    BREAKING CHANGE:
    
    Previously the use of deep watch by ng-style would trigger styles to be
    re-applied when nested state changed. Now only changes to direct
    properties of the watched object will trigger changes.
    
    Closes #15947

--- src/ng/directive/ngStyle.js
+++ src/ng/directive/ngStyle.js
@@ -52,10 +52,10 @@
    </example>
  */
 var ngStyleDirective = ngDirective(function(scope, element, attr) {
-  scope.$watch(attr.ngStyle, function ngStyleWatchAction(newStyles, oldStyles) {
+  scope.$watchCollection(attr.ngStyle, function ngStyleWatchAction(newStyles, oldStyles) {
     if (oldStyles && (newStyles !== oldStyles)) {
       forEach(oldStyles, function(val, style) { element.css(style, '');});
     }
     if (newStyles) element.css(newStyles);
-  }, true);
+  });
 });
