c3afde62db8ce58670135120d4d985f62d2bc1d9
OKNoah <OKNoah@users.noreply.github.com>
2014-12-24 13:55:10 -0800

    docs($http): describe your change...
    adds missing markdown formatter (backtick)

--- src/ng/http.js
+++ src/ng/http.js
@@ -347,7 +347,7 @@ function $HttpProvider() {
      * To add or overwrite these defaults, simply add or remove a property from these configuration
      * objects. To add headers for an HTTP method other than POST or PUT, simply add a new object
      * with the lowercased HTTP method name as the key, e.g.
-     * `$httpProvider.defaults.headers.get = { 'My-Header' : 'value' }.
+     * `$httpProvider.defaults.headers.get = { 'My-Header' : 'value' }`.
      *
      * The defaults can also be set at runtime via the `$http.defaults` object in the same
      * fashion. For example:
