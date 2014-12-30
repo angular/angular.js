9241b9e9038e8360002e46bf2d9591630e3fc289
Lucas Galfaso <lgalfaso@gmail.com>
2014-12-30 21:34:27 +0100

    chore(safari): workaround Safari async css updates
    Workaround Safari CSS async updates

--- test/helpers/privateMocksSpec.js
+++ test/helpers/privateMocksSpec.js
@@ -5,33 +5,40 @@ describe('private mocks', function() {
 
     it('should allow custom styles to be created and removed when the stylesheet is destroyed',
       inject(function($compile, $document, $window, $rootElement, $rootScope) {
+        var elm;
+        var stylesheet;
+        runs(function() {
+          var doc = $document[0];
+          var count = doc.styleSheets.length;
+          stylesheet = createMockStyleSheet($document, $window);
+          expect(doc.styleSheets.length).toBe(count + 1);
 
-      var doc = $document[0];
-      var count = doc.styleSheets.length;
-      var stylesheet = createMockStyleSheet($document, $window);
-      expect(doc.styleSheets.length).toBe(count + 1);
+          angular.element(doc.body).append($rootElement);
 
-      angular.element(doc.body).append($rootElement);
+          elm = $compile('<div class="padded">...</div>')($rootScope);
+          $rootElement.append(elm);
 
-      var elm = $compile('<div class="padded">...</div>')($rootScope);
-      $rootElement.append(elm);
+          expect(getStyle(elm, 'paddingTop')).toBe('0px');
 
-      expect(getStyle(elm, 'paddingTop')).toBe('0px');
+          stylesheet.addRule('.padded', 'padding-top:2px');
+        });
 
-      stylesheet.addRule('.padded', 'padding-top:2px');
+        waitsFor(function() {
+          return getStyle(elm, 'paddingTop') === '2px';
+        });
 
-      expect(getStyle(elm, 'paddingTop')).toBe('2px');
+        runs(function() {
+          stylesheet.destroy();
 
-      stylesheet.destroy();
+          expect(getStyle(elm, 'paddingTop')).toBe('0px');
+        });
 
-      expect(getStyle(elm, 'paddingTop')).toBe('0px');
-
-      function getStyle(element, key) {
-        var node = element[0];
-        return node.currentStyle ?
-          node.currentStyle[key] :
-          $window.getComputedStyle(node)[key];
-      }
+        function getStyle(element, key) {
+          var node = element[0];
+          return node.currentStyle ?
+            node.currentStyle[key] :
+            $window.getComputedStyle(node)[key];
+        }
     }));
 
   });
