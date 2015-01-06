83ba6299df1a94c6a5e29e1b0e0faf73523ff5ab
Palmer <jesselpalmer@gmail.com>
2015-01-05 21:50:10 -0500

    docs(app): makes minor updates

--- docs/app/src/footer-controller.js
+++ docs/app/src/footer-controller.js
@@ -3,7 +3,9 @@ angular.
     controller('FooterController', FooterController);
 
 function FooterController() {
+  var vm = this;
   var v = angular.version;
-  this.versionNumber = v.full;
-  this.version = v.full + '  ' + v.codeName;
+
+  vm.versionNumber = v.full;
+  vm.version = v.full + '  ' + v.codeName;
 }
