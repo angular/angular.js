3abb3fefe653df2a4cb730cface0049939c18efd
bluepnume <chuntheunavoidable@gmail.com>
2015-01-09 05:05:29 -0800

    fix($q): Use extend to avoid overwriting prototype
    Use `extend` on `Promise.prototype` and `Deferred.prototype`, to avoid having to
    manually set `constructor` on the overwritten prototypes.
    
    Closes #10697

--- src/ng/q.js
+++ src/ng/q.js
@@ -272,7 +272,7 @@ function qFactory(nextTick, exceptionHandler) {
     this.$$state = { status: 0 };
   }
 
-  Promise.prototype = {
+  extend(Promise.prototype, {
     then: function(onFulfilled, onRejected, progressBack) {
       var result = new Deferred();
 
@@ -294,7 +294,7 @@ function qFactory(nextTick, exceptionHandler) {
         return handleCallback(error, false, callback);
       }, progressBack);
     }
-  };
+  });
 
   //Faster, more basic than angular.bind http://jsperf.com/angular-bind-vs-custom-vs-native
   function simpleBind(context, fn) {
@@ -341,7 +341,7 @@ function qFactory(nextTick, exceptionHandler) {
     this.notify = simpleBind(this, this.notify);
   }
 
-  Deferred.prototype = {
+  extend(Deferred.prototype, {
     resolve: function(val) {
       if (this.promise.$$state.status) return;
       if (val === this.promise) {
@@ -404,7 +404,7 @@ function qFactory(nextTick, exceptionHandler) {
         });
       }
     }
-  };
+  });
 
   /**
    * @ngdoc method
