6e69b85f9a6570872b776ed937099add1f12b6cf
thorn0 <thorn.mailbox@gmail.com>
2015-01-17 18:05:27 +0200

    docs(angular.bootstrap): passed fns are called on config stage
    Closes #10789

--- src/Angular.js
+++ src/Angular.js
@@ -1338,7 +1338,7 @@ function angularInit(element, bootstrap) {
  * @param {DOMElement} element DOM element which is the root of angular application.
  * @param {Array<String|Function|Array>=} modules an array of modules to load into the application.
  *     Each item in the array should be the name of a predefined module or a (DI annotated)
- *     function that will be invoked by the injector as a run block.
+ *     function that will be invoked by the injector as a `config` block.
  *     See: {@link angular.module modules}
  * @param {Object=} config an object for defining configuration options for the application. The
  *     following keys are supported:
