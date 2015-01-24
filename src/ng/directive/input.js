80a72f0937dc347c458e7944ece502523bba764f
Mark Hoffmeyer <mhoffmeyerDC@users.noreply.github.com>
2015-01-23 20:00:59 -0500

    Update input.js
    Update tests to reflect new model.

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -918,14 +918,14 @@ var inputType = {
         </file>
         <file name="protractor.js" type="protractor">
           it('should change state', function() {
-            var value1 = element(by.binding('value1'));
-            var value2 = element(by.binding('value2'));
+            var value1 = element(by.binding('checkboxModel.value1'));
+            var value2 = element(by.binding('checkboxModel.value2'));
 
             expect(value1.getText()).toContain('true');
             expect(value2.getText()).toContain('YES');
 
-            element(by.model('value1')).click();
-            element(by.model('value2')).click();
+            element(by.model('checkboxModel.value1')).click();
+            element(by.model('checkboxModel.value2')).click();
 
             expect(value1.getText()).toContain('false');
             expect(value2.getText()).toContain('NO');
