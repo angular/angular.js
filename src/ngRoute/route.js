b462f5dcf233a202c4e904c7587feec8401e5990
Caitlin Potter <caitpotter88@gmail.com>
2015-01-29 13:16:31 -0500

    docs($compile,$route): reword "controller alias" to clarify
    Clarify that these aliases are identifier names used to reference the controller.

--- src/ngRoute/route.js
+++ src/ngRoute/route.js
@@ -73,8 +73,8 @@ function $RouteProvider() {
    *    - `controller` – `{(string|function()=}` – Controller fn that should be associated with
    *      newly created scope or the name of a {@link angular.Module#controller registered
    *      controller} if passed as a string.
-   *    - `controllerAs` – `{string=}` – A controller alias name. If present the controller will be
-   *      published to scope under the `controllerAs` name.
+   *    - `controllerAs` – `{string=}` – An identifier name for a reference to the controller.
+   *      If present, the controller will be published to scope under the `controllerAs` name.
    *    - `template` – `{string=|function()=}` – html template as a string or a function that
    *      returns an html template as a string which should be used by {@link
    *      ngRoute.directive:ngView ngView} or {@link ng.directive:ngInclude ngInclude} directives.
