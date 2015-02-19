d8c81f8591f7723306787e4d559fbb34111b09e5
Dav <davidj.utbm@gmail.com>
2015-02-19 18:09:59 +0100

    removing trailing whitespace

--- test/ng/filter/filterSpec.js
+++ test/ng/filter/filterSpec.js
@@ -428,7 +428,7 @@ describe('Filter: filter', function() {
   });
 
 
-  it ('should not throw an error if property is null when comparing object', function() {
+  it('should not throw an error if property is null when comparing object', function() {
       var items = [
           { office:1, people: {name:'john'}},
           { office:2, people: {name:'jane'}},
@@ -436,22 +436,22 @@ describe('Filter: filter', function() {
       ];
       var f = { };
       expect(filter(items, f).length).toBe(3);
-      
+
       f = { people:null };
       expect(filter(items, f).length).toBe(3);
-      
+
       //should throw an error in 1.3.12
       f = { people:{ name:'john' }};
       expect(filter(items, f).length).toBe(1);
-      
+
       //should throw an error in 1.3.12
       f = { people:{ name:'j' }};
       expect(filter(items, f).length).toBe(2);
-      
+
       //should throw an error in 1.3.12
       f = { people:{ name: '' }};
       expect(filter(items, f).length).toBe(2);
-      
+
   });
 
   describe('should support comparator', function() {
