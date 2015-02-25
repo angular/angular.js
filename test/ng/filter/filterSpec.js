01161a0e9fb1af93e9f06535aed8392ed7f116a4
Dav <davidj.utbm@gmail.com>
2015-02-25 13:08:06 +0100

    fix(filterFilter): do not throw an error if property is null when comparing objects
    Closes #10991
    Closes #10992
    Closes #11116

--- test/ng/filter/filterSpec.js
+++ test/ng/filter/filterSpec.js
@@ -399,6 +399,33 @@ describe('Filter: filter', function() {
   });
 
 
+  it('should not throw an error if property is null when comparing object', function() {
+      var items = [
+          { office:1, people: {name:'john'}},
+          { office:2, people: {name:'jane'}},
+          { office:3, people: null}
+      ];
+      var f = { };
+      expect(filter(items, f).length).toBe(3);
+
+      f = { people:null };
+      expect(filter(items, f).length).toBe(1);
+
+      f = { people: {}};
+      expect(filter(items, f).length).toBe(2);
+
+      f = { people:{ name: '' }};
+      expect(filter(items, f).length).toBe(2);
+
+      f = { people:{ name:'john' }};
+      expect(filter(items, f).length).toBe(1);
+
+      f = { people:{ name:'j' }};
+      expect(filter(items, f).length).toBe(2);
+
+  });
+
+
   describe('should support comparator', function() {
 
     it('not consider `object === "[object Object]"` in non-strict comparison', function() {
