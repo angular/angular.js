620086f5883d39f60994af6d64b5f26c10f49ddd
Shahar Talmi <shahar.talmi@gmail.com>
2015-03-02 20:52:58 +0200

    feat($cookies): move logic into $cookies and deprecate $cookieStore
    The new API on `$cookies` includes:
    
     * `get`
     * `put`
     * `getObject`
     * `putObject`
     * `getAll`
     * `remove`
    
    The new API no longer polls the browser for changes to the cookies and no longer copy
    cookie values onto the `$cookies` object.
    
    The polling is expensive and caused issues with the `$cookies` properties not
    synchronizing correctly with the actual browser cookie values.
    
    The reason the polling was originally added was to allow communication between
    different tabs, but there are better ways to do this today (for example `localStorage`).
    
    DEPRECATION NOTICE:
    
    `$cookieStore` is now deprecated as all the useful logic
    has been moved to `$cookies`, to which `$cookieStore` now simply
    delegates calls.
    
    BREAKING CHANGE:
    
    `$cookies` no longer exposes properties that represent the current browser cookie
    values. Now you must explicitly the methods described above to access the cookie
    values. This also means that you can no longer watch the `$cookies` properties for
    changes to the browser's cookies.
    
    This feature is generally only needed if a 3rd party library was programmatically
    changing the cookies at runtime. If you rely on this then you must either write code that
    can react to the 3rd party library making the changes to cookies or implement your own polling
    mechanism.
    
    Closes #6411
    Closes #7631

--- src/ngCookies/cookies.js
+++ src/ngCookies/cookies.js
@@ -25,9 +25,9 @@ angular.module('ngCookies', ['ng']).
    * @description
    * Provides read/write access to browser's cookies.
    *
-   * Only a simple Object is exposed and by adding or removing properties to/from this object, new
-   * cookies are created/deleted at the end of current $eval.
-   * The object's properties can only be strings.
+   * BREAKING CHANGE: `$cookies` no longer exposes properties that represent the
+   * current browser cookie values. Now you must use the get/put/remove/etc. methods
+   * as described below.
    *
    * Requires the {@link ngCookies `ngCookies`} module to be installed.
    *
@@ -37,87 +37,95 @@ angular.module('ngCookies', ['ng']).
    * angular.module('cookiesExample', ['ngCookies'])
    *   .controller('ExampleController', ['$cookies', function($cookies) {
    *     // Retrieving a cookie
-   *     var favoriteCookie = $cookies.myFavorite;
+   *     var favoriteCookie = $cookies.get('myFavorite');
    *     // Setting a cookie
-   *     $cookies.myFavorite = 'oatmeal';
+   *     $cookies.put('myFavorite', 'oatmeal');
    *   }]);
    * ```
    */
-   factory('$cookies', ['$rootScope', '$browser', '$$cookieReader', '$$cookieWriter', function($rootScope, $browser, $$cookieReader, $$cookieWriter) {
-      var cookies = {},
-          lastCookies = {},
-          lastBrowserCookies,
-          runEval = false,
-          copy = angular.copy,
-          isUndefined = angular.isUndefined;
+   factory('$cookies', ['$$cookieReader', '$$cookieWriter', function($$cookieReader, $$cookieWriter) {
+      return {
+        /**
+         * @ngdoc method
+         * @name $cookies#get
+         *
+         * @description
+         * Returns the value of given cookie key
+         *
+         * @param {string} key Id to use for lookup.
+         * @returns {string} Raw cookie value.
+         */
+        get: function(key) {
+          return $$cookieReader()[key];
+        },
 
-      //creates a poller fn that copies all cookies from the $browser to service & inits the service
-      $browser.addPollFn(function() {
-        var currentCookies = $$cookieReader();
-        if (lastBrowserCookies != currentCookies) { //relies on browser.cookies() impl
-          lastBrowserCookies = currentCookies;
-          copy(currentCookies, lastCookies);
-          copy(currentCookies, cookies);
-          if (runEval) $rootScope.$apply();
+        /**
+         * @ngdoc method
+         * @name $cookies#getObject
+         *
+         * @description
+         * Returns the deserialized value of given cookie key
+         *
+         * @param {string} key Id to use for lookup.
+         * @returns {Object} Deserialized cookie value.
+         */
+        getObject: function(key) {
+          var value = $$cookieReader()[key];
+          return value ? angular.fromJson(value) : value;
+        },
+
+        /**
+         * @ngdoc method
+         * @name $cookies#getAll
+         *
+         * @description
+         * Returns a key value object with all the cookies
+         *
+         * @returns {Object} All cookies
+         */
+        getAll: function() {
+          return $$cookieReader();
+        },
+
+        /**
+         * @ngdoc method
+         * @name $cookies#put
+         *
+         * @description
+         * Sets a value for given cookie key
+         *
+         * @param {string} key Id for the `value`.
+         * @param {string} value Raw value to be stored.
+         */
+        put: function(key, value) {
+          $$cookieWriter(key, value);
+        },
+
+        /**
+         * @ngdoc method
+         * @name $cookies#putObject
+         *
+         * @description
+         * Serializes and sets a value for given cookie key
+         *
+         * @param {string} key Id for the `value`.
+         * @param {Object} value Value to be stored.
+         */
+        putObject: function(key, value) {
+          $$cookieWriter(key, angular.toJson(value));
+        },
+
+        /**
+         * @ngdoc method
+         * @name $cookies#remove
+         *
+         * @description
+         * Remove given cookie
+         *
+         * @param {string} key Id of the key-value pair to delete.
+         */
+        remove: function(key) {
+          $$cookieWriter(key, undefined);
         }
-      })();
-
-      runEval = true;
-
-      //at the end of each eval, push cookies
-      //TODO: this should happen before the "delayed" watches fire, because if some cookies are not
-      //      strings or browser refuses to store some cookies, we update the model in the push fn.
-      $rootScope.$watch(push);
-
-      return cookies;
-
-
-      /**
-       * Pushes all the cookies from the service to the browser and verifies if all cookies were
-       * stored.
-       */
-      function push() {
-        var name,
-            value,
-            browserCookies,
-            updated;
-
-        //delete any cookies deleted in $cookies
-        for (name in lastCookies) {
-          if (isUndefined(cookies[name])) {
-            $$cookieWriter(name, undefined);
-          }
-        }
-
-        //update all cookies updated in $cookies
-        for (name in cookies) {
-          value = cookies[name];
-          if (!angular.isString(value)) {
-            value = '' + value;
-            cookies[name] = value;
-          }
-          if (value !== lastCookies[name]) {
-            $$cookieWriter(name, value);
-            updated = true;
-          }
-        }
-
-        //verify what was actually stored
-        if (updated) {
-          updated = false;
-          browserCookies = $$cookieReader();
-
-          for (name in cookies) {
-            if (cookies[name] !== browserCookies[name]) {
-              //delete or reset all cookies that the browser dropped from $cookies
-              if (isUndefined(browserCookies[name])) {
-                delete cookies[name];
-              } else {
-                cookies[name] = browserCookies[name];
-              }
-              updated = true;
-            }
-          }
-        }
-      }
+      };
     }]);
--- test/ngCookies/cookiesSpec.js
+++ test/ngCookies/cookiesSpec.js
@@ -4,93 +4,65 @@ describe('$cookies', function() {
   var mockedCookies;
 
   beforeEach(function() {
-    var lastCookies = {};
-    mockedCookies = {preexisting:'oldCookie'};
+    mockedCookies = {};
     module('ngCookies', {
       $$cookieWriter: function(name, value) {
         mockedCookies[name] = value;
       },
       $$cookieReader: function() {
-        if (!angular.equals(lastCookies, mockedCookies)) {
-          lastCookies = angular.copy(mockedCookies);
-          mockedCookies = angular.copy(mockedCookies);
-        }
         return mockedCookies;
       }
     });
   });
 
 
-  it('should provide access to existing cookies via object properties and keep them in sync',
-      inject(function($cookies, $browser, $rootScope) {
-    expect($cookies).toEqual({'preexisting': 'oldCookie'});
-
-    // access internal cookie storage of the browser mock directly to simulate behavior of
-    // document.cookie
-    mockedCookies['brandNew'] = 'cookie';
-    $browser.poll();
-
-    expect($cookies).toEqual({'preexisting': 'oldCookie', 'brandNew':'cookie'});
-
-    mockedCookies['brandNew'] = 'cookie2';
-    $browser.poll();
-    expect($cookies).toEqual({'preexisting': 'oldCookie', 'brandNew':'cookie2'});
-
-    delete mockedCookies['brandNew'];
-    $browser.poll();
-    expect($cookies).toEqual({'preexisting': 'oldCookie'});
+  it('should serialize objects to json', inject(function($cookies) {
+    $cookies.putObject('objectCookie', {id: 123, name: 'blah'});
+    expect($cookies.get('objectCookie')).toEqual('{"id":123,"name":"blah"}');
   }));
 
 
-  it('should create or update a cookie when a value is assigned to a property',
-      inject(function($cookies, $$cookieReader, $rootScope) {
-    $cookies.oatmealCookie = 'nom nom';
-    $rootScope.$digest();
-
-    expect($$cookieReader()).
-      toEqual({'preexisting': 'oldCookie', 'oatmealCookie':'nom nom'});
-
-    $cookies.oatmealCookie = 'gone';
-    $rootScope.$digest();
-
-    expect($$cookieReader()).
-      toEqual({'preexisting': 'oldCookie', 'oatmealCookie': 'gone'});
+  it('should deserialize json to object', inject(function($cookies) {
+    $cookies.put('objectCookie', '{"id":123,"name":"blah"}');
+    expect($cookies.getObject('objectCookie')).toEqual({id: 123, name: 'blah'});
   }));
 
 
-  it('should convert non-string values to string',
-      inject(function($cookies, $$cookieReader, $rootScope) {
-    $cookies.nonString = [1, 2, 3];
-    $cookies.nullVal = null;
-    $cookies.undefVal = undefined;
-    var preexisting = $cookies.preexisting = function() {};
-    $rootScope.$digest();
-    expect($$cookieReader()).toEqual({
-      'preexisting': '' + preexisting,
-      'nonString': '1,2,3',
-      'nullVal': 'null',
-      'undefVal': 'undefined'
-    });
-    expect($cookies).toEqual({
-      'preexisting': '' + preexisting,
-      'nonString': '1,2,3',
-      'nullVal': 'null',
-      'undefVal': 'undefined'
-    });
+  it('should delete objects from the store when remove is called', inject(function($cookies) {
+    $cookies.putObject('gonner', { "I'll":"Be Back"});
+    expect($cookies.get('gonner')).toEqual('{"I\'ll":"Be Back"}');
+    $cookies.remove('gonner');
+    expect($cookies.get('gonner')).toEqual(undefined);
   }));
 
 
-  it('should remove a cookie when a $cookies property is deleted',
-      inject(function($cookies, $browser, $rootScope, $$cookieReader) {
-    $cookies.oatmealCookie = 'nom nom';
-    $rootScope.$digest();
-    $browser.poll();
-    expect($$cookieReader()).
-      toEqual({'preexisting': 'oldCookie', 'oatmealCookie':'nom nom'});
-
-    delete $cookies.oatmealCookie;
-    $rootScope.$digest();
-
-    expect($$cookieReader()).toEqual({'preexisting': 'oldCookie'});
+  it('should handle empty string value cookies', inject(function($cookies) {
+    $cookies.putObject("emptyCookie",'');
+    expect($cookies.get('emptyCookie')).toEqual('""');
+    expect($cookies.getObject("emptyCookie")).toEqual('');
+    mockedCookies['blankCookie'] = '';
+    expect($cookies.getObject("blankCookie")).toEqual('');
   }));
-});
+
+
+  it('should put cookie value without serializing', inject(function($cookies) {
+    $cookies.put('name', 'value');
+    $cookies.put('name2', '"value2"');
+    expect($cookies.get('name')).toEqual('value');
+    expect($cookies.getObject('name2')).toEqual('value2');
+  }));
+
+
+  it('should get cookie value without deserializing', inject(function($cookies) {
+    $cookies.put('name', 'value');
+    $cookies.putObject('name2', 'value2');
+    expect($cookies.get('name')).toEqual('value');
+    expect($cookies.get('name2')).toEqual('"value2"');
+  }));
+
+  it('should get all the cookies', inject(function($cookies) {
+    $cookies.put('name', 'value');
+    $cookies.putObject('name2', 'value2');
+    expect($cookies.getAll()).toEqual({name: 'value', name2: '"value2"'});
+  }));
+ });
