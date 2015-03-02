0e765cbc2c80cf661feb0a98050e6afc8a3694b5
Shahar Talmi <shahar.talmi@gmail.com>
2015-03-02 21:46:51 +0200

    feat($cookies): allow passing cookie options
    The `put`, `putObject` and `remove` methods now take an options parameter
    where you can provide additional options for the cookie value, such as `expires`,
    `path`, `domain` and `secure`.
    
    Closes #8324
    Closes #3988
    Closes #1786
    Closes #950

--- src/ngCookies/cookieWriter.js
+++ src/ngCookies/cookieWriter.js
@@ -9,31 +9,47 @@
  *
  * @param {string} name Cookie name
  * @param {string=} value Cookie value (if undefined, cookie will be deleted)
+ * @param {Object=} options Object with options that need to be stored for the cookie.
  */
 function $$CookieWriter($document, $log, $browser) {
   var cookiePath = $browser.baseHref();
   var rawDocument = $document[0];
 
-  return function(name, value) {
+  function buildCookieString(name, value, options) {
+    var path, expires;
+    options = options || {};
+    expires = options.expires;
+    path = angular.isDefined(options.path) ? options.path : cookiePath;
     if (value === undefined) {
-      rawDocument.cookie = encodeURIComponent(name) + "=;path=" + cookiePath +
-                              ";expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    } else {
-      if (angular.isString(value)) {
-        var cookieLength = (rawDocument.cookie = encodeURIComponent(name) + '=' + encodeURIComponent(value) +
-                              ';path=' + cookiePath).length + 1;
-
-        // per http://www.ietf.org/rfc/rfc2109.txt browser must allow at minimum:
-        // - 300 cookies
-        // - 20 cookies per unique domain
-        // - 4096 bytes per cookie
-        if (cookieLength > 4096) {
-          $log.warn("Cookie '" + name +
-            "' possibly not set or overflowed because it was too large (" +
-            cookieLength + " > 4096 bytes)!");
-        }
-      }
+      expires = 'Thu, 01 Jan 1970 00:00:00 GMT';
+      value = '';
     }
+    if (angular.isString(expires)) {
+      expires = new Date(expires);
+    }
+
+    var str = encodeURIComponent(name) + '=' + encodeURIComponent(value);
+    str += path ? ';path=' + path : '';
+    str += options.domain ? ';domain=' + options.domain : '';
+    str += expires ? ';expires=' + expires.toUTCString() : '';
+    str += options.secure ? ';secure' : '';
+
+    // per http://www.ietf.org/rfc/rfc2109.txt browser must allow at minimum:
+    // - 300 cookies
+    // - 20 cookies per unique domain
+    // - 4096 bytes per cookie
+    var cookieLength = str.length + 1;
+    if (cookieLength > 4096) {
+      $log.warn("Cookie '" + name +
+        "' possibly not set or overflowed because it was too large (" +
+        cookieLength + " > 4096 bytes)!");
+    }
+
+    return str;
+  }
+
+  return function(name, value, options) {
+    rawDocument.cookie = buildCookieString(name, value, options);
   };
 }
 
