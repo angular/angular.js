ca2d742f0415ba7167870d4bd3672a4de5cfbe33
Martin Staffa <mjstaffa@googlemail.com>
2015-03-13 18:46:18 +0100

    wip-chore: refactor npm install

---
+++ Gruntfile-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var npmInstall = require('./scripts/npm/install-dependencies');
+
+module.exports = function(grunt) {
+
+  grunt.registerTask('npm-install', function() {
+    npmInstall.installDependencies();
+  });
+}
