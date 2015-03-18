ca16116e75aeafe3dc7bb06344956e8fd03d3422
Fred Sauer <fredsa@google.com>
2015-03-18 13:57:08 -0700

    fix(ngAria): ensure boolean values for aria-hidden and aria-disabled
    aria-hidden should mirror the boolean representation of their ng-*
    counterpart (ng-show, ng-hide) instead of their actual value. Same
    applies to aria-disabled and ng-disabled
    
    Closes #11365

--- src/ngAria/aria.js
+++ src/ngAria/aria.js
@@ -115,9 +115,8 @@ function $AriaProvider() {
       var ariaCamelName = attr.$normalize(ariaAttr);
       if (config[ariaCamelName] && !attr[ariaCamelName]) {
         scope.$watch(attr[attrName], function(boolVal) {
-          if (negate) {
-            boolVal = !boolVal;
-          }
+          // ensure boolean value
+          boolVal = negate ? !boolVal : !!boolVal;
           elem.attr(ariaAttr, boolVal);
         });
       }
