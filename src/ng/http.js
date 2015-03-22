20ffe2ada4c3d6cd186104a8e5c98dd11b2eda9c
Bradley Price <bradleyprice@gmail.com>
2015-03-22 04:47:08 -0500

    docs($http): remove trailing comma
    Remove trailing comma to keep the same flow with all other code examples on page.

--- src/ng/http.js
+++ src/ng/http.js
@@ -377,7 +377,7 @@ function $HttpProvider() {
      *  headers: {
      *    'Content-Type': undefined
      *  },
-     *  data: { test: 'test' },
+     *  data: { test: 'test' }
      * }
      *
      * $http(req).success(function(){...}).error(function(){...});
