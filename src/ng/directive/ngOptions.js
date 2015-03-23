42c97c5db5921e9e5447fb32bdae1f48da42844f
Martin Staffa <mjstaffa@googlemail.com>
2015-03-23 23:05:47 +0100

    fix(ngOptions): prevent frozen select ui in IE
    In certain scenarios, IE10/11/Edge create unresponsive select elements.
    The following contribute to the bug:
    - There need to be at least 2 selects next to each other
    - The option elements are added via javascript
    - the option.value is accessed before it is set
    - the option.label is added after the option.value has been set
    - The first select is wrappend in an element with display: inline or
    display: inline-block,
    
    This cannot be tested in a unit-test or e2e test.
    
    Closes #11314
    Closes #11795

--- src/ng/directive/ngOptions.js
+++ src/ng/directive/ngOptions.js
@@ -576,11 +576,16 @@ var ngOptionsDirective = ['$compile', '$parse', function($compile, $parse) {
       function updateOptionElement(option, element) {
         option.element = element;
         element.disabled = option.disabled;
-        if (option.value !== element.value) element.value = option.selectValue;
+        // NOTE: The label must be set before the value, otherwise IE10/11/EDGE create unresponsive
+        // selects in certain circumstances when multiple selects are next to each other and display
+        // the option list in listbox style, i.e. the select is [multiple], or specifies a [size].
+        // See https://github.com/angular/angular.js/issues/11314 for more info.
+        // This is unfortunately untestable with unit / e2e tests
         if (option.label !== element.label) {
           element.label = option.label;
           element.textContent = option.label;
         }
+        if (option.value !== element.value) element.value = option.selectValue;
       }
 
       function addOrReuseElement(parent, current, type, templateElement) {
