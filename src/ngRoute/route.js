b5685e23f0198f2a89084578126ffcdf7fe5a017
Fred Sauer <fredsa@google.com>
2015-03-24 14:48:39 -0700

    docs($route): add param info for `$routeUpdate` event
    Closes #11419

--- src/ngRoute/route.js
+++ src/ngRoute/route.js
@@ -435,9 +435,11 @@ function $RouteProvider() {
      * @name $route#$routeUpdate
      * @eventType broadcast on root scope
      * @description
-     *
      * The `reloadOnSearch` property has been set to false, and we are reusing the same
      * instance of the Controller.
+     *
+     * @param {Object} angularEvent Synthetic event object
+     * @param {Route} current Current/previous route information.
      */
 
     var forceReload = false,
