0b87f6b6cab4aff722c307f847871984fab3071c
Adam <adam77@gmail.com>
2015-04-16 22:33:16 +1200

    docs(angular.element): css() api incompatibility.
    "When a number is passed as the value, jQuery will convert it to a string and add px to the end of that string."
    http://api.jquery.com/css/#css2
    
    jqLite does not appear to do this.
    
    I can submit if fix desired.
    
    Closes #11614

--- src/jqLite.js
+++ src/jqLite.js
@@ -55,7 +55,7 @@
  * - [`children()`](http://api.jquery.com/children/) - Does not support selectors
  * - [`clone()`](http://api.jquery.com/clone/)
  * - [`contents()`](http://api.jquery.com/contents/)
- * - [`css()`](http://api.jquery.com/css/) - Only retrieves inline-styles, does not call `getComputedStyle()`
+ * - [`css()`](http://api.jquery.com/css/) - Only retrieves inline-styles, does not call `getComputedStyle()`. As a setter, does not convert numbers to strings and append 'px'.
  * - [`data()`](http://api.jquery.com/data/)
  * - [`detach()`](http://api.jquery.com/detach/)
  * - [`empty()`](http://api.jquery.com/empty/)
