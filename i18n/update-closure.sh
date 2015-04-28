f03112716026dc9035110f3becd5403565c26ad2
Chirayu Krishnappa <chirayu@chirayuk.com>
2015-04-28 00:49:37 -0700

    fix(i18n): update obsolete/404 code.google.com links

--- i18n/update-closure.sh
+++ i18n/update-closure.sh
@@ -7,8 +7,8 @@ cd $BASE_DIR
 
 set -x  # Trace commands as they're executed.
 
-curl http://closure-library.googlecode.com/svn/trunk/closure/goog/i18n/currency.js > closure/currencySymbols.js
-curl http://closure-library.googlecode.com/svn/trunk/closure/goog/i18n/datetimesymbols.js > closure/datetimeSymbols.js
-curl http://closure-library.googlecode.com/svn/trunk/closure/goog/i18n/datetimesymbolsext.js > closure/datetimeSymbolsExt.js
-curl http://closure-library.googlecode.com/svn/trunk/closure/goog/i18n/numberformatsymbols.js > closure/numberSymbols.js
-curl http://closure-library.googlecode.com/svn/trunk/closure/goog/i18n/pluralrules.js > closure/pluralRules.js
+curl http://google.github.io/closure-library/source/closure/goog/i18n/currency.js > closure/currencySymbols.js
+curl http://google.github.io/closure-library/source/closure/goog/i18n/datetimesymbols.js > closure/datetimeSymbols.js
+curl http://google.github.io/closure-library/source/closure/goog/i18n/datetimesymbolsext.js > closure/datetimeSymbolsExt.js
+curl http://google.github.io/closure-library/source/closure/goog/i18n/numberformatsymbols.js > closure/numberSymbols.js
+curl http://google.github.io/closure-library/source/closure/goog/i18n/pluralrules.js > closure/pluralRules.js
