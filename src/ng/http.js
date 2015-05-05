dda69622f6fbd6563fa217458d4ba83d3caa99c6
Peter Bacon Darwin <pete@bacondarwin.com>
2015-05-05 20:44:59 +0100

    style($http): add missing semi-colon

--- src/ng/http.js
+++ src/ng/http.js
@@ -82,7 +82,7 @@ function $HttpParamSerializerJQLikeProvider() {
           parts.push(encodeUriQuery(prefix) + '=' + encodeUriQuery(serializeValue(toSerialize)));
         }
       }
-    }
+    };
   };
 }
 
