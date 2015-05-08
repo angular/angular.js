e789f3b1ecf14276078366ca1540f7f42f2e1980
Dmitry Maslov <maslov.dmitrij@gmail.com>
2015-05-08 14:30:11 +0300

    ngTransclude: Existing content of the element that this directive is placed on will be removed if transcluded content exists. In other case the content will be saved and used as content by default.

--- src/ng/directive/ngTransclude.js
+++ src/ng/directive/ngTransclude.js
@@ -8,7 +8,7 @@
  * @description
  * Directive that marks the insertion point for the transcluded DOM of the nearest parent directive that uses transclusion.
  *
- * Any existing content of the element that this directive is placed on will be removed before the transcluded content is inserted.
+ * Existing content of the element that this directive is placed on will be removed before the transcluded content is inserted if transcluded content exists. In other case the content will be saved and used as content by default.
  *
  * @element ANY
  *
@@ -66,8 +66,10 @@ var ngTranscludeDirective = ngDirective({
     }
 
     $transclude(function(clone) {
-      $element.empty();
-      $element.append(clone);
+      if(clone.length) {
+        $element.empty();
+        $element.append(clone);
+      }
     });
   }
 });
