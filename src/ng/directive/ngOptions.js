d7dc14dc0cdeb9c187d227e19acc8aca7df9d740
Steven Easter <seaster@levelsbeyond.com>
2015-06-01 10:19:28 -0600

    fix(ngOptions): use reference check only when not using trackBy
    Change the check on changed value for ngOptions to ensure that a reference check is only done
    when trackBy is not used.  Previously, if trackBy was being used but the values of the objects were
    equal a reference check was then used which would cause the check to be true if the objects had
    different memory references but the values were equal.  This can cause issues with forms being
    marked dirty when the value of the model as not actually changed.
    
    Closes #11936
    Closes #11996

--- src/ng/directive/ngOptions.js
+++ src/ng/directive/ngOptions.js
@@ -706,8 +706,7 @@ var ngOptionsDirective = ['$compile', '$parse', function($compile, $parse) {
         // Check to see if the value has changed due to the update to the options
         if (!ngModelCtrl.$isEmpty(previousValue)) {
           var nextValue = selectCtrl.readValue();
-          if (ngOptions.trackBy && !equals(previousValue, nextValue) ||
-                previousValue !== nextValue) {
+          if (ngOptions.trackBy ? !equals(previousValue, nextValue) : previousValue !== nextValue) {
             ngModelCtrl.$setViewValue(nextValue);
             ngModelCtrl.$render();
           }
