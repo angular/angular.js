9d75b0bf1bc1e10f7ebf46b0c6262a8dc88bb3e1
qbzzt <qbzzt1@gmail.com>
2015-06-04 16:35:34 -0500

    Fixed the tests to deal with the new class name

--- src/ng/directive/ngClass.js
+++ src/ng/directive/ngClass.js
@@ -216,13 +216,13 @@ function classDirective(name, selector) {
        it('should let you toggle the class', function() {
 
          expect(ps.first().getAttribute('class')).not.toMatch(/bold/);
-         expect(ps.first().getAttribute('class')).not.toMatch(/red/);
+         expect(ps.first().getAttribute('class')).not.toMatch(/has-error/);
 
          element(by.model('important')).click();
          expect(ps.first().getAttribute('class')).toMatch(/bold/);
 
          element(by.model('error')).click();
-         expect(ps.first().getAttribute('class')).toMatch(/red/);
+         expect(ps.first().getAttribute('class')).toMatch(/has-error/);
        });
 
        it('should let you toggle string example', function() {
