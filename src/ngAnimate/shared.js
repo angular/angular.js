f3fa3e717dfed23919d7287f122f98816a267c68
sreeramu <sreeramu@gmail.com>
2015-07-05 09:35:43 +0800

    fix($animate): leave animation callback should not overridden by follow-up animation
    Closes #12271
    Closes #12249
    Closes #12161

--- src/ngAnimate/shared.js
+++ src/ngAnimate/shared.js
@@ -172,7 +172,15 @@ function mergeAnimationOptions(element, target, newOptions) {
   var toRemove = (target.removeClass || '') + ' ' + (newOptions.removeClass || '');
   var classes = resolveElementClasses(element.attr('class'), toAdd, toRemove);
 
+  // noop is basically when there is no callback; otherwise something has been set
+  var realDomOperation = target.domOperation !== noop ? target.domOperation : null;
+
   extend(target, newOptions);
+
+  // TODO(matsko or sreeramu): proper fix is to maintain all animation callback in array and call at last,but now only leave has the callback so no issue with this.
+  if (realDomOperation) {
+    target.domOperation = realDomOperation;
+  }
 
   if (classes.addClass) {
     target.addClass = classes.addClass;
