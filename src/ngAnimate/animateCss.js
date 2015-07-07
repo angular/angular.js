594f36e458803589b406222eb65ea50f842093f7
startswithaj <jake.mc@icloud.com>
2015-07-07 14:57:33 +1000

    fix(ngAnimate): $timeout without invokeApply
    This change calls $timeout with the invokeApply
    parameter set to false which stops ngAnimate
    from invoking its changes inside an $apply block

--- src/ngAnimate/animateCss.js
+++ src/ngAnimate/animateCss.js
@@ -912,7 +912,7 @@ var $AnimateCssProvider = ['$animateProvider', function($animateProvider) {
 
           startTime = Date.now();
           element.on(events.join(' '), onAnimationProgress);
-          $timeout(onAnimationExpired, maxDelayTime + CLOSING_TIME_BUFFER * maxDurationTime);
+          $timeout(onAnimationExpired, maxDelayTime + CLOSING_TIME_BUFFER * maxDurationTime, false);
 
           applyAnimationToStyles(element, options);
         }
