17715fa3668b1fcabaedcd82e2e57b2a80e0a0c2
luckylooke <luckylooke@gmail.com>
2015-07-07 12:04:41 +0200

    fix(merge): clone elements instead of treating them like simple objects
    	Similar fix to #11720
    
    Closes #12286

--- src/Angular.js
+++ src/Angular.js
@@ -360,6 +360,8 @@ function baseExtend(dst, objs, deep) {
           dst[key] = new Date(src.valueOf());
         } else if (isRegExp(src)) {
           dst[key] = new RegExp(src);
+        } else if (isElement(src)) {
+          dst[key] = src[0] ? jqLite(src).clone()[0] : jqLite(src).clone();
         } else {
           if (!isObject(dst[key])) dst[key] = isArray(src) ? [] : {};
           baseExtend(dst[key], [src], true);
