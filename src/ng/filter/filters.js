2b3e9db77060e3eeedde376c57ea03fcdc897787
Peter Bacon Darwin <pete@bacondarwin.com>
2015-07-16 19:02:50 +0100

    fix(i18n): by default put negative sign before currency symbol
    It seems that the case where the negative sign goes between the currency
    symbol and the numeric value is actually the special case and that locales
    that require this have it built in. So we should default to having the
    negative sign before the symbol.
    
    See http://cldr.unicode.org/translation/number-patterns and
    http://unicode.org/cldr/trac/ticket/5674
    
    Closes #10158

--- src/ng/filter/filters.js
+++ src/ng/filter/filters.js
@@ -45,9 +45,9 @@
          }
          element(by.model('amount')).clear();
          element(by.model('amount')).sendKeys('-1234');
-         expect(element(by.id('currency-default')).getText()).toBe('($1,234.00)');
-         expect(element(by.id('currency-custom')).getText()).toBe('(USD$1,234.00)');
-         expect(element(by.id('currency-no-fractions')).getText()).toBe('(USD$1,234)');
+         expect(element(by.id('currency-default')).getText()).toBe('-$1,234.00');
+         expect(element(by.id('currency-custom')).getText()).toBe('-USD$1,234.00');
+         expect(element(by.id('currency-no-fractions')).getText()).toBe('-USD$1,234');
        });
      </file>
    </example>
