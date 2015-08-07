cec00078bd50f00e71af4c0fa655aa63e675173b
Martin Staffa <mjstaffa@googlemail.com>
2015-08-07 19:28:11 +0200

    docs(form): explain why the empty-string setter is created

--- src/ng/directive/form.js
+++ src/ng/directive/form.js
@@ -517,6 +517,7 @@ var formDirectiveFactory = function(isNgForm) {
 
     function getSetter(expression) {
       if (expression === '') {
+        //create an assignable expression, so forms with an empty name can be renamed later
         return $parse('this[""]').assign;
       }
       return $parse(expression).assign || noop;
