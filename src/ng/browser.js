5a8f364dbbfe148e750e7d3c2f24e7c2fbaf8b18
unknown <carrojd@MDCDSK01CARROJD.wellmanage.com>
2015-08-20 09:56:38 -0400

    Workaround for multiple invocations on iOS 9 Cordova UIWebView

--- src/ng/browser.js
+++ src/ng/browser.js
@@ -317,8 +317,10 @@ function Browser(window, document, $log, $sniffer) {
     var timeoutId;
     outstandingRequestCount++;
     timeoutId = setTimeout(function() {
-      delete pendingDeferIds[timeoutId];
-      completeOutstandingRequest(fn);
+      if (pendingDeferIds[timeoutId]) {
+        delete pendingDeferIds[timeoutId];
+        completeOutstandingRequest(fn);
+      }
     }, delay || 0);
     pendingDeferIds[timeoutId] = true;
     return timeoutId;
