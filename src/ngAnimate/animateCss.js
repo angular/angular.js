ece195231f8ae7ab84cd24b43114887f42cedaf2
Matias Niemelä <matias@yearofmoo.com>
2015-08-21 13:40:53 -0700

    fix($animateCss): do not throw errors when a closing timeout is fired on a removed element
    Closes #12650

--- src/ngAnimate/animateCss.js
+++ src/ngAnimate/animateCss.js
@@ -894,10 +894,16 @@ var $AnimateCssProvider = ['$animateProvider', function($animateProvider) {
 
         function onAnimationExpired() {
           var animationsData = element.data(ANIMATE_TIMER_KEY);
-          for (var i = 1; i < animationsData.length; i++) {
-            animationsData[i]();
+
+          // this will be false in the event that the element was
+          // removed from the DOM (via a leave animation or something
+          // similar)
+          if (animationsData) {
+            for (var i = 1; i < animationsData.length; i++) {
+              animationsData[i]();
+            }
+            element.removeData(ANIMATE_TIMER_KEY);
           }
-          element.removeData(ANIMATE_TIMER_KEY);
         }
 
         function onAnimationProgress(event) {
