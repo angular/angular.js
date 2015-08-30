7691f0a993fb197a074fbc34c6183fbc215a716b
danprince <danielprince@hotmail.co.uk>
2015-08-30 18:16:01 +0100

    style(filters) fix coding style on if statement

--- src/ng/filter/filters.js
+++ src/ng/filter/filters.js
@@ -175,7 +175,7 @@ function formatNumber(number, pattern, groupSep, decimalSep, fractionSize) {
     }
 
     // round the number only if it's decimal
-    if(number.toString().indexOf('.') >= 0) {
+    if (number.toString().indexOf('.') >= 0) {
       // safely round numbers in JS without hitting imprecisions of floating-point arithmetics
       // inspired by:
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/round
