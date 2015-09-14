f19202791f031c160948ba40ff19b9140756d1d9
Samuel Santos <ssantos@present-technologies.com>
2015-09-15 00:09:19 +0100

    fix($locale): change default country for 'pt' locale
    The default country for the locale 'pt' is Portugal. The content of the file 'angular-locale_pt.js' should be similar to 'angular-locale_pt-pt.js' instead of 'angular-locale_pt-br.js'.

--- src/ngLocale/angular-locale_pt.js
+++ src/ngLocale/angular-locale_pt.js
@@ -4,8 +4,8 @@ var PLURAL_CATEGORY = {ZERO: "zero", ONE: "one", TWO: "two", FEW: "few", MANY: "
 $provide.value("$locale", {
   "DATETIME_FORMATS": {
     "AMPMS": [
-      "AM",
-      "PM"
+      "da manh\u00e3",
+      "da tarde"
     ],
     "DAY": [
       "domingo",
@@ -17,14 +17,14 @@ $provide.value("$locale", {
       "s\u00e1bado"
     ],
     "ERANAMES": [
-      "Antes de Cristo",
-      "Ano do Senhor"
+      "antes de Cristo",
+      "depois de Cristo"
     ],
     "ERAS": [
       "a.C.",
       "d.C."
     ],
-    "FIRSTDAYOFWEEK": 6,
+    "FIRSTDAYOFWEEK": 0,
     "MONTH": [
       "janeiro",
       "fevereiro",
@@ -68,17 +68,17 @@ $provide.value("$locale", {
     ],
     "fullDate": "EEEE, d 'de' MMMM 'de' y",
     "longDate": "d 'de' MMMM 'de' y",
-    "medium": "d 'de' MMM 'de' y HH:mm:ss",
-    "mediumDate": "d 'de' MMM 'de' y",
+    "medium": "dd/MM/y HH:mm:ss",
+    "mediumDate": "dd/MM/y",
     "mediumTime": "HH:mm:ss",
     "short": "dd/MM/yy HH:mm",
     "shortDate": "dd/MM/yy",
     "shortTime": "HH:mm"
   },
   "NUMBER_FORMATS": {
-    "CURRENCY_SYM": "R$",
+    "CURRENCY_SYM": "\u20ac",
     "DECIMAL_SEP": ",",
-    "GROUP_SEP": ".",
+    "GROUP_SEP": "\u00a0",
     "PATTERNS": [
       {
         "gSize": 3,
@@ -97,10 +97,10 @@ $provide.value("$locale", {
         "maxFrac": 2,
         "minFrac": 2,
         "minInt": 1,
-        "negPre": "-\u00a4",
-        "negSuf": "",
-        "posPre": "\u00a4",
-        "posSuf": ""
+        "negPre": "-",
+        "negSuf": "\u00a0\u00a4",
+        "posPre": "",
+        "posSuf": "\u00a0\u00a4"
       }
     ]
   },
