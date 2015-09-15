681594677645ad75de59a325d19b15c119e9f0c4
rrsivabalan <rrsivabalan@gmail.com>
2015-09-15 23:33:54 +0530

    fix($location): $locationchangesuccess not fires with browser back when path ends with '/#'. #12175

--- src/ng/location.js
+++ src/ng/location.js
@@ -922,12 +922,12 @@ function $LocationProvider() {
         $location.$$parse(newUrl);
         $location.$$state = newState;
 
-        defaultPrevented = $rootScope.$broadcast('$locationChangeStart', newUrl, oldUrl,
+        defaultPrevented = $rootScope.$broadcast('$locationChangeStart', trimEmptyHash(newUrl), oldUrl,
             newState, oldState).defaultPrevented;
 
         // if the location was changed by a `$locationChangeStart` handler then stop
         // processing this location change
-        if ($location.absUrl() !== newUrl) return;
+        if ($location.absUrl() !== trimEmptyHash(newUrl)) return;
 
         if (defaultPrevented) {
           $location.$$parse(oldUrl);
