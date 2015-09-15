angular/angular.js

commit 634e467172efa696eb32ef8942ffbedeecbd030e

fix($compile): throw error on invalid directive name
Directive names must start with a lower case letter.
Previously the compiler would quietly fail.
This change adds an assertion that fails if this is not the case.

Closes #11281
Closes #11109

docs/content/error/$compile/baddir.ngdoc
@@ -1,0 +1,8 @@
+@ngdoc error
+@name $compile:baddir
+@fullName Invalid Directive Name
+@description
+
+This error occurs when the name of a directive is not valid.
+
+Directives must start with a lowercase character.

src/ng/compile.js
@@ -750,6 +750,14 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
     return bindings;
   }
 
+  function assertValidDirectiveName(name) {
+    var letter = name.charAt(0);
+    if (!letter || letter !== lowercase(letter)) {
+      throw $compileMinErr('baddir', "Directive name '{0}' is invalid. The first character must be a lowercase letter", name);
+    }
+    return name;
+  }
+
   /**
    * @ngdoc method
    * @name $compileProvider#directive
@@ -768,6 +776,7 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
    this.directive = function registerDirective(name, directiveFactory) {
     assertNotHasOwnProperty(name, 'directive');
     if (isString(name)) {
+      assertValidDirectiveName(name);
       assertArg(directiveFactory, 'directiveFactory');
       if (!hasDirectives.hasOwnProperty(name)) {
         hasDirectives[name] = [];

test/ng/compileSpec.js
@@ -147,6 +147,7 @@ describe('$compile', function() {
 
 
   describe('configuration', function() {
+
     it('should register a directive', function() {
       module(function() {
         directive('div', function(log) {
@@ -198,6 +199,15 @@ describe('$compile', function() {
         expect(function() {
           directive('hasOwnProperty', function() { });
         }).toThrowMinErr('ng','badname', "hasOwnProperty is not a valid directive name");
+      });
+      inject(function($compile) {});
+    });
+
+    it('should throw an exception if a directive name starts with a non-lowercase letter', function() {
+      module(function() {
+        expect(function() {
+          directive('BadDirectiveName', function() { });
+        }).toThrowMinErr('$compile','baddir', "Directive name 'BadDirectiveName' is invalid. The first character must be a lowercase letter");
       });
       inject(function($compile) {});
     });

commit 3a6bf0d5bc755983ad786ef22a3de02bd22c80e7

revert: fix($compile): throw error on invalid directive name
This reverts commit 634e467172efa696eb32ef8942ffbedeecbd030e, which introduced
a breaking change between 1.3.15 and 1.3.16.

Closes #12169

docs/content/error/$compile/baddir.ngdoc
@@ -1,8 +1,0 @@
-@ngdoc error
-@name $compile:baddir
-@fullName Invalid Directive Name
-@description
-
-This error occurs when the name of a directive is not valid.
-
-Directives must start with a lowercase character.

src/ng/compile.js
@@ -757,14 +757,6 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
     return bindings;
   }
 
-  function assertValidDirectiveName(name) {
-    var letter = name.charAt(0);
-    if (!letter || letter !== lowercase(letter)) {
-      throw $compileMinErr('baddir', "Directive name '{0}' is invalid. The first character must be a lowercase letter", name);
-    }
-    return name;
-  }
-
   /**
    * @ngdoc method
    * @name $compileProvider#directive
@@ -783,7 +775,6 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
    this.directive = function registerDirective(name, directiveFactory) {
     assertNotHasOwnProperty(name, 'directive');
     if (isString(name)) {
-      assertValidDirectiveName(name);
       assertArg(directiveFactory, 'directiveFactory');
       if (!hasDirectives.hasOwnProperty(name)) {
         hasDirectives[name] = [];

test/ng/compileSpec.js
@@ -147,7 +147,6 @@ describe('$compile', function() {
 
 
   describe('configuration', function() {
-
     it('should register a directive', function() {
       module(function() {
         directive('div', function(log) {
@@ -199,15 +198,6 @@ describe('$compile', function() {
         expect(function() {
           directive('hasOwnProperty', function() { });
         }).toThrowMinErr('ng','badname', "hasOwnProperty is not a valid directive name");
-      });
-      inject(function($compile) {});
-    });
-
-    it('should throw an exception if a directive name starts with a non-lowercase letter', function() {
-      module(function() {
-        expect(function() {
-          directive('BadDirectiveName', function() { });
-        }).toThrowMinErr('$compile','baddir', "Directive name 'BadDirectiveName' is invalid. The first character must be a lowercase letter");
       });
       inject(function($compile) {});
     });