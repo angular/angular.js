b7eaa956e4f1eda96a71f21a484a7e31570968b4
Igor Minar <igor@angularjs.org>
2015-09-22 21:28:17 -0700

    build(travis): gracefully shut down the sauce connect tunnel after the tests are done running
    This is to prevent sauce connect tunnel leaks.
    
    Closes #12921

---
+++ lib/saucelabs/teardown_tunnel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+
+echo "Shutting down Sauce Connect tunnel"
+
+killall sc
+
+while [[ -n `ps -ef | grep "bin/sc" | grep -v "grep"` ]]; do
+  printf "."
+  sleep .5
+done
+
+echo ""
+echo "Sauce Connect tunnel has bee shut down"
