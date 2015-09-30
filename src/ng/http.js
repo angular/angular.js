7c0731edb2f72bdf0efa186f641dab3b6aecc5d5
Georgios Kalpakas <g.kalpakas@hotmail.com>
2015-09-30 16:56:48 +0300

    fix($http): apply `transformResponse` even when `data` is empty
    Note, that (as a by-product of the previous implementation) only non-empty
    data was passed through the `transformResponse` pipeline. This is no
    longer the case.
    
    When using a custom `transformResponse` function, one should make sure it
    can also handle an empty (i.e. falsy) `data` argument appropriately.
    
    Fixes #12976
    Closes #12979

--- src/ng/http.js
+++ src/ng/http.js
@@ -999,11 +999,8 @@ function $HttpProvider() {
       function transformResponse(response) {
         // make a copy since the response must be cacheable
         var resp = extend({}, response);
-        if (!response.data) {
-          resp.data = response.data;
-        } else {
-          resp.data = transformData(response.data, response.headers, response.status, config.transformResponse);
-        }
+        resp.data = transformData(response.data, response.headers, response.status,
+                                  config.transformResponse);
         return (isSuccess(response.status))
           ? resp
           : $q.reject(resp);
