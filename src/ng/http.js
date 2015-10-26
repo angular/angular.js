a05f940e5e32318d7a2212b56a629df9ca98ea6c
Sen <kalone.cool@163.com>
2015-10-26 11:12:06 +0800

    console

--- src/ng/http.js
+++ src/ng/http.js
@@ -1316,7 +1316,7 @@ function $HttpProvider() {
       if (serializedParams.length > 0) {
         url += ((url.indexOf('?') == -1) ? '?' : '&') + serializedParams;
       }
-      console.log(url);
+      //console.log(url);
       return url;
     }
   }];
