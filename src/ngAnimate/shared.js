bc41ad8aa8fc41ff30e9f68220a7c7c5fe194478
Martin Staffa <mjstaffa@googlemail.com>
2015-12-08 14:11:22 +0100

    fix($animate): correctly access minErr
    ngMinErr is available during unit tests, but not in the build. There's
    currently no way to catch these access errors in automated testing.

--- src/ngAnimate/shared.js
+++ src/ngAnimate/shared.js
@@ -73,6 +73,7 @@ var isPromiseLike = function(p) {
   return p && p.then ? true : false;
 };
 
+var ngMinErr = angular.$$minErr('ng');
 function assertArg(arg, name, reason) {
   if (!arg) {
     throw ngMinErr('areq', "Argument '{0}' is {1}", (name || '?'), (reason || "required"));
