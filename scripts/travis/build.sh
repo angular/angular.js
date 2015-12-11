572285488200a63cacf5494f6c0c13375baa2df7
Martin Staffa <mjstaffa@googlemail.com>
2015-12-11 18:26:18 +0100

    chore(travis): add a new job that runs ci-checks
    Previously, ddescribe, merge-conflicts, jshint, and jscs would run
    after unit & e2e tests ran. The order was orginally changed as part of
    https://github.com/angular/angular.js/pull/9792.
    
    While the logic is sound that style errors shouldn't block tests from
    running, ddescribe should always run. This was not guaraneteed; when
    Travis exits with a warning after some browsers have run, ddescribe
    doesn't get run and it doesn't become apparent that not
    all tests have run.
    
    Additionally, a separate job clearly separates style from test errors,
    which e.g. means you can open a PR that includes an iit to speed up
    the job, and see immediately if the test passes, because the ddescribe
    error is in another job.

--- scripts/travis/build.sh
+++ scripts/travis/build.sh
@@ -5,7 +5,9 @@ set -e
 export BROWSER_STACK_ACCESS_KEY=`echo $BROWSER_STACK_ACCESS_KEY | rev`
 export SAUCE_ACCESS_KEY=`echo $SAUCE_ACCESS_KEY | rev`
 
-if [ $JOB = "unit" ]; then
+if [ $JOB = "ci-checks" ]; then
+  grunt ci-checks
+elif [ $JOB = "unit" ]; then
   if [ "$BROWSER_PROVIDER" == "browserstack" ]; then
     BROWSERS="BS_Chrome,BS_Safari,BS_Firefox,BS_IE_9,BS_IE_10,BS_IE_11,BS_iOS"
   else
@@ -14,7 +16,6 @@ if [ $JOB = "unit" ]; then
 
   grunt test:promises-aplus
   grunt test:unit --browsers $BROWSERS --reporters dots
-  grunt ci-checks
   grunt tests:docs --browsers $BROWSERS --reporters dots
 elif [ $JOB = "docs-e2e" ]; then
   grunt test:travis-protractor --specs "docs/app/e2e/**/*.scenario.js"
@@ -31,5 +32,5 @@ elif [ $JOB = "e2e" ]; then
   export TARGET_SPECS="test/e2e/tests/**/*.js,$TARGET_SPECS"
   grunt test:travis-protractor --specs "$TARGET_SPECS"
 else
-  echo "Unknown job type. Please set JOB=unit or JOB=e2e-*."
+  echo "Unknown job type. Please set JOB=ci-checks, JOB=unit or JOB=e2e-*."
 fi
