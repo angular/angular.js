77435fd5dceda05a9dc2d88ac3bfc8c11ab9d6c3
Georgios Kalpakas <g.kalpakas@hotmail.com>
2015-12-15 12:16:14 +0200

    test(privateMocks): allow replacing `$prop` with strings with special RegExp semantics
    `baseThey` used to construct the testcase description by replacing `$prop` using a RegExp.
    If the replacement string contained `$&` (which has a special meaning with RegExps), the resulting
    string was not as expected.x

--- test/helpers/privateMocksSpec.js
+++ test/helpers/privateMocksSpec.js
@@ -22,6 +22,13 @@ describe('private mocks', function() {
         expect(window.it).toHaveBeenCalledWith('should fight "fire" with "fire"', jasmine.any(Function));
       });
 
+      it('should handle replacement strings containing `$&` correctly', function() {
+        spyOn(window, 'it');
+
+        they('should replace dollar-prop with $prop', ['$&']);
+        expect(window.it).toHaveBeenCalledWith('should replace dollar-prop with "$&"', jasmine.any(Function));
+      });
+
       it('should pass each item in an array to the handler', function() {
         var handlerSpy = jasmine.createSpy('handler');
         spyOn(window, 'it').andCallFake(function(msg, handler) {
