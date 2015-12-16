a0897537e7cafbf85670054d712975f216f66e18
mkalish <kalishmichael@gmail.com>
2015-12-16 07:23:24 -0500

    docs(ngMock): update $http example to use standard promise syntax
    Examples were using deprecated .success and .error rather .then
    
    Closes #13557

--- src/ngMock/angular-mocks.js
+++ src/ngMock/angular-mocks.js
@@ -1023,18 +1023,18 @@ angular.mock.dump = function(object) {
   function MyController($scope, $http) {
     var authToken;
 
-    $http.get('/auth.py').success(function(data, status, headers) {
-      authToken = headers('A-Token');
-      $scope.user = data;
+    $http.get('/auth.py').then(function(response) {
+      authToken = response.headers('A-Token');
+      $scope.user = response.data;
     });
 
     $scope.saveMessage = function(message) {
       var headers = { 'Authorization': authToken };
       $scope.status = 'Saving...';
 
-      $http.post('/add-msg.py', message, { headers: headers } ).success(function(response) {
+      $http.post('/add-msg.py', message, { headers: headers } ).then(function(response) {
         $scope.status = '';
-      }).error(function() {
+      }).catch(function() {
         $scope.status = 'Failed...';
       });
     };
