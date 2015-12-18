2fc954d33a3a4c5d4f355be1e15a381664e02f1b
Matias Niemelä <matias@yearofmoo.com>
2015-12-17 16:53:07 -0800

    fix(ngAnimate): only copy over the animation options once
    A bug in material has exposed that ngAnimate makes a copy of
    the provided animation options twice. By making two copies,
    the same DOM operations are performed during and at the end
    of the animation. If the CSS classes being added/
    removed contain existing transition code, then this will lead
    to rendering issues.
    
    Closes #13722
    Closes #13578

--- test/ng/animateCssSpec.js
+++ test/ng/animateCssSpec.js
@@ -31,6 +31,28 @@ describe("$animateCss", function() {
       expect(copiedOptions).toEqual(initialOptions);
     }));
 
+    it("should not create a copy of the provided options if they have already been prepared earlier",
+      inject(function($animateCss, $$rAF) {
+
+      var options = {
+        from: { height: '50px' },
+        to: { width: '50px' },
+        addClass: 'one',
+        removeClass: 'two'
+      };
+
+      options.$$prepared = true;
+      var runner = $animateCss(element, options).start();
+      runner.end();
+
+      $$rAF.flush();
+
+      expect(options.addClass).toBeFalsy();
+      expect(options.removeClass).toBeFalsy();
+      expect(options.to).toBeFalsy();
+      expect(options.from).toBeFalsy();
+    }));
+
     it("should apply the provided [from] CSS to the element", inject(function($animateCss) {
       $animateCss(element, { from: { height: '50px' }}).start();
       expect(element.css('height')).toBe('50px');
