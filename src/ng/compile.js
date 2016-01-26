8348365df9b9e2d4c9c8d5211e3424d4b9a29767
Georgios Kalpakas <g.kalpakas@hotmail.com>
2016-01-26 15:04:03 +0200

    fix($compile): properly denormalize templates when only one of the start/end symbols is different
    Previously, if either of the start/end interpolation symbols remained unchanged (i.e. `{{` or `}}`),
    then directive templates would not be denormalized properly. Changing only one of the start/end
    symbols (but not both) is an uncommon but legitimate usecase.
    
    Closes #13848

--- src/ng/compile.js
+++ src/ng/compile.js
@@ -1559,7 +1559,7 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
 
     var startSymbol = $interpolate.startSymbol(),
         endSymbol = $interpolate.endSymbol(),
-        denormalizeTemplate = (startSymbol == '{{' || endSymbol  == '}}')
+        denormalizeTemplate = (startSymbol == '{{' && endSymbol  == '}}')
             ? identity
             : function denormalizeTemplate(template) {
               return template.replace(/\{\{/g, startSymbol).replace(/}}/g, endSymbol);
