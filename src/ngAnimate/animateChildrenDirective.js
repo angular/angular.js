02046d640c709f528a78642a8a7ca1a355fe96ab
Martin Staffa <mjstaffa@googlemail.com>
2016-01-28 17:03:36 +0100

    fix(ngAnimateChildren): make it compatible with ngIf
    Previously, ngAnimateChildren would set the data on the element
    in an $observe listener, which means the data was available after one digest happend.
    This is too late when the element is animated immediately after compilation, as happens with ngIf.
    Now the data is also set right in the linking function.
    
    Fixes #13865
    Closes #13876

--- src/ngAnimate/animateChildrenDirective.js
+++ src/ngAnimate/animateChildrenDirective.js
@@ -1,15 +1,22 @@
 'use strict';
 
-var $$AnimateChildrenDirective = [function() {
-  return function(scope, element, attrs) {
-    var val = attrs.ngAnimateChildren;
-    if (angular.isString(val) && val.length === 0) { //empty attribute
-      element.data(NG_ANIMATE_CHILDREN_DATA, true);
-    } else {
-      attrs.$observe('ngAnimateChildren', function(value) {
+var $$AnimateChildrenDirective = ['$interpolate', function($interpolate) {
+  return {
+    link: function(scope, element, attrs) {
+      var val = attrs.ngAnimateChildren;
+      if (angular.isString(val) && val.length === 0) { //empty attribute
+        element.data(NG_ANIMATE_CHILDREN_DATA, true);
+      } else {
+        // Interpolate and set the value, so that it is available to
+        // animations that run right after compilation
+        setData($interpolate(attrs.ngAnimateChildren)(scope));
+        attrs.$observe('ngAnimateChildren', setData);
+      }
+
+      function setData(value) {
         value = value === 'on' || value === 'true';
         element.data(NG_ANIMATE_CHILDREN_DATA, value);
-      });
+      }
     }
   };
 }];
