f0e1eb06aa074cf5846b2c9126d374d71115f046
Georgios Kalpakas <g.kalpakas@hotmail.com>
2016-01-28 20:47:36 +0200

    fix(select): handle corner case of adding options via a custom directive
    Under specific circumstances (e.g. adding options via a directive with `replace: true` and a
    structural directive in its template), an error occurred when trying to call `hasAttribute()` on a
    comment node (which doesn't support that method).
    This commit fixes it by first checking if `hasAttribute()` is available.
    
    Fixes #13874

--- src/ng/directive/select.js
+++ src/ng/directive/select.js
@@ -6,8 +6,9 @@ function chromeHack(optionElement) {
   // Workaround for https://code.google.com/p/chromium/issues/detail?id=381459
   // Adding an <option selected="selected"> element to a <select required="required"> should
   // automatically select the new element
-  if (optionElement[0].hasAttribute('selected')) {
-    optionElement[0].selected = true;
+  var optionNode = optionElement[0];
+  if (optionNode.hasAttribute && optionNode.hasAttribute('selected')) {
+    optionNode.selected = true;
   }
 }
 
