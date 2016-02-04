566b31ce7e470cbafcf6dfee38280bce45f01501
Georgios Kalpakas <g.kalpakas@hotmail.com>
2016-02-04 12:53:44 +0200

    test($sniffer): make the structure and layout of tests more consistent

--- test/ng/snifferSpec.js
+++ test/ng/snifferSpec.js
@@ -1,10 +1,9 @@
 'use strict';
 
 describe('$sniffer', function() {
-
   function sniffer($window, $document) {
     /* global $SnifferProvider: false */
-    $window.navigator = {};
+    $window.navigator = $window.navigator || {};
     $document = jqLite($document || {});
     if (!$document[0].body) {
       $document[0].body = window.document.body;
@@ -12,14 +11,37 @@ describe('$sniffer', function() {
     return new $SnifferProvider().$get[2]($window, $document);
   }
 
+
   describe('history', function() {
     it('should be true if history.pushState defined', function() {
-      expect(sniffer({history: {pushState: noop, replaceState: noop}}).history).toBe(true);
+      var mockWindow = {
+        history: {
+          pushState: noop,
+          replaceState: noop
+        }
+      };
+
+      expect(sniffer(mockWindow).history).toBe(true);
     });
 
+
     it('should be false if history or pushState not defined', function() {
-      expect(sniffer({history: {}}).history).toBe(false);
       expect(sniffer({}).history).toBe(false);
+      expect(sniffer({history: {}}).history).toBe(false);
+    });
+
+
+    it('should be false on Boxee box with an older version of Webkit', function() {
+      var mockWindow = {
+        history: {
+          pushState: noop
+        },
+        navigator: {
+          userAgent: 'boxee (alpha/Darwin 8.7.1 i386 - 0.9.11.5591)'
+        }
+      };
+
+      expect(sniffer(mockWindow).history).toBe(false);
     });
   });
 
@@ -28,11 +50,10 @@ describe('$sniffer', function() {
     var mockDocument, mockDivElement, $sniffer;
 
     beforeEach(function() {
-      mockDocument = {createElement: jasmine.createSpy('createElement')};
-      mockDocument.createElement.andCallFake(function(elm) {
-        if (elm === 'div') return mockDivElement;
-      });
+      var mockCreateElementFn = function(elm) { if (elm === 'div') return mockDivElement; };
+      var createElementSpy = jasmine.createSpy('createElement').andCallFake(mockCreateElementFn);
 
+      mockDocument = {createElement: createElementSpy};
       $sniffer = sniffer({}, mockDocument);
     });
 
@@ -83,7 +104,6 @@ describe('$sniffer', function() {
 
 
   describe('vendorPrefix', function() {
-
     it('should return the correct vendor prefix based on the browser', function() {
       inject(function($sniffer, $window) {
         var expectedPrefix;
@@ -101,237 +121,171 @@ describe('$sniffer', function() {
       });
     });
 
-    it('should still work for an older version of Webkit', function() {
-      module(function($provide) {
-        var doc = {
-          body: {
-            style: {
-              WebkitOpacity: '0'
-            }
-          }
-        };
-        $provide.value('$document', jqLite(doc));
-      });
-      inject(function($sniffer) {
-        expect($sniffer.vendorPrefix).toBe('webkit');
-      });
-    });
 
+    it('should still work for an older version of Webkit', function() {
+      var mockDocument = {
+        body: {
+          style: {
+            WebkitOpacity: '0'
+          }
+        }
+      };
+
+      expect(sniffer({}, mockDocument).vendorPrefix).toBe('webkit');
+    });
   });
+
 
   describe('animations', function() {
-    it('should be either true or false', function() {
-      inject(function($sniffer) {
-        expect($sniffer.animations).not.toBe(undefined);
-      });
-    });
+    it('should be either true or false', inject(function($sniffer) {
+      expect($sniffer.animations).toBeDefined();
+    }));
+
 
     it('should be false when there is no animation style', function() {
-      module(function($provide) {
-        var doc = {
-          body: {
-            style: {}
-          }
-        };
-        $provide.value('$document', jqLite(doc));
-      });
-      inject(function($sniffer) {
-        expect($sniffer.animations).toBe(false);
-      });
+      var mockDocument = {
+        body: {
+          style: {}
+        }
+      };
+
+      expect(sniffer({}, mockDocument).animations).toBe(false);
     });
+
 
     it('should be true with vendor-specific animations', function() {
-      module(function($provide) {
-        var animationStyle = 'some_animation 2s linear';
-        var doc = {
-          body: {
-            style: {
-              WebkitAnimation: animationStyle,
-              MozAnimation: animationStyle
-            }
+      var animationStyle = 'some_animation 2s linear';
+      var mockDocument = {
+        body: {
+          style: {
+            WebkitAnimation: animationStyle,
+            MozAnimation: animationStyle
           }
-        };
-        $provide.value('$document', jqLite(doc));
-      });
-      inject(function($sniffer) {
-        expect($sniffer.animations).toBe(true);
-      });
+        }
+      };
+
+      expect(sniffer({}, mockDocument).animations).toBe(true);
     });
+
 
     it('should be true with w3c-style animations', function() {
-      module(function($provide) {
-        var doc = {
-          body: {
-            style: {
-              animation: 'some_animation 2s linear'
-            }
+      var mockDocument = {
+        body: {
+          style: {
+            animation: 'some_animation 2s linear'
           }
-        };
-        $provide.value('$document', jqLite(doc));
-      });
-      inject(function($sniffer) {
-        expect($sniffer.animations).toBe(true);
-      });
+        }
+      };
+
+      expect(sniffer({}, mockDocument).animations).toBe(true);
     });
+
 
     it('should be true on android with older body style properties', function() {
-      module(function($provide) {
-        var doc = {
-          body: {
-            style: {
-              webkitAnimation: ''
-            }
-          }
-        };
-        var win = {
-          navigator: {
-            userAgent: 'android 2'
-          }
-        };
-        $provide.value('$document', jqLite(doc));
-        $provide.value('$window', win);
-      });
-      inject(function($sniffer) {
-        expect($sniffer.animations).toBe(true);
-      });
-    });
-
-    it('should be true when an older version of Webkit is used', function() {
-      module(function($provide) {
-        var doc = {
-          body: {
-            style: {
-              WebkitOpacity: '0'
-            }
-          }
-        };
-        $provide.value('$document', jqLite(doc));
-      });
-      inject(function($sniffer) {
-        expect($sniffer.animations).toBe(false);
-      });
-    });
-
-  });
-
-  describe('transitions', function() {
-
-    it('should be either true or false', function() {
-      inject(function($sniffer) {
-        expect($sniffer.transitions).not.toBe(undefined);
-      });
-    });
-
-    it('should be false when there is no transition style', function() {
-      module(function($provide) {
-        var doc = {
-          body: {
-            style: {}
-          }
-        };
-        $provide.value('$document', jqLite(doc));
-      });
-      inject(function($sniffer) {
-        expect($sniffer.transitions).toBe(false);
-      });
-    });
-
-    it('should be true with vendor-specific transitions', function() {
-      module(function($provide) {
-        var transitionStyle = '1s linear all';
-        var doc = {
-          body: {
-            style: {
-              WebkitTransition: transitionStyle,
-              MozTransition: transitionStyle
-            }
-          }
-        };
-        $provide.value('$document', jqLite(doc));
-      });
-      inject(function($sniffer) {
-        expect($sniffer.transitions).toBe(true);
-      });
-    });
-
-    it('should be true with w3c-style transitions', function() {
-      module(function($provide) {
-        var doc = {
-          body: {
-            style: {
-              transition: '1s linear all'
-            }
-          }
-        };
-        $provide.value('$document', jqLite(doc));
-      });
-      inject(function($sniffer) {
-        expect($sniffer.transitions).toBe(true);
-      });
-    });
-
-    it('should be true on android with older body style properties', function() {
-      module(function($provide) {
-        var doc = {
-          body: {
-            style: {
-              webkitTransition: ''
-            }
-          }
-        };
-        var win = {
-          navigator: {
-            userAgent: 'android 2'
-          }
-        };
-        $provide.value('$document', jqLite(doc));
-        $provide.value('$window', win);
-      });
-      inject(function($sniffer) {
-        expect($sniffer.transitions).toBe(true);
-      });
-    });
-
-  });
-
-
-  describe('history', function() {
-    it('should be true on Boxee box with an older version of Webkit', function() {
-      module(function($provide) {
-        var doc = {
-          body: {
-            style: {}
-          }
-        };
-        var win = {
-          history: {
-            pushState: noop
-          },
-          navigator: {
-            userAgent: 'boxee (alpha/Darwin 8.7.1 i386 - 0.9.11.5591)'
-          }
-        };
-        $provide.value('$document', jqLite(doc));
-        $provide.value('$window', win);
-      });
-      inject(function($sniffer) {
-        expect($sniffer.history).toBe(false);
-      });
-    });
-  });
-
-  it('should provide the android version', function() {
-    module(function($provide) {
-      var win = {
+      var mockWindow = {
         navigator: {
           userAgent: 'android 2'
         }
       };
-      $provide.value('$document', jqLite({}));
-      $provide.value('$window', win);
+      var mockDocument = {
+        body: {
+          style: {
+            webkitAnimation: ''
+          }
+        }
+      };
+
+      expect(sniffer(mockWindow, mockDocument).animations).toBe(true);
     });
-    inject(function($sniffer) {
-      expect($sniffer.android).toBe(2);
+
+
+    it('should be true when an older version of Webkit is used', function() {
+      var mockDocument = {
+        body: {
+          style: {
+            WebkitOpacity: '0'
+          }
+        }
+      };
+
+      expect(sniffer({}, mockDocument).animations).toBe(false);
+    });
+  });
+
+
+  describe('transitions', function() {
+    it('should be either true or false', inject(function($sniffer) {
+      expect($sniffer.transitions).toBeOneOf(true, false);
+    }));
+
+
+    it('should be false when there is no transition style', function() {
+      var mockDocument = {
+        body: {
+          style: {}
+        }
+      };
+
+      expect(sniffer({}, mockDocument).transitions).toBe(false);
+    });
+
+
+    it('should be true with vendor-specific transitions', function() {
+      var transitionStyle = '1s linear all';
+      var mockDocument = {
+        body: {
+          style: {
+            WebkitTransition: transitionStyle,
+            MozTransition: transitionStyle
+          }
+        }
+      };
+
+      expect(sniffer({}, mockDocument).transitions).toBe(true);
+    });
+
+
+    it('should be true with w3c-style transitions', function() {
+      var mockDocument = {
+        body: {
+          style: {
+            transition: '1s linear all'
+          }
+        }
+      };
+
+      expect(sniffer({}, mockDocument).transitions).toBe(true);
+    });
+
+
+    it('should be true on android with older body style properties', function() {
+      var mockWindow = {
+        navigator: {
+          userAgent: 'android 2'
+        }
+      };
+      var mockDocument = {
+        body: {
+          style: {
+            webkitTransition: ''
+          }
+        }
+      };
+
+      expect(sniffer(mockWindow, mockDocument).transitions).toBe(true);
+    });
+  });
+
+
+  describe('android', function() {
+    it('should provide the android version', function() {
+      var mockWindow = {
+        navigator: {
+          userAgent: 'android 2'
+        }
+      };
+
+      expect(sniffer(mockWindow).android).toBe(2);
     });
   });
 });
