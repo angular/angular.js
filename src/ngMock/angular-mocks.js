85ef70f42835900d9394e8b0006b60dd3155d70c
Georgios Kalpakas <g.kalpakas@hotmail.com>
2016-02-20 22:52:32 +0200

    fix(ngMock): prevent memory leak due to data attached to `$rootElement`
    Starting with 88bb551, `ngMock` will attach the `$injector` to the `$rootElement`, but will never
    clean it up, resulting in a memory leak. Since a new `$rootElement` is created for every test,
    this leak causes Karma to crash on large test-suites.
    The problem was not detected by our internal tests, because we do our own clean-up in
    `testabilityPatch.js`.
    
    88bb551 was revert with 1b8590a.
    This commit incorporates the changes from 88bb551 and prevents the memory leak, by cleaning up all
    data attached to `$rootElement` after each test.
    
    Fixes #14094
    
    Closes #14098

--- src/ngMock/angular-mocks.js
+++ src/ngMock/angular-mocks.js
@@ -127,12 +127,12 @@ angular.mock.$Browser = function() {
 };
 angular.mock.$Browser.prototype = {
 
-/**
-  * @name $browser#poll
-  *
-  * @description
-  * run all fns in pollFns
-  */
+  /**
+   * @name $browser#poll
+   *
+   * @description
+   * run all fns in pollFns
+   */
   poll: function poll() {
     angular.forEach(this.pollFns, function(pollFn) {
       pollFn();
@@ -2089,10 +2089,12 @@ angular.mock.$RAFDecorator = ['$delegate', function($delegate) {
 /**
  *
  */
+var originalRootElement;
 angular.mock.$RootElementProvider = function() {
-  this.$get = function() {
-    return angular.element('<div ng-app></div>');
-  };
+  this.$get = ['$injector', function($injector) {
+    originalRootElement = angular.element('<div ng-app></div>').data('$injector', $injector);
+    return originalRootElement;
+  }];
 };
 
 /**
@@ -2577,6 +2579,7 @@ if (window.jasmine || window.mocha) {
 
 
   (window.beforeEach || window.setup)(function() {
+    originalRootElement = null;
     annotatedFunctions = [];
     currentSpec = this;
   });
@@ -2600,7 +2603,15 @@ if (window.jasmine || window.mocha) {
     currentSpec = null;
 
     if (injector) {
-      injector.get('$rootElement').off();
+      // Ensure `$rootElement` is instantiated, before checking `originalRootElement`
+      var $rootElement = injector.get('$rootElement');
+      var rootNode = $rootElement && $rootElement[0];
+      var cleanUpNodes = !originalRootElement ? [] : [originalRootElement[0]];
+      if (rootNode && (!originalRootElement || rootNode !== originalRootElement[0])) {
+        cleanUpNodes.push(rootNode);
+      }
+      angular.element.cleanData(cleanUpNodes);
+
       injector.get('$rootScope').$destroy();
     }
 
