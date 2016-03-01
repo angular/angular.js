c51fca4ce664d19a3656d5f4cf9d6ff588b6e143
Jason Bedard <jason+github@jbedard.ca>
2016-03-01 00:15:36 -0800

    style($compile,$controller): adding function names for debug/tracing

--- src/Angular.js
+++ src/Angular.js
@@ -457,7 +457,7 @@ function identity($) {return $;}
 identity.$inject = [];
 
 
-function valueFn(value) {return function() {return value;};}
+function valueFn(value) {return function valueRef() {return value;};}
 
 function hasCustomToString(obj) {
   return isFunction(obj.toString) && obj.toString !== toString;
