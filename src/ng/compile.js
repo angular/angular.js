f01d6f2788737bcf6767985bcd43ab4d417839bd
pmadruga <pema@colego.dk>
2016-03-10 13:36:05 +0100

    docs(error/$compile.baddir): mention "components" in directive name error
    Closes #14212

--- src/ng/compile.js
+++ src/ng/compile.js
@@ -928,11 +928,11 @@ function $CompileProvider($provide, $$sanitizeUriProvider) {
   function assertValidDirectiveName(name) {
     var letter = name.charAt(0);
     if (!letter || letter !== lowercase(letter)) {
-      throw $compileMinErr('baddir', "Directive name '{0}' is invalid. The first character must be a lowercase letter", name);
+      throw $compileMinErr('baddir', "Directive/Component name '{0}' is invalid. The first character must be a lowercase letter", name);
     }
     if (name !== name.trim()) {
       throw $compileMinErr('baddir',
-            "Directive name '{0}' is invalid. The name should not contain leading or trailing whitespaces",
+            "Directive/Component name '{0}' is invalid. The name should not contain leading or trailing whitespaces",
             name);
     }
   }
