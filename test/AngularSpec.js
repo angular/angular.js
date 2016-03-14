b0ae534306282bf1783dfc0649e89f7b8db900d1
BobChao87 <BobChao87@users.noreply.github.com>
2016-03-13 17:28:15 -0700

    fix(toJson): correct serialization of invalid dates
    Fix for error thrown by Safari when an invalid date is serialized. Use 'null' always.
    
    Closes #13415

--- test/AngularSpec.js
+++ test/AngularSpec.js
@@ -2060,7 +2060,7 @@ describe('angular', function() {
     });
 
     it('should serialize invalid dates to null', function() {
-      expect(toJson(new Date(1/0))).toEqual('null');
+      expect(toJson(new Date(1 / 0))).toEqual('null');
     });
   });
 
