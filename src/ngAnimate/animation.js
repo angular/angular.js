e65afb107306a40972b10de6deb03108aaad2ce7
CarlosMiguelDesarrollo <carlos.cuevas.desarrollo@gmail.com>
2016-03-20 11:26:32 -0700

    refactor(): delete a usless conditional sentence
    delete a conditional sentences. it is marked with a TODO tag to be deleted.

--- src/ngAnimate/animation.js
+++ src/ngAnimate/animation.js
@@ -348,8 +348,6 @@ var $$AnimationProvider = ['$animateProvider', function($animateProvider) {
         // may attempt more elements, but custom drivers are more particular
         for (var i = drivers.length - 1; i >= 0; i--) {
           var driverName = drivers[i];
-          if (!$injector.has(driverName)) continue; // TODO(matsko): remove this check
-
           var factory = $injector.get(driverName);
           var driver = factory(animationDetails);
           if (driver) {
