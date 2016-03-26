5ceb5dbfa6d9b6d15232a1f5c767b2f431325948
thorn0 <thorn.mailbox@gmail.com>
2016-03-26 16:30:08 +0200

    perf(injector): cache the results of the native class detection check
    Closes: #14322

--- src/auto/injector.js
+++ src/auto/injector.js
@@ -837,13 +837,16 @@ function createInjector(modulesToLoad, strictDi) {
 
     function isClass(func) {
       // IE 9-11 do not support classes and IE9 leaks with the code below.
-      if (msie <= 11) {
+      if (msie <= 11 || typeof func !== 'function') {
         return false;
       }
-      // Workaround for MS Edge.
-      // Check https://connect.microsoft.com/IE/Feedback/Details/2211653
-      return typeof func === 'function'
-        && /^(?:class\s|constructor\()/.test(Function.prototype.toString.call(func));
+      var result = func.$$ngIsClass;
+      if (!isBoolean(result)) {
+        // Workaround for MS Edge.
+        // Check https://connect.microsoft.com/IE/Feedback/Details/2211653
+        result = func.$$ngIsClass = /^(?:class\s|constructor\()/.test(Function.prototype.toString.call(func));
+      }
+      return result;
     }
 
     function invoke(fn, self, locals, serviceName) {
