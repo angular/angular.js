0dd89330bace1916e64bbf25ec7d50d878b43f5f
Georgii Dolzhykov <thorn.mailbox@gmail.com>
2016-04-12 01:24:17 +0300

    docs(Module): fix parameter names for `decorate`

--- src/loader.js
+++ src/loader.js
@@ -197,9 +197,9 @@ function setupModuleLoader(window) {
            * @ngdoc method
            * @name angular.Module#decorator
            * @module ng
-           * @param {string} The name of the service to decorate.
-           * @param {Function} This function will be invoked when the service needs to be
-           *                                    instantiated and should return the decorated service instance.
+           * @param {string} name The name of the service to decorate.
+           * @param {Function} decorFn This function will be invoked when the service needs to be
+           *                           instantiated and should return the decorated service instance.
            * @description
            * See {@link auto.$provide#decorator $provide.decorator()}.
            */
