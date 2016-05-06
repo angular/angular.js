ac650cb2e6d469804cd5ccd9c62ad14168fa026a
dmandelin <dmandelin@google.com>
2016-05-06 06:35:28 -0700

    docs($sanitizeProvider): fix param comment for enableSvg
    Change the text of the doc comment for the parameter of enableSvg to match the type of the comment and the behavior of the code (#14543).

--- src/ngSanitize/sanitize.js
+++ src/ngSanitize/sanitize.js
@@ -182,7 +182,7 @@ function $SanitizeProvider() {
    *   </code></pre>
    * </div>
    *
-   * @param {boolean=} regexp New regexp to whitelist urls with.
+   * @param {boolean=} flag Enable or disable SVG support in the sanitizer.
    * @returns {boolean|ng.$sanitizeProvider} Returns the currently configured value if called
    *    without an argument or self for chaining otherwise.
    */
