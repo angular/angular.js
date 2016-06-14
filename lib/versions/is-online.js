angular/angular.js

commit aa38ded59de5dfea4e519f29aaa656755e3cf995

chore(version-info): disable remote requests when offline
When the internet is not accessible build scripts are unnecessarily slow
because of failed attempts to download git tag and CDN version information
from remote servers.

This commit does a synchronous check of internet connectivity before attempting
any remote requests.

Closes #14759

lib/versions/is-online.js
@@ -1,0 +1,7 @@
+"use strict";
+
+var isOnlineAsync = require('is-online');
+
+isOnlineAsync(function(err, isOnline) {
+  console.log(isOnline ? 'online' : 'offline');
+});

lib/versions/version-info.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var childProcess = require('child_process');
 var fs = require('fs');
 var path = require('path');
 var shell = require('shelljs');
@@ -7,7 +8,9 @@ var semver = require('semver');
 var _ = require('lodash');
 
 var currentPackage, previousVersions, cdnVersion, gitRepoInfo;
-
+var connectivityStatus = childProcess.execSync('node is-online.js', { cwd: __dirname, encoding: 'utf8' }).trim();
+var isOnline = connectivityStatus == 'online';
+console.log('Running ' + connectivityStatus);
 
 /**
  * Load information about this project from the package.json
@@ -100,58 +103,68 @@ var getPreviousVersions =  function() {
   // not contain all commits when cloned with git clone --depth=...
   // Needed e.g. for Travis
   var repo_url = currentPackage.repository.url;
-  var tagResults = shell.exec('git ls-remote --tags ' + repo_url,
-                              {silent: true});
-  if (tagResults.code === 0) {
-    return _(tagResults.output.match(/v[0-9].*[0-9]$/mg))
-      .map(function(tag) {
-        var version = semver.parse(tag);
-        return version;
-      })
-      .filter()
-      .map(function(version) {
-        // angular.js didn't follow semantic version until 1.20rc1
-        if ((version.major === 1 && version.minor === 0 && version.prerelease.length > 0) || (version.major === 1 && version.minor === 2 && version.prerelease[0] === 'rc1')) {
-          version.version = [version.major, version.minor, version.patch].join('.') + version.prerelease.join('');
-          version.raw = 'v' + version.version;
-        }
-        version.docsUrl = 'http://code.angularjs.org/' + version.version + '/docs';
-        // Versions before 1.0.2 had a different docs folder name
-        if (version.major < 1 || (version.major === 1 && version.minor === 0 && version.patch < 2)) {
-          version.docsUrl += '-' + version.version;
-          version.isOldDocsUrl = true;
-        }
-        return version;
-      })
-      .sort(semver.compare)
-      .value();
+  if (isOnline) {
+    console.log('looking up remote git tags...');
+    var tagResults = shell.exec('git ls-remote --tags ' + repo_url,
+                                {silent: true});
+    if (tagResults.code === 0) {
+      return _(tagResults.output.match(/v[0-9].*[0-9]$/mg))
+        .map(function(tag) {
+          var version = semver.parse(tag);
+          return version;
+        })
+        .filter()
+        .map(function(version) {
+          // angular.js didn't follow semantic version until 1.20rc1
+          if ((version.major === 1 && version.minor === 0 && version.prerelease.length > 0) || (version.major === 1 && version.minor === 2 && version.prerelease[0] === 'rc1')) {
+            version.version = [version.major, version.minor, version.patch].join('.') + version.prerelease.join('');
+            version.raw = 'v' + version.version;
+          }
+          version.docsUrl = 'http://code.angularjs.org/' + version.version + '/docs';
+          // Versions before 1.0.2 had a different docs folder name
+          if (version.major < 1 || (version.major === 1 && version.minor === 0 && version.patch < 2)) {
+            version.docsUrl += '-' + version.version;
+            version.isOldDocsUrl = true;
+          }
+          return version;
+        })
+        .sort(semver.compare)
+        .value();
+    }
   } else {
-    return [];
+    console.log('-- skipping remote git tag lookup');
   }
+  return [];
 };
 
 var getCdnVersion = function() {
-  return _(previousVersions)
-    .filter(function(tag) {
-      return semver.satisfies(tag, currentPackage.branchVersion);
-    })
-    .reverse()
-    .reduce(function(cdnVersion, version) {
-      if (!cdnVersion) {
-        // Note: need to use shell.exec and curl here
-        // as version-infos returns its result synchronously...
-        var cdnResult = shell.exec('curl http://ajax.googleapis.com/ajax/libs/angularjs/' + version + '/angular.min.js ' +
-                  '--head --write-out "%{http_code}" -o /dev/null -silent',
-                                    {silent: true});
-        if (cdnResult.code === 0) {
-          var statusCode = cdnResult.output.trim();
-          if (statusCode === '200') {
-            cdnVersion = version;
+  if (isOnline) {
+    console.log('searching for CDN versions...');
+    return _(previousVersions)
+      .filter(function(tag) {
+        return semver.satisfies(tag, currentPackage.branchVersion);
+      })
+      .reverse()
+      .reduce(function(cdnVersion, version) {
+        if (!cdnVersion) {
+          // Note: need to use shell.exec and curl here
+          // as version-infos returns its result synchronously...
+          var cdnResult = shell.exec('curl http://ajax.googleapis.com/ajax/libs/angularjs/' + version + '/angular.min.js ' +
+                    '--head --write-out "%{http_code}" -o /dev/null -silent',
+                                      {silent: true});
+          if (cdnResult.code === 0) {
+            var statusCode = cdnResult.output.trim();
+            if (statusCode === '200') {
+              console.log('-- found CDN version ' + version);
+              cdnVersion = version;
+            }
           }
         }
-      }
-      return cdnVersion;
-    }, null);
+        return cdnVersion;
+      }, null);
+  } else {
+    console.log('-- skipping CDN version lookup');
+  }
 };
 
 /**
@@ -197,7 +210,6 @@ var getSnapshotVersion = function() {
 
   return version;
 };
-
 
 exports.currentPackage = currentPackage = getPackage();
 exports.gitRepoInfo = gitRepoInfo = getGitRepoInfo();

commit 29190f931831b7a37a33af789ea2d8c5bd8b1752

revert: chore(version-info): disable remote requests when offline
This reverts commit aa38ded59de5dfea4e519f29aaa656755e3cf995, which
caused the docs to build incorrectly causing the docs website to crash.

lib/versions/is-online.js
@@ -1,7 +1,0 @@
-"use strict";
-
-var isOnlineAsync = require('is-online');
-
-isOnlineAsync(function(err, isOnline) {
-  console.log(isOnline ? 'online' : 'offline');
-});

lib/versions/version-info.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var childProcess = require('child_process');
 var fs = require('fs');
 var path = require('path');
 var shell = require('shelljs');
@@ -8,9 +7,7 @@ var semver = require('semver');
 var _ = require('lodash');
 
 var currentPackage, previousVersions, cdnVersion, gitRepoInfo;
-var connectivityStatus = childProcess.execSync('node is-online.js', { cwd: __dirname, encoding: 'utf8' }).trim();
-var isOnline = connectivityStatus == 'online';
-console.log('Running ' + connectivityStatus);
+
 
 /**
  * Load information about this project from the package.json
@@ -103,68 +100,58 @@ var getPreviousVersions =  function() {
   // not contain all commits when cloned with git clone --depth=...
   // Needed e.g. for Travis
   var repo_url = currentPackage.repository.url;
-  if (isOnline) {
-    console.log('looking up remote git tags...');
-    var tagResults = shell.exec('git ls-remote --tags ' + repo_url,
-                                {silent: true});
-    if (tagResults.code === 0) {
-      return _(tagResults.output.match(/v[0-9].*[0-9]$/mg))
-        .map(function(tag) {
-          var version = semver.parse(tag);
-          return version;
-        })
-        .filter()
-        .map(function(version) {
-          // angular.js didn't follow semantic version until 1.20rc1
-          if ((version.major === 1 && version.minor === 0 && version.prerelease.length > 0) || (version.major === 1 && version.minor === 2 && version.prerelease[0] === 'rc1')) {
-            version.version = [version.major, version.minor, version.patch].join('.') + version.prerelease.join('');
-            version.raw = 'v' + version.version;
-          }
-          version.docsUrl = 'http://code.angularjs.org/' + version.version + '/docs';
-          // Versions before 1.0.2 had a different docs folder name
-          if (version.major < 1 || (version.major === 1 && version.minor === 0 && version.patch < 2)) {
-            version.docsUrl += '-' + version.version;
-            version.isOldDocsUrl = true;
-          }
-          return version;
-        })
-        .sort(semver.compare)
-        .value();
-    }
+  var tagResults = shell.exec('git ls-remote --tags ' + repo_url,
+                              {silent: true});
+  if (tagResults.code === 0) {
+    return _(tagResults.output.match(/v[0-9].*[0-9]$/mg))
+      .map(function(tag) {
+        var version = semver.parse(tag);
+        return version;
+      })
+      .filter()
+      .map(function(version) {
+        // angular.js didn't follow semantic version until 1.20rc1
+        if ((version.major === 1 && version.minor === 0 && version.prerelease.length > 0) || (version.major === 1 && version.minor === 2 && version.prerelease[0] === 'rc1')) {
+          version.version = [version.major, version.minor, version.patch].join('.') + version.prerelease.join('');
+          version.raw = 'v' + version.version;
+        }
+        version.docsUrl = 'http://code.angularjs.org/' + version.version + '/docs';
+        // Versions before 1.0.2 had a different docs folder name
+        if (version.major < 1 || (version.major === 1 && version.minor === 0 && version.patch < 2)) {
+          version.docsUrl += '-' + version.version;
+          version.isOldDocsUrl = true;
+        }
+        return version;
+      })
+      .sort(semver.compare)
+      .value();
   } else {
-    console.log('-- skipping remote git tag lookup');
+    return [];
   }
-  return [];
 };
 
 var getCdnVersion = function() {
-  if (isOnline) {
-    console.log('searching for CDN versions...');
-    return _(previousVersions)
-      .filter(function(tag) {
-        return semver.satisfies(tag, currentPackage.branchVersion);
-      })
-      .reverse()
-      .reduce(function(cdnVersion, version) {
-        if (!cdnVersion) {
-          // Note: need to use shell.exec and curl here
-          // as version-infos returns its result synchronously...
-          var cdnResult = shell.exec('curl http://ajax.googleapis.com/ajax/libs/angularjs/' + version + '/angular.min.js ' +
-                    '--head --write-out "%{http_code}" -o /dev/null -silent',
-                                      {silent: true});
-          if (cdnResult.code === 0) {
-            var statusCode = cdnResult.output.trim();
-            if (statusCode === '200') {
-              console.log('-- found CDN version ' + version);
-              cdnVersion = version;
-            }
+  return _(previousVersions)
+    .filter(function(tag) {
+      return semver.satisfies(tag, currentPackage.branchVersion);
+    })
+    .reverse()
+    .reduce(function(cdnVersion, version) {
+      if (!cdnVersion) {
+        // Note: need to use shell.exec and curl here
+        // as version-infos returns its result synchronously...
+        var cdnResult = shell.exec('curl http://ajax.googleapis.com/ajax/libs/angularjs/' + version + '/angular.min.js ' +
+                  '--head --write-out "%{http_code}" -o /dev/null -silent',
+                                    {silent: true});
+        if (cdnResult.code === 0) {
+          var statusCode = cdnResult.output.trim();
+          if (statusCode === '200') {
+            cdnVersion = version;
           }
         }
-        return cdnVersion;
-      }, null);
-  } else {
-    console.log('-- skipping CDN version lookup');
-  }
+      }
+      return cdnVersion;
+    }, null);
 };
 
 /**
@@ -210,6 +197,7 @@ var getSnapshotVersion = function() {
 
   return version;
 };
+
 
 exports.currentPackage = currentPackage = getPackage();
 exports.gitRepoInfo = gitRepoInfo = getGitRepoInfo();