angular/angular.js

commit 71cf28cf06295e0936f706c048bc07e6c963acc4

refactor(ngResource): handle success and error callbacks using the same promise

src/ngResource/resource.js
@@ -701,19 +701,15 @@ angular.module('ngResource', ['ng']).
               response.resource = value;
 
               return response;
-            }, function(response) {
-              (error || noop)(response);
-              return $q.reject(response);
             });
 
-            promise = promise['finally'](function(response) {
+            promise = promise['finally'](function() {
               value.$resolved = true;
               if (!isInstanceCall && cancellable) {
                 value.$cancelRequest = angular.noop;
                 $timeout.cancel(numericTimeoutPromise);
                 timeoutDeferred = numericTimeoutPromise = httpConfig.timeout = null;
               }
-              return response;
             });
 
             promise = promise.then(
@@ -722,7 +718,13 @@ angular.module('ngResource', ['ng']).
                 (success || noop)(value, response.headers);
                 return value;
               },
-              responseErrorInterceptor);
+              responseErrorInterceptor || error ?
+              function(response) {
+                (error || noop)(response);
+                (responseErrorInterceptor || noop)(response);
+                return response;
+              }
+              : undefined);
 
             if (!isInstanceCall) {
               // we are creating instance / collection
@@ -730,13 +732,18 @@ angular.module('ngResource', ['ng']).
               // - return the instance / collection
               value.$promise = promise;
               value.$resolved = false;
-              if (cancellable) value.$cancelRequest = timeoutDeferred.resolve;
+              if (cancellable) value.$cancelRequest = cancelRequest;
 
               return value;
             }
 
             // instance call
             return promise;
+
+            function cancelRequest(value) {
+              promise.catch(noop);
+              timeoutDeferred.resolve(value);
+            }
           };
 
 

test/ngResource/resourceSpec.js
@@ -1052,8 +1052,7 @@ describe("basic usage", function() {
     it('should call the error callback if provided on non 2xx response', function() {
       $httpBackend.expect('GET', '/CreditCard/123').respond(ERROR_CODE, ERROR_RESPONSE);
 
-      var ccs = CreditCard.get({id:123}, callback, errorCB);
-      ccs.$promise.then(noop, noop);
+      CreditCard.get({id:123}, callback, errorCB);
       $httpBackend.flush();
       expect(errorCB).toHaveBeenCalledOnce();
       expect(callback).not.toHaveBeenCalled();
@@ -1063,8 +1062,7 @@ describe("basic usage", function() {
     it('should call the error callback if provided on non 2xx response (without data)', function() {
       $httpBackend.expect('GET', '/CreditCard').respond(ERROR_CODE, ERROR_RESPONSE);
 
-      var ccs = CreditCard.get(callback, errorCB);
-      ccs.$promise.then(noop, noop);
+      CreditCard.get(callback, errorCB);
       $httpBackend.flush();
       expect(errorCB).toHaveBeenCalledOnce();
       expect(callback).not.toHaveBeenCalled();
@@ -1564,7 +1562,6 @@ describe('cancelling requests', function() {
     });
 
     var ccs = CreditCard.get();
-    ccs.$promise.catch(noop);
     ccs.$cancelRequest();
     expect($httpBackend.flush).toThrow(new Error('No pending request to flush !'));
 
@@ -1584,7 +1581,6 @@ describe('cancelling requests', function() {
     });
 
     var ccs = CreditCard.get();
-    ccs.$promise.catch(noop);
     ccs.$cancelRequest();
     expect($httpBackend.flush).toThrow(new Error('No pending request to flush !'));
 

test/ng/animateRunnerSpec.js
@@ -206,7 +206,7 @@ describe("$$AnimateRunner", function() {
         var animationComplete = false;
         runner.finally(function() {
           animationComplete = true;
-        }).then(noop, noop);
+        }).catch(noop);
         runner[method]();
         $rootScope.$digest();
         expect(animationComplete).toBe(true);

commit 3d40936bf34bbf69468ffbee8650a220739248af

fix($resource): fulfill promise with the correct value on error
This fixes a regression introduced with 71cf28c. See #14837 for more info.

Fixes #14837

src/ngResource/resource.js
@@ -695,6 +695,8 @@ angular.module('ngResource', ['ng']).
               defaultResponseInterceptor;
             var responseErrorInterceptor = action.interceptor && action.interceptor.responseError ||
               undefined;
+            var hasError = !!error;
+            var hasResponseErrorInterceptor = !!responseErrorInterceptor;
             var timeoutDeferred;
             var numericTimeoutPromise;
 
@@ -776,13 +778,18 @@ angular.module('ngResource', ['ng']).
                 (success || noop)(value, response.headers);
                 return value;
               },
-              responseErrorInterceptor || error ?
+              (hasError || hasResponseErrorInterceptor) ?
               function(response) {
-                (error || noop)(response);
-                (responseErrorInterceptor || noop)(response);
-                return response;
-              }
-              : undefined);
+                if (hasError) error(response);
+                return hasResponseErrorInterceptor ?
+                    responseErrorInterceptor(response) :
+                    $q.reject(response);
+              } :
+              undefined);
+            if (hasError && !hasResponseErrorInterceptor) {
+              // Avoid PUR, but still reject the returned promise
+              promise.catch(noop);
+            }
 
             if (!isInstanceCall) {
               // we are creating instance / collection

test/ngResource/resourceSpec.js
@@ -1457,12 +1457,17 @@ describe('errors', function() {
 });
 
 describe('handling rejections', function() {
+  var $exceptionHandler;
   var $httpBackend;
   var $resource;
 
   beforeEach(module('ngResource'));
+  beforeEach(module(function($exceptionHandlerProvider) {
+    $exceptionHandlerProvider.mode('log');
+  }));
 
-  beforeEach(inject(function(_$httpBackend_, _$resource_) {
+  beforeEach(inject(function(_$exceptionHandler_, _$httpBackend_, _$resource_) {
+    $exceptionHandler = _$exceptionHandler_;
     $httpBackend = _$httpBackend_;
     $resource = _$resource_;
 
@@ -1481,6 +1486,70 @@ describe('handling rejections', function() {
     expect(errorCb1).toHaveBeenCalledOnce();
     expect(errorCb2).toHaveBeenCalledOnce();
   });
+
+
+  it('should report a PUR when no error callback or responseError interceptor is provided',
+    function() {
+      var CreditCard = $resource('/CreditCard');
+
+      CreditCard.get();
+      $httpBackend.flush();
+
+      expect($exceptionHandler.errors.length).toBe(1);
+      expect($exceptionHandler.errors[0]).toMatch(/^Possibly unhandled rejection/);
+    }
+  );
+
+
+  it('should not report a PUR when an error callback or responseError interceptor is provided',
+    function() {
+      var CreditCard = $resource('/CreditCard', {}, {
+        test1: {
+          method: 'GET'
+        },
+        test2: {
+          method: 'GET',
+          interceptor: {responseError: function() { return {}; }}
+        }
+      });
+
+      // With error callback
+      CreditCard.test1(noop, noop);
+      $httpBackend.flush();
+
+      expect($exceptionHandler.errors.length).toBe(0);
+
+      // With responseError interceptor
+      CreditCard.test2();
+      $httpBackend.flush();
+
+      expect($exceptionHandler.errors.length).toBe(0);
+
+      // With error callback and responseError interceptor
+      CreditCard.test2(noop, noop);
+      $httpBackend.flush();
+
+      expect($exceptionHandler.errors.length).toBe(0);
+    }
+  );
+
+
+  it('should report a PUR when the responseError interceptor returns a rejected promise',
+    inject(function($q) {
+      var CreditCard = $resource('/CreditCard', {}, {
+        test: {
+          method: 'GET',
+          interceptor: {responseError: function() { return $q.reject({}); }}
+        }
+      });
+
+      CreditCard.test();
+      $httpBackend.flush();
+
+      expect($exceptionHandler.errors.length).toBe(1);
+      expect($exceptionHandler.errors[0]).toMatch(/^Possibly unhandled rejection/);
+    })
+  );
 });
 
 describe('cancelling requests', function() {