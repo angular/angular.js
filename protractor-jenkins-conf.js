5fe823948f3af9d6ef34bb42112d87d4d7ae360f
Georgios Kalpakas <kalpakas.g@gmail.com>
2016-07-08 19:05:27 +0300

    chore(Jenkins): fix e2e tests on Jenkins
    Follow-up to c72e13f and a82a8a5.
    
    Closes #14889

--- protractor-jenkins-conf.js
+++ protractor-jenkins-conf.js
@@ -29,9 +29,10 @@ exports.config = {
 
     browser.addMockModule('disableNgAnimate', disableNgAnimate);
 
-    require('jasmine-reporters');
-    jasmine.getEnv().addReporter(
-      new jasmine.JUnitXmlReporter('test_out/docs-e2e-' + exports.config.capabilities.browserName + '-', true, true));
+    var reporters = require('jasmine-reporters');
+    jasmine.getEnv().addReporter(new reporters.JUnitXmlReporter({
+      savePath: 'test_out/docs-e2e-' + exports.config.capabilities.browserName + '-'
+    }));
   },
 
   jasmineNodeOpts: {
