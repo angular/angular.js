5dfb328c944fc3f4cdd301f0875958bdc6a8ab86
Georgios Kalpakas <kalpakas.g@gmail.com>
2016-07-19 15:31:45 +0300

    refactor(test/e2e): make file/directory names consistent

--- test/e2e/tests/ng-jq.spec.js
+++ test/e2e/tests/ng-jq.spec.js
@@ -1,11 +1,11 @@
 describe('Customizing the jqLite / jQuery version', function() {
   it('should be able to force jqLite', function() {
-    loadFixture('ngJq');
+    loadFixture('ng-jq');
     expect(element(by.binding('jqueryVersion')).getText()).toBe('jqLite');
   });
 
   it('should be able to use a specific version jQuery', function() {
-    loadFixture('ngJqJquery');
+    loadFixture('ng-jq-jquery');
     expect(element(by.binding('jqueryVersion')).getText()).toBe('2.1.0');
   });
 });
