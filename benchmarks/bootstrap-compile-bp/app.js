775c247085765e08845ae45ed19dd0120c61acc1
David Rodenas <david.rodenas+github@gmail.com>
2016-08-08 11:01:08 +0200

    perf($compile): add provider option to turn off compilation of css class and comment directives
    When the functions `cssClassDirectivesEnabled()` / `commentDirectivesEnabled()` on the `$compileProvider` are called with `false`, then the compiler won't look for directives on css classes / comment elements.
    
    This can result in a compilation speed-up of around 10%.
    
    PR (#14850)

---
+++ benchmarks/bootstrap-compile-bp/app.js
@@ -0,0 +1,61 @@
+var app = angular.module('boostrapCompileBenchmark', []);
+
+var commentDirectivesEnabled;
+var cssClassDirectivesEnabled;
+
+app.config(function($compileProvider) {
+  $compileProvider.debugInfoEnabled(false);
+
+  commentDirectivesEnabled = window.location.toString().indexOf('comment=disabled') === -1;
+  cssClassDirectivesEnabled = window.location.toString().indexOf('css=disabled') === -1;
+
+  $compileProvider
+    .commentDirectivesEnabled(commentDirectivesEnabled)
+    .cssClassDirectivesEnabled(cssClassDirectivesEnabled);
+})
+.controller('DataController', function($compile, $http, $rootScope) {
+
+  this.isEA = !commentDirectivesEnabled && !cssClassDirectivesEnabled;
+  this.isEAC = !commentDirectivesEnabled && cssClassDirectivesEnabled;
+  this.isEAM = commentDirectivesEnabled && !cssClassDirectivesEnabled;
+  this.isEACM = commentDirectivesEnabled && cssClassDirectivesEnabled;
+
+  this.repeats = 50;
+
+  this.templates = [
+    'bootstrap-carousel.tpl.html',
+    'bootstrap-theme.tpl.html'
+  ];
+
+  this.html = null;
+  this.loadTemplate = function() {
+    this.html = null;
+    $http.get(location.pathname + this.selectedTemplate)
+      .then(function(response) { this.html = response.data; }.bind(this));
+  };
+
+  this.selectedTemplate = this.templates[0];
+  this.loadTemplate();
+
+
+  var linkers = [];
+  benchmarkSteps.push({
+    name: 'create',
+    fn: function() {
+      for (var i = 0; i < this.repeats; i++) {
+        var linker = $compile(this.html);
+        linkers.push(linker);
+      }
+    }.bind(this)
+  });
+
+  benchmarkSteps.push({
+    name: 'destroy',
+    fn: function() {
+      linkers.length = 0;
+    }
+  });
+
+});
+
+
