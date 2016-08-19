98048d572ba2bba27ab2ef46fe07eaa74eac4cbb
philip98 <philipschloesser98@gmail.com>
2016-08-19 09:54:51 +0200

    feat($q): Add notify functionality to ES6-style constructor
    The resolver function passed to the ES6-style constructor $q() can now take a third argument: notify, which wraps deferred.notify().

--- src/ng/q.js
+++ src/ng/q.js
@@ -32,7 +32,8 @@
  *
  *   function asyncGreet(name) {
  *     // perform some asynchronous operation, resolve or reject the promise when appropriate.
- *     return $q(function(resolve, reject) {
+ *     return $q(function(resolve, reject, notify) {
+ *       notify('Determining whether it\'s ok to greet.');
  *       setTimeout(function() {
  *         if (okToGreet(name)) {
  *           resolve('Hello, ' + name + '!');
@@ -48,10 +49,10 @@
  *     alert('Success: ' + greeting);
  *   }, function(reason) {
  *     alert('Failed: ' + reason);
+ *   }, function(progress) {
+ *     alert('Notification: ' + progress);
  *   });
  * ```
- *
- * Note: progress/notify callbacks are not currently supported via the ES6-style interface.
  *
  * Note: unlike ES6 behavior, an exception thrown in the constructor function will NOT implicitly reject the promise.
  *
@@ -89,8 +90,8 @@
  *     alert('Success: ' + greeting);
  *   }, function(reason) {
  *     alert('Failed: ' + reason);
- *   }, function(update) {
- *     alert('Got notification: ' + update);
+ *   }, function(value) {
+ *     alert('Got notification: ' + value);
  *   });
  * ```
  *
@@ -678,8 +679,12 @@ function qFactory(nextTick, exceptionHandler, errorOnUnhandledRejections) {
     function rejectFn(reason) {
       deferred.reject(reason);
     }
+    
+    function notifyFn(value) {
+      deferred.notify(value);
+    }
 
-    resolver(resolveFn, rejectFn);
+    resolver(resolveFn, rejectFn, notifyFn);
 
     return deferred.promise;
   };
