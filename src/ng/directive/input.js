7ec663fc708aa7a9a9ce62d2306f24d7a733a86d
Peter Bacon Darwin <pete@bacondarwin.com>
2016-10-07 14:18:30 +0100

    fix(input): ensure that hidden input values are correct after history.back
    Due to the nature of some browser's PageCache/BFCache, returning to an Angular
    app sometimes causes `input[hidden]` elements to retain the last value
    that was stored before the page was navigated away from previously.
    
    This is particularly problematic if the input has an interpolated value.
    E.g. `<input type="hidden" value="{{ 1 + 2 }}">` since when the browser
    returns, instead of the original interpolation template, the HTML contains
    the previous value `<input type="hidden" value="3">`.
    
    This commit instructs the browser not to attempt to reinstate the previous
    value when navigating back in history by setting `autocomplete="off"` on
    the hidden input element element.

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -899,11 +899,16 @@ var inputDirective = ['$browser', '$sniffer', function($browser, $sniffer) {
   return {
     restrict: 'E',
     require: '?ngModel',
-    link: function(scope, element, attr, ctrl) {
-      if (ctrl) {
-        (inputType[lowercase(attr.type)] || inputType.text)(scope, element, attr, ctrl, $sniffer,
-                                                            $browser);
-      }
+    compile: function(tElement, tAttr) {
+      if (lowercase(tAttr.type) === 'hidden') tAttr.$set('autocomplete', 'off');
+      return {
+        pre: function(scope, element, attr, ctrl) {
+          if (ctrl) {
+            (inputType[lowercase(attr.type)] || inputType.text)(scope, element, attr, ctrl, $sniffer,
+                                                                $browser);
+          }
+        }
+      };
     }
   };
 }];
