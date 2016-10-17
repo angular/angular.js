daa47e33e38d1c097a94494f19e4e492d049f608
Martin Staffa <mjstaffa@gmail.com>
2016-10-17 23:16:41 +0200

    Revert "chore(doc-gen, docs-app): create plnkr examples with correct Angular version"
    This patch relies on a change in the dgeni example package, which has not been added to dgeni yet.
    
    This reverts commit db02008fe274410d2b8e2715fa4a3c8c9b2ce809.

--- docs/app/src/examples.js
+++ docs/app/src/examples.js
@@ -181,8 +181,9 @@ angular.module('examples', [])
           filePromises.push($http.get(exampleFolder + '/' + filename, { transformResponse: [] })
             .then(function(response) {
 
-              // Plunkr needs an index file that's simply named index.html
-              if (filename === 'index-plnkr.html') {
+              // The manifests provide the production index file but Plunkr wants
+              // a straight index.html
+              if (filename === 'index-production.html') {
                 filename = 'index.html';
               }
 
