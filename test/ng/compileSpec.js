586e2acb269016a0fee66ac33f4a385f631afad0
Jonathan Yates <jonathantyates@gmail.com>
2016-10-19 09:33:15 -0700

    fix($compile): clean up `@`-binding observers when re-assigning bindings
    Fixes #15268
    
    Closes #15298

--- test/ng/compileSpec.js
+++ test/ng/compileSpec.js
@@ -4378,6 +4378,37 @@ describe('$compile', function() {
             });
           });
 
+          it('should clean up `@`-binding observers when re-assigning bindings', function() {
+            var constructorSpy = jasmine.createSpy('constructor');
+            var prototypeSpy = jasmine.createSpy('prototype');
+
+            function TestController() {
+              return {$onChanges: constructorSpy};
+            }
+            TestController.prototype.$onChanges = prototypeSpy;
+
+            module(function($compileProvider) {
+              $compileProvider.component('test', {
+                bindings: {attr: '@'},
+                controller: TestController
+              });
+            });
+
+            inject(function($compile, $rootScope) {
+              var template = '<test attr="{{a}}"></test>';
+              $rootScope.a = 'foo';
+
+              element = $compile(template)($rootScope);
+              $rootScope.$digest();
+              expect(constructorSpy).toHaveBeenCalled();
+              expect(prototypeSpy).not.toHaveBeenCalled();
+
+              constructorSpy.calls.reset();
+              $rootScope.$apply('a = "bar"');
+              expect(constructorSpy).toHaveBeenCalled();
+              expect(prototypeSpy).not.toHaveBeenCalled();
+            });
+          });
 
           it('should not call `$onChanges` twice even when the initial value is `NaN`', function() {
             var onChangesSpy = jasmine.createSpy('$onChanges');
