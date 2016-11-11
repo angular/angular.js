5e914a5720bf1cbc3b073846ac1d20c02bd18775
Peter Bacon Darwin <pete@bacondarwin.com>
2016-11-11 19:45:02 +0000

    chore(docs): improve version picker
    Closes #15385

--- docs/config/services/deployments/debug.js
+++ docs/config/services/deployments/debug.js
@@ -22,7 +22,8 @@ module.exports = function debugDeployment(getVersion) {
       'components/lunr.js-' + getVersion('lunr.js') + '/lunr.js',
       'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/prettify.js',
       'components/google-code-prettify-' + getVersion('google-code-prettify') + '/src/lang-css.js',
-      'js/versions-data.js',
+      'js/current-version-data.js',
+      'js/all-versions-data.js',
       'js/pages-data.js',
       'js/nav-data.js',
       'js/docs.js'
