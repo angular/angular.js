3d68b9502848ff6714ef89bfb95b8e70ae34eff6
Georgios Kalpakas <kalpakas.g@gmail.com>
2016-12-08 11:12:45 +0200

    fix(jqLite): silently ignore `after()` if element has no parent
    Previously, the element was always assumed to have a parent and an error was
    thrown when that was not the case.
    This commit makes jqLite consistent with jQuery, which silently ignores a call
    on elements that do not have a parent.
    
    Fixes #15331
    Closes #15367
    
    Closes #15475

--- src/jqLite.js
+++ src/jqLite.js
@@ -979,12 +979,15 @@ forEach({
 
   after: function(element, newElement) {
     var index = element, parent = element.parentNode;
-    newElement = new JQLite(newElement);
 
-    for (var i = 0, ii = newElement.length; i < ii; i++) {
-      var node = newElement[i];
-      parent.insertBefore(node, index.nextSibling);
-      index = node;
+    if (parent) {
+      newElement = new JQLite(newElement);
+
+      for (var i = 0, ii = newElement.length; i < ii; i++) {
+        var node = newElement[i];
+        parent.insertBefore(node, index.nextSibling);
+        index = node;
+      }
     }
   },
 
