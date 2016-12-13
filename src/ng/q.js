d03cac4cd5aa406aeebfae30f20d31d7e980587a
Georgios Kalpakas <kalpakas.g@gmail.com>
2016-12-14 00:41:09 +0200

    docs($q): document the default value for `errorOnUnhandledRejections`

--- src/ng/q.js
+++ src/ng/q.js
@@ -239,6 +239,7 @@ function $QProvider() {
    *
    * @description
    * Retrieves or overrides whether to generate an error when a rejected promise is not handled.
+   * This feature is enabled by default.
    *
    * @param {boolean=} value Whether to generate an error when a rejected promise is not handled.
    * @returns {boolean|ng.$qProvider} Current value when called without a new value or self for
