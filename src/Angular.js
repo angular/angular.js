88478a7e358dbe6acb7f797f01b7301c5451dfb3
vteremasov <vteremasov@klika-tech.com>
2017-01-19 19:09:34 +0300

    fix(ngResource) /. escaping for dynamic params Added replacing of '%5c' (url encoded backslash) with \ (backslash) in Angular.js Added tests for this behavior in resourceSpec.js No breaking changes

--- src/Angular.js
+++ src/Angular.js
@@ -1440,7 +1440,8 @@ function encodeUriSegment(val) {
   return encodeUriQuery(val, true).
              replace(/%26/gi, '&').
              replace(/%3D/gi, '=').
-             replace(/%2B/gi, '+');
+             replace(/%2B/gi, '+').
+             replace(/%5c/gi, '\\');
 }
 
 
