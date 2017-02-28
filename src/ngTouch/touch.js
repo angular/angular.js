25f49ad3b5d4bd514a7125b9dab2bbd1f9a3d8b3
Martin Staffa <mjstaffa@gmail.com>
2017-02-28 18:10:54 +0100

    fix(ngTouch): remove ngClick override and $touchProvider.ngClickOverrideEnabled()
    Closes #15755
    
    BREAKING CHANGE:
    
    The `ngClick` directive from the ngTouch module has been removed, and with the also the
    corresponding $touchProvider.ngClickOverrideEnabled() method.
    
    If you have included ngTouch in your application with a version of 1.5.0 or higher, and have not
    changed the value of $touchProvider.ngClickOverrideEnabled()`, then there are no migration steps.
    
    The `ngClick` override directive had been deprecated and by default disabled since v1.5.0,
    because of buggy behavior in edge cases, and a general trend to avoid special touch based
    overrides of click events. If you still need a touch override, consider using
    [Fastclick](https://github.com/ftlabs/fastclick).
    
    Note that the `$touch` service still offers the `ngClickOverrideEnabled()` function for
    libraries that want to detect if an app uses the click override. Starting with 1.7.0, it
    will always return `false`.

--- src/ngTouch/touch.js
+++ src/ngTouch/touch.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/* global ngTouchClickDirectiveFactory: false */
-
 /**
  * @ngdoc module
  * @name ngTouch
@@ -9,7 +7,7 @@
  *
  * # ngTouch
  *
- * The `ngTouch` module provides touch events and other helpers for touch-enabled devices.
+ * The `ngTouch` module provides helpers for touch-enabled devices.
  * The implementation is based on jQuery Mobile touch event handling
  * ([jquerymobile.com](http://jquerymobile.com/)).
  *
@@ -21,7 +19,7 @@
  */
 
 // define ngTouch module
-/* global -ngTouch */
+/* global ngTouch */
 var ngTouch = angular.module('ngTouch', []);
 
 ngTouch.info({ angularVersion: '"NG_VERSION_FULL"' });
@@ -37,69 +35,11 @@ function nodeName_(element) {
  * @name $touchProvider
  *
  * @description
- * The `$touchProvider` allows enabling / disabling {@link ngTouch.ngClick ngTouch's ngClick directive}.
+ * The `$touchProvider` allows configuration of the ngTouch module. It currently contains
+ * no methods.
  */
-$TouchProvider.$inject = ['$provide', '$compileProvider'];
-function $TouchProvider($provide, $compileProvider) {
-
-  /**
-   * @ngdoc method
-   * @name  $touchProvider#ngClickOverrideEnabled
-   *
-   * @param {boolean=} enabled update the ngClickOverrideEnabled state if provided, otherwise just return the
-   * current ngClickOverrideEnabled state
-   * @returns {*} current value if used as getter or itself (chaining) if used as setter
-   *
-   * @kind function
-   *
-   * @description
-   * Call this method to enable/disable {@link ngTouch.ngClick ngTouch's ngClick directive}. If enabled,
-   * the default ngClick directive will be replaced by a version that eliminates the 300ms delay for
-   * click events on browser for touch-devices.
-   *
-   * The default is `false`.
-   *
-   */
-  var ngClickOverrideEnabled = false;
-  var ngClickDirectiveAdded = false;
-  // eslint-disable-next-line no-invalid-this
-  this.ngClickOverrideEnabled = function(enabled) {
-    if (angular.isDefined(enabled)) {
-
-      if (enabled && !ngClickDirectiveAdded) {
-        ngClickDirectiveAdded = true;
-
-        // Use this to identify the correct directive in the delegate
-        ngTouchClickDirectiveFactory.$$moduleName = 'ngTouch';
-        $compileProvider.directive('ngClick', ngTouchClickDirectiveFactory);
-
-        $provide.decorator('ngClickDirective', ['$delegate', function($delegate) {
-          if (ngClickOverrideEnabled) {
-            // drop the default ngClick directive
-            $delegate.shift();
-          } else {
-            // drop the ngTouch ngClick directive if the override has been re-disabled (because
-            // we cannot de-register added directives)
-            var i = $delegate.length - 1;
-            while (i >= 0) {
-              if ($delegate[i].$$moduleName === 'ngTouch') {
-                $delegate.splice(i, 1);
-                break;
-              }
-              i--;
-            }
-          }
-
-          return $delegate;
-        }]);
-      }
-
-      ngClickOverrideEnabled = enabled;
-      return this;
-    }
-
-    return ngClickOverrideEnabled;
-  };
+$TouchProvider.$inject = [];
+function $TouchProvider() {
 
   /**
   * @ngdoc service
@@ -107,7 +47,7 @@ function $TouchProvider($provide, $compileProvider) {
   * @kind object
   *
   * @description
-  * Provides the {@link ngTouch.$touch#ngClickOverrideEnabled `ngClickOverrideEnabled`} method.
+  * Provides the legacy {@link ngTouch.$touch#ngClickOverrideEnabled `ngClickOverrideEnabled`} method.
   *
   */
   // eslint-disable-next-line no-invalid-this
@@ -116,14 +56,13 @@ function $TouchProvider($provide, $compileProvider) {
       /**
        * @ngdoc method
        * @name  $touch#ngClickOverrideEnabled
-       *
-       * @returns {*} current value of `ngClickOverrideEnabled` set in the {@link ngTouch.$touchProvider $touchProvider},
-       * i.e. if {@link ngTouch.ngClick ngTouch's ngClick} directive is enabled.
+       *       *
+       * @returns {*} `false` as of AngularJS 1.7.0
        *
        * @kind function
        */
       ngClickOverrideEnabled: function() {
-        return ngClickOverrideEnabled;
+        return false;
       }
     };
   };
