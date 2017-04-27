c7e48c05b3b6178e61a95ababc1dc2d13c2eeef4
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-04-27 19:36:32 +0300

    fixup! fix(angular-loader): do not depend on "closure" globals that may not be available

--- src/stringify.js
+++ src/stringify.js
@@ -2,10 +2,6 @@
 
 /* exported toDebugString */
 
-// This file is also included in `angular-loader`, so `copy()` might not always be available in the
-// closure. In such cases, it is lazily retrieved as `angular.copy()` when needed.
-var copyFn;
-
 function serializeObject(obj, maxDepth) {
   var seen = [];
 
@@ -13,10 +9,9 @@ function serializeObject(obj, maxDepth) {
   // and a very deep object can cause a performance issue, so we copy the object
   // based on this specific depth and then stringify it.
   if (isValidObjectMaxDepth(maxDepth)) {
-    if (!copyFn) {
-      copyFn = copy || angular.copy;
-    }
-    obj = copyFn(obj, null, maxDepth);
+    // This file is also included in `angular-loader`, so `copy()` might not always be available in
+    // the closure. Therefore, it is lazily retrieved as `angular.copy()` when needed.
+    obj = angular.copy(obj, null, maxDepth);
   }
   return JSON.stringify(obj, function(key, val) {
     val = toJsonReplacer(key, val);
