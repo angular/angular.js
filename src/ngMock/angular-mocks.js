a1e3f8728e0a80396f980e48f8dc68dde6721b2b
Lucio Martinez <martinezlucio.com@gmail.com>
2017-05-01 23:39:29 -0300

    fix(ngMock/$interval): add support for zero-delay intervals in tests
    Previously, trying to test code thaat contained zero-delay intervals (e.g.
    `$interval(fn, 0)` or `$interval(fn)`) would result in an infinite loop.
    This commit avoids the infinite loop, by treating zero-delay intervals as one
    second intervals (except for the initial trigger, where they can also be
    executed with `$interval.flush(0)`).
    
    Fixes #15952
    
    Closes #15953

--- src/ngMock/angular-mocks.js
+++ src/ngMock/angular-mocks.js
@@ -504,8 +504,8 @@ angular.mock.$IntervalProvider = function() {
       }
 
       repeatFns.push({
-        nextTime:(now + delay),
-        delay: delay,
+        nextTime: (now + (delay || 0)),
+        delay: delay || 1,
         fn: tick,
         id: nextRepeatId,
         deferred: deferred
@@ -555,10 +555,16 @@ angular.mock.$IntervalProvider = function() {
      * @return {number} The amount of time moved forward.
      */
     $interval.flush = function(millis) {
+      var before = now;
       now += millis;
       while (repeatFns.length && repeatFns[0].nextTime <= now) {
         var task = repeatFns[0];
         task.fn();
+        if (task.nextTime === before) {
+          // this can only happen the first time
+          // a zero-delay interval gets triggered
+          task.nextTime++;
+        }
         task.nextTime += task.delay;
         repeatFns.sort(function(a, b) { return a.nextTime - b.nextTime;});
       }
