c4fc1aff62cc23a7064f90ca981a822aad128d2c
Martin Staffa <mjstaffa@gmail.com>
2017-06-08 17:10:14 +0200

    wait longer for uploads to finish

--- scripts/travis/before_build.sh
+++ scripts/travis/before_build.sh
@@ -9,10 +9,9 @@ mkdir -p $LOGS_DIR
 if [ $JOB != "ci-checks" ]; then
   echo "start_browser_provider"
   ./scripts/travis/start_browser_provider.sh
-fi
 
-if [ $JOB != "ci-checks" ]; then
   grunt package
+
   echo "wait_for_browser_provider"
   ./scripts/travis/wait_for_browser_provider.sh
 fi
