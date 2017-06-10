1102c411961b1d2389171faf82ff9f25764acd53
Martin Staffa <mjstaffa@gmail.com>
2017-06-11 00:41:24 +0200

    chore(travis): don't build package for the "unit" job
    Source test:unit do not need built or packaged code,
    and, and tests:docs only need built code.

--- scripts/travis/before_build.sh
+++ scripts/travis/before_build.sh
@@ -4,15 +4,26 @@ set -e
 
 yarn global add grunt-cli@1.2.0
 
-mkdir -p $LOGS_DIR
+mkdir -p "$LOGS_DIR"
 
-if [ $JOB != "ci-checks" ]; then
+if [ "$JOB" != "ci-checks" ]; then
   echo "start_browser_provider"
   ./scripts/travis/start_browser_provider.sh
 fi
 
-if [ $JOB != "ci-checks" ]; then
+# ci-checks and unit tests do not run against the packaged code
+if [ "$JOB" != "ci-checks" ] && [ "$JOB" != "unit" ]; then
   grunt package
+fi
+
+# unit runs the docs tests too which need a built version of the code
+if [ "$JOB" = "unit" ]; then
+  grunt build
+fi
+
+# check this after the package, because at this point the browser_provider
+# has probably arrived
+if [ "$JOB" != "ci-checks" ]; then
   echo "wait_for_browser_provider"
   ./scripts/travis/wait_for_browser_provider.sh
 fi
