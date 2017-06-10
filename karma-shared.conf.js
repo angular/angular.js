2f9bbd093a1891e0f2dd02576baf8660a22c8844
Martin Staffa <mjstaffa@gmail.com>
2017-06-10 23:36:38 +0200

    chore(travis): use "karma-spec-reporter"
    dots reporter creates lots of empty space that makes it tedious to scroll
    through the logs. "Spec" is configured to only report on failures.

--- karma-shared.conf.js
+++ karma-shared.conf.js
@@ -10,8 +10,16 @@ module.exports = function(config, specificOptions) {
     browserDisconnectTimeout: 10000,
     browserDisconnectTolerance: 2,
     browserNoActivityTimeout: 30000,
-
-
+    reporters: ['spec'],
+    specReporter: {
+      maxLogLines: 5,             // limit number of lines logged per test
+      suppressErrorSummary: true, // do not print error summary
+      suppressFailed: false,      // do not print information about failed tests
+      suppressPassed: true,      // do not print information about passed tests
+      suppressSkipped: false,      // do not print information about skipped tests
+      showSpecTiming: false,      // print the time elapsed for each spec
+      failFast: false              // test would finish with error when a first fail occurs.
+    },
     // SauceLabs config for local development.
     sauceLabs: {
       testName: specificOptions.testName || 'AngularJS',
