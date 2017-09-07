21a2f4bb2310f5cea69d637568ca6c21f2815270
melbourne2991 <wleach1992@gmail.com>
2017-09-07 11:35:15 +1000

    docs(input[checkbox]): fix @param type for `ngChange`
    Closes #16219

--- src/ng/directive/input.js
+++ src/ng/directive/input.js
@@ -1100,8 +1100,8 @@ var inputType = {
    *                  Can be interpolated.
    * @param {string=} step Sets the `step` validation to ensure that the value entered matches the `step`
    *                  Can be interpolated.
-   * @param {string=} ngChange AngularJS expression to be executed when the ngModel value changes due
-   *                  to user interaction with the input element.
+   * @param {expression=} ngChange AngularJS expression to be executed when the ngModel value changes due
+   *                      to user interaction with the input element.
    * @param {expression=} ngChecked If the expression is truthy, then the `checked` attribute will be set on the
    *                      element. **Note** : `ngChecked` should not be used alongside `ngModel`.
    *                      Checkout {@link ng.directive:ngChecked ngChecked} for usage.
