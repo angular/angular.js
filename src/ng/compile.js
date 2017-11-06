4dc7675dc68fea2a7acfe88f7c94f6d0e739a62e
Denys Berzoy <dberzoy@gmail.com>
2017-11-06 14:06:29 +0200

    fix($compile): code review remarks fixed

--- src/ng/compile.js
+++ src/ng/compile.js
@@ -3639,27 +3639,17 @@ SimpleChange.prototype.isFirstChange = function() { return this.previousValue ==
 
 var PREFIX_REGEXP = /^((?:x|data)[:\-_])/i;
 var SPECIAL_CHARS_REGEXP = /([:\-_]+(.))/g;
-var MOZ_HACK_REGEXP = /^moz([A-Z])/;
 
 /**
  * Converts all accepted directives format into proper directive name.
  * @param name Name to normalize
  */
 function directiveNormalize(name) {
-  return camelCase(name.replace(PREFIX_REGEXP, ''));
-}
-
-/**
- * Converts snake_case to camelCase.
- * Also there is special case for Moz prefix starting with upper case letter.
- * @param name Name to normalize
- */
-function camelCase(name) {
   return name
+    .replace(PREFIX_REGEXP, '')
     .replace(SPECIAL_CHARS_REGEXP, function(_, separator, letter, offset) {
       return offset ? letter.toUpperCase() : letter;
-    })
-    .replace(MOZ_HACK_REGEXP, 'Moz$1');
+    });
 }
 
 /**
