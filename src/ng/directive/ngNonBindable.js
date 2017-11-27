0fa5a37838a895ca01e0c63b3bb4141bc97a0238
jugglinmike <mike@mikepennisi.com>
2017-11-27 07:11:50 -0500

    docs(ngNonBindable): document effect on the element's directives
    The phrase "contents of the current DOM element" may be interpreted either as
    inclusive of the DOM element's attributes or as exclusive of the attributes.
    This situation concerns markup such as:
    
        <div ng-non-bindable ng-controller="MyController"></div>
    
    In practice, AngularJS does not compile or bind attribute values for elements
    which specify the `ng-non-bindable` directive. Extend the documentation to
    definitely describe this behavior.
    
    Closes #16338

--- src/ng/directive/ngNonBindable.js
+++ src/ng/directive/ngNonBindable.js
@@ -9,9 +9,10 @@
  *
  * @description
  * The `ngNonBindable` directive tells AngularJS not to compile or bind the contents of the current
- * DOM element. This is useful if the element contains what appears to be AngularJS directives and
- * bindings but which should be ignored by AngularJS. This could be the case if you have a site that
- * displays snippets of code, for instance.
+ * DOM element, including directives on the element itself that have a lower priority than
+ * `ngNonBindable`. This is useful if the element contains what appears to be AngularJS directives
+ * and bindings but which should be ignored by AngularJS. This could be the case if you have a site
+ * that displays snippets of code, for instance.
  *
  * @example
  * In this example there are two locations where a simple interpolation binding (`{{}}`) is present,
