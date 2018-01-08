98eb86f020734b66a41b18fe74d5df16c268e760
Martin Staffa <mjstaffa@gmail.com>
2018-01-08 12:30:15 +0100

    refactor(getVersion.js): remove unused parameters
    The parameters have been unused since we moved most of the bower
    package dependencies to npm

--- docs/config/services/getVersion.js
+++ docs/config/services/getVersion.js
@@ -7,11 +7,10 @@ var path = require('canonical-path');
  * Find the current version of the node module
  */
 module.exports = function getVersion(readFilesProcessor) {
-  var basePath = readFilesProcessor.basePath;
+  var sourceFolder = path.resolve(readFilesProcessor.basePath, 'node_modules');
+  var packageFile = 'package.json';
 
-  return function(component, sourceFolder, packageFile) {
-    sourceFolder = path.resolve(basePath, sourceFolder || 'node_modules');
-    packageFile = packageFile || 'package.json';
-    return require(path.join(sourceFolder,component,packageFile)).version;
+  return function(component) {
+    return require(path.join(sourceFolder, component, packageFile)).version;
   };
 };
