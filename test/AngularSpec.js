f3c7ad1036324a3245069adda8fff1349e798880
Martin Staffa <mjstaffa@googlemail.com>
2018-05-07 17:09:36 +0200

    remove comments

--- test/AngularSpec.js
+++ test/AngularSpec.js
@@ -1785,17 +1785,6 @@ describe('angular', function() {
             protocol = 'browserext:';  // Upcoming standard scheme.
           }
 
-          // if (protocol === 'ms-browser-extension:') {
-          //   // Support: Edge 13-15
-          //   // In Edge, URLs with protocol 'ms-browser-extension:' return "null" for the origin,
-          //   // therefore it's impossible to know if a script is same-origin.
-          //   it('should not bootstrap for same-origin documents', function() {
-          //     expect(allowAutoBootstrap(createFakeDoc({src: protocol + '//something'}, protocol))).toBe(false);
-          //   });
-
-          // } else {
-          // }
-
           it('should bootstrap for same-origin documents', function() {
             expect(allowAutoBootstrap(createFakeDoc({src: protocol + '//something'}, protocol))).toBe(true);
           });
