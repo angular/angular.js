6a1d7dd7dce767b78411528de318e419a16769b1
Martin Staffa <mjstaffa@gmail.com>
2018-05-25 20:16:39 +0200

    chore(saucelabs): remove platform for ios11

--- karma-shared.conf.js
+++ karma-shared.conf.js
@@ -113,7 +113,6 @@ module.exports = function(config, specificOptions) {
       'SL_iOS_11': {
         base: 'SauceLabs',
         browserName: 'iphone',
-        platform: 'OS X 10.12',
         version: '11.3'
       },
 
