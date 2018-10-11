79efd2087cf9876be98a7f94f193411e890fdbf0
Michał Gołębiowski-Owczarek <m.goleb@gmail.com>
2018-10-11 16:35:57 +0200

    chore(*): update minimum Yarn version, do some Yarn-related cleanups
    Included changes:
    
    *Update minimum Yarn version from 1.3.2 to 1.10.1*
    
    Yarn 1.10 added the integrity field to the lockfile, making newer Yarn users
    have their lockfile changed a lot if they run `yarn`. This commit updates the
    required Yarn version to be at least 1.10.1 and changes Travis & Jenkins to use
    Yarn 1.10.1
    
    *Change the package.json's engines grunt field to grunt-cli*
    
    The grunt field suggested it's the grunt package version we're checking while
    we check the grunt-cli version instead.
    
    *Stop separating Yarn script arguments from script names via " -- "*
    
    The " -- " separator is necessary in npm but not in Yarn. In fact, it's
    deprecated in Yarn and some future version is supposed to start passing this
    parameter directly to the scripts which may break them.
    
    *Don't install grunt-cli globally during the build*
    
    It's enough to use `yarn grunt` instead of `grunt` and the global grunt-cli
    installation is no longer needed.
    
    *Use `yarn grunt` instead of `yarn run grunt`*
    
    The former form is shorter.
    
    *Don't define the `grunt` Yarn script*
    
    As opposed to npm, `yarn binName` invokes a binary named `binName` exposed
    by the respective package so the `grant` Yarn script is no longer needed.
    
    *Allow Node versions newer than 8; bump the minimum*
    
    Closes #16714

--- scripts/jenkins/init-node.sh
+++ scripts/jenkins/init-node.sh
@@ -8,11 +8,11 @@ nvm install
 
 # clean out and install yarn
 rm -rf ~/.yarn
-curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.3.2
+curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 1.10.1
 export PATH="$HOME/.yarn/bin:$PATH"
 
 # Ensure that we have the local dependencies installed
 yarn install
 
 echo testing grunt version
-yarn run grunt -- --version
+yarn grunt --version
